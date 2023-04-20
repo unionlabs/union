0111529fc2b40c350f67727f061ee2f65525e2a7
cor <cor@pruijs.dev>
2023-04-20 06:45:15 +0200

    chore: bump ignite-cli to v0.26.1

--- flake.nix
+++ flake.nix
@@ -6,24 +6,26 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    ignite-cli-src.flake = false;
-    ignite-cli-src.url = "github:ignite/cli/v0.24.0";
+    ignite-cli-src = {
+      flake = false;
+      url = "github:ignite/cli/v0.26.1";      
+    };
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
-        packages = rec {
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }: rec {
+        packages = {
           # ignite cli package for build/devshell
           ignite-cli = pkgs.buildGoModule rec {
             name = "ignite-cli";
             src = inputs.ignite-cli-src;
-            vendorSha256 = "sha256-P1NYgvdobi6qy1sSKFwkBwPRpLuvCJE5rCD2s/vvm14=";
+            vendorSha256 = "sha256-4ajrp1UQ6mF75ZnZ69Y3QtRbsEsdDNm0J6pVZG+EwiY=";
             doCheck = false;
             ldflags = ''
               -X github.com/ignite/cli/ignite/version.Head=${src.rev}
-              -X github.com/ignite/cli/ignite/version.Version=v0.24.0
+              -X github.com/ignite/cli/ignite/version.Version=v0.26.1
               -X github.com/ignite/cli/ignite/version.Date=${builtins.toString (src.lastModified)}
             '';
           };
@@ -31,6 +33,7 @@
 
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            self'.packages.ignite-cli
             nixfmt
             go
             gopls
