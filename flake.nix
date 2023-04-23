a07fa060fd7cc1c31161c76aaffaf4f52b9b9777
cor <cor@pruijs.dev>
2023-04-23 21:26:43 +0200

    feat: integrate `treefmt`

--- flake.nix
+++ flake.nix
@@ -8,18 +8,21 @@
     };
     ignite-cli-src = {
       flake = false;
-      url = "github:ignite/cli/4098ae9a5941fd1875c8eb62540482076bd6f6d6";      
+      url = "github:ignite/cli/4098ae9a5941fd1875c8eb62540482076bd6f6d6";
     };
 
     swagger-combine-src = {
       flake = false;
       url = "github:maxdome/swagger-combine";
     };
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      imports = [ inputs.treefmt-nix.flakeModule ];
       perSystem = { config, self', inputs', pkgs, system, lib, ... }: rec {
         packages = rec {
           # ignite cli package for build/devshell
@@ -37,14 +40,14 @@
             vendorSha256 = null;
             doCheck = true;
           };
-          
+
           ignite-cli = pkgs.buildGoModule rec {
             allowGoReference = true;
             patches = [
               ./patches/protoc.patch
             ];
-            nativeBuildInputs = [pkgs.protobuf];
-            buildInputs = [pkgs.protobuf];
+            nativeBuildInputs = [ pkgs.protobuf ];
+            buildInputs = [ pkgs.protobuf ];
             name = "ignite-cli";
             src = inputs.ignite-cli-src;
             vendorSha256 = "sha256-TWOxdq2LTnxd718Ra0viD1z2tBnNmcN92A1wpX97xtc=";
@@ -68,10 +71,22 @@
             gopls
             nodejs
           ];
-          PROTOC="${pkgs.protobuf}/bin/protoc";
-          SWAGGER_BIN="${self'.packages.swagger-combine}/bin/swagger-combine";
+          nativeBuildInputs = [
+            config.treefmt.build.wrapper
+          ];
+          PROTOC = "${pkgs.protobuf}/bin/protoc";
+          SWAGGER_BIN = "${self'.packages.swagger-combine}/bin/swagger-combine";
         };
-        
+
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.nixpkgs-fmt.enable = true;
+          programs.gofmt.enable = true;
+          settings.global.excludes = [
+            "uniond/vendor/**"
+          ];
+        };
+
         apps = {
           ignite-cli.program = "${config.packages.ignite-cli}/bin/ignite";
         };
