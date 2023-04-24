8d1e61f3119ba39335da43fab8b4b5990f997376
cor <cor@pruijs.dev>
2023-04-24 15:02:13 +0200

    chore: remove ignite-cli,  ,

--- flake.nix
+++ flake.nix
@@ -2,21 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    ignite-cli-src = {
-      flake = false;
-      url = "github:ignite/cli/4098ae9a5941fd1875c8eb62540482076bd6f6d6";
-    };
-
-    swagger-combine-src = {
-      flake = false;
-      url = "github:maxdome/swagger-combine";
-    };
-
-    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -24,11 +14,7 @@
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       imports = [
         inputs.treefmt-nix.flakeModule
-        ./tools/ignite-cli/ignite-cli.nix
-        ./tools/swagger-combine/swagger-combine.nix
-
         ./uniond/uniond.nix
-
         ./docs/docs.nix
       ];
       perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
@@ -38,7 +24,6 @@
 
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            self'.packages.ignite-cli
             protobuf
             nixfmt
             go
@@ -52,9 +37,6 @@
           nativeBuildInputs = [
             config.treefmt.build.wrapper
           ];
-
-          PROTOC = "${pkgs.protobuf}/bin/protoc";
-          SWAGGER_BIN = "${self'.packages.swagger-combine}/bin/swagger-combine";
         };
 
         treefmt = {
@@ -64,10 +46,6 @@
           settings.global.excludes = [
             "uniond/vendor/**"
           ];
-        };
-
-        apps = {
-          ignite-cli.program = "${config.packages.ignite-cli}/bin/ignite";
         };
       };
     };
