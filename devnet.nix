045ed3915f086e4e99ad9ae6338a6c0510b3fb55
cor <cor@pruijs.dev>
2023-04-25 11:45:02 +0200

    feat: run uniond in devnet

--- devnet.nix
+++ devnet.nix
@@ -1,13 +1,36 @@
 { ... }: {
   perSystem = { pkgs, self', inputs', ... }: {
     packages = {
-      devnet = pkgs.writeShellApplication {
-        name = "union-devnet";
-        runtimeInputs = [ inputs'.arion.packages.default ];
-        text = ''
-          arion
-        '';
-      };
+      devnet =
+        let
+          arion = inputs'.arion.packages.default;
+          spec = {
+            modules = [{
+
+              project.name = "union-devnet";
+              services = {
+                uniond = {
+                  image.enableRecommendedContents = true;
+                  service.useHostStore = true;
+                  service.command = [ "sh" "-c" "${self'.packages.uniond}/bin/uniond start" ];
+                  service.ports = [
+                    "8000:8000" # host:container
+                  ];
+                  service.stop_signal = "SIGINT";
+                };
+              };
+
+            }];
+          };
+          build = arion.build spec;
+        in
+        pkgs.writeShellApplication {
+          name = "union-devnet";
+          runtimeInputs = [ arion ];
+          text = ''
+            arion --prebuilt-file ${build} up --build --force-recreate -V --always-recreate-deps --remove-orphans
+          '';
+        };
     };
 
     checks = { };
