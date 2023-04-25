f16b8e6f49764a149ec8d1b5b756f993a6fa5149
cor <cor@pruijs.dev>
2023-04-25 14:13:46 +0200

    feat: wip aggregate

--- genesis/genesis.nix
+++ genesis/genesis.nix
@@ -4,7 +4,6 @@
       let
         chainId = "union-devnet-1";
         uniond = self'.packages.uniond;
-        KEY = "val-1";
         N = 10;
       in
       pkgs.runCommand "genesis" { } ''
@@ -12,13 +11,24 @@
         				cd $out
         				export HOME=$(pwd)
                 # for validators
+
+                mkdir master
+        				${uniond}/bin/uniond init testnet bn254 --chain-id ${chainId} --home ./master
+                
                 for i in {1..${builtins.toString N}}
                 do
+                  export HOME=$(pwd)
           				mkdir val-$i
           				${uniond}/bin/uniond init testnet bn254 --chain-id ${chainId} --home ./val-$i
-                  ${uniond}/bin/uniond keys add ${KEY} --keyring-backend test --home ./val-$i # not saved yet
-                  ${uniond}/bin/uniond add-genesis-account ${KEY} 100000000000000000000000000stake --keyring-backend test --home ./val-$i
-                  ${uniond}/bin/uniond gentx ${KEY} 1000000000000000000000stake "bn254" --keyring-backend test --chain-id ${chainId} --home ./val-$i --ip "0.0.0.0"
+                  ${uniond}/bin/uniond keys add val-$i --keyring-backend test --home ./val-$i # not saved yet
+
+
+                  ${uniond}/bin/uniond add-genesis-account val-$i 100000000000000000000000000stake --keyring-backend test --home ./val-$i
+                  ${uniond}/bin/uniond keys list --home ./val-$i 
+                  # PUBKEY=`${uniond}/bin/uniond keys show val-$i --address --home ./val-$i` 
+                  # ${uniond}/bin/uniond add-genesis-account $PUBKEY 100000000000000000000000000stake --keyring-backend test --home ./master
+                  # ${uniond}/bin/uniond gentx val-$i 1000000000000000000000stake "bn254" --keyring-backend test --chain-id ${chainId} --home ./val-$i --ip "0.0.0.0"
+                  # cp ./val-$i/gentx/* ./master/gentx
                 done
                 # end
 
