08b6960cf84f1ffd3c9c5bba97640dab65881411
cor <cor@pruijs.dev>
2023-04-25 13:45:25 +0200

    feat: build genesis configs for N validators

--- genesis/genesis.nix
+++ genesis/genesis.nix
@@ -1,29 +1,28 @@
 { ... }: {
   perSystem = { pkgs, self', ... }: {
-    packages = {
+    packages.genesis =
+      let
+        chainId = "union-devnet-1";
+        uniond = self'.packages.uniond;
+        KEY = "val-1";
+        N = 10;
+      in
+      pkgs.runCommand "genesis" { } ''
+        				mkdir $out
+        				cd $out
+        				export HOME=$(pwd)
+                # for validators
+                for i in {1..${builtins.toString N}}
+                do
+          				mkdir val-$i
+          				${uniond}/bin/uniond init testnet bn254 --chain-id ${chainId} --home ./val-$i
+                  ${uniond}/bin/uniond keys add ${KEY} --keyring-backend test --home ./val-$i # not saved yet
+                  ${uniond}/bin/uniond add-genesis-account ${KEY} 100000000000000000000000000stake --keyring-backend test --home ./val-$i
+                  ${uniond}/bin/uniond gentx ${KEY} 1000000000000000000000stake "bn254" --keyring-backend test --chain-id ${chainId} --home ./val-$i --ip "0.0.0.0"
+                done
+                # end
 
-      genesis =
-        let
-          chainId = "union-devnet-1";
-          uniond = self'.packages.uniond;
-          KEY = "val-1";
-        in
-        pkgs.runCommand "genesis" { } ''
-          				mkdir $out
-          				cd $out
-          				export HOME=$(pwd)
-          				
-
-                  # for validators
-          				mkdir val-1
-          				${uniond}/bin/uniond init testnet bn254 --chain-id ${chainId} --home ./val-1
-                  ${uniond}/bin/uniond keys add ${KEY} --keyring-backend test --home ./val-1 # not saved yet
-                  # end
-
-                  ${uniond}/bin/uniond add-genesis-account ${KEY} 100000000000000000000000000stake --keyring-backend test --home ./val-1
-                  ${uniond}/bin/uniond gentx ${KEY} 1000000000000000000000stake "bn254" --keyring-backend test --chain-id ${chainId} --home ./val-1 --ip "0.0.0.0"
-        '';
-    };
+      '';
 
     checks = { };
   };
