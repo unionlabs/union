5b97078e8c31acf169e16846f379c3ef87f5b128
cor <cor@pruijs.dev>
2023-04-25 11:57:27 +0200

    refactor(devnet): mkUniondService

--- devnet.nix
+++ devnet.nix
@@ -1,38 +1,38 @@
 { ... }: {
   perSystem = { pkgs, self', inputs', ... }: {
-    packages = {
-      devnet =
-        let
-          arion = inputs'.arion.packages.default;
-          spec = {
-            modules = [{
+    packages.devnet =
+      let
+        arion = inputs'.arion.packages.default;
 
-              project.name = "union-devnet";
-              services = {
-                uniond = {
-                  image.enableRecommendedContents = true;
-                  service.useHostStore = true;
-                  service.command = [ "sh" "-c" "${self'.packages.uniond}/bin/uniond start" ];
-                  service.ports = [
-                    "8000:8000" # host:container
-                  ];
-                  service.stop_signal = "SIGINT";
-                };
-              };
-
-            }];
-          };
-          build = arion.build spec;
-        in
-        pkgs.writeShellApplication {
-          name = "union-devnet";
-          runtimeInputs = [ arion ];
-          text = ''
-            arion --prebuilt-file ${build} up --build --force-recreate -V --always-recreate-deps --remove-orphans
-          '';
+        mkUniondService = id: {
+          image.enableRecommendedContents = true;
+          service.useHostStore = true;
+          service.command = [ "sh" "-c" "${self'.packages.uniond}/bin/uniond start" ];
+          service.ports = [
+            "8000:8000" # host:container
+          ];
+          service.stop_signal = "SIGINT";
         };
-    };
 
-    checks = { };
+        spec = {
+          modules = [{
+            project.name = "union-devnet";
+            services = {
+              uniond-1 = mkUniondService {
+                id = 1;
+              };
+            };
+
+          }];
+        };
+        build = arion.build spec;
+      in
+      pkgs.writeShellApplication {
+        name = "union-devnet";
+        runtimeInputs = [ arion ];
+        text = ''
+          arion --prebuilt-file ${build} up --build --force-recreate -V --always-recreate-deps --remove-orphans
+        '';
+      };
   };
 }
