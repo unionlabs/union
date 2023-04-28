5e9cd392872be03ce3621a6ab301c69ee5a5510f
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2023-04-28 10:50:38 +0200

    feat(evm): remove initial fork of yui

--- evm/contracts/apps/20-transfer/IICS20Bank.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.19;
-
-interface IICS20Bank {
-    function transferFrom(address from, address to, string calldata id, uint256 amount) external;
-    function mint(address account, string calldata id, uint256 amount) external;
-    function burn(address account, string calldata id, uint256 amount) external;
-}
