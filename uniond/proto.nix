3dae0dc452550c388c729b57546b12e780ffa0bc
cor <cor@pruijs.dev>
2023-04-28 05:55:27 +0200

    fix(nix): don't expose gen-proto dependency packages

--- uniond/proto.nix
+++ uniond/proto.nix
@@ -6,71 +6,72 @@
 #
 { ... }: {
   perSystem = { pkgs, self', ... }: {
-    packages = {
-      grpc-gateway = pkgs.buildGoModule {
-        pname = "grpc-gateway";
-        version = "1.16.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "grpc-ecosystem";
-          repo = "grpc-gateway";
-          rev = "v1.16.0";
-          sha256 = "sha256-jJWqkMEBAJq50KaXccVpmgx/hwTdKgTtNkz8/xYO+Dc=";
+    packages =
+      let
+        grpc-gateway = pkgs.buildGoModule {
+          pname = "grpc-gateway";
+          version = "1.16.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "grpc-ecosystem";
+            repo = "grpc-gateway";
+            rev = "v1.16.0";
+            sha256 = "sha256-jJWqkMEBAJq50KaXccVpmgx/hwTdKgTtNkz8/xYO+Dc=";
+          };
+
+          vendorSha256 = "sha256-jVOb2uHjPley+K41pV+iMPNx67jtb75Rb/ENhw+ZMoM=";
         };
 
-        vendorSha256 = "sha256-jVOb2uHjPley+K41pV+iMPNx67jtb75Rb/ENhw+ZMoM=";
-      };
+        cosmos-proto = pkgs.buildGoModule {
+          pname = "protoc-gen-cosmos";
+          version = "1.0.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "cosmos";
+            repo = "cosmos-proto";
+            rev = "v1.0.0-beta.3";
+            sha256 = "sha256-kFm1ChSmm5pU9oJqKmWq4KfO/hxgxzvcSzr66oTulos=";
+          };
+          doCheck = false;
 
-      cosmos-proto = pkgs.buildGoModule {
-        pname = "protoc-gen-cosmos";
-        version = "1.0.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "cosmos";
-          repo = "cosmos-proto";
-          rev = "v1.0.0-beta.3";
-          sha256 = "sha256-kFm1ChSmm5pU9oJqKmWq4KfO/hxgxzvcSzr66oTulos=";
+          vendorSha256 = "sha256-7kDz0RAon2L/3NTHIxya8nWMyN28G9rAfqUu+lbkea4=";
         };
-        doCheck = false;
 
-        vendorSha256 = "sha256-7kDz0RAon2L/3NTHIxya8nWMyN28G9rAfqUu+lbkea4=";
-      };
+        gogoproto = pkgs.buildGoModule {
+          pname = "gogoproto";
+          version = "1.4.7";
+          src = pkgs.fetchFromGitHub {
+            owner = "cosmos";
+            repo = "gogoproto";
+            rev = "v1.4.7";
+            sha256 = "sha256-oaGwDFbz/xgL7hDtvdh/mIcRIGBdp+/xuKeuBE2ZpqY=";
+          };
+          nativeBuildInputs = with pkgs; [ protobuf ];
+          doCheck = false;
 
-      gogoproto = pkgs.buildGoModule {
-        pname = "gogoproto";
-        version = "1.4.7";
-        src = pkgs.fetchFromGitHub {
-          owner = "cosmos";
-          repo = "gogoproto";
-          rev = "v1.4.7";
-          sha256 = "sha256-oaGwDFbz/xgL7hDtvdh/mIcRIGBdp+/xuKeuBE2ZpqY=";
+          vendorSha256 = "sha256-nfeqVsPMQz7EL+qWxFzRukCE3YqXErhS9urRaJo44Fg=";
         };
-        nativeBuildInputs = with pkgs; [ protobuf ];
-        doCheck = false;
+      in
+      {
+        gen-proto = pkgs.writeShellApplication {
+          name = "gen-proto";
+          runtimeInputs = (with pkgs; [ buf go ]) ++ [ grpc-gateway cosmos-proto gogoproto ];
+          text = ''
+            cd uniond
+            set -eo pipefail
 
-        vendorSha256 = "sha256-nfeqVsPMQz7EL+qWxFzRukCE3YqXErhS9urRaJo44Fg=";
-      };
+            echo "Generating go code based on ./uniond/proto"
+            cd proto
+            buf mod update
+            buf generate --template ./buf.gen.gogo.yaml
+            cd ..
 
-      gen-proto = pkgs.writeShellApplication {
-        name = "gen-proto";
-        runtimeInputs = (with pkgs; [ buf go ]) ++ (with self'.packages; [ grpc-gateway cosmos-proto gogoproto ]);
-        text = ''
-          cd uniond
-          set -eo pipefail
+            # move proto files to the right places
+            cp -r ./union/x/* x/
+            rm -rf ./union
+          '';
+        };
 
-          echo "Generating go code based on ./uniond/proto"
-          cd proto
-          buf mod update
-          buf generate --template ./buf.gen.gogo.yaml
-          cd ..
-
-          # move proto files to the right places
-          cp -r ./union/x/* x/
-          rm -rf ./union
-        '';
 
       };
-
-
-    };
     checks = { };
   };
 }
