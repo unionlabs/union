8f454141beb16eebf8354f925f509c0682a3a046
cor <cor@pruijs.dev>
2023-05-02 17:49:44 +0200

    refactor(devnet): separate geth-init and lodestar-init parts, move genesis

--- networks/devnet.nix
+++ networks/devnet.nix
@@ -4,56 +4,13 @@
       let
         arion = inputs'.arion.packages.default;
         uniond = pkgs.lib.getExe self'.packages.uniond;
-
         lodestarService =
-          let
-            config = self'.packages.devnet-lodestar-config;
-            script = pkgs.writeShellApplication {
-              name = "lodestar-init";
-              runtimeInputs = [
-                pkgs.curl
-                pkgs.jq
-                self'.packages.lodestar-cli
-                config
-              ];
-              text = ''
-                ETH_ENDPOINT=http://geth:8545
-                EXECUTION_ENDPOINT=http://geth:8551
-                curl "$ETH_ENDPOINT" \
-                                  -X POST \
-                                  -H 'Content-Type: application/json' \
-                                  -d '{"jsonrpc": "2.0", "id": "1", "method": "eth_getBlockByNumber","params": ["0x0", false]}'
-                ETH_GENESIS_HASH=$(curl "$ETH_ENDPOINT" \
-                  -X POST \
-                  -H 'Content-Type: application/json' \
-                  -d '{"jsonrpc": "2.0", "id": "1", "method": "eth_getBlockByNumber","params": ["0x0", false]}' | jq -r '.result.hash')
-                GENESIS_TIMESTAMP=$(date -d'+10second' +%s)
-                lodestar-cli dev \
-                  --genesisTime "$GENESIS_TIMESTAMP" \
-                  --genesisEth1Hash "$ETH_GENESIS_HASH" \
-                  --genesisValidators 8 \
-                  --startValidators "0..7" \
-                  --rest.address "0.0.0.0" \
-                  --rest.cors "*" \
-                  --eth1.providerUrls "$ETH_ENDPOINT" \
-                  --execution.urls "$EXECUTION_ENDPOINT" \
-                  --reset \
-                  --terminal-total-difficulty-override 0 \
-                  --params.ALTAIR_FORK_EPOCH 0 \
-                  --params.BELLATRIX_FORK_EPOCH 0 \
-                  --params.CAPELLA_FORK_EPOCH 0 \
-                  --eth1=true \
-                  --jwt-secret ${config}/dev-jwt.prv \
-                  --rest.namespace "*"
-              '';
-            };
-          in
           {
             image = {
               enableRecommendedContents = true;
               contents = [
                 pkgs.coreutils
-                script
+                self'.packages.devnet-lodestar-init
               ];
             };
             service = {
@@ -63,11 +20,7 @@
                 # Beacon node rest API
                 "9596:9596"
               ];
-              command = [
-                "sh"
-                "-c"
-                "${script}/bin/lodestar-init"
-              ];
+              command = [ "sh" "-c" "${self'.packages.devnet-geth-init}/bin/lodestar-init" ];
               depends_on = {
                 geth = {
                   condition = "service_healthy";
@@ -76,50 +29,7 @@
             };
           };
 
-        # @hussein-aitlahcen: maybe move the command part to its own package to run it outside of devnet
         gethService =
-          let
-            config = self'.packages.devnet-geth-config;
-            script = pkgs.writeShellApplication {
-              name = "geth-init";
-              runtimeInputs = [
-                pkgs.go-ethereum
-                config
-              ];
-              text = ''
-                DATADIR=.
-                ETH_DATADIR=$DATADIR/geth
-                cp ${config}/genesis.json "$DATADIR/genesis.json"
-                geth init --datadir "$ETH_DATADIR" "$DATADIR/genesis.json"
-                geth account import --datadir "$ETH_DATADIR" --password /dev/null ${config}/dev-key0.prv
-                geth account import --datadir "$ETH_DATADIR" --password /dev/null ${config}/dev-key1.prv
-                geth --vmdebug \
-                  --datadir "$ETH_DATADIR" \
-                  --networkid 15 \
-                  --http \
-                  --http.api debug,personal,eth,net,web3,txpool,admin,engine,miner --ws --ws.api debug,eth,net,web3,engine \
-                  --rpc.allow-unprotected-txs \
-                  --mine \
-                  --miner.threads 1 \
-                  --miner.etherbase 0xBe68fC2d8249eb60bfCf0e71D5A0d2F2e292c4eD \
-                  --authrpc.addr "0.0.0.0" \
-                  --http.addr "0.0.0.0" \
-                  --http.corsdomain "*" \
-                  --http.vhosts "*" \
-                  --ws.addr "0.0.0.0" \
-                  --ws.origins "*" \
-                  --authrpc.vhosts "*" \
-                  --authrpc.jwtsecret ${config}/dev-jwt.prv \
-                  --allow-insecure-unlock \
-                  --unlock 0xBe68fC2d8249eb60bfCf0e71D5A0d2F2e292c4eD,0x89b4AB1eF20763630df9743ACF155865600daFF2 \
-                  --password /dev/null \
-                  --rpc.gascap 0 \
-                  --gcmode archive \
-                  --syncmode full \
-                  --maxpeers 0
-              '';
-            };
-          in
           {
             image = {
               enableRecommendedContents = true;
@@ -127,7 +37,7 @@
                 pkgs.coreutils
                 pkgs.curl
                 pkgs.jq
-                script
+                self'.packages.devnet-geth-init
               ];
             };
             service = {
@@ -139,11 +49,7 @@
                 # Auth RPC
                 "8551:8551"
               ];
-              command = [
-                "sh"
-                "-c"
-                "${script}/bin/geth-init"
-              ];
+              command = [ "sh" "-c" "${self'.packages.devnet-geth-init}/bin/geth-init" ];
               healthcheck = {
                 start_period = "5s";
                 interval = "10s";
@@ -172,7 +78,7 @@
         mkUniondService = { id }:
           let
             seedNode = builtins.readFile (getNodeID "valnode-0.json");
-            # All nodes connects to node 0
+            # All nodes connect to node 0
             params = if id == 0 then "" else "--p2p.persistent_peers ${seedNode}@uniond-0:26656";
           in
           {
