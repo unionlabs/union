6f4dd703243e4fdbd32f36a7d98bce908d70214a
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2023-05-10 10:27:56 +0200

    feat(prover): cleanup and improve `serve` command

--- unionpd/cmd/serve.go
+++ unionpd/cmd/serve.go
@@ -2,30 +2,54 @@ package cmd
 
 import (
 	provergrpc "cometbls-prover/grpc/api/v1"
-	"fmt"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
-var ServeCmd = &cobra.Command{
-	Use:   "serve",
-	Short: "",
-	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 9091))
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-		var opts []grpc.ServerOption
-		grpcServer := grpc.NewServer(opts...)
-		server, err := provergrpc.NewProverServer()
-		if err != nil {
-			panic(err)
-		}
-		provergrpc.RegisterUnionProverAPIServer(grpcServer, server)
-		fmt.Println("Serving...")
-		grpcServer.Serve(lis)
-	},
+const (
+	flagR1CS = "cs-path"
+	flagPK = "pk-path"
+	flagVK = "vk-path"
+)
+
+func ServeCmd() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "serve [uri]",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			uri := args[0]
+			lis, err := net.Listen("tcp", uri)
+			if err != nil {
+				return err
+			}
+			var opts []grpc.ServerOption
+			grpcServer := grpc.NewServer(opts...)
+			r1csPath, err := cmd.Flags().GetString(flagR1CS)
+			if err != nil {
+				return err
+			}
+			pkPath, err := cmd.Flags().GetString(flagPK)
+			if err != nil {
+				return err
+			}
+			vkPath, err := cmd.Flags().GetString(flagVK)
+			if err != nil {
+				return err
+			}
+			server, err := provergrpc.NewProverServer(r1csPath, pkPath, vkPath)
+			if err != nil {
+				return err
+			}
+			provergrpc.RegisterUnionProverAPIServer(grpcServer, server)
+			log.Println("Serving...")
+			return grpcServer.Serve(lis)
+		},
+	}
+	cmd.Flags().String(flagR1CS, "r1cs.bin", "Path to the compiled R1CS circuit.")
+	cmd.Flags().String(flagPK, "pk.bin", "Path to the proving key.")
+	cmd.Flags().String(flagVK, "vk.bin", "Path to the verifying key.")
+	return cmd
 }
+
--- unionpd/grpc/api/v1/server.go
+++ unionpd/grpc/api/v1/server.go
@@ -8,7 +8,10 @@ import (
 	context "context"
 	"crypto/hmac"
 	"fmt"
+	"io"
+	"log"
 	"math/big"
+	"os"
 
 	ce "github.com/cometbft/cometbft/crypto/encoding"
 	"github.com/cometbft/cometbft/crypto/merkle"
@@ -28,17 +31,43 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
+func hashToField(msg []byte) fp.Element {
+	hmac := hmac.New(sha3.NewLegacyKeccak256, []byte("CometBLS"))
+	hmac.Write(msg)
+	modMinusOne := new(big.Int).Sub(fp.Modulus(), big.NewInt(1))
+	num := new(big.Int).SetBytes(hmac.Sum(nil))
+	num.Mod(num, modMinusOne)
+	num.Add(num, big.NewInt(1))
+	val, overflow := uint256.FromBig(num)
+	if overflow {
+		panic("impossible; qed;")
+	}
+	valBytes := val.Bytes32()
+	var element fp.Element
+	err := element.SetBytesCanonical(valBytes[:])
+	if err != nil {
+		panic("impossible; qed;")
+	}
+	return element
+}
+
+func hashToField2(msg []byte) curve.E2 {
+	e0 := hashToField(append([]byte{0}, msg...))
+	e1 := hashToField(append([]byte{1}, msg...))
+	return curve.E2{A0: e0, A1: e1}
+}
+
 type proverServer struct {
 	UnimplementedUnionProverAPIServer
+	cs constraint.ConstraintSystem
 	pk backend.ProvingKey
 	vk backend.VerifyingKey
-	cs constraint.ConstraintSystem
 }
 
 func (*proverServer) mustEmbedUnimplementedUnionProverAPIServer() {}
 
 func (p *proverServer) Prove(c context.Context, request *ProveRequest) (*ProveResponse, error) {
-	fmt.Println("Proving...")
+	log.Println("Proving...")
 
 	reverseBytes := func(numbers []byte) []byte {
 		newNumbers := make([]byte, 0, len(numbers))
@@ -159,7 +188,7 @@ func (p *proverServer) Prove(c context.Context, request *ProveRequest) (*ProveRe
 		Message: [2]frontend.Variable{hm.A0.BigInt(new(big.Int)), hm.A1.BigInt(new(big.Int))},
 	}
 
-	fmt.Println(witness)
+	log.Println("Witness: ", witness)
 
 	privateWitness, err := frontend.NewWitness(&witness, ecc.BN254.ScalarField())
 	if err != nil {
@@ -180,7 +209,7 @@ func (p *proverServer) Prove(c context.Context, request *ProveRequest) (*ProveRe
 		return nil, fmt.Errorf("Invalid proving key type, must be BN254")
 	}
 
-	fmt.Println("Proving...")
+	log.Println("Proving...")
 	proof, err := backend.Prove(p.cs, p.pk, privateWitness)
 	if err != nil {
 		return nil, err
@@ -232,220 +261,98 @@ func (p *proverServer) Prove(c context.Context, request *ProveRequest) (*ProveRe
 	}, nil
 }
 
-func NewProverServer() (*proverServer, error) {
-	// TODO: load from file
+func loadOrCreate(r1csPath string, pkPath string, vkPath string) (constraint.ConstraintSystem, backend.ProvingKey, backend.VerifyingKey, error) {
 	r1csInstance := backend.NewCS(ecc.BN254)
 	pk := backend.NewProvingKey(ecc.BN254)
 	vk := backend.NewVerifyingKey(ecc.BN254)
 
-	var circuit lcgadget.Circuit
+	if _, err := os.Stat(r1csPath); err == nil {
+		if _, err = os.Stat(pkPath); err == nil {
+			if _, err = os.Stat(vkPath); err == nil {
+				readFrom := func(file string, obj io.ReaderFrom) error {
+					f, err := os.OpenFile(file, os.O_RDONLY, os.ModePerm)
+					if err != nil {
+						return err
+					}
+					defer f.Close()
+					obj.ReadFrom(f)
+					return nil
+				}
 
-	fmt.Println("Compiling circuit...")
-	r1csInstance, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit, frontend.WithCompressThreshold(300))
-	if err != nil {
-		return nil, err
-	}
+				log.Println("Loading R1CS...")
+				err := readFrom(r1csPath, r1csInstance)
+				if err != nil {
+					return nil, nil, nil, err
+				}
 
-	fmt.Println("Setup PK/VK")
-	pk, vk, err = backend.Setup(r1csInstance)
-	if err != nil {
-		return nil, err
-	}
+				log.Println("Loading proving key...")
+				err = readFrom(pkPath, pk)
+				if err != nil {
+					return nil, nil, nil, err
+				}
 
-	return &proverServer{pk: pk, vk: vk, cs: r1csInstance}, nil
-}
+				log.Println("Loading verifying key...")
+				err = readFrom(vkPath, vk)
+				if err != nil {
+					return nil, nil, nil, err
+				}
 
-var G2Cofactor big.Int
-
-var Hash = sha3.NewLegacyKeccak256
-
-func init() {
-	value, err := new(big.Int).SetString("30644e72e131a029b85045b68181585e06ceecda572a2489345f2299c0f9fa8d", 16)
-	if !err {
-		panic("Cannot build cofactor")
-	}
-	G2Cofactor.Set(value)
-}
-
-func g1NotZero(x *fp.Element) uint64 {
-
-	return x[0] | x[1] | x[2] | x[3]
-
-}
-
-// mapToCurve2 implements the Shallue and van de Woestijne method, applicable to any elliptic curve in Weierstrass form
-// No cofactor clearing or isogeny
-// https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html#straightline-svdw
-func mapToCurve2(u *curve.E2) curve.G2Affine {
-
-	var tv1, tv2, tv3, tv4 curve.E2
-	var x1, x2, x3, gx1, gx2, gx, x, y curve.E2
-	var one curve.E2
-	var gx1NotSquare, gx1SquareOrGx2Not int
-
-	//constants
-	//c1 = g(Z)
-	//c2 = -Z / 2
-	//c3 = sqrt(-g(Z) * (3 * Z² + 4 * A))     # sgn0(c3) MUST equal 0
-	//c4 = -4 * g(Z) / (3 * Z² + 4 * A)
-
-	Z := curve.E2{
-		A0: fp.Element{15230403791020821917, 754611498739239741, 7381016538464732716, 1011752739694698287},
-		A1: fp.Element{0},
-	}
-	c1 := curve.E2{
-		A0: fp.Element{15219334786797146878, 8431472696017589261, 15336528771359260718, 196732871012706162},
-		A1: fp.Element{4100506350182530919, 7345568344173317438, 15513160039642431658, 90557763186888013},
-	}
-	c2 := curve.E2{
-		A0: fp.Element{12997850613838968789, 14304628359724097447, 2950087706404981016, 1237622763554136189},
-		A1: fp.Element{0},
-	}
-	c3 := curve.E2{
-		A0: fp.Element{12298500088583694207, 17447120171744064890, 14097510924717921191, 2278398337453771183},
-		A1: fp.Element{4693446565795584099, 18320164443970680666, 6792758484113206563, 2989688171181581768},
-	}
-	c4 := curve.E2{
-		A0: fp.Element{7191623630069643826, 8333948550768170742, 13001081703983517696, 2062355016518372226},
-		A1: fp.Element{11163104453509316115, 7271947710149976975, 4894807947557820282, 3366254582553786647},
-	}
-
-	var bCurveCoeff fp.Element
-
-	// twist
-	var twist curve.E2
-
-	// bTwistCurveCoeff b coeff of the twist (defined over 𝔽p²) curve
-	var bTwistCurveCoeff curve.E2
-
-	bCurveCoeff.SetUint64(3)
-	// D-twist
-	twist.A0.SetUint64(9)
-	twist.A1.SetUint64(1)
-	bTwistCurveCoeff.Inverse(&twist).MulByElement(&bTwistCurveCoeff, &bCurveCoeff)
-
-	one.SetOne()
-
-	tv1.Square(u)       //    1.  tv1 = u²
-	tv1.Mul(&tv1, &c1)  //    2.  tv1 = tv1 * c1
-	tv2.Add(&one, &tv1) //    3.  tv2 = 1 + tv1
-	tv1.Sub(&one, &tv1) //    4.  tv1 = 1 - tv1
-	tv3.Mul(&tv1, &tv2) //    5.  tv3 = tv1 * tv2
-
-	tv3.Inverse(&tv3)   //    6.  tv3 = inv0(tv3)
-	tv4.Mul(u, &tv1)    //    7.  tv4 = u * tv1
-	tv4.Mul(&tv4, &tv3) //    8.  tv4 = tv4 * tv3
-	tv4.Mul(&tv4, &c3)  //    9.  tv4 = tv4 * c3
-	x1.Sub(&c2, &tv4)   //    10.  x1 = c2 - tv4
-
-	gx1.Square(&x1) //    11. gx1 = x1²
-	//12. gx1 = gx1 + A     All curves in gnark-crypto have A=0 (j-invariant=0). It is crucial to include this step if the curve has nonzero A coefficient.
-	gx1.Mul(&gx1, &x1)                 //    13. gx1 = gx1 * x1
-	gx1.Add(&gx1, &bTwistCurveCoeff)   //    14. gx1 = gx1 + B
-	gx1NotSquare = gx1.Legendre() >> 1 //    15.  e1 = is_square(gx1)
-	// gx1NotSquare = 0 if gx1 is a square, -1 otherwise
-
-	x2.Add(&c2, &tv4) //    16.  x2 = c2 + tv4
-	gx2.Square(&x2)   //    17. gx2 = x2²
-	//    18. gx2 = gx2 + A     See line 12
-	gx2.Mul(&gx2, &x2)               //    19. gx2 = gx2 * x2
-	gx2.Add(&gx2, &bTwistCurveCoeff) //    20. gx2 = gx2 + B
-
-	{
-		gx2NotSquare := gx2.Legendre() >> 1              // gx2Square = 0 if gx2 is a square, -1 otherwise
-		gx1SquareOrGx2Not = gx2NotSquare | ^gx1NotSquare //    21.  e2 = is_square(gx2) AND NOT e1   # Avoid short-circuit logic ops
-	}
-
-	x3.Square(&tv2)   //    22.  x3 = tv2²
-	x3.Mul(&x3, &tv3) //    23.  x3 = x3 * tv3
-	x3.Square(&x3)    //    24.  x3 = x3²
-	x3.Mul(&x3, &c4)  //    25.  x3 = x3 * c4
-
-	x3.Add(&x3, &Z)                  //    26.  x3 = x3 + Z
-	x.Select(gx1NotSquare, &x1, &x3) //    27.   x = CMOV(x3, x1, e1)   # x = x1 if gx1 is square, else x = x3
-	// Select x1 iff gx1 is square iff gx1NotSquare = 0
-	x.Select(gx1SquareOrGx2Not, &x2, &x) //    28.   x = CMOV(x, x2, e2)    # x = x2 if gx2 is square and gx1 is not
-	// Select x2 iff gx2 is square and gx1 is not, iff gx1SquareOrGx2Not = 0
-	gx.Square(&x) //    29.  gx = x²
-	//    30.  gx = gx + A
-
-	gx.Mul(&gx, &x)                //    31.  gx = gx * x
-	gx.Add(&gx, &bTwistCurveCoeff) //    32.  gx = gx + B
-
-	y.Sqrt(&gx)                             //    33.   y = sqrt(gx)
-	signsNotEqual := g2Sgn0(u) ^ g2Sgn0(&y) //    34.  e3 = sgn0(u) == sgn0(y)
-
-	tv1.Neg(&y)
-	y.Select(int(signsNotEqual), &y, &tv1) //    35.   y = CMOV(-y, y, e3)       # Select correct sign of y
-
-	return curve.G2Affine{X: x, Y: y}
-}
-
-// g2Sgn0 is an algebraic substitute for the notion of sign in ordered fields
-// Namely, every non-zero quadratic residue in a finite field of characteristic =/= 2 has exactly two square roots, one of each sign
-// https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-16.html#name-the-sgn0-function
-// The sign of an element is not obviously related to that of its Montgomery form
-func g2Sgn0(z *curve.E2) uint64 {
-
-	nonMont := z.Bits()
-
-	sign := uint64(0) // 1. sign = 0
-	zero := uint64(1) // 2. zero = 1
-	var signI uint64
-	var zeroI uint64
-
-	// 3. i = 1
-	signI = nonMont.A0[0] % 2 // 4.   sign_i = x_i mod 2
-	zeroI = g1NotZero(&nonMont.A0)
-	zeroI = 1 ^ (zeroI|-zeroI)>>63 // 5.   zero_i = x_i == 0
-	sign = sign | (zero & signI)   // 6.   sign = sign OR (zero AND sign_i) # Avoid short-circuit logic ops
-	zero = zero & zeroI            // 7.   zero = zero AND zero_i
-	// 3. i = 2
-	signI = nonMont.A1[0] % 2 // 4.   sign_i = x_i mod 2
-	// 5.   zero_i = x_i == 0
-	sign = sign | (zero & signI) // 6.   sign = sign OR (zero AND sign_i) # Avoid short-circuit logic ops
-	// 7.   zero = zero AND zero_i
-	return sign
-
-}
-
-// Naive scalar multiplication used for cofactor clearing
-func nativeNaiveScalarMul(p curve.G2Affine, s *big.Int) curve.G2Affine {
-	var result curve.G2Affine
-	result.X.SetZero()
-	result.Y.SetZero()
-	bits := s.BitLen()
-	for i := bits - 1; i >= 0; i-- {
-		result.Add(&result, &result)
-		if s.Bit(i) == 1 {
-			result.Add(&result, &p)
+				return r1csInstance, pk, vk, nil
+			}
 		}
 	}
-	return result
-}
 
-func hashToField(msg []byte) fp.Element {
-	hmac := hmac.New(Hash, []byte("CometBLS"))
-	hmac.Write(msg)
-	modMinusOne := new(big.Int).Sub(fp.Modulus(), big.NewInt(1))
-	num := new(big.Int).SetBytes(hmac.Sum(nil))
-	num.Mod(num, modMinusOne)
-	num.Add(num, big.NewInt(1))
-	val, overflow := uint256.FromBig(num)
-	if overflow {
-		panic("impossible; qed;")
-	}
-	valBytes := val.Bytes32()
-	var element fp.Element
-	err := element.SetBytesCanonical(valBytes[:])
+	var circuit lcgadget.Circuit
+
+	log.Println("Compiling circuit...")
+	r1csInstance, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit, frontend.WithCompressThreshold(300))
 	if err != nil {
-		panic("impossible; qed;")
+		return nil, nil, nil, err
 	}
-	return element
+
+	log.Println("Setup PK/VK")
+	pk, vk, err = backend.Setup(r1csInstance)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	saveTo := func(file string, x io.WriterTo) error {
+		log.Printf("Saving %s\n", file)
+		f, err := os.Create(file)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		w := bufio.NewWriter(f)
+		written, err := x.WriteTo(w)
+		if err != nil {
+			return err
+		}
+		log.Printf("Saved %d bytes\n", written)
+		w.Flush()
+		return nil
+	}
+
+	err = saveTo(r1csPath, r1csInstance)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	err = saveTo(pkPath, pk)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	err = saveTo(vkPath, vk)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return r1csInstance, pk, vk, nil
 }
 
-func hashToField2(msg []byte) curve.E2 {
-	e0 := hashToField(append([]byte{0}, msg...))
-	e1 := hashToField(append([]byte{1}, msg...))
-	return curve.E2{A0: e0, A1: e1}
+func NewProverServer(r1csPath string, pkPath string, vkPath string) (*proverServer, error) {
+	cs, pk, vk, err := loadOrCreate(r1csPath, pkPath, vkPath)
+	if err != nil {
+		return nil, err
+	}
+	return &proverServer{cs: cs, pk: pk, vk: vk}, nil
 }
