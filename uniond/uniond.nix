8190f069cee1ccd003b3b4245c7b312cede4368f
Ben Luelo <benluelo@hotmail.com>
2023-05-11 09:50:08 +0200

    chore: fmt

--- uniond/uniond.nix
+++ uniond/uniond.nix
@@ -1,6 +1,6 @@
 { ... }: {
-  perSystem = { pkgs, self', system, crane, ... }: 
-    let 
+  perSystem = { pkgs, self', system, crane, ... }:
+    let
       wasmvm = crane.lib.buildPackage {
         src = "${
             pkgs.fetchFromGitHub {
@@ -19,83 +19,83 @@
         '';
       };
     in
-  {
-    packages = {
-      uniond = pkgs.buildGoModule {
-        name = "uniond";
-        src = ./.;
-        vendorSha256 = null;
-        doCheck = true;
-        dontFixup = true;
-        ldflags = [
-          "-v -extldflags '-L${wasmvm}/lib'"
-        ];
-      };
-
-      uniond-image = pkgs.dockerTools.buildImage {
-        name = "uniond";
-
-        copyToRoot = pkgs.buildEnv {
-          name = "image-root";
-          paths = [ pkgs.coreutils-full pkgs.cacert ];
-          pathsToLink = [ "/bin" ];
+    {
+      packages = {
+        uniond = pkgs.buildGoModule {
+          name = "uniond";
+          src = ./.;
+          vendorSha256 = null;
+          doCheck = true;
+          dontFixup = true;
+          ldflags = [
+            "-v -extldflags '-L${wasmvm}/lib'"
+          ];
         };
 
-        config = {
-          Entrypoint = [ "${self'.packages.uniond}/bin/uniond" ];
-          Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
+        uniond-image = pkgs.dockerTools.buildImage {
+          name = "uniond";
+
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = [ pkgs.coreutils-full pkgs.cacert ];
+            pathsToLink = [ "/bin" ];
+          };
+
+          config = {
+            Entrypoint = [ "${self'.packages.uniond}/bin/uniond" ];
+            Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
+          };
         };
       };
+
+      checks = {
+        go-test = pkgs.go.stdenv.mkDerivation {
+          name = "go-test";
+          buildInputs = [ pkgs.go ];
+          src = ./.;
+          doCheck = true;
+          checkPhase = ''
+            # Go will try to create a .cache/ dir in $HOME.
+            # We avoid this by setting $HOME to the builder directory
+            export HOME=$(pwd)
+
+            go version
+            go test ./...
+            touch $out
+          '';
+        };
+
+        go-vet = pkgs.go.stdenv.mkDerivation {
+          name = "go-vet";
+          buildInputs = [ pkgs.go ];
+          src = ./.;
+          doCheck = true;
+          checkPhase = ''
+            # Go will try to create a .cache/ dir in $HOME.
+            # We avoid this by setting $HOME to the builder directory
+            export HOME=$(pwd)
+
+            go version
+            go vet ./...
+            touch $out
+          '';
+        };
+
+        go-staticcheck = pkgs.go.stdenv.mkDerivation {
+          name = "go-staticcheck";
+          buildInputs = [ pkgs.go pkgs.go-tools ];
+          src = ./.;
+          doCheck = true;
+          checkPhase = ''
+            # Go will try to create a .cache/ dir in $HOME.
+            # We avoid this by setting $HOME to the builder directory
+            export HOME=$(pwd)
+
+            staticcheck ./...
+            touch $out
+          '';
+        };
+
+      };
     };
-
-    checks = {
-      go-test = pkgs.go.stdenv.mkDerivation {
-        name = "go-test";
-        buildInputs = [ pkgs.go ];
-        src = ./.;
-        doCheck = true;
-        checkPhase = ''
-          # Go will try to create a .cache/ dir in $HOME.
-          # We avoid this by setting $HOME to the builder directory
-          export HOME=$(pwd)
-
-          go version
-          go test ./...
-          touch $out
-        '';
-      };
-
-      go-vet = pkgs.go.stdenv.mkDerivation {
-        name = "go-vet";
-        buildInputs = [ pkgs.go ];
-        src = ./.;
-        doCheck = true;
-        checkPhase = ''
-          # Go will try to create a .cache/ dir in $HOME.
-          # We avoid this by setting $HOME to the builder directory
-          export HOME=$(pwd)
-
-          go version
-          go vet ./...
-          touch $out
-        '';
-      };
-
-      go-staticcheck = pkgs.go.stdenv.mkDerivation {
-        name = "go-staticcheck";
-        buildInputs = [ pkgs.go pkgs.go-tools ];
-        src = ./.;
-        doCheck = true;
-        checkPhase = ''
-          # Go will try to create a .cache/ dir in $HOME.
-          # We avoid this by setting $HOME to the builder directory
-          export HOME=$(pwd)
-
-          staticcheck ./...
-          touch $out
-        '';
-      };
-
-    };
-  };
 }
