bfaf47e1158c0f312cf0313a3b0daf2e212fce6b
Ben Luelo <benluelo@hotmail.com>
2023-05-16 21:17:06 +0200

    fix: use github:<repo>/<owner> syntax for private flake input

--- flake.nix
+++ flake.nix
@@ -79,12 +79,12 @@
                 };
                 cometbls = builtins.fetchGit {
                   name = "cometbls";
-                  url = "https://git@github.com/UnionFi/cometbls";
+                  url = "github.com:UnionFi/cometbls";
                   rev = "f19ae296cf176b343ea214967810ba735813e73f";
                 };
                 cosmossdk = builtins.fetchGit {
                   name = "cosmos-sdk";
-                  url = "https://git@github.com/UnionFi/cosmos-sdk";
+                  url = "github.com:UnionFi/cosmos-sdk";
                   rev = "021566a5aba49e79356e2e6e246494e118f12605";
                 };
                 ibcgo = pkgs.fetchFromGitHub {
