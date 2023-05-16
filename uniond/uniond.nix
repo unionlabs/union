baae7ad522012dd7d6184d7f79b8cc5d18cdcd13
cor <cor@pruijs.dev>
2023-05-16 09:22:19 +0200

    feat(uniond): build statically on both x86_64-linux and aarch64-linux

--- uniond/uniond.nix
+++ uniond/uniond.nix
@@ -4,13 +4,15 @@
       packages = rec {
         wasmvm =
           let
-            rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-              targets = [ "x86_64-unknown-linux-musl" ];
-            };
-
+            rustToolchain = pkgs.rust-bin.stable.latest.default.override
+              (if system == "aarch64-linux" then {
+                targets = [ "aarch64-unknown-linux-musl" ];
+              } else if system == "x86_64-linux" then {
+                targets = [ "x86_64-unknown-linux-musl" ];
+              } else { });
             craneLib = crane.lib.overrideToolchain rustToolchain;
           in
-          craneLib.buildPackage rec {
+          craneLib.buildPackage ({
             src = "${
               pkgs.fetchFromGitHub {
                 owner = "CosmWasm";
@@ -19,24 +21,16 @@
                 hash = "sha256-zR47q8Z2znPigecPDmw5L4ef20/TXv8cPxaXTdJGxg0=";
               }
             }/libwasmvm";
-
-            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-            # -C target-feature=+crt-static
-            # CARGO_BUILD_RUSTFLAGS = "--crate-type=staticlib";
             doCheck = false;
+          } // (if pkgs.stdenv.isLinux then rec {
+            CARGO_BUILD_TARGET = if system == "aarch64-linux" then "aarch64-unknown-linux-musl" else "x86_64-linux";
             cargoBuildCommand = "cargo build --release --example=muslc";
             installPhase = ''
               mkdir -p $out/lib
-              # mv target/release/libwasmvm.so $out/lib/libwasmvm.$ {
-              #   builtins.head (pkgs.lib.strings.splitString "-" system)
-              # }.so
-              # ls -al target/release
-              # ls -al x86_64-unknown-linux-musl
               ls -al target/${CARGO_BUILD_TARGET}/release/examples/libmuslc.a
-
               mv target/${CARGO_BUILD_TARGET}/release/examples/libmuslc.a $out/lib/libwasmvm.${builtins.head (pkgs.lib.strings.splitString "-" system)}.a
             '';
-          };
+          } else { }));
 
         uniond = pkgs.pkgsStatic.buildGoModule ({
           name = "uniond";
