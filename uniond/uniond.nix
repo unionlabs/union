eb95b1b7a6c4e615a7d419d2bf2052b0c023e8c9
Ben Luelo <benluelo@hotmail.com>
2023-05-16 09:10:35 +0200

    feat: statically link uniond + wasmvm

--- uniond/uniond.nix
+++ uniond/uniond.nix
@@ -1,27 +1,44 @@
 { ... }: {
   perSystem = { pkgs, self', crane, system, ... }:
-    let
-      wasmvm = crane.lib.buildPackage {
-        src = "${
-            pkgs.fetchFromGitHub {
-              owner = "CosmWasm";
-              repo = "wasmvm";
-              rev = "a9e26c0e4e5a076d82556c4f44abeee2a64ff37e";
-              hash = "sha256-zR47q8Z2znPigecPDmw5L4ef20/TXv8cPxaXTdJGxg0=";
-            }
-          }/libwasmvm";
-        doCheck = false;
-        installPhase = ''
-          mkdir -p $out/lib
-          mv target/release/libwasmvm.so $out/lib/libwasmvm.${
-            builtins.head (pkgs.lib.strings.splitString "-" system)
-          }.so
-        '';
-      };
-    in
     {
-      packages = {
-        uniond = pkgs.buildGoModule ({
+      packages = rec {
+        wasmvm =
+          let
+            rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+              targets = [ "x86_64-unknown-linux-musl" ];
+            };
+
+            craneLib = crane.lib.overrideToolchain rustToolchain;
+          in
+          craneLib.buildPackage rec {
+            src = "${
+              pkgs.fetchFromGitHub {
+                owner = "CosmWasm";
+                repo = "wasmvm";
+                rev = "a9e26c0e4e5a076d82556c4f44abeee2a64ff37e";
+                hash = "sha256-zR47q8Z2znPigecPDmw5L4ef20/TXv8cPxaXTdJGxg0=";
+              }
+            }/libwasmvm";
+
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+            # -C target-feature=+crt-static
+            # CARGO_BUILD_RUSTFLAGS = "--crate-type=staticlib";
+            doCheck = false;
+            cargoBuildCommand = "cargo build --release --example=muslc";
+            installPhase = ''
+              mkdir -p $out/lib
+              # mv target/release/libwasmvm.so $out/lib/libwasmvm.$ {
+              #   builtins.head (pkgs.lib.strings.splitString "-" system)
+              # }.so
+              # ls -al target/release
+              # ls -al x86_64-unknown-linux-musl
+              ls -al target/${CARGO_BUILD_TARGET}/release/examples/libmuslc.a
+
+              mv target/${CARGO_BUILD_TARGET}/release/examples/libmuslc.a $out/lib/libwasmvm.${builtins.head (pkgs.lib.strings.splitString "-" system)}.a
+            '';
+          };
+
+        uniond = pkgs.pkgsStatic.buildGoModule ({
           name = "uniond";
           src = ./.;
           vendorSha256 = null;
@@ -29,12 +46,12 @@
         } // (if pkgs.stdenv.isLinux then {
           # statically link if we're on linux
           nativeBuildInputs = [ pkgs.musl wasmvm ];
-          CGO_ENABLED = 0;
           ldflags = [
             "-linkmode external"
-            "-extldflags '-static -L${pkgs.musl}/lib' '-static -L${wasmvm}/lib'"
+            "-extldflags '-static -L${pkgs.musl}/lib -L${wasmvm}/lib'"
           ];
         } else {
+          dontFixup = true;
           ldflags = [
             "-extldflags '-L${wasmvm}/lib'"
           ];
@@ -48,24 +65,11 @@
             paths = [ pkgs.coreutils-full pkgs.cacert ];
             pathsToLink = [ "/bin" ];
           };
-
-          uniond-image = pkgs.dockerTools.buildImage {
-            name = "uniond";
-
-            copyToRoot = pkgs.buildEnv {
-              name = "image-root";
-              paths = [ pkgs.coreutils-full pkgs.cacert ];
-              pathsToLink = [ "/bin" ];
-            };
-
-            config = {
-              Entrypoint = [ "${self'.packages.uniond}/bin/uniond" ];
-              Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
-            };
+          config = {
+            Entrypoint = [ "${self'.packages.uniond}/bin/uniond" ];
+            Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
           };
         };
-
-
       };
 
       checks = {
