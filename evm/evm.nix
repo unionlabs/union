7bf38d55c814b63fed93f49e13d36e91d546d404
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2023-05-20 17:23:19 +0200

    chore: fmt

--- evm/evm.nix
+++ evm/evm.nix
@@ -90,73 +90,72 @@
         FOUNDRY_GAS_REPORTS = ''["*"]'';
         SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
       };
-    in {
+    in
+    {
       # Beware, the generate solidity code is broken and require manual patch. Do not update unless you know that aliens exists.
       packages.generate-evm-proto = pkgs.writeShellApplication {
         name = "generate-evm-proto";
         runtimeInputs = [ pkgs.protobuf ];
-        text = let
-          solidity-protobuf = pkgs.stdenv.mkDerivation {
-            name = "solidity-protobuf";
-            version = "0.0.1";
-            src = pkgs.fetchFromGitHub {
-              owner = "CyrusVorwald";
-              repo = "solidity-protobuf";
-              rev = "1c323bed92d373d6c4d6c728c8dd9f76cf4b5a0c";
-              hash = "sha256-1obEhMjaLToaSk920CiJwfhkw+LDgY5Y/b7SpkeuqDE=";
+        text =
+          let
+            solidity-protobuf = pkgs.stdenv.mkDerivation {
+              name = "solidity-protobuf";
+              version = "0.0.1";
+              src = pkgs.fetchFromGitHub {
+                owner = "CyrusVorwald";
+                repo = "solidity-protobuf";
+                rev = "1c323bed92d373d6c4d6c728c8dd9f76cf4b5a0c";
+                hash = "sha256-1obEhMjaLToaSk920CiJwfhkw+LDgY5Y/b7SpkeuqDE=";
+              };
+              buildInputs =
+                [ (pkgs.python3.withPackages (ps: with ps; [ protobuf wrapt ])) ];
+              buildPhase = "true";
+              installPhase = ''
+                mkdir $out
+                cp -r $src/* $out
+              '';
             };
-            buildInputs =
-              [ (pkgs.python3.withPackages (ps: with ps; [ protobuf wrapt ])) ];
-            buildPhase = "true";
-            installPhase = ''
-              mkdir $out
-              cp -r $src/* $out
-            '';
-          };
-          protoIncludes = ''
-            -I"${proto.cometbls}/proto" -I"${proto.cosmossdk}/proto" -I"${proto.ibcgo}/proto" -I"${proto.cosmosproto}/proto" -I"${proto.ics23}/proto" -I"${proto.googleapis}" -I"${proto.gogoproto}" -I"${proto.uniond}"'';
-        in ''
-          plugindir="${solidity-protobuf}/protobuf-solidity/src/protoc"
-          find ${proto.ibcgo}/proto -name "$1" |\
-          while read -r file; do
-            echo "Generating $file"
-            protoc \
-              ${protoIncludes} \
-             -I"$plugindir/include" \
-             --plugin="protoc-gen-sol=$plugindir/plugin/gen_sol.py" \
-             --sol_out=gen_runtime="ProtoBufRuntime.sol&solc_version=0.8.18:$2" \
-              "$file"
-          done
-          find ${proto.cometbls}/proto -type f -regex ".*canonical.proto" |\
-          while read -r file; do
-            echo "Generating $file"
-            protoc \
-              ${protoIncludes} \
-             -I"$plugindir/include" \
-             --plugin="protoc-gen-sol=$plugindir/plugin/gen_sol.py" \
-             --sol_out=gen_runtime="ProtoBufRuntime.sol&solc_version=0.8.18:$2" \
-              "$file"
-          done
-          find ${proto.uniond} -type f -regex ".*ibc.*cometbls.*proto" |\
-          while read -r file; do
-            echo "Generating $file"
-            protoc \
-              ${protoIncludes} \
-             -I"$plugindir/include" \
-             --plugin="protoc-gen-sol=$plugindir/plugin/gen_sol.py" \
-             --sol_out=gen_runtime="ProtoBufRuntime.sol&solc_version=0.8.18:$2" \
-              "$file"
-          done
-        '';
+            protoIncludes = ''
+              -I"${proto.cometbls}/proto" -I"${proto.cosmossdk}/proto" -I"${proto.ibcgo}/proto" -I"${proto.cosmosproto}/proto" -I"${proto.ics23}/proto" -I"${proto.googleapis}" -I"${proto.gogoproto}" -I"${proto.uniond}"'';
+          in
+          ''
+            plugindir="${solidity-protobuf}/protobuf-solidity/src/protoc"
+            find ${proto.ibcgo}/proto -name "$1" |\
+            while read -r file; do
+              echo "Generating $file"
+              protoc \
+                ${protoIncludes} \
+               -I"$plugindir/include" \
+               --plugin="protoc-gen-sol=$plugindir/plugin/gen_sol.py" \
+               --sol_out=gen_runtime="ProtoBufRuntime.sol&solc_version=0.8.18:$2" \
+                "$file"
+            done
+            find ${proto.cometbls}/proto -type f -regex ".*canonical.proto" |\
+            while read -r file; do
+              echo "Generating $file"
+              protoc \
+                ${protoIncludes} \
+               -I"$plugindir/include" \
+               --plugin="protoc-gen-sol=$plugindir/plugin/gen_sol.py" \
+               --sol_out=gen_runtime="ProtoBufRuntime.sol&solc_version=0.8.18:$2" \
+                "$file"
+            done
+            find ${proto.uniond} -type f -regex ".*ibc.*cometbls.*proto" |\
+            while read -r file; do
+              echo "Generating $file"
+              protoc \
+                ${protoIncludes} \
+               -I"$plugindir/include" \
+               --plugin="protoc-gen-sol=$plugindir/plugin/gen_sol.py" \
+               --sol_out=gen_runtime="ProtoBufRuntime.sol&solc_version=0.8.18:$2" \
+                "$file"
+            done
+          '';
       };
 
       packages.evm-contracts = mkEvmContracts {
         optimizerRuns = 10000;
         doCheck = true;
-      };
-
-      checks.evm-contracts = mkEvmContracts {
-        optimizerRuns = 10;
       };
     };
 }
