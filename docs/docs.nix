a4066fdefa656d6a5917189bfdadafeb3dd1beda
Karel L. Kubat <k.l.kubat@gmail.com>
2023-05-22 17:39:47 -0400

    docs: update nodeDepsHash

--- docs/docs.nix
+++ docs/docs.nix
@@ -1,47 +1,51 @@
 { ... }: {
-  perSystem = { pkgs, ... }: {
-    checks =
-      {
+  perSystem = { pkgs, lib, ... }:
+    let
+      npmDepsHash = "sha256-A/8NpoJTWi6ylnrkpCwekfXbPH82jNdIA8Z58jMbYQI=";
+    in
+    {
+      checks =
+        {
+          docs = pkgs.buildNpmPackage {
+            name = "docs";
+            src = ./.;
+            buildPhase = ''
+              npm run build
+            '';
+            npmDepsHash = npmDepsHash;
+          };
+
+        };
+
+      packages = {
         docs = pkgs.buildNpmPackage {
           name = "docs";
           src = ./.;
           buildPhase = ''
             npm run build
           '';
-          npmDepsHash = "sha256-j/i0MM+kzvcsZs8aWab6xdHJ+QSW0S1MQcS+A2RiTY0=";
-        };
-
-      };
-
-    packages = {
-      docs = pkgs.buildNpmPackage {
-        name = "docs";
-        src = ./.;
-        buildPhase = ''
-          npm run build
-        '';
-        npmDepsHash = "sha256-j/i0MM+kzvcsZs8aWab6xdHJ+QSW0S1MQcS+A2RiTY0=";
-        installPhase = ''
-          mkdir -p $out
-          cp -dR ./build/* $out
-          echo "built docs"
-        '';
-      };
-    };
-
-    apps = {
-      docs-dev-server = {
-        type = "app";
-        program = pkgs.writeShellApplication {
-          name = "docs-dev-server";
-          runtimeInputs = [ pkgs.nodejs ];
-          text = ''
-            cd docs
-            npm install
-            npm run start
+          npmDepsHash = npmDepsHash;
+          installPhase = ''
+            mkdir -p $out
+            cp -dR ./build/* $out
+            echo "built docs"
           '';
         };
       };
+
+      apps = {
+        docs-dev-server = {
+          type = "app";
+          program = pkgs.writeShellApplication {
+            name = "docs-dev-server";
+            runtimeInputs = [ pkgs.nodejs ];
+            text = ''
+              cd docs
+              npm install
+              npm run start
+            '';
+          };
+        };
+      };
     };
-  };
 }
