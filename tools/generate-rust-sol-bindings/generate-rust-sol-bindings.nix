541825d36126662a54d63eacc8ce8d16e4c1dbbe
Ben Luelo <benluelo@hotmail.com>
2023-05-22 13:41:49 +0100

    fix: fix generate-rust-sol-bindings

--- tools/generate-rust-sol-bindings/generate-rust-sol-bindings.nix
+++ tools/generate-rust-sol-bindings/generate-rust-sol-bindings.nix
@@ -26,20 +26,20 @@
         src = ./.;
         buildInputs = [ config.treefmt.build.programs.rustfmt pkgs.taplo ];
         buildPhase = ''
-                    mkdir $out
+          mkdir $out
 
-          					${generate-rust-sol-bindings-crate}/bin/generate-rust-sol-bindings \
-          						--input ${self'.packages.evm-contracts}/IBCHandler.sol/IBCHandler.json \
-          						--outfile lib.rs
+					${generate-rust-sol-bindings-crate}/bin/generate-rust-sol-bindings \
+						--input ${self'.packages.evm-contracts}/out/IBCHandler.sol/IBCHandler.json \
+						--outfile lib.rs
 
-                    # format and normalize comments in generated code
-                    rustfmt --config normalize_comments=true --edition "2021" lib.rs 
+          # format and normalize comments in generated code
+          rustfmt --config normalize_comments=true --edition "2021" lib.rs 
 
-          					mkdir $out/src
-                    cp -r ./lib.rs $out/src/lib.rs
-                    cp -r ${cargo-toml} $out/Cargo.toml
+					mkdir $out/src
+          cp -r ./lib.rs $out/src/lib.rs
+          cp -r ${cargo-toml} $out/Cargo.toml
 
-                    taplo format $out/Cargo.toml
+          taplo format $out/Cargo.toml
         '';
       };
     in
@@ -51,21 +51,21 @@
           name = "generate-rust-sol-bindings";
           runtimeInputs = [ rust-sol-bindings ];
           text = ''
-            		          # If the current directory contains flake.nix, then we are at the repository root
-            		          if [[ -f flake.nix ]]
-            		          then
-            		            echo "We are at the repository root. Starting generation..."
-            		          else
-            		            echo "We are NOT at the repository root. Please cd to the repository root and try again."
-            		            exit 1
-            		          fi
+	          # If the current directory contains flake.nix, then we are at the repository root
+	          if [[ -f flake.nix ]]
+	          then
+	            echo "We are at the repository root. Starting generation..."
+	          else
+	            echo "We are NOT at the repository root. Please cd to the repository root and try again."
+	            exit 1
+	          fi
 
-            		          outdir="generated/contracts"
+	          outdir="generated/contracts"
 
-            		          cp -r ${rust-sol-bindings}/* $outdir
+	          cp -r ${rust-sol-bindings}/* $outdir
 
-            		          echo "Generation successful!"
-            		        '';
+	          echo "Generation successful!"
+	        '';
         };
       };
     };
