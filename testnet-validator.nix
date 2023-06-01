fb893e91219e5b9d286ef073dcc1459be2930692
cor <cor@pruijs.dev>
2023-06-01 22:39:38 +0200

    docs(testnet-validator): comments for module settings

--- testnet-validator.nix
+++ testnet-validator.nix
@@ -1,36 +1,39 @@
 { self, ... }: {
-  flake = {
-    nixosConfigurations.testnet-validator =
-      let
-        nixpkgs = self.inputs.nixpkgs;
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+  flake.nixosConfigurations.testnet-validator =
+    let
+      nixpkgs = self.inputs.nixpkgs;
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
 
-        modules = [
-          self.nixosModules.unionvisor
-          {
-            imports = [
-              "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix"
-            ];
-            system.stateVersion = "23.11";
-            environment.systemPackages = with pkgs; [
-              neofetch
-              bat
-              bottom
-              tree
-              helix
-              jq
-            ];
-            networking.firewall = { allowedTCPPorts = [ 80 443 26656 26657 ]; };
+      modules = [
+        self.nixosModules.unionvisor
+        {
+          system.stateVersion = "23.11";
 
-            services.unionvisor = {
-              enable = true;
-              moniker = "cor-systems";
-            };
-          }
-        ];
-      };
-  };
+          # Base configuration for openstack-based VPSs
+          imports = [ "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix" ];
+
+          # Allow other validators to reach you
+          networking.firewall.allowedTCPPorts = [ 80 443 26656 26657 ];
+
+          # Unionvisor module configuration
+          services.unionvisor = {
+            enable = true;
+            moniker = "your-testnet-moniker";
+          };
+
+          # OPTIONAL: Some useful inspection tools for when you SSH into your validator
+          environment.systemPackages = with pkgs; [
+            bat
+            bottom
+            helix
+            jq
+            neofetch
+            tree
+          ];
+        }
+      ];
+    };
 }
