3f56d427b51b634f75f7da69f02e369da25d26c2
cor <cor@pruijs.dev>
2023-06-01 22:26:41 +0200

    chore(testnet-validator): cleanup

--- testnet-validator.nix
+++ testnet-validator.nix
@@ -9,20 +9,22 @@
         system = "x86_64-linux";
 
         modules = [
+          self.nixosModules.unionvisor
           {
-            imports = [ 
-              "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix" 
+            imports = [
+              "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix"
             ];
             system.stateVersion = "23.11";
-
             environment.systemPackages = with pkgs; [
               neofetch
               bat
               bottom
+              tree
+              helix
+              jq
             ];
-          }
-          self.nixosModules.unionvisor  
-          {
+            networking.firewall = { allowedTCPPorts = [ 80 443 26656 26657 ]; };
+
             services.unionvisor = {
               enable = true;
               moniker = "cor-systems";
