88ecceb7368127689794e248b42286180315f47f
cor <cor@pruijs.dev>
2023-06-01 17:42:30 +0200

    feat(unionvisor): use `union-testnet-2` as the default network

--- unionvisor/src/network.rs
+++ unionvisor/src/network.rs
@@ -5,8 +5,9 @@ use color_eyre::eyre::bail;
 #[derive(Debug, Clone, Default, Copy)]
 pub enum Network {
     Union1,
-    #[default]
     Testnet1,
+    #[default]
+    Testnet2,
 }
 
 impl Display for Network {
@@ -14,6 +15,7 @@ impl Display for Network {
         match &self {
             Network::Union1 => write!(f, "union-1"),
             Network::Testnet1 => write!(f, "union-testnet-1"),
+            Network::Testnet2 => write!(f, "union-testnet-2"),
         }
     }
 }
@@ -25,6 +27,7 @@ impl FromStr for Network {
         match s {
             "union-1" => Ok(Network::Union1),
             "union-testnet-1" => Ok(Network::Testnet1),
+            "union-testnet-2" => Ok(Network::Testnet2),
             _ => bail!("unknown network"),
         }
     }
@@ -34,7 +37,7 @@ impl Network {
     // `self` will be used later on to determine which URL to use based on the network
     #[allow(clippy::unused_self)]
     pub fn seeds(&self) -> &str {
-        "c649931f0ef98bc3e086bbfbcf3b04896a9ec7de@uniontestnet.poisonphang.com:26656"
+        "7139b86ff37752437cf01a4970bf2b26c45c53e0@uniontestnet.poisonphang.com:26656"
     }
 
     // `self` will be used later on to determine which URL to use based on the network
@@ -42,6 +45,6 @@ impl Network {
     pub fn genesis_url(&self) -> &str {
         // We use a secret gist for the genesis.json until testnet is public
         // "https://raw.githubusercontent.com/unionfi/genesis/main/union-testnet-1/genesis.json"
-        "https://gist.githubusercontent.com/cor/53e083cc949ee4c63eb50c123a39bee8/raw/ec36b47ae60658307bfec383a477300213c933d3/genesis.json"
+        "https://gist.githubusercontent.com/cor/ffb8ec1a35a28fc1b3aad89f3fb466d4/raw/434b9f88a0ee6099cbbabafcd144f958a03ce655/union-testnet-2-gentx.json"
     }
 }
