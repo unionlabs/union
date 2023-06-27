f19fe566cd8a37d7c26172a51a580b194eda944d
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2023-06-27 16:16:46 +0200

    chore: add prettier solidity plugin

--- flake.nix
+++ flake.nix
@@ -270,18 +270,52 @@
               });
             };
 
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs.nixpkgs-fmt.enable = true;
-            programs.gofmt.enable = true;
-            programs.rustfmt.enable = true;
-            programs.prettier.enable = true;
-            programs.sort = {
-              enable = true;
-              file = "dictionary.txt";
+          treefmt =
+            let
+              prettier-solidity = pkgs.buildNpmPackage {
+                name = "prettier-plugin-solidity";
+                version = "1.1.3";
+                nativeBuildInputs = [ pkgs.pkg-config pkgs.python3 ];
+                src = pkgs.fetchFromGitHub {
+                  owner = "prettier-solidity";
+                  repo = "prettier-plugin-solidity";
+                  rev = "0f0b31bd1d76626cad4ce576d89088ef23ad87f3";
+                  hash = "sha256-zodOB5hARb7Jrb6d4gqmBKEFKUg0ZNZKbTN7H4vJk2w=";
+                };
+                npmInstallFlags = "--include=dev";
+                npmDepsHash = "sha256-Hzc4j9icNxTJNNaZ3PrmLKcUVR26nu4KqLireP4WmZM=";
+              };
+            in
+            {
+              projectRootFile = "flake.nix";
+              programs.nixpkgs-fmt.enable = true;
+              programs.gofmt.enable = true;
+              programs.rustfmt.enable = true;
+              programs.sort = {
+                enable = true;
+                file = "dictionary.txt";
+              };
+              settings.global.excludes = [ "**/vendor/**" ];
+              programs.prettier.enable = true;
+              settings.formatter.prettier = {
+                options = [ "--write" "--plugin-search-dir=${prettier-solidity}/lib" ];
+                includes = [
+                  "*.css"
+                  "*.html"
+                  "*.js"
+                  "*.json"
+                  "*.jsx"
+                  "*.md"
+                  "*.mdx"
+                  "*.scss"
+                  "*.ts"
+                  "*.tsx"
+                  "*.yaml"
+                  "*.yml"
+                  "*.sol"
+                ];
+              };
             };
-            settings.global.excludes = [ "**/vendor/**" "**/foundry/lib/**" ];
-          };
         };
     };
 
