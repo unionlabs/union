769baafe627c6b3b27f5a76eefeec030d141e9e6
aeryz <abdullaheryz@protonmail.com>
2023-07-11 11:42:17 +0300

    chore(ethereum-verifier): start with util tests
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- lib/ethereum-verifier/src/utils.rs
+++ lib/ethereum-verifier/src/utils.rs
@@ -103,12 +103,6 @@ pub fn validate_merkle_branch<'a>(
     'block: {
         let mut value = leaf.clone();
 
-        println!("{leaf:#?}");
-        println!("{branch:#?}");
-        println!("{depth:#?}");
-        println!("{index:#?}");
-        println!("{root:#?}");
-
         // TODO: This is just a fold
         // NB: zip ends when either iterator ends
         for (b, i) in branch.iter().zip(0..depth) {
@@ -139,41 +133,144 @@ pub fn validate_merkle_branch<'a>(
     }))
 }
 
-/// https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#is_valid_merkle_branch
-pub fn is_valid_merkle_branch<'a>(
-    leaf: &H256,
-    branch: impl IntoIterator<Item = &'a H256>,
-    depth: usize,
-    index: u64,
-    root: &H256,
-) -> bool {
-    let mut value = leaf.clone();
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ibc_types::{
+        ethereum_consts_traits::{
+            consts::{floorlog2, EXECUTION_PAYLOAD_INDEX},
+            Minimal,
+        },
+        ibc::lightclients::ethereum::header::Header,
+        TryFromProto,
+    };
 
-    let branch = branch.into_iter().collect::<Vec<_>>();
+    #[test]
+    fn compute_domain_works() {
+        let domain_type = DomainType([1, 2, 3, 4]);
+        let current_version = Version([5, 6, 7, 8]);
+        let genesis_validators_root = H256([1; 32]);
+        let fork_data_root =
+            compute_fork_data_root(current_version.clone(), genesis_validators_root.clone());
+        let genesis_version = Version([0, 0, 0, 0]);
 
-    println!("{leaf:#?}");
-    println!("{branch:#?}");
-    println!("{depth:#?}");
-    println!("{index:#?}");
-    println!("{root:#?}");
+        let mut domain = Domain::default();
+        domain.0[..4].copy_from_slice(&domain_type.0);
+        domain.0[4..].copy_from_slice(&fork_data_root.0[..28]);
 
-    // TODO: This is just a fold
-    // NB: zip ends when either iterator ends
-    for (b, i) in branch.into_iter().zip(0..depth) {
-        if let Some(v) = 2u64.checked_pow(i as u32) {
-            value = Sha256::digest(
-                if index / v % 2 == 1 {
-                    [b.0, value.0]
-                } else {
-                    [value.0, b.0]
-                }
-                .concat(),
+        // Uses the values instead of the default ones when `current_version` and
+        // `genesis_validators_root` is provided.
+        assert_eq!(
+            domain,
+            compute_domain(
+                domain_type.clone(),
+                Some(current_version),
+                Some(genesis_validators_root),
+                genesis_version.clone(),
             )
-            .into();
-        } else {
-            return false;
-        }
+        );
+
+        let fork_data_root = compute_fork_data_root(genesis_version.clone(), Default::default());
+        let mut domain = Domain::default();
+        domain.0[..4].copy_from_slice(&domain_type.0);
+        domain.0[4..].copy_from_slice(&fork_data_root.0[..28]);
+
+        // Uses default values when version and validators root is None
+        assert_eq!(
+            domain,
+            compute_domain(domain_type, None, None, genesis_version)
+        );
     }
 
-    value == *root
+    #[test]
+    fn compute_fork_data_root_works() {
+        let fork_data_root: H256 = ForkData {
+            current_version: Version(Default::default()),
+            genesis_validators_root: Default::default(),
+        }
+        .tree_hash_root()
+        .into();
+
+        assert_eq!(
+            fork_data_root,
+            compute_fork_data_root(Version(Default::default()), Default::default())
+        )
+    }
+
+    #[test]
+    fn compute_signing_root_works() {
+        // TODO(aeryz): implement using a dummy bls verifier
+    }
+
+    #[test]
+    fn valid_merkle_branch_works() {
+        // TODO(aeryz): move test data to ibc types
+        let header = <Header<Minimal>>::try_from_proto(
+            serde_json::from_str(include_str!(
+                "../../../light-clients/ethereum-light-client/src/test/finality_update_1.json"
+            ))
+            .unwrap(),
+        )
+        .unwrap();
+
+        let header = header.consensus_update.attested_header;
+
+        let valid_leaf = H256::from(header.execution.tree_hash_root());
+        let valid_branch = header.execution_branch.clone();
+        let valid_root = header.beacon.body_root.clone();
+
+        // Works for valid data
+        assert_eq!(
+            validate_merkle_branch(
+                &valid_leaf,
+                &valid_branch,
+                floorlog2(EXECUTION_PAYLOAD_INDEX),
+                EXECUTION_PAYLOAD_INDEX,
+                &valid_root,
+            ),
+            Ok(())
+        );
+
+        // Fails when index is wrong
+        assert!(validate_merkle_branch(
+            &valid_leaf,
+            &valid_branch,
+            floorlog2(EXECUTION_PAYLOAD_INDEX),
+            EXECUTION_PAYLOAD_INDEX + 1,
+            &valid_root,
+        )
+        .is_err());
+
+        let invalid_leaf = {
+            let mut header = header.clone();
+            header.execution.gas_limit += 1;
+            H256::from(header.execution.tree_hash_root())
+        };
+
+        // Fails when root is wrong
+        assert!(validate_merkle_branch(
+            &invalid_leaf,
+            &valid_branch,
+            floorlog2(EXECUTION_PAYLOAD_INDEX),
+            EXECUTION_PAYLOAD_INDEX,
+            &valid_root,
+        )
+        .is_err());
+
+        let invalid_branch = {
+            let mut header = header.clone();
+            header.execution_branch[0] = Default::default();
+            header.execution_branch
+        };
+
+        // Fails when branch is wrong
+        assert!(validate_merkle_branch(
+            &valid_leaf,
+            &invalid_branch,
+            floorlog2(EXECUTION_PAYLOAD_INDEX),
+            EXECUTION_PAYLOAD_INDEX,
+            &valid_root,
+        )
+        .is_err());
+    }
 }
