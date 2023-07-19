f661a0dd972cfccecea268949e7f74750c903f81
aeryz <abdullaheryz@protonmail.com>
2023-07-19 11:31:49 +0300

    chore(ethereum-lc): fix file paths in nix derivation
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- light-clients/ethereum-light-client/ethereum-light-client.nix
+++ light-clients/ethereum-light-client/ethereum-light-client.nix
@@ -10,8 +10,8 @@
       packages =
         lib.listToAttrs (map
           (config: lib.nameValuePair "ethereum-light-client-${config}" (crane.buildWasmContract {
-            cargoToml = ./ethereum-light-client/Cargo.toml;
-            cargoLock = ../Cargo.lock;
+            cargoToml = ./Cargo.toml;
+            cargoLock = ../../Cargo.lock;
             features = [ config ];
           })) [ "mainnet" "minimal" ]);
 
