40acf83a45a54452e4ca9ba081b418019223b6b6
aeryz <abdullaheryz@protonmail.com>
2023-07-21 11:29:19 +0300

    chore(uniond): extend bls tests
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- uniond/app/custom_query/crypto_test.go
+++ uniond/app/custom_query/crypto_test.go
@@ -11,11 +11,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAggregate(t *testing.T) {
-	entries, err := os.ReadDir("./testdata")
+const CORRECT_DATA_DIR = "./testdata/correct_data"
+const CORRECT_DATA_DIR_FORMAT = "./testdata/correct_data/%s"
+
+func TestVerifyWorks(t *testing.T) {
+	entries, err := os.ReadDir(CORRECT_DATA_DIR)
 	assert.NoError(t, err)
 	for _, entry := range entries {
-		content, err := os.ReadFile(fmt.Sprintf("./testdata/%s", entry.Name()))
+		content, err := os.ReadFile(fmt.Sprintf(CORRECT_DATA_DIR_FORMAT, entry.Name()))
 		assert.NoError(t, err)
 
 		var verify custom_query.QueryAggregateVerify
@@ -23,13 +26,94 @@ func TestAggregate(t *testing.T) {
 		assert.NoError(t, err)
 
 		msg := [32]byte{}
-		for i := 0; i < 32; i++ {
-			msg[i] = verify.Message[i]
-		}
+		copy(msg[:], verify.Message[:])
 
 		result, err := custom_query.VerifySignature(verify.Signature, msg, verify.PublicKeys)
 		assert.NoError(t, err)
 
 		assert.Equal(t, true, result)
 	}
+}
+
+func TestVerifyFailsToVerifyWhenIncorrectPublicKey(t *testing.T) {
+	entries, err := os.ReadDir(CORRECT_DATA_DIR)
+	assert.NoError(t, err)
+	for _, entry := range entries {
+		content, err := os.ReadFile(fmt.Sprintf(CORRECT_DATA_DIR_FORMAT, entry.Name()))
+		assert.NoError(t, err)
+
+		var verify custom_query.QueryAggregateVerify
+		err = json.Unmarshal(content, &verify)
+		assert.NoError(t, err)
+
+		msg := [32]byte{}
+		copy(msg[:], verify.Message[:])
+
+		// We copy a valid public key so that unmarshalling would still work
+		verify.PublicKeys[0] = verify.PublicKeys[1]
+
+		result, err := custom_query.VerifySignature(verify.Signature, msg, verify.PublicKeys)
+		assert.NoError(t, err)
+
+		assert.Equal(t, false, result)
+	}
+}
+
+func TestVerifyFailsToVerifyWhenEmptyPublicKeys(t *testing.T) {
+	entries, err := os.ReadDir(CORRECT_DATA_DIR)
+	assert.NoError(t, err)
+	for _, entry := range entries {
+		content, err := os.ReadFile(fmt.Sprintf(CORRECT_DATA_DIR_FORMAT, entry.Name()))
+		assert.NoError(t, err)
+
+		var verify custom_query.QueryAggregateVerify
+		err = json.Unmarshal(content, &verify)
+		assert.NoError(t, err)
+
+		msg := [32]byte{}
+		copy(msg[:], verify.Message[:])
+
+		_, err = custom_query.VerifySignature(verify.Signature, msg, [][]byte{})
+		assert.Error(t, err)
+	}
+}
+
+func TestVerifyFailsToVerifyWhenIncorrectMessage(t *testing.T) {
+	entries, err := os.ReadDir(CORRECT_DATA_DIR)
+	assert.NoError(t, err)
+	for _, entry := range entries {
+		content, err := os.ReadFile(fmt.Sprintf(CORRECT_DATA_DIR_FORMAT, entry.Name()))
+		assert.NoError(t, err)
+
+		var verify custom_query.QueryAggregateVerify
+		err = json.Unmarshal(content, &verify)
+		assert.NoError(t, err)
+
+		msg := [32]byte{}
+		copy(msg[:], verify.Message[:])
+
+		msg[0] = ^uint8(0) - msg[0]
+
+		result, err := custom_query.VerifySignature(verify.Signature, msg, verify.PublicKeys)
+		assert.NoError(t, err)
+
+		assert.Equal(t, false, result)
+	}
+}
+
+func TestVerifyFailsToVerifyWhenIncorrectSignature(t *testing.T) {
+	content, err := os.ReadFile("./testdata/incorrect_signature.json")
+	assert.NoError(t, err)
+
+	var verify custom_query.QueryAggregateVerify
+	err = json.Unmarshal(content, &verify)
+	assert.NoError(t, err)
+
+	msg := [32]byte{}
+	copy(msg[:], verify.Message[:])
+
+	result, err := custom_query.VerifySignature(verify.Signature, msg, verify.PublicKeys)
+	assert.NoError(t, err)
+
+	assert.Equal(t, false, result)
 }
