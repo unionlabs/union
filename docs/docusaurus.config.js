dde583e5f0af37db8e57fb06422692d5d4904171
cor <cor@pruijs.dev>
2023-07-28 19:14:19 +0200

    feat(docs): default match lander background
    but match system preference

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -51,6 +51,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        respectPrefersColorScheme: true
+      },
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
       navbar: {
