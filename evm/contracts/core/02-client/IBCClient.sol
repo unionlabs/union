564107001a3cf417fbb9be4d3007f9f93cb50090
Miguel Palhas <mpalhas@gmail.com>
2023-07-28 12:05:41 +0100

    code review

--- evm/contracts/core/02-client/IBCClient.sol
+++ evm/contracts/core/02-client/IBCClient.sol
@@ -11,60 +11,37 @@ import "../02-client/IIBCClient.sol";
  * @dev IBCClient is a contract that implements [ICS-2](https://github.com/cosmos/ibc/tree/main/spec/core/ics-002-client-semantics).
  */
 contract IBCClient is IBCStore, IIBCClient {
+    event GeneratedClientIdentifier(string);
+
     /**
      * @dev registerClient registers a new client type into the client registry
      */
-    function registerClient(
-        string calldata clientType,
-        ILightClient client
-    ) external override {
-        require(
-            address(clientRegistry[clientType]) == address(0),
-            "registerClient: client type already exists"
-        );
-        require(
-            address(client) != address(this),
-            "registerClient: must not be self"
-        );
+    function registerClient(string calldata clientType, ILightClient client) external override {
+        require(address(clientRegistry[clientType]) == address(0), "registerClient: client type already exists");
+        require(address(client) != address(this), "registerClient: must not be self");
         clientRegistry[clientType] = address(client);
     }
 
     /**
      * @dev createClient creates a new client state and populates it with a given consensus state
      */
-    function createClient(
-        IBCMsgs.MsgCreateClient calldata msg_
-    ) external override returns (string memory clientId) {
+    function createClient(IBCMsgs.MsgCreateClient calldata msg_) external override returns (string memory clientId) {
         address clientImpl = clientRegistry[msg_.clientType];
-        require(
-            clientImpl != address(0),
-            "createClient: unregistered client type"
-        );
+        require(clientImpl != address(0), "createClient: unregistered client type");
         clientId = generateClientIdentifier(msg_.clientType);
         clientTypes[clientId] = msg_.clientType;
         clientImpls[clientId] = clientImpl;
-        (
-            bytes32 clientStateCommitment,
-            ConsensusStateUpdate memory update,
-            bool ok
-        ) = ILightClient(clientImpl).createClient(
-                clientId,
-                msg_.clientStateBytes,
-                msg_.consensusStateBytes
-            );
+        (bytes32 clientStateCommitment, ConsensusStateUpdate memory update, bool ok) =
+            ILightClient(clientImpl).createClient(clientId, msg_.clientStateBytes, msg_.consensusStateBytes);
         require(ok, "createClient: failed to create client");
 
         // update commitments
-        commitments[
-            keccak256(IBCCommitment.clientStatePath(clientId))
-        ] = clientStateCommitment;
-        commitments[
-            IBCCommitment.consensusStateCommitmentKey(
-                clientId,
-                update.height.revision_number,
-                update.height.revision_height
-            )
-        ] = update.consensusStateCommitment;
+        commitments[keccak256(IBCCommitment.clientStatePath(clientId))] = clientStateCommitment;
+        commitments[IBCCommitment.consensusStateCommitmentKey(
+            clientId, update.height.revision_number, update.height.revision_height
+        )] = update.consensusStateCommitment;
+
+        emit GeneratedClientIdentifier(clientId);
 
         return clientId;
     }
@@ -72,50 +49,25 @@ contract IBCClient is IBCStore, IIBCClient {
     /**
      * @dev updateClient updates the consensus state and the state root from a provided header
      */
-    function updateClient(
-        IBCMsgs.MsgUpdateClient calldata msg_
-    ) external override {
+    function updateClient(IBCMsgs.MsgUpdateClient calldata msg_) external override {
         require(
-            commitments[
-                IBCCommitment.clientStateCommitmentKey(msg_.clientId)
-            ] != bytes32(0),
-            "updateClient: no state"
+            commitments[IBCCommitment.clientStateCommitmentKey(msg_.clientId)] != bytes32(0), "updateClient: no state"
         );
-        (
-            bytes32 clientStateCommitment,
-            ConsensusStateUpdate[] memory updates,
-            bool ok
-        ) = getClient(msg_.clientId).updateClient(
-                msg_.clientId,
-                msg_.clientMessage
-            );
+        (bytes32 clientStateCommitment, ConsensusStateUpdate[] memory updates, bool ok) =
+            getClient(msg_.clientId).updateClient(msg_.clientId, msg_.clientMessage);
         require(ok, "updateClient: failed to update client");
 
         // update commitments
-        commitments[
-            keccak256(IBCCommitment.clientStatePath(msg_.clientId))
-        ] = clientStateCommitment;
+        commitments[keccak256(IBCCommitment.clientStatePath(msg_.clientId))] = clientStateCommitment;
         for (uint256 i = 0; i < updates.length; i++) {
-            commitments[
-                IBCCommitment.consensusStateCommitmentKey(
-                    msg_.clientId,
-                    updates[i].height.revision_number,
-                    updates[i].height.revision_height
-                )
-            ] = updates[i].consensusStateCommitment;
+            commitments[IBCCommitment.consensusStateCommitmentKey(
+                msg_.clientId, updates[i].height.revision_number, updates[i].height.revision_height
+            )] = updates[i].consensusStateCommitment;
         }
     }
 
-    function generateClientIdentifier(
-        string calldata clientType
-    ) private returns (string memory) {
-        string memory identifier = string(
-            abi.encodePacked(
-                clientType,
-                "-",
-                Strings.toString(nextClientSequence)
-            )
-        );
+    function generateClientIdentifier(string calldata clientType) private returns (string memory) {
+        string memory identifier = string(abi.encodePacked(clientType, "-", Strings.toString(nextClientSequence)));
         nextClientSequence++;
         return identifier;
     }
--- evm/contracts/core/25-handler/IBCClientHandler.sol
+++ evm/contracts/core/25-handler/IBCClientHandler.sol
@@ -10,8 +10,6 @@ abstract contract IBCClientHandler {
     // IBC Client contract address
     address immutable ibcClientAddress;
 
-    event GeneratedClientIdentifier(string);
-
     constructor(address ibcClient) {
         ibcClientAddress = ibcClient;
     }
@@ -19,48 +17,37 @@ abstract contract IBCClientHandler {
     /**
      * @dev registerClient registers a new client type into the client registry
      */
-    function registerClient(
-        string calldata clientType,
-        ILightClient client
-    ) public virtual {
-        (bool success, bytes memory res) = ibcClientAddress.delegatecall(
-            abi.encodeWithSelector(
-                IIBCClient.registerClient.selector,
-                clientType,
-                client
-            )
-        );
-        if (!success) {
-            revert(_getRevertMsg(res));
-        }
+    function registerClient(string calldata clientType, ILightClient client) public virtual {
+        passthrough();
     }
 
     /**
      * @dev createClient creates a new client state and populates it with a given consensus state
      */
-    function createClient(
-        IBCMsgs.MsgCreateClient calldata msg_
-    ) external returns (string memory clientId) {
-        (bool success, bytes memory res) = ibcClientAddress.delegatecall(
-            abi.encodeWithSelector(IIBCClient.createClient.selector, msg_)
-        );
-        if (!success) {
-            revert(_getRevertMsg(res));
-        }
-        clientId = abi.decode(res, (string));
-        emit GeneratedClientIdentifier(clientId);
-        return clientId;
+    function createClient(IBCMsgs.MsgCreateClient calldata msg_) external returns (string memory clientId) {
+        passthrough();
     }
 
     /**
      * @dev updateClient updates the consensus state and the state root from a provided header
      */
     function updateClient(IBCMsgs.MsgUpdateClient calldata msg_) external {
-        (bool success, bytes memory res) = ibcClientAddress.delegatecall(
-            abi.encodeWithSelector(IIBCClient.updateClient.selector, msg_)
-        );
-        if (!success) {
-            revert(_getRevertMsg(res));
+        passthrough();
+    }
+
+    function passthrough() public {
+        address dest = ibcClientAddress;
+        assembly {
+            // copy function selector and any arguments
+            calldatacopy(0, 0, calldatasize())
+            // execute function call using the facet
+            let result := delegatecall(gas(), dest, 0, calldatasize(), 0, 0)
+            // get any return value
+            returndatacopy(0, 0, returndatasize())
+            // return any return value or error back to the caller
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
         }
     }
 }
