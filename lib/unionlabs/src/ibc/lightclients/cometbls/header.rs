ce8ba2381cfbb68186e98c736ac7df6ea7637ac9
Ben Luelo <benluelo@hotmail.com>
2023-08-14 09:39:59 -0400

    chore: remove dead code

--- lib/unionlabs/src/ibc/lightclients/cometbls/header.rs
+++ lib/unionlabs/src/ibc/lightclients/cometbls/header.rs
@@ -26,18 +26,6 @@ impl From<Header> for contracts::glue::UnionIbcLightclientsCometblsV1HeaderData 
     }
 }
 
-// #[cfg(feature = "ethabi")]
-// impl From<contracts::glue::UnionIbcLightclientsCometblsV1HeaderData> for Header {
-//     fn from(value: contracts::glue::UnionIbcLightclientsCometblsV1HeaderData) -> Self {
-//         Self {
-//             signed_header: value.signed_header.into(),
-//             untrusted_validator_set_root: value.untrusted_validator_set_root.into(),
-//             trusted_height: value.trusted_height.into(),
-//             zero_knowledge_proof: value.zero_knowledge_proof.into(),
-//         }
-//     }
-// }
-
 #[cfg(feature = "ethabi")]
 impl crate::EthAbi for Header {
     type EthAbi = contracts::glue::UnionIbcLightclientsCometblsV1HeaderData;
