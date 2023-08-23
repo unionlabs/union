0d797d3b5a0a164d8c6e87dae67f7ba92f0b450b
aeryz <abdullaheryz@protonmail.com>
2023-08-23 09:34:00 +0300

    feat(unionlabs): introduce tendermint light client types
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

---
+++ lib/unionlabs/src/cosmos/ics23.rs
@@ -0,0 +1,5 @@
+pub mod hash_op;
+pub mod inner_spec;
+pub mod leaf_op;
+pub mod length_op;
+pub mod proof_spec;
---
+++ lib/unionlabs/src/ibc/lightclients/tendermint/consensus_state.rs
@@ -0,0 +1,66 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    errors::{required, InvalidLength, MissingField},
+    ethereum::H256,
+    ibc::core::commitment::merkle_root::MerkleRoot,
+    Proto, TryFromProtoErrorOf, TypeUrl,
+};
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ConsensusState {
+    pub root: MerkleRoot,
+    pub next_validators_hash: H256,
+}
+
+#[derive(Debug)]
+pub enum TryFromConsensusStateError {
+    MissingField(MissingField),
+    Root(TryFromProtoErrorOf<MerkleRoot>),
+    NextValidatorsHash(InvalidLength),
+}
+
+impl TryFrom<protos::union::ibc::lightclients::cometbls::v1::ConsensusState> for ConsensusState {
+    type Error = TryFromConsensusStateError;
+
+    fn try_from(
+        value: protos::union::ibc::lightclients::cometbls::v1::ConsensusState,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            root: required!(value.root)?
+                .try_into()
+                .map_err(TryFromConsensusStateError::Root)?,
+            next_validators_hash: value
+                .next_validators_hash
+                .try_into()
+                .map_err(TryFromConsensusStateError::NextValidatorsHash)?,
+        })
+    }
+}
+
+impl TypeUrl for protos::union::ibc::lightclients::cometbls::v1::ConsensusState {
+    const TYPE_URL: &'static str = "/union.ibc.lightclients.cometbls.v1.ConsensusState";
+}
+
+impl Proto for ConsensusState {
+    type Proto = protos::union::ibc::lightclients::cometbls::v1::ConsensusState;
+}
+
+impl From<ConsensusState> for protos::union::ibc::lightclients::cometbls::v1::ConsensusState {
+    fn from(value: ConsensusState) -> Self {
+        Self {
+            root: Some(value.root.into()),
+            next_validators_hash: value.next_validators_hash.into(),
+        }
+    }
+}
+
+#[cfg(feature = "ethabi")]
+impl From<ConsensusState> for contracts::glue::UnionIbcLightclientsCometblsV1ConsensusStateData {
+    fn from(value: ConsensusState) -> Self {
+        Self {
+            root: value.root.into(),
+            next_validators_hash: value.next_validators_hash.into(),
+        }
+    }
+}
