bd9a1bef7185b567545c9be79555fec251cd0622
Karel L. Kubat <k.l.kubat@gmail.com>
2023-08-30 18:42:32 +0100

    ci(hubble): add hubble-image

--- hubble/hubble.nix
+++ hubble/hubble.nix
@@ -1,5 +1,5 @@
 { ... }: {
-  perSystem = { pkgs, crane, ... }:
+  perSystem = { self', pkgs, crane, ... }:
     let
       hubble = crane.buildWorkspaceMember {
         crateDirFromRoot = "hubble";
@@ -11,6 +11,22 @@
       };
     in
     {
-      inherit (hubble) checks packages;
+      inherit (hubble) checks ;
+      packages = {
+        hubble = hubble.packages.hubble;
+
+        hubble-image = pkgs.dockerTools.buildImage {
+          name = "hubble";
+
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = [ pkgs.coreutils-full  hubble ];
+            pathsToLink = [ "/bin" ];
+          };
+          config = {
+            Entrypoint = [ (pkgs.lib.getExe self'.packages.uniond) ];
+          };
+        };
+      };
     };
 }
