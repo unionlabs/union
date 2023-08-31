4b526df03bed316a0ac2b23221cdda08372c495f
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-08-31 08:23:56 -0500

    feat(uniond): v0.10.0 upgrade migration

---
+++ uniond/app/upgrades/v0_10_0/constants.go
@@ -0,0 +1,14 @@
+package v0_10_0
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+	"union/app/upgrades"
+)
+
+const UpgradeName = "v0.10.0"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades:        store.StoreUpgrades{Added: []string{}},
+}
