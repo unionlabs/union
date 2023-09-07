58c478b82e55a9859ce50d1f86c68bc28b5e81fb
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2023-09-07 14:14:47 +0200

    feat(ucs01): introduce api tests

--- cosmwasm/cosmwasm.nix
+++ cosmwasm/cosmwasm.nix
@@ -4,12 +4,15 @@
       ucs01-relay = crane.buildWasmContract {
         crateDirFromRoot = "cosmwasm/ucs01-relay";
       };
+      ucs01-relay-api = crane.buildWorkspaceMember {
+        crateDirFromRoot = "cosmwasm/ucs01-relay-api";
+      };
       ucs00-pingpong = crane.buildWasmContract {
         crateDirFromRoot = "cosmwasm/ucs00-pingpong";
       };
     in
     {
       packages = ucs01-relay.packages // ucs00-pingpong.packages;
-      checks = ucs01-relay.checks // ucs00-pingpong.checks;
+      checks = ucs01-relay.checks // ucs01-relay-api.checks // ucs00-pingpong.checks;
     };
 }
