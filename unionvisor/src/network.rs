52f96b71815dcf733d22cb42e6331d5097e71998
cor <cor@pruijs.dev>
2023-09-20 17:20:15 +0200

    fix(unionvisor): networks refer to existing networks

--- unionvisor/src/network.rs
+++ unionvisor/src/network.rs
@@ -1,14 +1,14 @@
 use std::{fmt::Display, str::FromStr};
 
-use color_eyre::eyre::bail;
+use thiserror::Error;
 
 #[derive(Debug, Clone, Default, Copy)]
 pub enum Network {
-    Union1,
-    Testnet1,
+    // Testnet1,
+    // Testnet2,
     #[default]
-    Testnet2,
-    DevnetMinimal,
+    Testnet3,
+    // DevnetMinimal,
 }
 
 impl Display for Network {
@@ -22,16 +22,20 @@ impl Display for Network {
     }
 }
 
+#[derive(Debug, Error)]
+#[error("unknown network {0}")]
+struct UnknownNetworkError(String);
+
 impl FromStr for Network {
-    type Err = color_eyre::Report;
+    type Err = UnknownNetworkError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "union-1" => Ok(Network::Union1),
-            "union-testnet-1" => Ok(Network::Testnet1),
-            "union-testnet-2" => Ok(Network::Testnet2),
-            "union-minimal-1" => Ok(Network::DevnetMinimal),
-            _ => bail!("unknown network"),
+            // "union-testnet-1" => Ok(Network::Testnet1),
+            // "union-testnet-2" => Ok(Network::Testnet2),
+            "union-testnet-3" => Ok(Network::Testnet3),
+            // "union-minimal-1" => Ok(Network::DevnetMinimal),
+            s => Err(UnknownNetworkError(s.to_owned())),
         }
     }
 }
@@ -48,6 +52,12 @@ impl Network {
     pub fn genesis_url(&self) -> &str {
         // We use a secret gist for the genesis.json until testnet is public
         // "https://raw.githubusercontent.com/unionlabs/genesis/main/union-testnet-1/genesis.json"
-        "https://gist.githubusercontent.com/cor/5cab203f0f41549a2e04e48fc10bf889/raw/0bb5a9d3c02f38b6a34f85314dff04100f3605bc/union-testnet-3-genesis.json"
+        match self {
+            // Network::Testnet1 => todo!(),
+            // Network::Testnet2 => todo!(),
+            Network::Testnet3 => "https://gist.githubusercontent.com/cor/5cab203f0f41549a2e04e48fc10bf889/raw/0bb5a9d3c02f38b6a34f85314dff04100f3605bc/union-testnet-3-genesis.json",
+            // _ => 
+            // Network::DevnetMinimal => todo!(),
+        }
     }
 }
