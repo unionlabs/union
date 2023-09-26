6f72060ece804abf986ceb4be9b7b4b39aeab99e
cor <cor@pruijs.dev>
2023-09-26 22:57:53 +0200

    feat(site): improve blog post font sizing

--- site/src/routes/blog/(blog-article)/the-journey-so-far/+layout.svelte
+++ site/src/routes/blog/(blog-article)/the-journey-so-far/+layout.svelte
@@ -6,16 +6,16 @@
 
 <style lang="postcss">
 	:global(.blog-post-container > h2) {
-		@apply text-5xl font-bold;
+		@apply text-3xl sm:text-5xl font-bold;
 	}
 	:global(.blog-post-container > h2 + span) {
-		@apply font-mono mb-2 text-lg;
+		@apply font-mono mb-2 mt-1 sm:text-lg;
 	}
 	:global(.blog-post-container > h2 + span > a) {
 		@apply text-accent;
 	}
 
 	:global(.blog-post-container > p) {
-		@apply text-xl mb-4;
+		@apply sm:text-xl mb-4;
 	}
 </style>
