a1170ee67a4a1bac7322de4649b0162cf9484a25
cor <cor@pruijs.dev>
2023-09-26 19:57:09 +0200

    feat(site): add header

---
+++ site/src/lib/Header.svelte
@@ -0,0 +1,9 @@
+<header class="bg-black flex items-center p-4 gap-4">
+	<img class="h-[32px]" src="logo.svg" alt="Union Logo" />
+	<nav class="flex flex-1 justify-end">
+		<ul class="flex gap-4 font-mono">
+			<li>docs</li>
+			<li>blog</li>
+		</ul>
+	</nav>
+</header>
--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/Header.svelte';
 </script>
+
+<Header />
 
 <svelte:head>
 	<title>Union</title>
