da3293d01901160728cc40208b68f875960ac19b
cor <cor@pruijs.dev>
2023-09-26 22:27:34 +0200

    feat(site): refactor blog layout system

---
+++ site/src/routes/blog/(blog-article)/the-journey-so-far/+layout.svelte
@@ -0,0 +1,15 @@
+<main
+	class="blog-post-container flex flex-col flex-1 p-4 justify-center max-w-2xl justify-center self-center"
+>
+	<slot />
+</main>
+
+<style lang="postcss">
+	:global(.blog-post-container > h2) {
+		@apply text-5xl font-bold;
+	}
+
+	:global(.blog-post-container > .subtitle) {
+		@apply font-mono mb-2 text-lg;
+	}
+</style>
---
+++ site/src/routes/blog/(blog-article)/the-journey-so-far/+page.svelte
@@ -0,0 +1,21 @@
+<h2>The Journey So Far</h2>
+<div class="subtitle">2023-09-26 - @union_build</div>
+<p class="mb-2 text-xl">
+	Today, we are happy to announce the work we have done so far on Union, the trustless
+	infrastructure layer for web3. Over the last six months, unionlabs was founded with nothing but an
+	idea, a diverse skillset, and a mission to ship zkp powered infra everywhere.
+</p>
+<p class="mb-2 text-xl">
+	We spent months building in stealthmode, heads-down, as we validated our technical assumptions.
+	The product of that is the Union testnet, CometBLS and Galois. These components allowed us to
+	achieve <b>IBC to Ethereum, the endgame.</b>
+</p>
+<p class="mb-2 text-xl">
+	Union is designed around a few key principles: permissonless interopability, censorship
+	resistance, and decentralized infrastrucutre. Anyone should be allowed to transact, with any
+	counterparty, on any chain, and this system may not rely on massive, closed source infrastructure.
+</p>
+<p class="mb-2 text-xl">
+	With Union, we can bring any asset natively to any chain, hook into account abstraction with ICQ,
+	and access coproccors and storage proofs.
+</p>
