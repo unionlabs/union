efa9c07e587e6cf44c844ea5e1d9c1634fd5c890
cor <cor@pruijs.dev>
2023-09-27 13:07:33 +0200

    feat(blog): make entire article preview clickable

--- site/src/routes/blog/+page.svelte
+++ site/src/routes/blog/+page.svelte
@@ -16,12 +16,12 @@
 <main class="flex flex-1 justify-center sm:mt-4 ">
 	<div class="flex flex-col gap-4 max-w-2xl">
 		{#each Object.entries(posts) as [path, {metadata}] }
-			<section class="p-4">
+			<a class="block p-4" href={extractSlug(path)}>
 				<h1 class="text-3xl sm:text-5xl font-bold mb-0">{metadata.title}</h1>
 				<div class="font-mono mb-2 mt-1 sm:text-lg">{metadata.date} - <a class="text-accent" href={`https://x.com/${metadata.author}`}>{metadata.author}</a></div>
 				<p class="sm:text-xl">{metadata.preview}</p>
-				<a class="block mt-2 font-mono text-lg" href={extractSlug(path)}>READ FULL POST -&gt;</a>
-			</section>
+				<p class="block mt-2 font-mono text-lg">READ FULL POST -&gt;</p>
+			</a>
 		{/each}
 	</div>
 </main>
