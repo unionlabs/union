bd0b5b919dc972344b26a63cc4172dbc35d2edc4
cor <cor@pruijs.dev>
2023-09-28 21:19:50 +0200

    refactor(site): refactor terminal

--- site/src/lib/Xterm.svelte
+++ site/src/lib/Xterm.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
-	import type { Terminal } from 'xterm';
 	import type { ApolloQueryResult } from '@apollo/client';
 	import BlogLayout from '../mdsvex/BlogLayout.svelte';
 	import ButtonA from './ButtonA.svelte';
@@ -10,6 +9,8 @@
 	  uri: 'https://graphql.union.build/v1/graphql',
 	  cache: new InMemoryCache(),
 	});
+
+	type LogLine = { network: string, action: string, logLine: string };
 
 	const FETCH_EVENT = gql`
 		query FetchEvent($id: Int!) {
@@ -28,20 +29,12 @@
 		}
 	`
 
-	let terminal: null | Terminal;
-	let terminalElement: HTMLElement;
-	let latestId: null | number = null;
 	const replayOffset = 300;
 
-	let logLines: {network: String, action: String, logLine: String}[] = [];
+	let terminalElement: HTMLElement;
+	let logLines: LogLine[] = [];
 
-
-	let scrollAnchor: HTMLElement;
-	let scroller: HTMLElement;
-
-
-
-    const filter = (r: ApolloQueryResult<any>): null | { network: String, action: String, logLine: String } => {
+  const formatLogline = (r: ApolloQueryResult<any>): null | LogLine => {
 			let data = r.data.demo_txes_by_pk;
 			if (data === null) {
 				return null
@@ -53,30 +46,30 @@
 
 			if ('EthereumMinimal' in data) {
 				network = "union"
-                data = data['EthereumMinimal']
+        data = data['EthereumMinimal']
 			}
 
 			if ('CometblsMinimal' in data) {
 				network = "sepolia"
-                data = data['CometblsMinimal']
+        data = data['CometblsMinimal']
 			}
 
 			if ('Fetch' in data) {
-                action = "fetching "
+        action = "fetching "
 				data = data["Fetch"]["data"]
 			}
 
 			if ('Event' in data) {
-                action = "observed event "
+        action = "observed event "
 				data = data["Event"]["data"]
 			}
 
 			if ('Msg' in data) {
-                action = "sending message "
+        action = "sending message "
 				data = data["Msg"]["data"]
 			}
 
-            if (network === undefined) {
+      if (network == undefined || action == undefined) {
 				return null
 			}
 
@@ -85,7 +78,7 @@
 
 	const sleep = (ms: number) =>  new Promise(r => setTimeout(r, ms));
 
-	const worker = async (latestIdWorker) => {
+	const worker = async (latestIdWorker: number) => {
 		const startHeight = latestIdWorker - replayOffset
 		let i = startHeight;
     while (true) {
@@ -96,20 +89,20 @@
        variables: { id: i },
       });
 
-			const newLine = filter(response);
+			const newLine = formatLogline(response);
 			if (newLine != null) {
 				logLines = [newLine, ...logLines];
 			}
 
-			if (i === (latestIdWorker - 1)) {
+			if (i > (latestIdWorker - 10)) {
 				i = startHeight;
 			} 
     }
   }
 	
 	onMount(async () => {
-		let response = await client.query({ query: FETCH_LATEST_ID, });
-		latestId = response.data.demo_txes[0].id; 		
+		const response = await client.query({ query: FETCH_LATEST_ID, });
+		const latestId = response.data.demo_txes[0].id; 		
     worker(latestId);
 	})
 </script>
