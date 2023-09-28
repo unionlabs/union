1f829662218dd4aa567c6f214ccfd8f8ba44aba2
Ben Luelo <benluelo@hotmail.com>
2023-09-28 14:43:22 -0400

    feat: build voyager for minimal and mainnet

--- voyager/voyager.nix
+++ voyager/voyager.nix
@@ -1,13 +1,19 @@
 { ... }: {
   perSystem = { self', pkgs, system, config, crane, stdenv, ... }:
     let
-      voyagerAll = (crane.buildWorkspaceMember {
+      mkVoyager = features: pnameSuffix: (crane.buildWorkspaceMember {
+        inherit pnameSuffix;
         crateDirFromRoot = "voyager";
+        cargoBuildExtraArgs = features;
         additionalTestSrcFilter = path: _:
           (pkgs.lib.hasPrefix "hubble/src/graphql" path);
       });
+
+      voyagerMainnet = (mkVoyager "--features eth-mainnet" "-mainnet");
+      voyagerMinimal = (mkVoyager "" "-minimal");
     in
     {
-      inherit (voyagerAll) packages checks;
+      packages = pkgs.lib.recursiveUpdate voyagerMainnet.packages voyagerMinimal.packages;
+      checks = pkgs.lib.recursiveUpdate voyagerMainnet.checks voyagerMinimal.checks;
     };
 }
