13246ee09443ba7fecdcbf564e3b2cda73ae33d1
cor <cor@pruijs.dev>
2023-09-28 15:28:41 +0200

    fix(site): actually hide scrollbar

--- site/src/lib/Xterm.svelte
+++ site/src/lib/Xterm.svelte
@@ -119,5 +119,15 @@
 		overflow-anchor: auto;
 		height: 1px;
 	}
+	/* For Webkit-based browsers (Chrome, Safari and Opera) */
+	.scrollbar-hide::-webkit-scrollbar {
+	    display: none;
+	}
+
+	/* For IE, Edge and Firefox */
+	.scrollbar-hide {
+	    -ms-overflow-style: none;  /* IE and Edge */
+	    scrollbar-width: none;  /* Firefox */
+	}
 </style>
 
