fef3b066a63e0da9da5a0bb82a7cd51b2b7ae40f
cor <cor@pruijs.dev>
2023-09-28 13:35:34 +0200

    fix(site): remove xterm

--- site/src/lib/Xterm.svelte
+++ site/src/lib/Xterm.svelte
@@ -88,10 +88,6 @@
              },
             }).then((result) => {
 
-								// if (terminal == null) {
-								// 	console.error("Terminal has not been initiated correctly prior to starting worker");
-								// 	return;
-								// }
                 console.log(result)
 								const newLine = filter(result);
 								if (newLine != null) {
@@ -100,211 +96,20 @@
 								}
 
 	        })
-            .catch(err => {
-							if (terminal == null) {
-								console.error("Terminal has not been initiated correctly prior to starting worker");
-								return;
-							}
-              console.error(err)
-              terminal.write(JSON.stringify(err) + "\n")
-            });
         }
     }
 	
 	onMount(async () => {
-		// const xterm = (await import("xterm"))	
-		// const addonFit= (await import('xterm-addon-fit'));
-		// const fitAddon = new addonFit.FitAddon();
-		// terminal = new xterm.Terminal({convertEol: true})
-		// terminal.loadAddon(fitAddon);
-		// terminal.open(terminalElement);
-		// fitAddon.fit();
     worker()
 	})
 </script>
 
 
 <div class="relative h-80">
-	<div bind:this={terminalElement} class=" overflow-y-auto break-words absolute left-0 right-0 bg-black h-80 text-sm font-jetbrains">
+	<div bind:this={terminalElement} class="overflow-auto absolute left-0 right-0 bg-black h-80 text-sm font-jetbrains">
 		{#each logLines as logline}
 			<div>{logline}</div>
 		{/each}
 	</div>
 </div>
 
-<!--<div bind:this={terminalElement} />!-->
-
-
-<!--
-
-<style>
-	.observer {
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-	}
-	
-	
-	/* Xterm.css */
-	:global(.xterm) {
-			cursor: text;
-			position: relative;
-			user-select: none;
-			-ms-user-select: none;
-			-webkit-user-select: none;
-	}
-
-	:global(.xterm.focus),
-	:global(.xterm:focus) {
-			outline: none;
-	}
-
-	:global(.xterm .xterm-helpers) {
-			position: absolute;
-			top: 0;
-			/**
-			 * The z-index of the helpers must be higher than the canvases in order for
-			 * IMEs to appear on top.
-			 */
-			z-index: 5;
-	}
-
-	:global(.xterm .xterm-helper-textarea) {
-			padding: 0;
-			border: 0;
-			margin: 0;
-			/* Move textarea out of the screen to the far left, so that the cursor is not visible */
-			position: absolute;
-			opacity: 0;
-			left: -9999em;
-			top: 0;
-			width: 0;
-			height: 0;
-			z-index: -5;
-			/** Prevent wrapping so the IME appears against the textarea at the correct position */
-			white-space: nowrap;
-			overflow: hidden;
-			resize: none;
-	}
-
-	:global(.xterm .composition-view) {
-			/* TODO: Composition position got messed up somewhere */
-			background: #000;
-			color: #FFF;
-			display: none;
-			position: absolute;
-			white-space: nowrap;
-			z-index: 1;
-	}
-
-	:global(.xterm .composition-view.active) {
-			display: block;
-	}
-
-	:global(.xterm .xterm-viewport) {
-			/* On OS X this is required in order for the scroll bar to appear fully opaque */
-			background-color: #000;
-			overflow-y: scroll;
-			cursor: default;
-			position: absolute;
-			right: 0;
-			left: 0;
-			top: 0;
-			bottom: 0;
-	}
-
-	:global(.xterm .xterm-screen) {
-			position: relative;
-	}
-
-	:global(.xterm .xterm-screen canvas) {
-			position: absolute;
-			left: 0;
-			top: 0;
-	}
-
-	:global(.xterm .xterm-scroll-area) {
-			visibility: hidden;
-	}
-
-	:global(.xterm-char-measure-element) {
-			display: inline-block;
-			visibility: hidden;
-			position: absolute;
-			top: 0;
-			left: -9999em;
-			line-height: normal;
-	}
-
-	:global(.xterm.enable-mouse-events) {
-			/* When mouse events are enabled (eg. tmux), revert to the standard pointer cursor */
-			cursor: default;
-	}
-
-	:global(.xterm.xterm-cursor-pointer),
-	:global(.xterm .xterm-cursor-pointer) {
-			cursor: pointer;
-	}
-
-	:global(.xterm.column-select.focus) {
-			/* Column selection mode */
-			cursor: crosshair;
-	}
-
-	:global(.xterm .xterm-accessibility),
-	:global(.xterm .xterm-message) {
-			position: absolute;
-			left: 0;
-			top: 0;
-			bottom: 0;
-			right: 0;
-			z-index: 10;
-			color: transparent;
-	}
-
-	:global(.xterm .live-region) {
-			position: absolute;
-			left: -9999px;
-			width: 1px;
-			height: 1px;
-			overflow: hidden;
-	}
-
-	:global(.xterm-dim) {
-			opacity: 0.5;
-	}
-
-	:global(.xterm-underline) {
-			text-decoration: underline;
-	}
-
-	:global(.xterm-strikethrough) {
-			text-decoration: line-through;
-	}
-
-	:global(.xterm-screen .xterm-decoration-container .xterm-decoration) {
-		z-index: 6;
-		position: absolute;
-	}
-
-	:global(.xterm-decoration-overview-ruler) {
-			z-index: 7;
-			position: absolute;
-			top: 0;
-			right: 0;
-			pointer-events: none;
-	}
-
-	:global(.xterm-decoration-top) {
-			z-index: 2;
-			position: relative;
-	}
-
-	.terminal-container {
-	}
-
-</style>
-
-!-->
