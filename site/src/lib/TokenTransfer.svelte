3c024d3371b9abc8592d5747d58a364831a21677
cor <cor@pruijs.dev>
2023-09-30 21:07:53 +0200

    feat(site): balance fetching and token transfers
    auto fetch balance

--- site/src/lib/TokenTransfer.svelte
+++ site/src/lib/TokenTransfer.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 
 import { onMount } from 'svelte';
-import type { AccountData } from '@cosmjs/amino';
+import type { AccountData, Coin } from '@cosmjs/amino';
 import { browser } from '$app/environment';
 import BlogLayout from '../mdsvex/BlogLayout.svelte';
 import Error from '../routes/+error.svelte';
 
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
 import type { ApolloQueryResult } from '@apollo/client';
-	import { Tendermint37Client } from '@cosmjs/tendermint-rpc';
-	import { SigningStargateClient } from '@cosmjs/stargate';
+import { Tendermint37Client } from '@cosmjs/tendermint-rpc';
+import { SigningStargateClient } from '@cosmjs/stargate';
 
-let accounts: null | AccountData[] = null;
+let account: null | AccountData = null;
+let balance: null | Coin = null;
 
 const graphqlClient = new ApolloClient({
   uri: 'https://graphql.union.build/v1/graphql',
@@ -35,33 +36,53 @@ if (browser) {
 }
 
 const getUnoFromFaucent = async () => {
-	if (accounts === null) {
+	if (account === null) {
 		console.error("trying to get uno from faucet before accounts are loaded");
 		return;
 	}
 	let response = await graphqlClient.mutate({ 
 		mutation: GET_UNO_FROM_FAUCET, 
-		variables: { addr: accounts[0].address }
+		variables: { addr: account.address }
 	});
 	console.log(response);
 }
 
 const sendTransfer = async () => {
-	if (accounts === null || stargateClient === null) {
+	if (account === null || stargateClient === null) {
 		console.error("trying to get uno from faucet before accounts are loaded");
 		return;
 	}
 	console.log("sending tokens")
 	const txResponse = await stargateClient.sendTokens(
-       accounts[0].address,
+       account.address,
        "union1v39zvpn9ff7quu9lxsawdwpg60lyfpz8pmhfey",
        [
-           { denom: "muno", amount: "1" },
+           { denom: "muno", amount: "1000" },
        ],
        "auto",
     )
 
 	console.log(txResponse);
+}
+
+const sleep = (ms: number) =>  new Promise(r => setTimeout(r, ms));
+const getBalanceWorker = async () => {
+	while (true) {
+		await sleep(2000);
+		getBalance();
+	}
+}
+
+const getBalance = async () => {
+	if(stargateClient == null) {
+		console.error("stargateClient is null while querying balance");
+		return;
+	}
+	if (account == null) {
+		console.error("fetching balance for nonexisting account");
+		return;
+	}
+	balance = await stargateClient.getBalance(account.address, "muno");
 }
 
 
@@ -73,10 +94,11 @@ const connect = async () => {
 	let { getSnap, connectSnap, suggestChain, getKey } = await import('@leapwallet/cosmos-snap-provider');
 	let { GasPrice, SigningStargateClient } = await import('@cosmjs/stargate');
 	let { Tendermint37Client } = await import("@cosmjs/tendermint-rpc");
+
 	const snapInstalled = await getSnap();
-	    if (!snapInstalled) {
-		    connectSnap(); // Initiates installation if not already present
-		}
+  if (!snapInstalled) {
+    connectSnap(); // Initiates installation if not already present
+	}
 
 	const chainId = "union-testnet-3";
 
@@ -93,7 +115,11 @@ const connect = async () => {
 	)
   const offlineSigner = new CosmjsOfflineSigner(chainId);
 
-  accounts = await offlineSigner.getAccounts();
+  let accounts = await offlineSigner.getAccounts();
+	if (accounts.length > 0) {
+		account = accounts[0];
+	}
+
 	const key = await getKey(chainId);
 	console.log(key)
   const rpcUrl = "wss://rpc.0xc0dejug.uno"; // Populate with an RPC URL corresponding to the given chainId
@@ -104,21 +130,26 @@ const connect = async () => {
 }  
 
 onMount(async () => {
-	connect()
+	await connect();
+	getBalanceWorker();
 })
 </script>
 
 
 <div class="my-8 h-[200px]">
-	<div style="margin: 0 auto;" class="absolute h-[200px] max-w-4xl p-4 md:shadow-2xl left-0 md:left-[16px] right-0 md:right-[16px] bg-black md:rounded-xl">
-	{#if accounts === null}
+	<div style="margin: 0 auto;" class="font-jetbrains absolute h-[200px] max-w-4xl p-4 md:shadow-2xl left-0 md:left-[16px] right-0 md:right-[16px] bg-black md:rounded-xl">
+	{#if account === null}
 		Loading account...
 	{:else}
-		Your Union Address:
+		<div>Union Address: {account.address}</div>
+		
 
-		{#each accounts as account}
-			<div class="font-jetbrains">{account.address}</div>
-		{/each}
+		{#if balance === null}
+			<div>Fetching balance...</div>
+		{:else}
+			<div>Union Balance: <b>{balance.amount}</b> {balance.denom}</div>
+		{/if}
+
 
 		<button class="px-4 mt-4 py-2 border-2 font-jetbrains border-accent text-accent" on:click={getUnoFromFaucent}>Get UNO from faucet</button>
 		<button class="px-4 mt-4 py-2 border-2 font-jetbrains border-accent text-accent" on:click={sendTransfer}>Send UNO</button>
