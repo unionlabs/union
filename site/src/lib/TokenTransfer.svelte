b3b28ed4002bb4f1b02d4f1bf8b91548b4f8cf74
cor <cor@pruijs.dev>
2023-10-02 02:34:57 +0200

    refactor(site): move `unionAccount` and `unionBalance` to stores/wallets

--- site/src/lib/TokenTransfer.svelte
+++ site/src/lib/TokenTransfer.svelte
@@ -8,12 +8,10 @@ import Error from '../routes/+error.svelte';
 
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
 import type { ApolloQueryResult } from '@apollo/client';
-import { tendermintClient, stargateClient } from '$lib/stores/wallets.ts'; 
+import { tendermintClient, stargateClient, unionAccount, unionBalance } from '$lib/stores/wallets'; 
 import { get } from 'svelte/store';
 
 
-let account: null | AccountData = null;
-let balance: null | Coin = null;
 
 const graphqlClient = new ApolloClient({
   uri: 'https://graphql.union.build/v1/graphql',
@@ -33,30 +31,34 @@ const GET_UNO_FROM_FAUCET = gql`
 
 // Hack to import cosmjs
 if (browser) {
+	// @ts-ignore
   window.process = { env: {} };
 }
 
 const getUnoFromFaucent = async () => {
-	if (account === null) {
+	const uAccount = get(unionAccount);
+	if (uAccount === null) {
 		console.error("trying to get uno from faucet before accounts are loaded");
 		return;
 	}
+
 	let response = await graphqlClient.mutate({ 
 		mutation: GET_UNO_FROM_FAUCET, 
-		variables: { addr: account.address }
+		variables: { addr: uAccount.address }
 	});
 	console.log(response);
 }
 
 const sendTransfer = async () => {
 	const sgClient = get(stargateClient);
-	if (sgClient === null || account === null) {
+	const uAccount = get(unionAccount);
+	if (sgClient === null || uAccount === null) {
 		console.error("trying to get uno from faucet before accounts are loaded");
 		return;
 	}
 	console.log("sending tokens")
 	const txResponse = await sgClient.sendTokens(
-       account.address,
+       uAccount.address,
        "union1v39zvpn9ff7quu9lxsawdwpg60lyfpz8pmhfey",
        [
            { denom: "muno", amount: "1000" },
@@ -77,15 +79,16 @@ const getBalanceWorker = async () => {
 
 const getBalance = async () => {
 	const sgClient = get(stargateClient);
+	const uAccount = get (unionAccount);
 	if (sgClient == null) {
 		console.error("stargateClient is null while querying balance");
 		return;
 	} 
-	if (account == null) {
+	if (uAccount == null) {
 		console.error("fetching balance for nonexisting account");
 		return;
 	}
-	balance = await sgClient.getBalance(account.address, "muno");
+	unionBalance.set(await sgClient.getBalance(uAccount.address, "muno"));
 }
 
 
@@ -118,7 +121,7 @@ const connect = async () => {
 
   let accounts = await offlineSigner.getAccounts();
 	if (accounts.length > 0) {
-		account = accounts[0];
+		unionAccount.set(accounts[0]);
 	}
 
 	const key = await getKey(chainId);
@@ -143,16 +146,16 @@ onMount(async () => {
 
 <div class="my-8 h-[200px]">
 	<div style="margin: 0 auto;" class="font-jetbrains absolute h-[200px] max-w-4xl p-4 md:shadow-2xl left-0 md:left-[16px] right-0 md:right-[16px] bg-black md:rounded-xl">
-	{#if account === null}
+	{#if $unionAccount === null}
 		Loading account...
 	{:else}
-		<div>Union Address: {account.address}</div>
+		<div>Union Address: {$unionAccount.address}</div>
 		
 
-		{#if balance === null}
+		{#if $unionBalance === null}
 			<div>Fetching balance...</div>
 		{:else}
-			<div>Union Balance: <b>{balance.amount}</b> {balance.denom}</div>
+			<div>Union Balance: <b>{$unionBalance.amount}</b> {$unionBalance.denom}</div>
 		{/if}
 
 
--- site/src/lib/stores/wallets.ts
+++ site/src/lib/stores/wallets.ts
@@ -2,6 +2,9 @@ import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 import type { Tendermint37Client } from '@cosmjs/tendermint-rpc';
 import type { SigningStargateClient } from '@cosmjs/stargate';
+import type { AccountData, Coin } from '@cosmjs/amino';
 
 export const tendermintClient: Writable<Tendermint37Client | null> = writable(null);
 export const stargateClient: Writable<SigningStargateClient | null> = writable(null);
+export const unionAccount: Writable<AccountData | null> = writable(null);
+export const unionBalance: Writable<Coin | null> = writable(null);
