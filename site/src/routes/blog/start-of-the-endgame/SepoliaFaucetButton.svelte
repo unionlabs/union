63013250d72a18faafdfaa4105f2cd23133f3383
Karel L. Kubat <k.l.kubat@gmail.com>
2023-10-04 00:05:00 +0300

    feat(site): add sepolia faucet

--- site/src/routes/blog/start-of-the-endgame/SepoliaFaucetButton.svelte
+++ site/src/routes/blog/start-of-the-endgame/SepoliaFaucetButton.svelte
@@ -1,1 +1,34 @@
-<div class="bg-black">Show link to sepolia faucet</div>
+<script>
+    import { ethereumAddress } from '$lib/stores/wallets'
+    import { get } from "svelte/store";
+	import TerminalContainer from "$lib/TerminalContainer.svelte";
+	import DemoButton from '$lib/DemoButton.svelte';
+
+    let message = "Copy address";
+    const faucetAddress ="https://sepoliafaucet.com/"
+
+    function copy() {
+        let address = get(ethereumAddress);
+        if (address == null) {
+            return
+        }
+        navigator.clipboard.writeText(address);
+        message = "Copied"
+	}
+
+    function goToFaucet() {
+        window.open(faucetAddress, "_blank");
+    }
+</script>
+
+<TerminalContainer>
+    {#if !$ethereumAddress == null}
+		Connect Metamask to continue
+	{:else}
+    <div>Ethereum Address: <span class="text-accent">{$ethereumAddress}</span></div>
+    <DemoButton on:click={copy}>
+        {message}
+    </DemoButton>
+    <DemoButton on:click={goToFaucet}>Open Sepolia faucet</DemoButton>
+    {/if}
+</TerminalContainer>
