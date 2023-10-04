5ab4aa0783f1daf1796ebc12ea88d3bf310de5dd
cor <cor@pruijs.dev>
2023-10-04 02:04:57 +0200

    feat(site): improved constants

--- site/src/lib/constants.ts
+++ site/src/lib/constants.ts
@@ -5,10 +5,10 @@ export const UNION_CHAIN_ID = 'union-testnet-3';
 export const MUNO_ERC20_ADDRESS = '0x1ea17f35801d9d7c160f66603ac4c5bb59fcec19';
 export const UCS01_EVM_ADDRESS = '0xd5DA8d1667227F0143Ded9d5f654e08CA5e3D3EB';
 export const UCS01_UNION_ADDRESS =
-	'union1uyc9cyetraney0rrufh0w5qrst5ugaqqahdy99mzn3cz05e5v6vqsgrsza';
+	'union1ya4lhhrxx00nskcqlt8k0sggyl7akxdlhkc95ydme75gnjr0hmrs6wkhm6';
 
 export const ERC20_CONTRACT_ABI = erc20abi.abi;
 export const IBC_CONTRACT_ABI = ibcAbi.abi;
-export const UCS01_UNION_SOURCE_CHANNEL = 'channel-15';
-export const UCS01_SEPOLIA_SOURCE_CHANNEL = 'channel-5';
+export const UCS01_UNION_SOURCE_CHANNEL = 'channel-16';
+export const UCS01_SEPOLIA_SOURCE_CHANNEL = 'channel-6';
 export const UCS01_SEPOLIA_PORT_ID = 'ucs01-relay-2';
