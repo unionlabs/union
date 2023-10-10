e02903d73fa5993d1651b4aeac1752fc6b6f3e28
aeryz <abdullaheryz@protonmail.com>
2023-10-10 12:10:53 +0300

    feat(zerg): transfer transaction works
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- zerg/src/main.rs
+++ zerg/src/main.rs
@@ -3,9 +3,10 @@ use std::fs::read_to_string;
 use clap::Parser;
 use cli::AppArgs;
 use serde::{Deserialize, Serialize};
+use ucs01_relay::msg::{ExecuteMsg, TransferMsg};
 use unionlabs::{
-    cosmwasm::wasm::msg_execute_contract::MsgExecuteContract, ibc::google::protobuf::any::Any,
-    IntoProto,
+    cosmos::base::coin::Coin, cosmwasm::wasm::msg_execute_contract::MsgExecuteContract,
+    ibc::google::protobuf::any::Any, IntoProto,
 };
 
 pub mod cli;
@@ -24,31 +25,27 @@ async fn do_main(args: AppArgs) {
 
     let union = chain_utils::union::Union::new(zerg_config.union).await;
 
-    let transfer_msg = TransferMsg {
+    let transfer_msg = ExecuteMsg::Transfer(TransferMsg {
         channel: zerg_config.channel,
-        receiver: "garbage".to_string(),
+        receiver: "0x1111111111111111111111111111111111111111".to_string(),
         // TODO: use uuid in memo
         memo: "garbage".to_string(),
-    };
-    let transfer_msg = format!("{{ {} }}", serde_json::to_string(&transfer_msg).unwrap());
+        timeout: None,
+    });
+    let transfer_msg = format!("{}", serde_json::to_string(&transfer_msg).unwrap());
 
     let msg = Any(MsgExecuteContract {
         sender: "union1jk9psyhvgkrt2cumz8eytll2244m2nnz4yt2g2".to_string(),
         contract: zerg_config.contract,
         msg: transfer_msg.as_bytes().to_vec(),
-        funds: vec![],
+        funds: vec![Coin {
+            denom: "stake".into(),
+            amount: "10000".into(),
+        }],
     })
     .into_proto();
 
     union.broadcast_tx_commit([msg]).await
-}
-
-/// Wrapper type for the JSON object used in constucting the message passed with `MsgExecuteContract`
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct TransferMsg {
-    channel: String,
-    receiver: String,
-    memo: String,
 }
 
 /// Event types tracked by Zerg when exporting to CSV
