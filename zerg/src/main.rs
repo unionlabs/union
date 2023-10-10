0a8a7908ac11633417135dcf9e991af2688a4680
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-10-09 21:47:24 -0500

    fix(zerg): use correct Any wrapper pattern

--- zerg/src/main.rs
+++ zerg/src/main.rs
@@ -1,6 +1,10 @@
 use std::fs::read_to_string;
 
-use unionlabs::{cosmwasm::wasm::MsgExecuteContract, ibc::google::protobuf::any::Any, IntoProto};
+use serde::{Deserialize, Serialize};
+use unionlabs::{
+    cosmwasm::wasm::msg_execute_contract::MsgExecuteContract, ibc::google::protobuf::any::Any,
+    IntoProto,
+};
 
 pub mod config;
 
@@ -17,24 +21,24 @@ async fn do_main() {
 
     let transfer_msg = TransferMsg {
         channel: zerg_config.channel,
-        receiver: &"garbage",
+        receiver: "garbage".to_string(),
         // TODO: use uuid in memo
-        memo: &"garbage",
+        memo: "garbage".to_string(),
     };
-    let transfer_msg = serde_json::to_string(&transfer_msg).unwrap();
-    let transfer_msg = format!("{ {} }", transfer_msg);
+    let transfer_msg = format!("{{ {} }}", serde_json::to_string(&transfer_msg).unwrap());
 
-    let msg = MsgExecuteContract {
-        sender: &"union1jk9psyhvgkrt2cumz8eytll2244m2nnz4yt2g2",
+    let msg = Any(MsgExecuteContract {
+        sender: "union1jk9psyhvgkrt2cumz8eytll2244m2nnz4yt2g2".to_string(),
         contract: zerg_config.contract,
         msg: transfer_msg.as_bytes().to_vec(),
         funds: vec![],
-    }
-    .into_proto_bytes();
+    })
+    .into_proto();
 
-    union.broadcast_tx_commit([Any(msg)])
+    union.broadcast_tx_commit([msg]).await
 }
 
+/// Wrapper type for the JSON object used in constucting the message passed with `MsgExecuteContract`
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct TransferMsg {
     channel: String,
@@ -42,6 +46,7 @@ struct TransferMsg {
     memo: String,
 }
 
+/// Event types tracked by Zerg when exporting to CSV
 enum EventType {
     /// Funds sent to Union
     SendToUnion,
