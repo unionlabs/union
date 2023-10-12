8395e102d43ebcf360d575ed5ac93007bc932280
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2023-10-12 14:17:11 +0200

    feat(cometbls-verifier): universal vk, verify v2, deprecate v1

--- light-clients/cometbls-light-client/src/zkp_verifier.rs
+++ light-clients/cometbls-light-client/src/zkp_verifier.rs
@@ -1,3 +1,4 @@
+#[deprecated(note = "the circuit has been generalized, use verify_zkp_v2() instead")]
 pub fn verify_zkp(
     trusted_validators_hash: &[u8],
     untrusted_validators_hash: &[u8],
@@ -6,6 +7,22 @@ pub fn verify_zkp(
 ) -> bool {
     cometbls_groth16_verifier::verify_zkp(
         cometbls_groth16_verifier::testnet_vk(),
+        trusted_validators_hash.into(),
+        untrusted_validators_hash.into(),
+        message,
+        zkp,
+    )
+    .unwrap();
+    true
+}
+
+pub fn verify_zkp_v2(
+    trusted_validators_hash: &[u8],
+    untrusted_validators_hash: &[u8],
+    message: &[u8],
+    zkp: &[u8],
+) -> bool {
+    cometbls_groth16_verifier::verify_zkp_v2(
         trusted_validators_hash.into(),
         untrusted_validators_hash.into(),
         message,
