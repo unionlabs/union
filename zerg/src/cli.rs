5262241b9b69143608c80f340e778b1218f47480
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-10-17 10:04:53 -0500

    feat(zerg): improved cli help

--- zerg/src/cli.rs
+++ zerg/src/cli.rs
@@ -22,12 +22,14 @@ pub struct AppArgs {
 #[derive(Debug, Subcommand, Clone)]
 pub enum Command {
     /// Gather analytics based off of a `process` output.
+    /// Each line of analytics includes: `<channel_id>,<mean_transfer_duration>,<median_transfer_duration>,<max_transfer_duration>,<min_transfer_duration>,<incomplete_transfers>`
     Analyze {
         input_file: String,
         #[arg(long, short = 'o', env, default_value = "zerg-analyze.csv")]
         output: String,
     },
     /// Process the output produced by `rush` or `observe` to get formatted details about whole transactions.
+    /// Each line of processed data includes: `<transaction_uuid>,<is_completed>,<arrivial_time?>,<duration?>`
     Process {
         input_file: String,
         #[arg(long, short = 'o', env, default_value = "zerg-process.csv")]
@@ -36,11 +38,13 @@ pub enum Command {
     /// Exports the config to stdout.
     PrintConfig,
     /// Conducts stress tests and benchmarks on the configured network.
+    /// Each line of observation includes: `<uuid>,<sender_address>,<timestamp>,<event_type>,<chain_id>`
     Rush {
         #[arg(long, short = 'o', env, default_value = "zerg-rush.csv")]
         output: String,
     },
     /// Observes and benchmarks the configured network.
+    /// Each line of observation includes: `<uuid>,<sender_address>,<timestamp>,<event_type>,<chain_id>`
     Observe {
         #[arg(long, short = 'o', env, default_value = "zerg-observe.csv")]
         output: String,
