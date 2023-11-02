6b93be2214791055635602835477712bec9d5d77
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2023-11-02 11:19:16 +0100

    chore: dead code clippy

--- light-clients/cometbls-light-client/src/zkp_verifier.rs
+++ light-clients/cometbls-light-client/src/zkp_verifier.rs
@@ -1,20 +1,3 @@
-#[deprecated(note = "the circuit has been generalized, use verify_zkp_v2() instead")]
-pub fn verify_zkp(
-    trusted_validators_hash: &[u8],
-    untrusted_validators_hash: &[u8],
-    message: &[u8],
-    zkp: &[u8],
-) -> bool {
-    cometbls_groth16_verifier::verify_zkp(
-        cometbls_groth16_verifier::testnet_vk(),
-        trusted_validators_hash.into(),
-        untrusted_validators_hash.into(),
-        message,
-        zkp,
-    )
-    .map_or(false, |_| true)
-}
-
 pub fn verify_zkp_v2(
     trusted_validators_hash: &[u8],
     untrusted_validators_hash: &[u8],
