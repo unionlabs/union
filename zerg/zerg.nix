9f028baf40e97aef01873b2b505d4a6b49ae962c
aeryz <abdullaheryz@protonmail.com>
2023-11-10 11:09:51 +0300

    chore(zerg): use `writeShellApplicationWithArgs`
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- zerg/zerg.nix
+++ zerg/zerg.nix
@@ -1,5 +1,5 @@
 { ... }: {
-  perSystem = { self', pkgs, system, config, crane, stdenv, ... }:
+  perSystem = { self', pkgs, system, config, crane, stdenv, writeShellApplicationWithArgs, ... }:
     let
       uniond = pkgs.lib.getExe self'.packages.uniond;
 
@@ -21,120 +21,78 @@
       packages = {
         zerg = zerg.packages.zerg;
 
-        generate-testnet-accounts = pkgs.writeShellApplication {
+        generate-testnet-accounts = writeShellApplicationWithArgs {
           name = "generate-testnet-accounts";
           runtimeInputs = [ ];
-          text = ''
-            usage() {
-              printf "\
-              Usage: generate-funded-testnet-accounts [OPTION]... \n\
-              \n\
-              Options: \n\
-                -n, --n-accounts Number of accounts to generate and fund \n\
-                -a, --amount     Amount of funds to send (Default: 1000) \n\
-                -d, --denom      Denom (Default: muno) \n\
-                -h, --home       Home directory of the chain \n\
-                --node           Rpc endpoint of the node (Default: \"http://localhost:26657\") \n\
-                -k, --key-name    Key to use when sending the funds \n\
-                -c, --chain-id   Chain id to use \n\
-                --no-fund        Don't fund the accounts \n\
-                --help           Print help \n\
-              "
-              exit "$1"
+          arguments = [
+            {
+              arg = "n_accounts";
+              help = "Number of accounts to generate and fund";
+              required = true;
             }
+            {
+              arg = "amount";
+              help = "Amount of funds to send";
+              default = "1000";
+            }
+            {
+              arg = "denom";
+              default = "muno";
+            }
+            {
+              arg = "key_name";
+              help = "Key to use when sending the funds";
+            }
+            {
+              arg = "no_fund";
+              help = "Don't fund the accounts";
+              type = "flag";
+            }
+            {
+              arg = "chain_id";
+            }
+            {
+              arg = "home";
+              help = "Chain home";
+            }
+            {
+              arg = "node";
+              help = "Union node's rpc endpoint";
+              default = "http://localhost:26657";
+            }
+          ];
+          text = ''
+            if [[ -z "$argc_key_name" ]] && [[ -z "$argc_no_fund" ]]; then
+              echo "--key-name must be specified since funds are gonna be sent to accounts."
+              echo "Use --no-fund to disable funding the accounts."
+              usage 1
+            fi
 
-            AMOUNT=1000
-            DENOM=muno
-            NUM_OF_ACCOUNTS=""
-            NO_FUND=""
-            HOME=""
-            NODE="http://localhost:26657"
-            KEY_NAME=""
-            CHAIN_ID=""
+            if [[ -z "$argc_chain_id" ]] && [[ -z "$argc_no_fund" ]]; then
+              echo "--chain-id must be specified since funds are gonna be sent to accounts."
+              usage 1
+            fi
 
-            while [[ $# -gt 0 ]]; do
-              case $1 in
-                -n|--n-accounts)
-                  NUM_OF_ACCOUNTS="$2"
-                  shift
-                  shift
-                  ;;
-                -a|--amount)
-                  AMOUNT="$2"
-                  shift
-                  shift
-                  ;;
-                -d|--denom)
-                  DENOM="$2"
-                  shift
-                  shift
-                  ;;
-                --no-fund)
-                  NO_FUND=1
-                  shift
-                  ;;
-                -h|--home)
-                  HOME="$2"
-                  shift
-                  shift
-                  ;;
-                -c|--chain-id)
-                  CHAIN_ID="$2"
-                  shift
-                  shift
-                  ;;
-                --node)
-                  NODE="$2"
-                  shift
-                  shift
-                  ;;
-                -k|--key-name)
-                  KEY_NAME="$2"
-                  shift
-                  shift
-                  ;;
-                --help)
-                  usage 0
-                  ;;
-                esac
-              done
+            if [[ -z "$argc_home" ]] && [[ -z "$argc_no_fund" ]]; then
+              echo "--home must be specified since funds are gonna be sent to accounts."
+              usage 1
+            fi
 
-              if [[ -z "$NUM_OF_ACCOUNTS" ]]; then
-                echo "--n-accounts must be specified."
-                usage 1
-              fi
+            accounts=""
+            for i in $(seq "$argc_n_accounts"); do
+              acc_name="zerg-gen-acc-$i"
+              addr=$(echo y | ${uniond} keys add "$acc_name" --keyring-backend test --home "$argc_home" --output json | jq .address -r)
+              accounts="$accounts $addr"
+              echo "---------------------"
+              echo "+ Generated $addr"
+              echo "+ Private key"
+              echo y | ${uniond} keys export "$acc_name" --unsafe --unarmored-hex --keyring-backend test --home "$argc_home"
+            done
 
-              if [[ -z "$KEY_NAME" ]] && [[ -z "$NO_FUND" ]]; then
-                echo "--key-name must be specified since funds are gonna be sent to accounts."
-                echo "Use --no-fund to disable funding the accounts."
-                usage 1
-              fi
-
-              if [[ -z "$CHAIN_ID" ]] && [[ -z "$NO_FUND" ]]; then
-                echo "--chain-id must be specified since funds are gonna be sent to accounts."
-                usage 1
-              fi
-
-              if [[ -z "$HOME" ]] && [[ -z "$NO_FUND" ]]; then
-                echo "--home must be specified since funds are gonna be sent to accounts."
-                usage 1
-              fi
-
-              ACCOUNTS=""
-              for i in $(seq "$NUM_OF_ACCOUNTS"); do
-                ACC_NAME="zerg-gen-acc-$i"
-                ADDR=$(echo y | ${uniond} keys add "$ACC_NAME" --keyring-backend test --home "$HOME" --output json | jq .address -r)
-                ACCOUNTS="$ACCOUNTS $ADDR"
-                echo "---------------------"
-                echo "+ Generated $ADDR"
-                echo "+ Private key"
-                echo y | ${uniond} keys export "$ACC_NAME" --unsafe --unarmored-hex --keyring-backend test --home "$HOME"
-              done
-
-              if [[ -z "$NO_FUND" ]]; then
-                echo ".. Sending $AMOUNT$DENOM to $ACCOUNTS by using key $KEY_NAME."
-                eval ${uniond} tx bank multi-send "$KEY_NAME" "$ACCOUNTS" "$AMOUNT$DENOM" --home "$HOME" --node "$NODE" --keyring-backend test -y --chain-id "$CHAIN_ID" --gas auto --gas-adjustment=1.3
-              fi
+            if [[ -z "$argc_no_fund" ]]; then
+              echo ".. Sending $argc_amount$argc_denom to $accounts by using key $argc_key_name."
+              eval ${uniond} tx bank multi-send "$argc_key_name" "$accounts" "$argc_amount$argc_denom" --home "$argc_home" --node "$argc_node" --keyring-backend test -y --chain-id "$argc_chain_id" --gas auto --gas-adjustment=1.3
+            fi
           '';
         };
       };
