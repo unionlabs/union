9cb7972a4f4b058b8266115407050f7b8e03035e
aeryz <abdullaheryz@protonmail.com>
2023-11-29 15:03:38 +0300

    chore(ics008): add proto `InstantiateMsg`
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- light-clients/ethereum-light-client/src/contract.rs
+++ light-clients/ethereum-light-client/src/contract.rs
@@ -1,37 +1,10 @@
-use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, QueryResponse, Response};
+use cosmwasm_std::{entry_point, Deps, DepsMut, Env, MessageInfo, QueryResponse, Response};
 use ics008_wasm_client::{
-    storage_utils::{save_client_state, save_consensus_state},
-    IbcClient, QueryMsg, SudoMsg,
-};
-use serde::{Deserialize, Serialize};
-use unionlabs::{
-    ibc::{
-        core::client::height::Height,
-        lightclients::{
-            ethereum::{client_state::ClientState, consensus_state::ConsensusState},
-            wasm::{
-                client_state::ClientState as WasmClientState,
-                consensus_state::ConsensusState as WasmConsensusState,
-            },
-        },
-    },
-    TryFromProto,
+    storage_utils::{save_proto_client_state, save_proto_consensus_state},
+    IbcClient, InstantiateMsg, QueryMsg, SudoMsg,
 };
 
 use crate::{client::EthereumLightClient, custom_query::CustomQuery, errors::Error};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct InputClientState {
-    pub data: Binary,
-    pub checksum: Binary,
-    pub latest_height: Height,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct InstantiateMsg {
-    client_state: InputClientState,
-    consensus_state: WasmConsensusState<Binary>,
-}
 
 #[entry_point]
 pub fn instantiate(
@@ -40,28 +13,18 @@ pub fn instantiate(
     _info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, Error> {
-    // TODO(aeryz): remove unwrap
-    let client_state = ClientState::try_from_proto_bytes(&msg.client_state.data).unwrap();
-    let consensus_state = ConsensusState::try_from_proto_bytes(&msg.consensus_state.data).unwrap();
-
-    save_consensus_state(
+    save_proto_consensus_state(
         deps.branch(),
-        WasmConsensusState::<ConsensusState> {
-            data: consensus_state,
-        },
-        &Height {
-            revision_number: 0,
-            revision_height: client_state.latest_slot,
-        },
+        msg.consensus_state,
+        &msg.client_state
+            .latest_height
+            .clone()
+            .ok_or(Error::DecodeFromProto {
+                reason: "`latest_height` is missing".to_string(),
+            })?
+            .into(),
     );
-    save_client_state(
-        deps,
-        WasmClientState::<ClientState> {
-            data: client_state,
-            checksum: msg.client_state.checksum.as_slice().try_into().unwrap(),
-            latest_height: msg.client_state.latest_height,
-        },
-    );
+    save_proto_client_state(deps, msg.client_state);
     Ok(Response::default())
 }
 
