30409d17369edba131a52387080e57c0226397a5
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-12-05 20:52:15 -0600

    feat(uniond): added v0.16.0 to upgrades e2e

--- uniond/app/upgrades/v0_16_0/upgrade.go
+++ uniond/app/upgrades/v0_16_0/upgrade.go
@@ -10,6 +10,21 @@ import (
 
 func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, keepers *upgrades.AppKeepers) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		consensusParams, err := keepers.ConsensusKeeper.Params(ctx, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		PubKeyTypes := [...]string{"bn254"}
+
+		consensusParams.Params.Block.MaxBytes = 22020096
+		consensusParams.Params.Block.MaxGas = -1
+		consensusParams.Params.Evidence.MaxAgeNumBlocks = 100000
+		consensusParams.Params.Evidence.MaxAgeDuration = 172800000000000
+		consensusParams.Params.Evidence.MaxBytes = 1048576
+		consensusParams.Params.Validator.PubKeyTypes = PubKeyTypes[:]
+		consensusParams.Params.Version.App = 0
+		consensusParams.Params.Abci.VoteExtensionsEnableHeight = 0 // TODO decide on when to enable vote extensions
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
