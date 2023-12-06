887de24ac1079cbf532192d3d224c0f8353f5ca6
aeryz <abdullaheryz@protonmail.com>
2023-12-06 16:36:12 +0300

    fix(uniond): add `PreBlocker` to app
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- uniond/app/app.go
+++ uniond/app/app.go
@@ -767,13 +767,16 @@ func NewUnionApp(
 	app.BasicModuleManager.RegisterLegacyAminoCodec(legacyAmino)
 	app.BasicModuleManager.RegisterInterfaces(interfaceRegistry)
 
+	// NOTE: upgrade module is required to be prioritized
+	app.ModuleManager.SetOrderPreBlockers(
+		upgradetypes.ModuleName,
+	)
+
 	// During begin block slashing happens after distr.BeginBlocker so that
 	// there is nothing left over in the validator fee pool, so as to keep the
 	// CanWithdrawInvariant invariant.
 	// NOTE: staking module is required if HistoricalEntries param > 0
 	app.ModuleManager.SetOrderBeginBlockers(
-		// upgrades should be run first
-		upgradetypes.ModuleName,
 		capabilitytypes.ModuleName,
 		minttypes.ModuleName,
 		distrtypes.ModuleName,
@@ -893,6 +896,7 @@ func NewUnionApp(
 
 	// initialize BaseApp
 	app.SetInitChainer(app.InitChainer)
+	app.SetPreBlocker(app.PreBlocker)
 	app.SetBeginBlocker(app.BeginBlocker)
 
 	anteHandler, err := NewAnteHandler(
@@ -959,6 +963,11 @@ func NewUnionApp(
 
 // Name returns the name of the App
 func (app *UnionApp) Name() string { return app.BaseApp.Name() }
+
+// PreBlocker application updates every pre block
+func (app *UnionApp) PreBlocker(ctx sdk.Context, _ *abci.RequestFinalizeBlock) (*sdk.ResponsePreBlock, error) {
+	return app.ModuleManager.PreBlock(ctx)
+}
 
 // BeginBlocker application updates every begin block
 func (app *UnionApp) BeginBlocker(ctx sdk.Context) (sdk.BeginBlock, error) {
