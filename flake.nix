5b28f930665ff283bf910cb802863dde33222bcf
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-12-13 08:30:44 -0600

    fix(unionvisor): added v0.16.0 input

--- flake.nix
+++ flake.nix
@@ -75,9 +75,12 @@
       url = "github:unionlabs/union/14007cbae6d464e70ca68220a36b30cb445f82fd";
       flake = false;
     };
-
     v0_15_0 = {
       url = "github:unionlabs/union/release-v0.15.0";
+      flake = false;
+    };
+    v0_16_0 = {
+      url = "github:unionlabs/union/release-v0.16.0";
       flake = false;
     };
   };
