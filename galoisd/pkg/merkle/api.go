c56e476f397400bab127757e61958dfb97642010
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-01-15 18:13:05 +0100

    feat(galois): merkle tests

--- galoisd/pkg/merkle/api.go
+++ galoisd/pkg/merkle/api.go
@@ -64,23 +64,3 @@ func (m *MerkleTreeAPI) RootHash(leafHashes []frontend.Variable, size frontend.V
 	}
 	return leafHashes[0]
 }
-
-// Compute merkle root in place at leafHashes[0]
-func (m *MerkleTreeAPI) RootHash2(leafHashes []frontend.Variable, size frontend.Variable) frontend.Variable {
-	maxLeaves := len(leafHashes)
-	for i := 0; i < int(math.Log2(float64(maxLeaves))); i++ {
-		r := size
-		w := 0
-		for j := 0; j < maxLeaves/int(math.Pow(2, float64(i))); j += 2 {
-			left := leafHashes[j]
-			right := leafHashes[j+1]
-			isOrphan := m.api.Or(m.api.IsZero(r), m.api.IsZero(m.api.Sub(r, 1)))
-			leafHashes[w] = m.api.Select(isOrphan, left, m.InnerHash(left, right))
-			size = m.api.Select(m.api.Or(isOrphan, m.api.IsZero(size)), size, m.api.Sub(size, 1))
-			r = m.api.Select(m.api.IsZero(r), r, m.api.Sub(r, 1))
-			r = m.api.Select(m.api.IsZero(r), r, m.api.Sub(r, 1))
-			w += 1
-		}
-	}
-	return leafHashes[0]
-}
---
+++ galoisd/pkg/merkle/api_test.go
@@ -0,0 +1,100 @@
+package merkle
+
+import (
+	"fmt"
+	"math/big"
+	"math/rand"
+	"testing"
+
+	"github.com/cometbft/cometbft/crypto/merkle"
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"github.com/consensys/gnark/backend"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/test"
+	"github.com/stretchr/testify/assert"
+)
+
+const MaxLeaves = 128
+
+type MerkleRoot struct {
+	Root       frontend.Variable
+	LeavesData [MaxLeaves]frontend.Variable
+	NbOfLeaves frontend.Variable
+}
+
+func (c *MerkleRoot) Define(api frontend.API) error {
+	merkle := NewMerkleTreeAPI(api)
+	leavesHash := make([]frontend.Variable, MaxLeaves)
+	for i := 0; i < MaxLeaves; i++ {
+		leavesHash[i] = merkle.LeafHash([]frontend.Variable{c.LeavesData[i]})
+	}
+	api.AssertIsEqual(c.Root, merkle.RootHash(leavesHash, c.NbOfLeaves))
+	return nil
+}
+
+func TestMerkleRoot(t *testing.T) {
+	t.Parallel()
+	for i := 1; i < MaxLeaves; i++ {
+		k := i
+		t.Run(fmt.Sprintf("%d", k), func(t *testing.T) {
+			t.Parallel()
+			leaves := [MaxLeaves][]byte{}
+			circuitLeaves := [MaxLeaves]frontend.Variable{}
+			for j := 0; j < k; j++ {
+				var leafValue fr.Element
+				_, err := leafValue.SetRandom()
+				assert.NoError(t, err)
+				leaves[j] = leafValue.Marshal()
+				circuitLeaves[j] = leafValue.Marshal()
+			}
+			for j := k; j < MaxLeaves; j++ {
+				circuitLeaves[j] = 0
+			}
+			test.NewAssert(t).CheckCircuit(
+				&MerkleRoot{},
+				test.WithValidAssignment(&MerkleRoot{
+					Root:       merkle.MimcHashFromByteSlices(leaves[:k]),
+					LeavesData: circuitLeaves,
+					NbOfLeaves: k,
+				}),
+				test.WithCurves(ecc.BN254),
+				test.NoFuzzing(),
+				test.WithBackends(backend.GROTH16),
+			)
+		})
+	}
+}
+
+func FuzzMerkleRoot(f *testing.F) {
+	f.Fuzz(func(t *testing.T, seed int64) {
+		t.Parallel()
+		rng := rand.New(rand.NewSource(seed))
+		k := max(1, rng.Intn(MaxLeaves))
+		leaves := [MaxLeaves][]byte{}
+		circuitLeaves := [MaxLeaves]frontend.Variable{}
+		buff := make([]byte, 32)
+		for j := 0; j < k; j++ {
+			_, err := rng.Read(buff)
+			assert.NoError(t, err)
+			var leafValue fr.Element
+			leafValue.SetBigInt(new(big.Int).SetBytes(buff))
+			leaves[j] = leafValue.Marshal()
+			circuitLeaves[j] = leafValue.Marshal()
+		}
+		for j := k; j < MaxLeaves; j++ {
+			circuitLeaves[j] = 0
+		}
+		test.NewAssert(t).CheckCircuit(
+			&MerkleRoot{},
+			test.WithValidAssignment(&MerkleRoot{
+				Root:       merkle.MimcHashFromByteSlices(leaves[:k]),
+				LeavesData: circuitLeaves,
+				NbOfLeaves: k,
+			}),
+			test.WithCurves(ecc.BN254),
+			test.NoFuzzing(),
+			test.WithBackends(backend.GROTH16),
+		)
+	})
+}
