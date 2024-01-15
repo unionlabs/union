4d8fe31446be2fa73e7c5f681a0676adc73744c6
cor <cor@pruijs.dev>
2024-01-15 18:59:11 +0100

    feat(site): add graph component

---
+++ site/src/components/graph.svelte
@@ -0,0 +1,27 @@
+<div class="flex-1 flex items-center justify-center">
+    <div class="flex-1 ">
+    <div class="flex gap-4 flex-1">
+        <div>
+            <div class="bg-white h-10 w-10 mb-4"></div>
+            <div class="bg-white h-10 w-10"></div>
+        </div>
+        <div>
+            <div class="bg-white h-10 w-10 mb-4"></div>
+            <div class="bg-white h-10 w-10"></div>
+        </div>
+        <div>
+            <div class="bg-white h-10 w-10 mb-4"></div>
+            <div class="bg-white h-10 w-10"></div>
+        </div>
+        <div>
+            <div class="bg-white h-10 w-10 mb-4"></div>
+            <div class="bg-white h-10 w-10"></div>
+        </div>
+    </div>
+    <div class="text-4xl mt-4">
+        <div>750x faster.</div>
+        <div>350x cheaper.</div>
+        <div>ZK. Decentralized.</div>
+    </div>
+    </div>
+</div>
