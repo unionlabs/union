1ae3a626d44f19cceafc5dd6db4fe240b1be56a5
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2024-01-16 15:54:42 -0600

    docs: convert to svelte comps

--- site/src/components/chain-versions.svelte
+++ site/src/components/chain-versions.svelte
@@ -1,13 +1,23 @@
+<script lang="ts">
 import versions from "../../../versions.json"
+
+export let chainId = 'union-testnet-4'
+export let genesis = false
 
 function isKeyof<T extends object>(obj: T, possibleKey: keyof any): possibleKey is keyof T {
   return possibleKey in obj;
 }
 
-export default function Version(chainId: string, genesis: boolean) {
+function Version(chainId: string, genesis: boolean) {
   if (isKeyof(versions, chainId)) {
     let version = genesis ? 0 : -1
     return versions[chainId].at(version)
   }
   throw new Error('Unknown chainId: ' + chainId)
 }
+
+$: _genesis = typeof genesis === 'string' ? JSON.parse(genesis) : genesis
+
+</script>
+
+{Version(chainId, _genesis)}
---
+++ site/src/components/genesis.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import genesis from '../../../networks/genesis/union-testnet-5/genesis.json'
+
+/**
+ * do some logic 
+ */
+</script>
+
+<button>
+  <a href={"data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(genesis,undefined,2))} download="genesis.json">
+    Download Genesis
+  </a>
+</button>
+
