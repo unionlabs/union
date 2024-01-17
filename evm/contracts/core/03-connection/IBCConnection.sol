001f9dfb15964ab234c6a1f185a713f01da9a6ea
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-01-17 18:09:02 +0100

    feat(evm): verify counterparty version in openTry

--- evm/contracts/core/03-connection/IBCConnection.sol
+++ evm/contracts/core/03-connection/IBCConnection.sol
@@ -61,6 +61,10 @@ contract IBCConnection is IBCStore, IIBCConnectionHandshake {
             msg_.counterpartyVersions.length > 0,
             "connectionOpenTry: counterpartyVersions length must be greater than 0"
         );
+        require(
+            isSupportedVersion(msg_.counterpartyVersions[0]),
+            "connectionOpenTry: the counterparty selected version is not supported by versions selected on INIT"
+        );
 
         string memory connectionId = generateConnectionIdentifier();
         IbcCoreConnectionV1ConnectionEnd.Data storage connection = connections[
@@ -131,17 +135,14 @@ contract IBCConnection is IBCStore, IIBCConnectionHandshake {
         IbcCoreConnectionV1ConnectionEnd.Data storage connection = connections[
             msg_.connectionId
         ];
-        if (
-            connection.state != IbcCoreConnectionV1GlobalEnums.State.STATE_INIT
-        ) {
-            revert("connectionOpenAck: connection state is not INIT");
-        }
-        if (!isSupportedVersion(msg_.version)) {
-            revert(
-                "connectionOpenAck: the counterparty selected version is not supported by versions selected on INIT"
-            );
-        }
-
+        require(
+            connection.state == IbcCoreConnectionV1GlobalEnums.State.STATE_INIT,
+            "connectionOpenAck: connection state is not INIT"
+        );
+        require(
+            isSupportedVersion(msg_.version),
+            "connectionOpenAck: the counterparty selected version is not supported by versions selected on INIT"
+        );
         require(
             validateSelfClient(msg_.clientStateBytes),
             "connectionOpenAck: failed to validate self client state"
