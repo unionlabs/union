894862b7713cb381ce7f0fe67980568cae558199
omar <omaraziz.dev@proton.me>
2024-01-18 01:30:06 -0800

    chore: add comment to code component

--- site/src/components/code.svelte
+++ site/src/components/code.svelte
@@ -2,7 +2,14 @@
   import { onMount } from 'svelte'
   import { highlightCode } from '#/lib/highlight-code.ts'
 
-  export let code: string
+  /**
+   * This code block component is used to display code snippets outside of md/mdx files.
+   * The snippet has to have a language tag, e.g. ```[language] [code] ```.
+   * If you're doing a multi-line string with backticks, you have to escape the backticks with a backslash.
+   * @example: https://stackblitz.com/edit/github-j3wpz9?file=src%2Fpages%2Fsnippet.astro
+   */
+  
+   export let code: string
   let highlightedCode = ''
 
   onMount(async () => {
