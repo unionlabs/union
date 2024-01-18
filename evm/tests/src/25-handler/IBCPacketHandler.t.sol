1d19d9e0d61d0aa76598fe4e9f8d0b6dc11c9142
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-01-18 15:57:17 +0100

    feat(evm): setUp instead of constructor

--- evm/tests/src/25-handler/IBCPacketHandler.t.sol
+++ evm/tests/src/25-handler/IBCPacketHandler.t.sol
@@ -20,11 +20,7 @@ contract IBCPacketHandlerTest is TestPlus {
     MockApp app;
     string constant CLIENT_TYPE = "mock";
 
-    uint64 proofHeight = 1;
     string clientId;
-    string connId;
-    string portId;
-    string channelId;
 
     event SendPacket(
         uint64 sequence,
@@ -35,76 +31,50 @@ contract IBCPacketHandlerTest is TestPlus {
         bytes data
     );
 
-    constructor() {
+    function setUp() public {
         handler = new IBCHandler_Testable();
         client = new MockClient(address(handler));
         app = new MockApp();
         handler.registerClient(CLIENT_TYPE, client);
-        setupConnection();
-        setupChannel();
     }
 
-    function test_sendPacket() public {
-        ClientHeight.Data memory timeoutHeight = ClientHeight.Data({
-            revision_number: 0,
-            revision_height: type(uint64).max
-        });
-        uint64 timeoutTimestamp = type(uint64).max;
-
-        vm.prank(address(app));
-        vm.expectEmit(false, false, false, false);
-        emit SendPacket(0, "", "", timeoutHeight, 0, hex"");
-        handler.sendPacket(
-            portId,
-            channelId,
-            timeoutHeight,
-            timeoutTimestamp,
-            hex"12345678"
-        );
-    }
-
-    function test_recvPacket() public {
-        // TODO: read the TODO notes on IBCPacket.t.sol
-    }
-
-    /// sets up an IBC Connection from the perspective of chain A
-    function setupConnection() internal {
-        // 1. createClient
+    function createClient(uint64 proofHeight) internal returns (string memory) {
         IBCMsgs.MsgCreateClient memory m = MsgMocks.createClient(
             CLIENT_TYPE,
             proofHeight
         );
-        clientId = handler.createClient(m);
+        return handler.createClient(m);
+    }
 
-        // 2. ConnOpenInit
+    function setupConnection(
+        uint64 proofHeight
+    ) internal returns (string memory) {
         IBCMsgs.MsgConnectionOpenInit memory msg_init = MsgMocks
             .connectionOpenInit(clientId);
-        connId = handler.connectionOpenInit(msg_init);
-
-        // 3. ConnOpenAck
+        string memory connId = handler.connectionOpenInit(msg_init);
         IBCMsgs.MsgConnectionOpenAck memory msg_ack = MsgMocks
             .connectionOpenAck(clientId, connId, proofHeight);
         handler.connectionOpenAck(msg_ack);
+        return connId;
     }
 
-    /// sets up an IBC Connection from the perspective of chain B
-    function setupChannel() internal {
-        // 1. bindPort
+    function setupChannel(
+        uint64 proofHeight,
+        string memory portId,
+        string memory connId
+    ) internal returns (string memory) {
         handler.bindPort(portId, address(app));
-
-        // 2. channelOpenInit
         IBCMsgs.MsgChannelOpenInit memory msg_init = MsgMocks.channelOpenInit(
             connId,
             portId
         );
-        channelId = handler.channelOpenInit(msg_init);
-
-        // 3. channelOpenAck
+        string memory channelId = handler.channelOpenInit(msg_init);
         IBCMsgs.MsgChannelOpenAck memory msg_ack = MsgMocks.channelOpenAck(
             portId,
             channelId,
             proofHeight
         );
         handler.channelOpenAck(msg_ack);
+        return channelId;
     }
 }
