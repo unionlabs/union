d9db457722eec389d488ecc2fa65203bc739a6ca
omar <omaraziz.dev@proton.me>
2024-01-20 09:33:34 -0800

    feat: performance graph starts correctly

--- site/src/components/apple-graph-container.svelte
+++ site/src/components/apple-graph-container.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
-  import { afterUpdate, onMount, tick } from 'svelte'
   import AppleGraph from '#/components/apple-graph.svelte'
 
   type BarProps = {
-  	primary: boolean,
+  	primary?: boolean,
   	width: number,
   	label: string,
   }
 
   export let bars: BarProps[];
-
-  let div: HTMLElement;
-
-  onMount(() => {})
 </script>
 
-<div class="grid grid-5 gap-4 w-full min-w-full" bind:this={div}>
-	<!-- hack???? first one doesnt lmfao -->
-	<AppleGraph width={70} label="" element={div}/>
+<div class="grid grid-5 gap-4 w-full min-w-full">
+	<!-- hack???? first one doesn't lmfao -->
+	<AppleGraph width={70} label="" />
 
 	{#each bars as { primary, width, label }}
-		<AppleGraph {primary} {width} {label}  element={div}/>
+		<AppleGraph {primary} {width} {label} />
 	{/each}
 </div>
 
--- site/src/components/apple-graph.svelte
+++ site/src/components/apple-graph.svelte
@@ -1,40 +1,45 @@
 <script lang="ts">
   import { onMount } from 'svelte'
 
-  // The HTML Element to observe.
-  export let element: HTMLElement;
-
   // The percentage width of the bar. number from 0-100.
-  export let width: number;
-  export let primary = false;
-  export let label: string;
+  export let width: number
+  export let primary = false
+  export let label: string
 
-  let observer: null | IntersectionObserver = null;
+  let observer: null | IntersectionObserver = null
 
-  let isVisible = false;
+  let isVisible = false
 
   onMount(() => {
-    console.log(element)
-    observer = new IntersectionObserver(
-      (entries) => {
-        isVisible = isVisible || entries[0].isIntersecting
-      },
-    );
+    // The HTML Element to observe.
+    const element = document.querySelector('div[data-graph="apple"]') as HTMLElement
+    observer = new IntersectionObserver(entries => {
+      isVisible = isVisible || entries[0].isIntersecting
+    })
 
-    observer.observe(element);
+    observer.observe(element)
 
     return () => {
       if (observer) {
-        observer.disconnect();
-        observer = null;
+        observer.disconnect()
+        observer = null
       }
-    };
-  });
+    }
+  })
 </script>
 
-<div class="bg-transparent flex meter h-2 w-full">
-  <span class="flex" style={`width:${width}%;`}>
-    <span class={`rounded-full flex ${primary ? "bg-white" : "bg-gray-500"}` + (isVisible ? " progress" : "")}>
+<div
+  data-graph="apple"
+  class="bg-transparent flex meter h-2 w-full"
+>
+  <span
+    class="flex"
+    style={`width:${width}%;`}
+  >
+    <span
+      class={`rounded-full flex ${primary ? 'bg-white' : 'bg-gray-500'}` +
+        (isVisible ? ' progress' : '')}
+    >
     </span>
   </span>
 </div>
@@ -43,18 +48,18 @@
 </span>
 
 <style>
-.progress {
-  animation: progressBar 1.5s ease-in-out;
-  animation-fill-mode: both;
-}
-
-@keyframes progressBar {
-  0% {
-    width: 0;
+  .progress {
+    animation: progressBar 1.5s ease-in-out;
+    animation-fill-mode: both;
   }
 
-  100% {
-    width: 100%;
+  @keyframes progressBar {
+    0% {
+      width: 0;
+    }
+
+    100% {
+      width: 100%;
+    }
   }
-}
 </style>
