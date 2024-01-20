894b27b7c56c741c98baa14c9b41392401b6ce05
cor <cor@pruijs.dev>
2024-01-20 06:48:37 +0100

    fix(site): improve blog styling

--- site/src/layouts/layout.svelte
+++ site/src/layouts/layout.svelte
@@ -16,14 +16,14 @@
     <title>Union</title>
   </head>
   <body
-    class='font-sans flex-1 shadow-effect-bg pb-12 bg-black text-white not-content'
+    class='font-sans flex flex-col items-stretch flex-1 shadow-effect-bg pb-12 bg-black text-white not-content'
   >
     <Header/>
 
     <slot />
 
     <footer
-      class='bg-white text-black flex items-center p-4 gap-4 justify-center text-lg font-normal bottom-0 mx-auto w-full'
+      class='flex items-center p-4 gap-4 justify-center text-lg font-normal bottom-0 mx-auto w-full'
     >
       <p>&copy; Union.fi Labs, Inc.</p>
     </footer>
