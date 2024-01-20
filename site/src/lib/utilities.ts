6b5bd374e45b8e84542b44569ce11f3d1f7993f2
omar <omaraziz.dev@proton.me>
2024-01-20 13:26:48 -0800

    feat: performance graph karel feedback

--- site/src/lib/utilities.ts
+++ site/src/lib/utilities.ts
@@ -3,3 +3,6 @@ export function sleep(ms: number): Promise<void> {
 }
 
 export const arraySizeN = (n: number) => Array.from(Array(n).keys());
+
+export const roundNumber = (_number: number, decimalPlaces: number) =>
+  Math.round(_number * 10 ** decimalPlaces) / 10 ** decimalPlaces;
