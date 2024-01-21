6c869553c8a826a0d9965c480187fc3e29d65b75
omar <omaraziz.dev@proton.me>
2024-01-21 06:39:41 -0800

    fix: revert global view transitions

--- site/astro.config.ts
+++ site/astro.config.ts
@@ -40,9 +40,6 @@ export default defineConfig({
         useStarlightDarkModeSwitch: true,
         useStarlightUiThemeColors: true,
       },
-      components: {
-        Head: "./src/components/Head.astro",
-      },
       head: [
         {
           tag: "meta",
