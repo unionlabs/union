2dcfc7fb5a77a0a2f972f017c1982fc3d302f0e6
omar <omaraziz.dev@proton.me>
2024-01-22 08:08:47 -0800

    chore: improving header layout in landing

--- site/src/components/Header.svelte
+++ site/src/components/Header.svelte
@@ -1,29 +1,29 @@
-<header class='flex justify-around p-4'>
-    <div class='max-w-7xl flex flex-1 items-center p-4 gap-4'>
+<header class='flex justify-around py-3 px-2 sm:px-4'>
+    <div class='max-w-7xl flex flex-1 items-center p-3 sm:p-4 gap-4'>
       <a
         class='flex items-center gap-3 decoration-transparent visited:text-white'
         href='/'
       >
         <img
-          class='h-[40px] md:h-[48px]'
+          class='h-[40px] md:h-[48px] mb-1'
           src='/logo.svg'
           alt='Union Logo'
         />
-        <p class='min-[420px]:block font-semibold text-3xl sm:text-5xl py-0 my-0'>Union</p>
+        <p class='min-[420px]:block font-semibold text-2xl sm:text-5xl py-0 my-0 mb-0.5'>Union</p>
       </a>
       <nav class='flex flex-1 justify-end'>
         <ul class='flex gap-4 !text-xl! !md:text-2xl font-normal list-none align-middle my-auto'>
           <li>
-            <a href='/blog' class="text-xl !md:text-2xl">Build</a>
+            <a href='/blog' class="text-md md:text-2xl">Build</a>
           </li>
           <li>
-            <a href='/docs' class="text-xl !md:text-2xl">Discover</a>
+            <a href='/docs' class="text-md md:text-2xl">Discover</a>
           </li>
           <li>
-            <a href='/docs' class="text-xl !md:text-2xl">Research</a>
+            <a href='/docs' class="text-md md:text-2xl">Research</a>
           </li>
           <li>
-            <a href='/docs' class="text-xl !md:text-2xl">Contribute</a>
+            <a href='/docs' class="text-md md:text-2xl">Contribute</a>
           </li>
           <!-- <li class='my-auto mt-1'>
             <a
