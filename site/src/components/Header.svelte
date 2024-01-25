c2bf0fdf96383c213638a7f49314d53318a3275b
omar <omaraziz.dev@proton.me>
2024-01-24 19:26:28 -0800

    header items

--- site/src/components/Header.svelte
+++ site/src/components/Header.svelte
@@ -1,49 +1,69 @@
-<header class='flex justify-around py-3 px-2 sm:px-4'>
-    <div class='max-w-7xl flex flex-1 items-center p-3 sm:p-4 gap-4'>
-      <a
-        class='flex items-center gap-3 decoration-transparent visited:text-white'
-        href='/'
+<script lang="ts">
+  import xIcon from '#/assets/images/x.svg'
+  import discordIcon from '#/assets/images/discord.svg'
+</script>
+
+<header class="flex justify-around pt-3 px-2 sm:px-4">
+  <div class="max-w-7xl flex flex-1 items-center p-3 sm:p-4 gap-4">
+    <a
+      class="flex items-center gap-3 decoration-transparent visited:text-white"
+      href="/"
+    >
+      <img
+        class="h-[48px] mb-1"
+        src="/logo.svg"
+        alt="Union Logo"
+      />
+      <p
+        class="min-[420px]:block font-semibold text-4xl sm:text-5xl py-0 my-0 mb-0.5 sm:block hidden"
       >
-        <img
-          class='h-[40px] md:h-[48px] mb-1'
-          src='/logo.svg'
-          alt='Union Logo'
-        />
-        <p class='min-[420px]:block font-semibold text-2xl sm:text-5xl py-0 my-0 mb-0.5'>Union</p>
-      </a>
-      <nav class='flex flex-1 justify-end'>
-        <ul class='flex gap-4 !text-xl! !md:text-2xl font-normal list-none align-middle my-auto'>
-          <li>
-            <a href='/blog' class="text-md md:text-2xl">Blog</a>
-          </li>
-          <li>
-            <a href='/docs' class="text-md md:text-2xl">Docs</a>
-          </li>
-          <!--
-          <li>
-            <a href='/docs' class="text-md md:text-2xl">Discover</a>
-          </li>
-          <li>
-            <a href='/docs' class="text-md md:text-2xl">Research</a>
-          </li>
-          <li>
-            <a href='/docs' class="text-md md:text-2xl">Contribute</a>
-          </li>
-          <!-- <li class='my-auto mt-1'>
-            <a
-              href='https://x.com/union_build'
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              <span class='sr-only'>X</span>
-              <img
-                src={XLogo.src}
-                alt='x.com logo'
-                class='w-6 h-6'
-              />
-            </a>
-          </li> -->
-        </ul>
-      </nav>
-    </div>
-  </header>
+        Union
+      </p>
+    </a>
+    <nav class="flex flex-1 justify-end">
+      <ul class="flex gap-4 !text-xl! !md:text-2xl font-normal list-none align-middle my-auto">
+        <li>
+          <a
+            href="/blog"
+            class="text-2xl"
+            >Blog
+          </a>
+        </li>
+        <li>
+          <a
+            href="/docs"
+            class="text-2xl"
+            >Docs
+          </a>
+        </li>
+
+        <li class="my-auto">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://x.com/union_build"
+          >
+            <img
+              alt="x.com"
+              src={xIcon.src}
+              class="w-6 h-6"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://discord.union.build"
+          >
+            <img
+              alt="discord"
+              src={discordIcon.src}
+              class="w-8 h-8 my-auto"
+            />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
