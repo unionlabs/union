2acdcabfe1f71e8dab47fda0d1dbfdcaf5810de5
omar <omaraziz.dev@proton.me>
2024-01-25 07:23:19 -0800

    feat: embedded code snippet

--- site/src/lib/utilities.ts
+++ site/src/lib/utilities.ts
@@ -1,3 +1,9 @@
+import url from "node:url";
+import path from "node:path";
+import fs from "node:fs/promises";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -9,3 +15,9 @@ export const roundNumber = (_number: number, decimalPlaces: number) =>
 
 export const generateRandomNumber = (min: number, max: number) =>
   Math.random() * (max - min) + min;
+
+export async function getFileContent({ filepath }: { filepath: string }) {
+  const file = path.resolve(__dirname, filepath);
+  if (!fs.stat(file)) throw new Error(`File not found: ${file}`);
+  return await fs.readFile(file, { encoding: "utf8" });
+}
