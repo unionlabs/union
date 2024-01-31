23fcc51df1022974c138d63d73c799d36af1c654
omar <omaraziz.dev@proton.me>
2024-01-30 21:37:29 -0800

    fix: add label to input

--- site/src/components/LogoVariable.svelte
+++ site/src/components/LogoVariable.svelte
@@ -12,7 +12,8 @@
 		<path transform={`translate(20, ${-cubeDelta})`} d="M50 30C50 18.9543 41.0457 10 30 10C18.9543 10 10 18.9543 10 30"  stroke="white" stroke-width={cubeWidth}/>
 		<path transform={`translate(-20, ${cubeDelta})`} d="M30 50C30 61.0457 38.9543 70 50 70C61.0457 70 70 61.0457 70 50" stroke="white" stroke-width={cubeWidth}/>
 	</svg>
-	<input class="w-full max-w-80" type=range min=0 max=20 step=0.01 bind:value={cubeWidth} />
+	<label class="sr-only" for="range-slider">Thickness range:</label>
+	<input id="range-slider" class="w-full max-w-80" type=range min=0 max=20 step=0.01 bind:value={cubeWidth} />
 </div>
 
 <style>
