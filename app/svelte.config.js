9b9893614fb62d0ee7923160a31bc4454b73bb9a
omar <omaraziz.dev@proton.me>
2024-01-31 20:53:36 -0800

    feat: begin app development

---
+++ app/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    alias: {
+      '#/*': './src/*'
+    },
+    adapter: adapter({
+      strict: true
+    })
+  }
+}
---
+++ app/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default <Config>{
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
