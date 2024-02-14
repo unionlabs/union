7b061244b7f4577849411fe47f959b79ec351ca8
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-02-14 18:37:38 +0100

    fix(devnet): timescale postgres

--- networks/services/postgres.nix
+++ networks/services/postgres.nix
@@ -1,11 +1,11 @@
 { lib, pkgs, ... }:
 let
   postgres = pkgs.dockerTools.pullImage {
-    imageName = "postgres";
-    imageDigest = "sha256:a5e89e5f2679863bedef929c4a7ec5d1a2cb3c045f13b47680d86f8701144ed7";
-    sha256 = "0703g8p91bi3ybw35zj51zh2105r6wjjikyvliq9phwxsqrcfrxz";
-    finalImageName = "postgres";
-    finalImageTag = "latest";
+    imageName = "timescale/timescaledb";
+    imageDigest = "sha256:eb8a3142384e8fd93ebd311783b297a04398ca61902b41233912a1a115279b69";
+    sha256 = "sha256-zJ6HTYhxO7h+brEQOoJgDbHp74JfFe0Jcsfnz8MCFHM=";
+    finalImageName = "timescaledb";
+    finalImageTag = "2.14.1-pg16";
   };
 in
 {
@@ -16,6 +16,7 @@ in
     ports = [
       "5432:5432"
     ];
+    command = "postgres -c shared_buffers=1024MB -c effective_cache_size=2048MB";
     environment = {
       POSTGRES_PASSWORD = "postgrespassword";
       POSTGRES_DB = "default";
