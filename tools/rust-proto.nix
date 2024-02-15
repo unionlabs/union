c15a4c826eca2794f37dd04f0ebe5cbd2570a911
Jurriaan Pruijs <jurriaan@pruijs.nl>
2024-02-15 08:08:33 +0100

    chore(test): verify build runs without permission issue - debug CI

--- tools/rust-proto.nix
+++ tools/rust-proto.nix
@@ -372,6 +372,7 @@
           name = "rust-proto-is-committed";
           description = "check that rust protos in git repo are the same as those that are generated in rust-proto derivation";
           src = ../.;
+          buildInputs = [ pkgs.git self'.packages.rust-proto ];
           doCheck = true;
           checkPhase = ''
             echo ${self'.packages.rust-proto}
