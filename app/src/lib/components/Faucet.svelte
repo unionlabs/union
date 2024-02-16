8bbb7f95d7cb64d1a4fb3d02ca0c261b04b3e482
omar <omaraziz.dev@proton.me>
2024-02-16 07:13:13 -0800

    feat: primitive faucet button

---
+++ app/src/lib/components/Faucet.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { getUnoFromFaucet } from '$/lib/faucet.ts'
+  import { unionAddress } from '$/lib/wallet/config'
+</script>
+
+<div>
+  <button on:click={() => getUnoFromFaucet({ address: $unionAddress })}>
+    get UNO from faucet
+  </button>
+</div>
