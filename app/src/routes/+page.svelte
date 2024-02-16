bcb0d1da2616bed6ea9b7fc4afd2191746f16920
omar <omaraziz.dev@proton.me>
2024-02-16 07:13:13 -0800

    feat: primitive faucet button

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-  import ConnectButton from '$lib/components/Connect.svelte'
+  import Faucet from '$/lib/components/Faucet.svelte'
+  import Connect from '$lib/components/Connect.svelte'
 </script>
 
-<main class="mx-auto mt-12 flex min-h-full min-w-full items-center justify-center">
-  <ConnectButton />
+<main class="mx-auto mt-12 flex min-h-full min-w-full items-center justify-center flex-col space-y-6">
+  <Connect />
+  <Faucet />
 </main>
