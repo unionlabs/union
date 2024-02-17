effd6393373eb2980a45c89117e9cd47dcaa831b
Jurriaan Pruijs <jurriaan@pruijs.nl>
2024-02-17 22:45:11 +0100

    chore: improve canonical types - fraction denominator

--- lib/unionlabs/src/ibc/lightclients/tendermint/fraction.rs
+++ lib/unionlabs/src/ibc/lightclients/tendermint/fraction.rs
@@ -1,3 +1,4 @@
+use std::num::NonZeroU64;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -5,14 +6,14 @@ use serde::{Deserialize, Serialize};
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 pub struct Fraction {
     pub numerator: u64,
-    pub denominator: u64,
+    pub denominator: NonZeroU64,
 }
 
 impl From<Fraction> for protos::ibc::lightclients::tendermint::v1::Fraction {
     fn from(value: Fraction) -> Self {
         Self {
             numerator: value.numerator,
-            denominator: value.denominator,
+            denominator: value.denominator.get(),
         }
     }
 }
@@ -21,7 +22,7 @@ impl From<protos::ibc::lightclients::tendermint::v1::Fraction> for Fraction {
     fn from(value: protos::ibc::lightclients::tendermint::v1::Fraction) -> Self {
         Self {
             numerator: value.numerator,
-            denominator: value.denominator,
+            denominator: NonZeroU64::new(value.denominator).expect("non-zero denominator"),
         }
     }
 }
@@ -32,7 +33,7 @@ impl From<Fraction> for contracts::glue::IbcLightclientsTendermintV1FractionData
     fn from(value: Fraction) -> Self {
         Self {
             numerator: value.numerator,
-            denominator: value.denominator,
+            denominator: value.denominator.get(),
         }
     }
 }
@@ -42,7 +43,7 @@ impl From<contracts::glue::IbcLightclientsTendermintV1FractionData> for Fraction
     fn from(value: contracts::glue::IbcLightclientsTendermintV1FractionData) -> Self {
         Self {
             numerator: value.numerator,
-            denominator: value.denominator,
+            denominator: NonZeroU64::new(value.denominator).expect("non-zero denominator"),
         }
     }
 }
