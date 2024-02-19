5324bf591e81be01a0974bd9535b27c0e5d97186
omar <omaraziz.dev@proton.me>
2024-02-19 15:16:29 -0800

    fix: add approval

--- app/src/lib/union-actions.ts
+++ app/src/lib/union-actions.ts
@@ -1,16 +1,21 @@
 import { usc01relayAbi } from '$/lib/abi'
 import { fromBech32 } from '@cosmjs/encoding'
-import { writable, type Writable, get } from 'svelte/store'
-import { CHAIN, CONTRACT, UNO, URLS } from '$/lib/constants.ts'
-import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
-import { config, unionAddress } from '$/lib/wallet/config'
-import { GasPrice } from '@cosmjs/stargate'
-
-import { Tendermint37Client } from '@cosmjs/tendermint-rpc'
-import { readContract, simulateContract, writeContract } from '@wagmi/core'
-import { type Address, type Hash, bytesToHex, erc20Abi, getAddress } from 'viem'
 import { snapAddress } from '$/lib/snap'
-import { CosmjsOfflineSigner } from '@leapwallet/cosmos-snap-provider'
+import { CONTRACT, UNO, URLS } from '$/lib/constants.ts'
+import { writable, type Writable, get } from 'svelte/store'
+import { config, unionAddress, wallet } from '$/lib/wallet/config'
+import { readContract, simulateContract, writeContract } from '@wagmi/core'
+import {
+  type Address,
+  type Hash,
+  bytesToHex,
+  erc20Abi,
+  getAddress,
+  BaseError,
+  ContractFunctionRevertedError,
+  type Account
+} from 'viem'
+import toast from 'svelte-french-toast'
 
 export const erc20balanceStore: Writable<bigint | null> = writable(null)
 export async function getUnoERC20Balance(address: Address) {
@@ -25,24 +30,22 @@ export async function getUnoERC20Balance(address: Address) {
   })
 }
 
-export const cosmosOfflineSigner = writable<CosmjsOfflineSigner | null>(null)
-export async function initiateCosmosOfflineSigner() {
-  const offlineSigner = new CosmjsOfflineSigner(CHAIN.UNION.ID)
-  cosmosOfflineSigner.set(offlineSigner)
-}
+export async function approveUnoERC20Spending({ amount }: { amount: bigint }) {
+  const ethereumAddress = get(wallet).address
+  if (!ethereumAddress) return
+  const denomAddress = await getDenomAddress()
+  if (BigInt(denomAddress) === 0n) return
 
-export const cosmWasmClient = writable<SigningCosmWasmClient | null>(null)
-export async function initCosmWasmClient() {
-  const tendermintClient = await Tendermint37Client.connect(URLS.UNION.RPC)
+  const contractParamteres = {
+    abi: erc20Abi,
+    functionName: 'approve',
+    address: denomAddress,
+    args: [getAddress(CONTRACT.SEPOLIA.ADDRESS), 69420n],
+    account: get(wallet) as unknown as Account
+  } as const
 
-  const offlineSigner = get(cosmosOfflineSigner)
-  if (!offlineSigner) throw new Error('cosmos offline signer not initiated')
-  const cosmwasmClient = await SigningCosmWasmClient.createWithSigner(
-    tendermintClient,
-    offlineSigner,
-    { gasPrice: GasPrice.fromString(`0.001${UNO.NATIVE_DENOM}`) }
-  )
-  cosmWasmClient.set(cosmwasmClient)
+  const { request } = await simulateContract(config, contractParamteres)
+  return writeContract(config, request)
 }
 
 export const sepoliaTransactions = writable<Array<string>>([])
@@ -55,11 +58,22 @@ export async function sendAssetFromEthereumToUnion({
 }): Promise<Hash> {
   const _unionAddress = get(snapAddress)
   if (!_unionAddress) throw new Error('snap address not set')
+  const ethereumAddress = get(wallet).address
+  if (!ethereumAddress) throw new Error('ethereum address not set')
+
+  const approvalResult = await approveUnoERC20Spending({ amount })
   // TODO: make dynamic?
   const counterpartyTimeoutRevisionNumber = 6n
   // TODO: make dynamic?
   const counterpartyTimeoutRevisionHeight = 800_000_000n
   try {
+    const sepoliaUnoBalance = await getUnoERC20Balance(ethereumAddress)
+    if (sepoliaUnoBalance < amount) {
+      throw new Error(
+        `insufficient balance to send ${amount} muno to ${get(unionAddress)}: ${sepoliaUnoBalance}`
+      )
+    }
+
     const denomAddress = await getDenomAddress()
 
     const writeContractParameters = {
@@ -75,14 +89,23 @@ export async function sendAssetFromEthereumToUnion({
         counterpartyTimeoutRevisionHeight
       ]
     } as const
-
     if (!simulate) return await writeContract(config, writeContractParameters)
 
     const { request } = await simulateContract(config, writeContractParameters)
     const transactionHash = await writeContract(config, request)
+
     sepoliaTransactions.update(transactions => [...transactions, transactionHash])
     return transactionHash
   } catch (error) {
+    if (error instanceof BaseError) {
+      const revertError = error.walk(error => error instanceof ContractFunctionRevertedError)
+      if (revertError instanceof ContractFunctionRevertedError) {
+        const errorName = revertError.data?.errorName ?? ''
+        toast.error(`error while sending ${amount} UNO [${errorName}-${revertError?.signature}]`, {
+          position: 'bottom-center'
+        })
+      }
+    }
     const errorMessage = error instanceof Error ? error.message : error
     throw new Error(`error while sending ${amount} muno to ${get(unionAddress)}: ${errorMessage}`)
   }
@@ -92,7 +115,7 @@ export async function getDenomAddress(): Promise<Address> {
   const [sourcePort, sourceChannel, denom] = [
     CONTRACT.SEPOLIA.PORT_ID,
     CONTRACT.SEPOLIA.SOURCE_CHANNEL,
-    UNO.NATIVE_DENOM || 'muno'
+    'muno'
   ]
 
   return readContract(config, {
