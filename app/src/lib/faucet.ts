dbbf96a15111f1a2e6f6528b214f33d53105de14
omar <omaraziz.dev@proton.me>
2024-02-19 11:33:53 -0800

    fix: faucet and connect button

--- app/src/lib/faucet.ts
+++ app/src/lib/faucet.ts
@@ -1,31 +1,35 @@
 import { fetcher } from './utilities'
 
 export async function getUnoFromFaucet({ address }: { address: string }) {
-  const response = await fetcher<
-    | { data: { union: { send: undefined } } }
-    | { errors: Array<{ message: string; extensions: { path: string; code: string } }> }
-  >('https://graphql.union.build/v1/graphql', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query: /* GraphQL */ `
-        mutation GetUno($address: Address!) {
-          union {
-            send(input: { toAddress: $address })
+  try {
+    const response = await fetcher<
+      | { data: { union: { send: undefined } } }
+      | { errors: Array<{ message: string; extensions: { path: string; code: string } }> }
+    >('https://graphql.union.build/v1/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          mutation GetUno($address: Address!) {
+            union {
+              send(input: { toAddress: $address })
+            }
           }
-        }
-      `,
-      variables: { address },
-      operationName: 'GetUno'
+        `,
+        variables: { address },
+        operationName: 'GetUno'
+      })
     })
-  })
 
-  if ('errors' in response) {
-    const [error] = response.errors
-    console.error(error)
-    throw new Error(error?.message)
+    if ('errors' in response) {
+      const [error] = response.errors
+      console.error(error)
+      throw new Error(error?.message)
+    }
+
+    return response.data
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : error
+    console.error(errorMessage)
   }
-
-  console.log(JSON.stringify(response.data, undefined, 2))
-  return response.data
 }
