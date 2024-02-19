34797a4ebb489a498946f9e70b0c2a1a97440054
omar <omaraziz.dev@proton.me>
2024-02-18 18:11:38 -0800

    fix: xterm component

--- site/src/lib/utilities.ts
+++ site/src/lib/utilities.ts
@@ -1,9 +1,3 @@
-import url from "node:url";
-import path from "node:path";
-import fs from "node:fs/promises";
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -15,9 +9,3 @@ export const roundNumber = (_number: number, decimalPlaces: number) =>
 
 export const generateRandomNumber = (min: number, max: number) =>
   Math.random() * (max - min) + min;
-
-export async function getFileContent({ filepath }: { filepath: string }) {
-  const file = path.resolve(__dirname, filepath);
-  if (!fs.stat(file)) throw new Error(`File not found: ${file}`);
-  return fs.readFile(file, { encoding: "utf8" });
-}
