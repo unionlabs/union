bc8f6a7a96f7147d743b6a179d250cb4b275531c
omar <omaraziz.dev@proton.me>
2024-02-19 11:33:53 -0800

    fix: faucet and connect button

--- app/src/lib/components/Faucet.svelte
+++ app/src/lib/components/Faucet.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import clsx from 'clsx'
   import { Button } from 'bits-ui'
+  import { snapAddress } from '$/lib/snap.ts'
   import { getUnoFromFaucet } from '$/lib/faucet.ts'
-  import { unionAddress } from '$/lib/wallet/config'
 </script>
 
 <Button.Root
   class={clsx('rounded-md border-[1px] px-4 py-2')}
-  on:click={() => getUnoFromFaucet({ address: $unionAddress })}
+  on:click={() => {
+    if (!$snapAddress) return
+    return getUnoFromFaucet({ address: $snapAddress })
+  }}
 >
   get UNO from faucet
 </Button.Root>
