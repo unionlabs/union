c6d8a73193b4abe51bb934ff3405599be390b855
omar <omaraziz.dev@proton.me>
2024-02-19 16:32:14 -0800

    fix: handle theme switch to auto-mode

--- site/src/lib/on-theme-change.ts
+++ site/src/lib/on-theme-change.ts
@@ -3,12 +3,20 @@
  * The callback gets the new theme as a string (optional)
  */
 
-export function onThemeChange<T extends (theme: "dark" | "light") => void>(
+export type ThemeMode = "dark" | "light";
+
+export function onThemeChange<T extends (theme: ThemeMode) => void>(
   callback: T
 ): void {
+  const systemPreference = window.matchMedia("(prefers-color-scheme: dark)")
+    .matches
+    ? "dark"
+    : "light";
   const themeToggle = document.querySelector("starlight-theme-select select");
   if (!themeToggle) return;
-  themeToggle.addEventListener("change", (_event) => {
-    callback(themeToggle.value as "dark" | "light");
+  themeToggle.addEventListener("change", (_) => {
+    const newTheme =
+      themeToggle.value === "auto" ? systemPreference : themeToggle.value;
+    callback(newTheme as ThemeMode);
   });
 }
