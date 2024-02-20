541a1b9992ca0bde1000d169af8364bf7789adcc
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-02-20 16:05:34 +0100

    chore: clippy

--- lib/zktrie-rs/src/proof.rs
+++ lib/zktrie-rs/src/proof.rs
@@ -1,6 +1,6 @@
 use crate::{Error, HashScheme, Node};
 
-pub const MAGIC_SMT_BYTES: &'static [u8] = b"THIS IS SOME MAGIC BYTES FOR SMT m1rRXgP2xpDI";
+pub const MAGIC_SMT_BYTES: &[u8] = b"THIS IS SOME MAGIC BYTES FOR SMT m1rRXgP2xpDI";
 
 pub fn decode_smt_proofs<H: HashScheme>(buf: &[u8]) -> Result<Option<Node<H>>, Error> {
     if MAGIC_SMT_BYTES.eq(buf) {
