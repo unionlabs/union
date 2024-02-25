710d685919c4b2480a976e23dde70f896e6bceab
omar <omaraziz.dev@proton.me>
2024-02-24 19:48:19 -0800

    fix: remove commented out code

--- app/src/lib/index.ts
+++ app/src/lib/index.ts
@@ -1,12 +1,1 @@
 export const BASE_URL = 'https://app.union.build'
-
-// import { AnkrProvider } from '@ankr.com/ankr.js'
-
-// const provider = new AnkrProvider(
-//   'https://rpc.ankr.com/eth_sepolia/bced07c1a0ee36409ee84dae4e4f65a25b57715ddd8f3f2fd261f2a6b5508505'
-// )
-
-// provider.getTransactionsByAddress({
-//   blockchain: 'eth',
-//   address: ['0xca091fe8005596e64ba9cf028a75755a2380021a']
-// }).then(console.log)
