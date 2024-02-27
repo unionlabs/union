3009aad5f4210024ccf701282e5227ecfeee02ae
omar <omaraziz.dev@proton.me>
2024-02-27 02:13:51 -0800

    fix: fix landing page menu icon on ios safari

--- site/src/components/ChainVersions.svelte
+++ site/src/components/ChainVersions.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
+  import versions from '../../../versions/versions.json'
 
-import versions from '../../../versions/versions.json'
+  export let chainId = Object.keys(versions).at(-1)
+  export let genesis = false
 
-export let chainId = 'union-testnet-4'
-export let genesis = false
-
-function isKeyof<T extends object>(obj: T, possibleKey: keyof any): possibleKey is keyof T {
-  return possibleKey in obj;
-}
-
-function Version(chainId: string, genesis: boolean) {
-  if (isKeyof(versions, chainId)) {
-    let version = genesis ? 0 : -1
-    return versions[chainId].at(version)
+  function isKeyof<T extends object>(obj: T, possibleKey: keyof any): possibleKey is keyof T {
+    return possibleKey in obj
   }
-  throw new Error('Unknown chainId: ' + chainId)
-}
 
-$: _genesis = typeof genesis === 'string' ? JSON.parse(genesis) : genesis
+  function Version(chainId: string, genesis: boolean) {
+    if (isKeyof(versions, chainId)) {
+      let version = genesis ? 0 : -1
+      return versions[chainId].at(version)
+    }
+    throw new Error('Unknown chainId: ' + chainId)
+  }
 
+  $: _genesis = typeof genesis === 'string' ? JSON.parse(genesis) : genesis
 </script>
 
 {Version(chainId, _genesis)}
