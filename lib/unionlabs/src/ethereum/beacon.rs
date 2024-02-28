5585b6d40950b095740e63bf76f512371a7c2ae5
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-02-28 20:02:01 +0100

    fix(eth): beacon api returns strings for u64

--- lib/unionlabs/src/ethereum/beacon.rs
+++ lib/unionlabs/src/ethereum/beacon.rs
@@ -125,6 +125,7 @@ pub struct BeaconBlockBody<
 
 #[derive(Clone, Debug, PartialEq, Encode, Decode, TreeHash, Serialize, Deserialize)]
 pub struct BlsToExecutionChange {
+    #[serde(with = "::serde_utils::string")]
     pub validator_index: u64,
     pub from_bls_public_key: BlsPublicKey,
     pub to_execution_address: H160,
