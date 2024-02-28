a87e2434243a1d064d2a548bea0fc2bacdcd314e
omar <omaraziz.dev@proton.me>
2024-02-28 00:53:29 -0800

    feat: blog url preview images

--- site/src/lib/utilities.ts
+++ site/src/lib/utilities.ts
@@ -1,11 +1,10 @@
 export function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-export const arraySizeN = (n: number) => Array.from(Array(n).keys());
+export const arraySizeN = (n: number) => Array.from(Array(n).keys())
 
 export const roundNumber = (_number: number, decimalPlaces: number) =>
-  Math.round(_number * 10 ** decimalPlaces) / 10 ** decimalPlaces;
+  Math.round(_number * 10 ** decimalPlaces) / 10 ** decimalPlaces
 
-export const generateRandomNumber = (min: number, max: number) =>
-  Math.random() * (max - min) + min;
+export const generateRandomNumber = (min: number, max: number) => Math.random() * (max - min) + min
