ad57aab44dc534636bf55b08478ea26e60461612
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-02-28 19:10:20 -0600

    fix(e2e): use new devnet name

--- e2e/upgrades.nix
+++ e2e/upgrades.nix
@@ -31,16 +31,16 @@ let
   '';
 
   upgradeTo = version: height: ''
-    union.succeed('docker cp ${mkUpgradeProposal version height}/proposal-${version}.json devnet-union-minimal-union-0-1:/proposal-${version}.json')
-    print(union.succeed('docker exec devnet-union-minimal-union-0-1 ${unionvisorBin} --root . call --bundle /bundle -- tx gov submit-proposal proposal-${version}.json --from val-0 --keyring-backend test --home ./home -y --gas 3000000000'))
+    union.succeed('docker cp ${mkUpgradeProposal version height}/proposal-${version}.json devnet-union-minimal-union-minimal-union-0-1:/proposal-${version}.json')
+    print(union.succeed('docker exec devnet-union-minimal-union-minimal-union-0-1 ${unionvisorBin} --root . call --bundle /bundle -- tx gov submit-proposal proposal-${version}.json --from val-0 --keyring-backend test --home ./home -y --gas 3000000000'))
 
     time.sleep(6)
 
-    union.wait_until_succeeds("[[ $(docker exec devnet-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query gov proposal ${toString (height / 10)} --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.status == \"PROPOSAL_STATUS_VOTING_PERIOD\"') == true ]]", timeout=30)
+    union.wait_until_succeeds("[[ $(docker exec devnet-union-minimal-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query gov proposal ${toString (height / 10)} --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.status == \"PROPOSAL_STATUS_VOTING_PERIOD\"') == true ]]", timeout=30)
 
-    ${forEachNode (id: "print(union.succeed('docker exec devnet-union-minimal-union-${id}-1 ${unionvisorBin} --root . call --bundle /bundle -- tx gov vote ${toString (height / 10)} yes --keyring-backend test --from val-${id} --home ./home -y'))")}
+    ${forEachNode (id: "print(union.succeed('docker exec devnet-union-minimal-union-minimal-union-${id}-1 ${unionvisorBin} --root . call --bundle /bundle -- tx gov vote ${toString (height / 10)} yes --keyring-backend test --from val-${id} --home ./home -y'))")}
 
-    union.wait_until_succeeds("[[ $(docker exec devnet-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query gov proposal ${toString (height / 10)} --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.status == \"PROPOSAL_STATUS_PASSED\"') == true ]]", timeout=30)
+    union.wait_until_succeeds("[[ $(docker exec devnet-union-minimal-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query gov proposal ${toString (height / 10)} --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.status == \"PROPOSAL_STATUS_PASSED\"') == true ]]", timeout=30)
 
     union.wait_until_succeeds('[[ $(curl "http://localhost:26660/block" --fail --silent | ${pkgs.lib.meta.getExe pkgs.jq} ".result.block.header.height | tonumber > ${toString height}") == "true" ]]')
   '';
@@ -53,7 +53,7 @@ in
       import time
       union.wait_for_open_port(${toString e2e.unionNode.wait_for_open_port})
 
-      ${forEachNode (id: "union.succeed('docker cp ${bundle} devnet-union-minimal-union-${id}-1:/bundle')")}
+      ${forEachNode (id: "union.succeed('docker cp ${bundle} devnet-union-minimal-union-minimal-union-${id}-1:/bundle')")}
 
       # Ensure the union network commits more than one block
       union.wait_until_succeeds('[[ $(curl "http://localhost:26660/block" --fail --silent | ${pkgs.lib.meta.getExe pkgs.jq} ".result.block.header.height | tonumber > 1") == "true" ]]')
@@ -73,17 +73,17 @@ in
       import time
       union.wait_for_open_port(${toString e2e.unionNode.wait_for_open_port})
 
-      ${forEachNode (id: "union.succeed('docker cp ${bundle} devnet-union-minimal-union-${id}-1:/bundle')")}
+      ${forEachNode (id: "union.succeed('docker cp ${bundle} devnet-union-minimal-union-minimal-union-${id}-1:/bundle')")}
 
       # Ensure the union network commits more than one block
       union.wait_until_succeeds('[[ $(curl "http://localhost:26660/block" --fail --silent | ${pkgs.lib.meta.getExe pkgs.jq} ".result.block.header.height | tonumber > 1") == "true" ]]')
 
-      union.succeed("docker exec devnet-union-minimal-union-0-1 ${unionvisorBin} --root . call --bundle /bundle -- tx tokenfactory create-denom bazinga --from val-0 --keyring-backend test --home ./home -y --gas 3000000000")
+      union.succeed("docker exec devnet-union-minimal-union-minimal-union-0-1 ${unionvisorBin} --root . call --bundle /bundle -- tx tokenfactory create-denom bazinga --from val-0 --keyring-backend test --home ./home -y --gas 3000000000")
       time.sleep(6)
-      union.succeed("[[ $(docker exec devnet-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query tokenfactory denom-authority-metadata factory/union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw/bazinga --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.authority_metadata.admin == \"union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw\"') == true ]]")
+      union.succeed("[[ $(docker exec devnet-union-minimal-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query tokenfactory denom-authority-metadata factory/union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw/bazinga --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.authority_metadata.admin == \"union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw\"') == true ]]")
 
       ${upgradeTo "v0.20.0" 10}
-      union.succeed("[[ $(docker exec devnet-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query tokenfactory denom-authority-metadata factory/union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw/bazinga --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.authority_metadata.admin == \"union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw\"') == true ]]")
+      union.succeed("[[ $(docker exec devnet-union-minimal-union-minimal-union-0-1 ${unionvisorBin} -l off --root . call --bundle /bundle -- query tokenfactory denom-authority-metadata factory/union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw/bazinga --home ./home --output json | ${pkgs.lib.meta.getExe pkgs.jq} '.authority_metadata.admin == \"union14fldwd959h7glh2e3k45veuqfszvgm69q07jhw\"') == true ]]")
     '';
 
     nodes = {
