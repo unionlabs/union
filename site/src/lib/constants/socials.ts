2d36783a8df1c8382bc03231ae4b74b7ce254ece
cor <cor@pruijs.dev>
2024-03-27 17:23:26 +0100

    feat(site): update copy

--- site/src/lib/constants/socials.ts
+++ site/src/lib/constants/socials.ts
@@ -10,16 +10,11 @@ export const socialLinks: Array<{
   {
     href: "https://x.com/union_build",
     icon: "x.com",
-    cta: "Join us on X"
+    cta: "Follow us on X"
   },
   {
     href: "https://discord.union.build",
     icon: "discord",
     cta: "Join our Discord"
   },
-  {
-    href: "https://x.com/union_build",
-    icon: "seti:csv",
-    cta: "Join our Events"
-  }
 ]
