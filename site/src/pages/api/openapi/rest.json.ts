5d5f5ce83c210354a45d3d82f4fd73e516f2b196
omar <omaraziz.dev@proton.me>
2024-04-02 16:39:07 -0700

    chore: skip stringifying and return json

--- site/src/pages/api/openapi/rest.json.ts
+++ site/src/pages/api/openapi/rest.json.ts
@@ -1,4 +1,4 @@
 import type { APIRoute } from "astro"
 import endpoints from "~root/uniond/docs/static/openapi.json" with { type: "json" }
 
-export const GET: APIRoute = () => new Response(JSON.stringify(endpoints, null, 2))
+export const ALL: APIRoute = () => Response.json(endpoints)
