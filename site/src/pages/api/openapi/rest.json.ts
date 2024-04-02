48b1abee4cf98f81acb69bf36919a3e7577bb9ce
omar <omaraziz.dev@proton.me>
2024-04-02 16:39:59 -0700

    chore: only allow GET

--- site/src/pages/api/openapi/rest.json.ts
+++ site/src/pages/api/openapi/rest.json.ts
@@ -1,4 +1,4 @@
 import type { APIRoute } from "astro"
 import endpoints from "~root/uniond/docs/static/openapi.json" with { type: "json" }
 
-export const ALL: APIRoute = () => Response.json(endpoints)
+export const GET: APIRoute = () => Response.json(endpoints)
