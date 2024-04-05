35e5095aa85ff0ccc261363ef4dd166fa6d75e72
cor <cor@pruijs.dev>
2024-04-05 16:20:21 +0200

    fix(nostromo): use axum v0.6.2

--- nostromo/src/main.rs
+++ nostromo/src/main.rs
@@ -8,7 +8,9 @@ async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
