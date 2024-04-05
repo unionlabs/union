f3ede9bfae001c7657ad4e1b8535b0ac40999b4b
benluelo <git@luelo.dev>
2024-04-05 12:54:21 -0400

    feat(osmosis): add devnet to flake

--- networks/devnet.nix
+++ networks/devnet.nix
@@ -5,7 +5,7 @@
 
       mkCosmosDevnet = import ./mkCosmosDevnet.nix { inherit pkgs dbg; };
 
-      devnet-union = dbg (mkCosmosDevnet {
+      devnet-union = mkCosmosDevnet {
         node = self'.packages.uniond;
         chainId = "union-devnet-1";
         chainName = "union";
@@ -31,7 +31,7 @@
           self'.packages.ucs01-relay
         ];
         portIncrease = 0;
-      });
+      };
 
       devnet-stargaze = mkCosmosDevnet {
         node = self'.packages.starsd;
@@ -48,6 +48,23 @@
           self'.packages.ucs01-relay
         ];
         portIncrease = 100;
+      };
+
+      devnet-osmosis = mkCosmosDevnet {
+        node = self'.packages.starsd;
+        chainId = "osmosis-devnet-1";
+        chainName = "osmosis";
+        denom = "uosmo";
+        keyType = "ed25519";
+        validatorCount = 4;
+        lightClients = [
+          self'.packages.cometbls-light-client
+        ];
+        cosmwasmContracts = [
+          self'.packages.ucs00-pingpong
+          self'.packages.ucs01-relay
+        ];
+        portIncrease = 200;
       };
 
       devnet-simd = mkCosmosDevnet {
@@ -121,10 +138,9 @@
 
       services = {
         devnet-union = devnet-union.services;
-
         devnet-simd = devnet-simd.services;
-
         devnet-stargaze = devnet-stargaze.services;
+        devnet-osmosis = devnet-osmosis.services;
 
         devnet-union-minimal = devnet-union-minimal.services;
 
@@ -150,127 +166,93 @@
         # hubble = { hubble = import ./services/hubble.nix { inherit lib; image = self'.packages.hubble-image; }; };
       };
 
+      mkNamedModule = name: {
+        ${name} = {
+          project.name = name;
+          services = services.${name};
+        };
+      };
+
       modules = {
         full-dev-setup = {
           project.name = "full-dev-setup";
           services = services.devnet-eth // services.devnet-union // services.postgres;
         };
+      }
+      // mkNamedModule "postgres"
+      // mkNamedModule "devnet-eth"
+      // mkNamedModule "devnet-stargaze"
+      // mkNamedModule "devnet-osmosis"
+      // mkNamedModule "devnet-simd"
+      // mkNamedModule "devnet-union-minimal"
+      // mkNamedModule "devnet-union";
 
-        devnet-union = {
-          project.name = "devnet-union";
-          services = services.devnet-union;
-        };
-
-        devnet-union-minimal = {
-          project.name = "devnet-union-minimal";
-          services = services.devnet-union-minimal;
-        };
-
-        devnet-simd = {
-          project.name = "devnet-simd";
-          services = services.devnet-simd;
-        };
-
-        devnet-stargaze = {
-          project.name = "devnet-stargaze";
-          services = services.devnet-stargaze;
-        };
-
-        devnet-eth = {
-          project.name = "devnet-eth";
-          services = services.devnet-eth;
-        };
-
-        postgres = {
-          project.name = "postgres";
-          services = services.postgres;
+      mkNamedSpec = name: {
+        ${name} = {
+          modules = [ modules.${name} ];
         };
       };
 
       specs = {
-        full-dev-setup = {
-          modules = [ (modules.full-dev-setup // { networks.full-dev-setup = { }; }) ];
-        };
-
-        devnet-union = {
-          modules = [ (modules.devnet-union // { networks.devnet-union = { }; }) ];
-        };
-
-        devnet-union-minimal = {
-          modules = [ (modules.devnet-union-minimal // { networks.devnet-union-minimal = { }; }) ];
-        };
-
-        devnet-simd = {
-          modules = [ (modules.devnet-simd // { networks.devnet-simd = { }; }) ];
-        };
-
-        devnet-stargaze = {
-          modules = [ (modules.devnet-stargaze // { networks.devnet-stargaze = { }; }) ];
-        };
-
-        devnet-eth = {
-          modules = [ (modules.devnet-eth // { networks.devnet-eth = { }; }) ];
-        };
-
         voyager-queue = {
           modules = [ modules.postgres ];
         };
+      }
+      // mkNamedSpec "full-dev-setup"
+      // mkNamedSpec "devnet-eth"
+      // mkNamedSpec "devnet-stargaze"
+      // mkNamedSpec "devnet-osmosis"
+      // mkNamedSpec "devnet-simd"
+      // mkNamedSpec "devnet-union-minimal"
+      // mkNamedSpec "devnet-union";
+
+      mkNamedBuild = name: {
+        ${name} = arion.build specs.${name};
       };
 
-      build = {
-        full-dev-setup = arion.build specs.full-dev-setup;
+      build = mkNamedBuild "full-dev-setup"
+        // mkNamedBuild "voyager-queue"
+        // mkNamedBuild "devnet-eth"
+        // mkNamedBuild "devnet-stargaze"
+        // mkNamedBuild "devnet-osmosis"
+        // mkNamedBuild "devnet-simd"
+        // mkNamedBuild "devnet-union-minimal"
+        // mkNamedBuild "devnet-union";
 
-        devnet-union = arion.build specs.devnet-union;
-
-        devnet-union-minimal = arion.build specs.devnet-union-minimal;
-
-        devnet-simd = arion.build specs.devnet-simd;
-
-        devnet-stargaze = arion.build specs.devnet-stargaze;
-
-        devnet-eth = arion.build specs.devnet-eth;
-
-        voyager-queue = arion.build specs.voyager-queue;
-      };
-
-      mkArionBuild = target: name: pkgs.writeShellApplication {
-        inherit name;
-        runtimeInputs = [ arion ];
-        text = ''
-          arion --prebuilt-file ${target} up --build --force-recreate -V --always-recreate-deps --remove-orphans
-        '';
+      mkArionBuild = name: ciCondition: {
+        ${name} = mkCi ciCondition (pkgs.writeShellApplication {
+          inherit name;
+          runtimeInputs = [ arion ];
+          text = ''
+            arion --prebuilt-file ${build.${name}} up --build --force-recreate -V --always-recreate-deps --remove-orphans
+          '';
+        });
       };
     in
     {
       packages = {
-        full-dev-setup = mkCi (system == "x86_64-linux") (mkArionBuild build.full-dev-setup "full-dev-setup");
-
-        devnet-union = mkCi (system == "x86_64-linux") (mkArionBuild build.devnet-union "devnet-union");
-
         devnet-union-home = mkCi false (devnet-union.devnet-home);
-
-        devnet-union-minimal = mkCi (system == "x86_64-linux") (mkArionBuild build.devnet-union-minimal "devnet-union-minimal");
-
-        devnet-simd = mkCi (system == "x86_64-linux") (mkArionBuild build.devnet-simd "devnet-simd");
-
         devnet-simd-home = mkCi false (devnet-simd.devnet-home);
-
-        devnet-stargaze = mkCi (system == "x86_64-linux") (mkArionBuild build.devnet-stargaze "devnet-stargaze");
-
         devnet-stargaze-home = mkCi false (devnet-stargaze.devnet-home);
-
-        devnet-eth = mkCi (system == "x86_64-linux") (mkArionBuild build.devnet-eth "devnet-eth");
-
-        voyager-queue = mkCi false (mkArionBuild build.voyager-queue "voyager-queue");
+        devnet-osmosis-home = mkCi false (devnet-osmosis.devnet-home);
 
         # FIXME: This shouldn't be defined in this file
         devnet-eth-config = pkgs.linkFarm "devnet-eth-config" [
+
           { name = "genesis.json"; path = "${./genesis/devnet-eth/genesis.json}"; }
           { name = "dev-key0.prv"; path = "${./genesis/devnet-eth/dev-key0.prv}"; }
           { name = "dev-key1.prv"; path = "${./genesis/devnet-eth/dev-key1.prv}"; }
           { name = "dev-jwt.prv"; path = "${./genesis/devnet-eth/dev-jwt.prv}"; }
         ];
-      };
+      }
+      // (mkArionBuild "full-dev-setup" (system == "x86_64-linux"))
+      // (mkArionBuild "voyager-queue" false)
+      // (mkArionBuild "devnet-union" (system == "x86_64-linux"))
+      // (mkArionBuild "devnet-simd" (system == "x86_64-linux"))
+      // (mkArionBuild "devnet-stargaze" (system == "x86_64-linux"))
+      // (mkArionBuild "devnet-osmosis" (system == "x86_64-linux"))
+      // (mkArionBuild "devnet-eth" (system == "x86_64-linux"))
+      // (mkArionBuild "devnet-union-minimal" (system == "x86_64-linux"));
 
       _module.args.networks.modules = modules;
     };
