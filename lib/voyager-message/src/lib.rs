e4bb5a0e816785ca4a7cda6780c4d8045b313d7e
benluelo <git@luelo.dev>
2024-04-26 09:17:26 -0400

    feat(voyager): implement read_ack on scroll

--- lib/voyager-message/src/lib.rs
+++ lib/voyager-message/src/lib.rs
@@ -382,6 +382,28 @@ impl HandleData<VoyagerMessageTypes> for VoyagerData {
                         _ => unimplemented!(),
                     },
                 ),
+                QueueMsg::Data(block_message::AnyChainIdentified::Scroll(
+                    block_message::Identified {
+                        chain_id,
+                        t: block_message::data::Data::IbcEvent(ibc_event),
+                    },
+                )) => <VoyagerMessageTypes as FromQueueMsg<RelayMessageTypes>>::from_queue_msg(
+                    match ibc_event.client_type {
+                        unionlabs::ClientType::Cometbls => {
+                            event(relay_message::id::<Scroll, Wasm<Union>, _>(
+                                chain_id,
+                                relay_message::event::IbcEvent {
+                                    tx_hash: ibc_event.tx_hash,
+                                    height: ibc_event.height,
+                                    event: chain_event_to_lc_event::<Scroll, Wasm<Union>>(
+                                        ibc_event.event,
+                                    ),
+                                },
+                            ))
+                        }
+                        _ => unimplemented!(),
+                    },
+                ),
                 msg => {
                     <VoyagerMessageTypes as FromQueueMsg<BlockMessageTypes>>::from_queue_msg(msg)
                 }
