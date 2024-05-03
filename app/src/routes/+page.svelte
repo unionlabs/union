f01c62c11cb8fdc869ec2ae0afda782c98ff3edb
omar <omaraziz.dev@proton.me>
2024-05-02 18:45:58 -0700

    fix: icons path

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 import { cn } from "$lib/utilities/shadcn.ts"
+import SvelteLogo from 'virtual:icons/logos/svelte-icon'
 </script>
 
 <main
   class={cn(['mt-12 flex min-w-full flex-col items-center justify-center space-y-6 text-white'])}
-></main>
+>
+  <SvelteLogo style="font-size:2em" />
+</main>
