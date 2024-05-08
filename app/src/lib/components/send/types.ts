644d1d09b948a32a22b27b0537fc680c7dbc162f
omar <omaraziz.dev@proton.me>
2024-05-07 20:30:42 -0700

    feat: assets types

--- app/src/lib/components/send/types.ts
+++ app/src/lib/components/send/types.ts
@@ -1,12 +1,29 @@
+export type ChainId = "union-testnet-8" | "11155111" | "osmo-test-5" | "stargaze-1" | "534352"
+
 export interface Asset {
-  destination: "union-testnet-8" | "11155111" | "osmo-test-5" | "stargaze-1" | "534352"
-  port: string
-  client: string
-  channel: string
-  connection: string
+  source: {
+    port: string
+    channel: string
+    client: string
+    connection: string
+    chain: ChainId
+    explorerLink?: string
+  }
+  destination: {
+    port: string
+    channel: string
+    client: string
+    connection: string
+    chain: ChainId
+    explorerLink?: string
+  }
   contractAddress?: string
   symbol: string
   denom: string
   display: string
   explorerLink?: string
+  /**
+   * id: source.chain + destination.chain + symbol
+   */
+  id: string
 }
