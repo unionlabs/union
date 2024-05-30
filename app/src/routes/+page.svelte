651bbaee758e2b6d2b03d595d5de813959640dd1
cor <cor@pruijs.dev>
2024-05-30 16:21:40 +0200

    fix(app): fmt

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import * as Card from "$lib/components/ui/card/index.ts"
+import * as Card from "$lib/components/ui/card/index.ts"
 </script>
 <main class="flex flex-col items-center size-full p-4 mt-16">
   <Card.Root class="max-w-lg">
