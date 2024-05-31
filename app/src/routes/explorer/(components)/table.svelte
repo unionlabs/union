4a73aa382adecd889f82446d1d6ebba11892a83a
cor <cor@pruijs.dev>
2024-05-31 18:21:52 +0200

    refactor(app): decouple table from data fetching and fix reactivity issues

---
+++ app/src/routes/explorer/(components)/table.svelte
@@ -0,0 +1,102 @@
+<script lang="ts" generics="T extends object" >
+import { derived, get } from "svelte/store"
+import {
+  flexRender,
+  type ColumnDef,
+  getCoreRowModel,
+  type TableOptions,
+  createSvelteTable,
+  getFilteredRowModel,
+  getPaginationRowModel
+} from "@tanstack/svelte-table"
+import { writable, type Writable } from "svelte/store"
+import { cn } from "$lib/utilities/shadcn.ts"
+import * as Table from "$lib/components/ui/table"
+import { createVirtualizer } from "@tanstack/svelte-virtual"
+import * as Card from "$lib/components/ui/card/index.ts"
+
+export let columns: Array<ColumnDef<any>>
+
+// https://github.com/TanStack/table/issues/4241
+// @ts-ignore
+export let blocksStore: Writable<Array<any>>
+
+const options = writable<TableOptions<any>>({
+  data: $blocksStore,
+  enableHiding: true,
+  enableFilters: true,
+  // https://github.com/TanStack/table/issues/4241
+  // @ts-ignore
+  columns,
+  autoResetPageIndex: true, // Automatically update pagination when data or page size changes
+  enableColumnFilters: true,
+  enableColumnResizing: true,
+  enableMultiRowSelection: true,
+  getCoreRowModel: getCoreRowModel(),
+  getFilteredRowModel: getFilteredRowModel(),
+  getPaginationRowModel: getPaginationRowModel()
+})
+
+let virtualListElement: HTMLDivElement
+
+const rerender = () => {
+  options.update(options => ({ ...options, data: $blocksStore as unknown as Array<T> }))
+}
+
+const table = createSvelteTable(options)
+const rows = derived(table, $t => $t.getRowModel().rows)
+
+$: virtualizer = createVirtualizer<HTMLDivElement, HTMLTableRowElement>({
+  overscan: 20,
+  count: $rows.length,
+  estimateSize: () => 34,
+  getScrollElement: () => virtualListElement
+})
+
+$: blocksStore.subscribe(() => {
+  if (!$blocksStore) return
+  $table.setPageSize($blocksStore.length)
+  rerender()
+})
+</script>
+
+
+<Card.Root>
+  <div bind:this={virtualListElement} >
+    <Table.Root>
+      <Table.Header>
+        {#each $table.getHeaderGroups() as headerGroup (headerGroup.id)}
+          <Table.Row>
+            {#each headerGroup.headers as header (header.id)}
+              <Table.Head
+                colspan={header.colSpan}
+                class={cn(`w-[${header.getSize()}px]`)}
+              >
+                  <svelte:component
+                    this={flexRender(header.column.columnDef.header, header.getContext())}
+                  />
+              </Table.Head>
+            {/each}
+          </Table.Row>
+        {/each}
+      </Table.Header>
+      <Table.Body class={cn(`h-[${$virtualizer.getTotalSize()}px]`)}>
+        {#each $virtualizer.getVirtualItems() as row, index (row.index)}
+          <Table.Row
+            class={cn(
+              index % 2 === 0 ? 'bg-secondary/10' : 'bg-transparent',
+            )}
+          >
+            {#each $rows[row.index].getVisibleCells() as cell, index (cell.id)}
+              <Table.Cell>
+                <svelte:component
+                  this={flexRender(cell.column.columnDef.cell, cell.getContext())}
+                />
+              </Table.Cell>
+            {/each}
+          </Table.Row>
+        {/each}
+      </Table.Body>
+    </Table.Root>
+  </div>
+</Card.Root>
--- app/src/routes/explorer/blocks/+page.svelte
+++ app/src/routes/explorer/blocks/+page.svelte
@@ -2,28 +2,30 @@
 import {
   flexRender,
   type ColumnDef,
-  getCoreRowModel,
-  type TableOptions,
-  createSvelteTable,
-  getFilteredRowModel,
-  getPaginationRowModel
+  getCoreRowModel
+  //   type TableOptions,
+  //   createSvelteTable,
+  //   getFilteredRowModel,
+  //   getPaginationRowModel
 } from "@tanstack/svelte-table"
 import request from "graphql-request"
 import { URLS } from "$lib/constants"
 import { writable } from "svelte/store"
 import { DurationUnits } from "svelte-ux"
-import { cn } from "$lib/utilities/shadcn.ts"
+// import { cn } from "$lib/utilities/shadcn.ts"
 import { CHAIN_MAP } from "$lib/constants/chains"
-import * as Table from "$lib/components/ui/table"
+// import * as Table from "$lib/components/ui/table"
 import { createQuery } from "@tanstack/svelte-query"
 import { removeArrayDuplicates } from "$lib/utilities"
 import type { Override } from "$lib/utilities/types.ts"
-import { createVirtualizer } from "@tanstack/svelte-virtual"
+// import { createVirtualizer } from "@tanstack/svelte-virtual"
 import Button from "$lib/components/ui/button/button.svelte"
 import CellText from "../(components)/cell-plain-text.svelte"
 import CellDurationText from "../(components)/cell-duration-text.svelte"
 import { cosmosBlocksQuery } from "$lib/graphql/documents/cosmos-blocks.ts"
-import * as Card from "$lib/components/ui/card/index.ts"
+// import * as Card from "$lib/components/ui/card/index.ts"
+
+import Table from "../(components)/table.svelte"
 
 $: cosmosBlocks = createQuery({
   queryKey: ["cosmos-blocks"],
@@ -46,7 +48,7 @@ $: if (blockData) {
   )
 }
 
-const defaultColumns: Array<ColumnDef<CosmosBlock>> = [
+const columns = [
   {
     accessorKey: "time",
     size: 100,
@@ -110,43 +112,7 @@ const defaultColumns: Array<ColumnDef<CosmosBlock>> = [
         href: `https://rpc.testnet.bonlulu.uno/block_by_hash?hash=${info.getValue()}`
       })
   }
-]
-
-const options = writable<TableOptions<CosmosBlock>>({
-  data: $blocksStore,
-  enableHiding: true,
-  enableFilters: true,
-  columns: defaultColumns,
-  autoResetPageIndex: true, // Automatically update pagination when data or page size changes
-  enableColumnFilters: true,
-  enableColumnResizing: true,
-  enableMultiRowSelection: true,
-  getCoreRowModel: getCoreRowModel(),
-  getFilteredRowModel: getFilteredRowModel(),
-  getPaginationRowModel: getPaginationRowModel()
-})
-
-let virtualListElement: HTMLDivElement
-
-const rerender = () =>
-  options.update(options => ({ ...options, data: $blocksStore as unknown as Array<CosmosBlock> }))
-
-const table = createSvelteTable(options)
-
-$: blocksStore.subscribe(() => {
-  if (!$blocksStore) return
-  $table.setPageSize($blocksStore.length)
-  rerender()
-})
-
-$: rows = $table.getRowModel().rows
-
-$: virtualizer = createVirtualizer<HTMLDivElement, HTMLTableRowElement>({
-  overscan: 20,
-  count: rows.length,
-  estimateSize: () => 34,
-  getScrollElement: () => virtualListElement
-})
+] as Array<ColumnDef<CosmosBlock>>
 </script>
 
 <svelte:head>
@@ -154,43 +120,8 @@ $: virtualizer = createVirtualizer<HTMLDivElement, HTMLTableRowElement>({
 </svelte:head>
 
 
-<Card.Root>
-  <div bind:this={virtualListElement} >
-    <Table.Root>
-      <Table.Header>
-        {#each $table.getHeaderGroups() as headerGroup (headerGroup.id)}
-          <Table.Row>
-            {#each headerGroup.headers as header (header.id)}
-              <Table.Head
-                colspan={header.colSpan}
-                class={cn(`w-[${header.getSize()}px]`)}
-              >
-                  <svelte:component
-                    this={flexRender(header.column.columnDef.header, header.getContext())}
-                  />
-              </Table.Head>
-            {/each}
-          </Table.Row>
-        {/each}
-      </Table.Header>
-      <Table.Body class={cn(`h-[${$virtualizer.getTotalSize()}px]`)}>
-        {#each $virtualizer.getVirtualItems() as row, index (row.index)}
-          <Table.Row
-            class={cn(
-              index % 2 === 0 ? 'bg-secondary/10' : 'bg-transparent',
-            )}
-          >
-            {#each rows[row.index].getVisibleCells() as cell, index (cell.id)}
-              <Table.Cell>
-                <svelte:component
-                  this={flexRender(cell.column.columnDef.cell, cell.getContext())}
-                />
-              </Table.Cell>
-            {/each}
-          </Table.Row>
-        {/each}
-      </Table.Body>
-    </Table.Root>
-  </div>
-</Card.Root>
+
+
+<Table columns={columns} bind:blocksStore={blocksStore}/>
+{JSON.stringify($blocksStore, null, 2)}
 
