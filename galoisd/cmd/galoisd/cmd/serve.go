c1fc3014bb5dce71078ba805e131fc4e6e9018e1
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-05-31 12:26:55 +0200

    feat(galois): move to structured logging

--- galoisd/cmd/galoisd/cmd/serve.go
+++ galoisd/cmd/galoisd/cmd/serve.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"fmt"
 	provergrpc "galois/grpc"
 	provergrpcapi "galois/grpc/api/v3"
-	"log"
 	"net"
+	"os"
 	"time"
+
+	"github.com/consensys/gnark/logger"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/net/netutil"
@@ -14,10 +19,11 @@ import (
 )
 
 const (
-	flagR1CS    = "cs-path"
-	flagPK      = "pk-path"
-	flagVK      = "vk-path"
-	flagMaxConn = "max-conn"
+	flagR1CS     = "cs-path"
+	flagPK       = "pk-path"
+	flagVK       = "vk-path"
+	flagMaxConn  = "max-conn"
+	flagLogLevel = "log-level"
 )
 
 func ServeCmd() *cobra.Command {
@@ -42,6 +48,17 @@ func ServeCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
+			logLevel, err := cmd.Flags().GetInt(flagLogLevel)
+			if err != nil {
+				return err
+			}
+			if logLevel > int(zerolog.PanicLevel) || logLevel < int(zerolog.TraceLevel) {
+				return fmt.Errorf("log level must be between TraceLevel and PanicLevel")
+			}
+			zerolog.SetGlobalLevel(zerolog.Level(logLevel))
+			log.Logger = log.With().Caller().Logger().Output(os.Stdout)
+			logger.Set(log.Logger)
+
 			server, err := provergrpc.NewProverServer(uint32(maxConn), r1csPath, pkPath, vkPath)
 			if err != nil {
 				return err
@@ -60,7 +77,7 @@ func ServeCmd() *cobra.Command {
 				Timeout:               20 * time.Second,
 			}))
 			provergrpcapi.RegisterUnionProverAPIServer(grpcServer, server)
-			log.Println("Serving...")
+			log.Info().Msg("Serving...")
 			return grpcServer.Serve(limitedLis)
 		},
 	}
@@ -68,5 +85,6 @@ func ServeCmd() *cobra.Command {
 	cmd.Flags().String(flagPK, "pk.bin", "Path to the proving key.")
 	cmd.Flags().String(flagVK, "vk.bin", "Path to the verifying key.")
 	cmd.Flags().Int(flagMaxConn, 1, "Maximum number of concurrent connection.")
+	cmd.Flags().Int(flagLogLevel, int(zerolog.InfoLevel), "Log level see https://github.com/rs/zerolog/blob/c78e50e2da70f4ae63e1b65222c3acf12e9ba699/README.md#leveled-logging")
 	return cmd
 }
