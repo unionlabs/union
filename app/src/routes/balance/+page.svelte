1435b2a195021437f74ea1a8672193383c73c5a3
omar <omaraziz.dev@proton.me>
2024-06-03 08:25:50 -0700

    feat: app balances

---
+++ app/src/routes/balance/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { createQuery } from '@tanstack/svelte-query'
+  import { evmBalancesQuery } from '$lib/queries/balance'
+  import { CHAINS } from '$lib/constants'
+
+  $: evmBalances = evmBalancesQuery({
+    address: '0x8478B37E983F520dBCB5d7D3aAD8276B82631aBd',
+    chainId: '11155111',
+    tokenSpecification: 'erc20',
+  })
+
+  $: data = $evmBalances?.data || []
+</script>
+
+<main>
+  <pre>{JSON.stringify(data, undefined, 2)}</pre>
+</main>
