01573c8597195733e91fef0598c6a70d60f40a88
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-06-03 05:26:40 -0500

    feat(uniond): added da cv to vm

--- uniond/app/upgrades/v0_23_0/upgrade.go
+++ uniond/app/upgrades/v0_23_0/upgrade.go
@@ -3,6 +3,8 @@ package v0_23_0
 import (
 	"context"
 	"union/app/upgrades"
+	"union/x/deferredack"
+	datypes "union/x/deferredack/types"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -10,6 +12,7 @@ import (
 
 func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, keepers *upgrades.AppKeepers) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		vm[datypes.ModuleName] = deferredack.ConsensusVersion
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
