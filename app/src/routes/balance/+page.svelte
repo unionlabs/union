0acc7c64450a6fb12f10d076f082e16af11ed6f3
omar <omaraziz.dev@proton.me>
2024-06-03 08:26:56 -0700

    chore: cleanup

--- app/src/routes/balance/+page.svelte
+++ app/src/routes/balance/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { createQuery } from '@tanstack/svelte-query'
   import { evmBalancesQuery } from '$lib/queries/balance'
-  import { CHAINS } from '$lib/constants'
 
   $: evmBalances = evmBalancesQuery({
     address: '0x8478B37E983F520dBCB5d7D3aAD8276B82631aBd',
