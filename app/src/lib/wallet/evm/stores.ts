1adc580535326c3212540a4b769b6499fa9f2bab
omar <omaraziz.dev@proton.me>
2024-06-04 11:03:01 -0700

    chore: save progress

--- app/src/lib/wallet/evm/stores.ts
+++ app/src/lib/wallet/evm/stores.ts
@@ -27,34 +27,6 @@ export const evmConnections = readable(getConnections(config), set =>
   watchConnections(config, { onChange: set })
 )
 
-// export const connectorClient = derived(evmConnectors, async $connectors => {
-//   const connector = $connectors.find(async connector => await connector?.isAuthorized())
-//   if (!connector) return undefined
-//   const client = await getConnectorClient(config, { connector })
-//   return client
-// })
-
-// export const provider = readable<() => Promise<undefined | unknown>>(
-//   async () =>
-//     await getConnectors(config)
-//       .find(async connector => await connector.isAuthorized())
-//       ?.getProvider(),
-//   set => {
-//     watchConnectors(config, {
-//       onChange: (connections, previousConnectors) => {
-//         const connector = connections.find(connector => connector.isAuthorized())
-//         if (connector) set(() => connector.getProvider({ chainId: getChainId(config) }))
-//       }
-//     })
-//     watchAccount(config, {
-//       onChange: account => {
-//         if (!account.connector) return set(async () => undefined)
-//         set(async () => await account.connector?.getProvider({ chainId: getChainId(config) }))
-//       }
-//     })
-//   }
-// )
-
 export const evmProvider = derived(
   [evmAccount, evmConnectors, evmChainId],
   ([$account, $connectors, $chainId]) => {
