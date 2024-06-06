bc63f850705fbe8c5d15b3f0ab4b289f6771591f
Lukas <36674091+Swepool@users.noreply.github.com>
2024-06-06 23:48:31 +0200

    chore: lint

--- app/src/lib/wallet/cosmos/chain-info.ts
+++ app/src/lib/wallet/cosmos/chain-info.ts
@@ -1,109 +1,109 @@
-import type { ChainInfo as KeplrChainInfo } from "@keplr-wallet/types";
-import type { ChainInfo as LeapChainInfo } from "@leapwallet/types";
+import type {ChainInfo as KeplrChainInfo} from "@keplr-wallet/types";
+import type {ChainInfo as LeapChainInfo} from "@leapwallet/types";
 
 //This exist according to docs
 interface LeapExtendedInfo extends LeapChainInfo {
-    theme: {
-        primaryColor: string;
-        gradient: string;
-    };
-    image: string;
+  theme: {
+    primaryColor: string;
+    gradient: string;
+  };
+  image: string;
 }
 
 //todo handle this for mainnet
 export const keplrChainInfo: KeplrChainInfo = {
-    chainId: "union-testnet-8",
-    chainName: "uniontestnet",
-    rest: "https://api.testnet.bonlulu.uno",
-    rpc: "https://rpc.testnet.bonlulu.uno",
-    bip44: {
-        coinType: 118,
+  chainId: "union-testnet-8",
+  chainName: "uniontestnet",
+  rest: "https://api.testnet.bonlulu.uno",
+  rpc: "https://rpc.testnet.bonlulu.uno",
+  bip44: {
+    coinType: 118,
+  },
+  bech32Config: {
+    bech32PrefixAccAddr: "union",
+    bech32PrefixAccPub: "unionpub",
+    bech32PrefixValAddr: "unionvaloper",
+    bech32PrefixValPub: "unionvaloperpub",
+    bech32PrefixConsAddr: "unionvalcons",
+    bech32PrefixConsPub: "unionvalconspub",
+  },
+  currencies: [
+    {
+      coinDenom: "UNO",
+      coinMinimalDenom: "muno",
+      coinDecimals: 6,
+      coinGeckoId: "cosmos",
     },
-    bech32Config: {
-        bech32PrefixAccAddr: "union",
-        bech32PrefixAccPub: "union" + "pub",
-        bech32PrefixValAddr: "union" + "valoper",
-        bech32PrefixValPub: "union" + "valoperpub",
-        bech32PrefixConsAddr: "union" + "valcons",
-        bech32PrefixConsPub: "union" + "valconspub",
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: "UNO",
+      coinMinimalDenom: "muno",
+      coinDecimals: 6,
+      coinGeckoId: "union",
+      gasPriceStep: {
+        low: 0.0025,
+        average: 0.025,
+        high: 0.04,
+      },
     },
-    currencies: [
-        {
-            coinDenom: "UNO",
-            coinMinimalDenom: "muno",
-            coinDecimals: 6,
-            coinGeckoId: "cosmos",
-        },
-    ],
-    feeCurrencies: [
-        {
-            coinDenom: "UNO",
-            coinMinimalDenom: "muno",
-            coinDecimals: 6,
-            coinGeckoId: "union",
-            gasPriceStep: {
-                low: 0.0025,
-                average: 0.025,
-                high: 0.04,
-            },
-        },
-    ],
-    stakeCurrency: {
-        coinDenom: "UNO",
-        coinMinimalDenom: "muno",
-        coinDecimals: 6,
-        coinGeckoId: "union",
-    },
+  ],
+  stakeCurrency: {
+    coinDenom: "UNO",
+    coinMinimalDenom: "muno",
+    coinDecimals: 6,
+    coinGeckoId: "union",
+  },
 }
 
 //todo handle this for mainnet
 export const leapChainInfo: LeapExtendedInfo = {
-    chainId: "union-testnet-8",
-    chainName: "uniontestnet",
-    rest: "https://api.testnet.bonlulu.uno",
-    rpc: "https://rpc.testnet.bonlulu.uno",
-    bip44: {
-        coinType: 118,
+  chainId: "union-testnet-8",
+  chainName: "uniontestnet",
+  rest: "https://api.testnet.bonlulu.uno",
+  rpc: "https://rpc.testnet.bonlulu.uno",
+  bip44: {
+    coinType: 118,
+  },
+  bech32Config: {
+    bech32PrefixAccAddr: "union",
+    bech32PrefixAccPub: "unionpub",
+    bech32PrefixValAddr: "unionvaloper",
+    bech32PrefixValPub: "unionvaloperpub",
+    bech32PrefixConsAddr: "unionvalcons",
+    bech32PrefixConsPub: "unionvalconspub",
+  },
+  currencies: [
+    {
+      coinDenom: "UNO",
+      coinMinimalDenom: "muno",
+      coinDecimals: 6,
+      coinGeckoId: "cosmos",
     },
-    bech32Config: {
-        bech32PrefixAccAddr: "union",
-        bech32PrefixAccPub: "union" + "pub",
-        bech32PrefixValAddr: "union" + "valoper",
-        bech32PrefixValPub: "union" + "valoperpub",
-        bech32PrefixConsAddr: "union" + "valcons",
-        bech32PrefixConsPub: "union" + "valconspub",
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: "UNO",
+      coinMinimalDenom: "muno",
+      coinDecimals: 6,
+      coinGeckoId: "union",
     },
-    currencies: [
-        {
-            coinDenom: "UNO",
-            coinMinimalDenom: "muno",
-            coinDecimals: 6,
-            coinGeckoId: "cosmos",
-        },
-    ],
-    feeCurrencies: [
-        {
-            coinDenom: "UNO",
-            coinMinimalDenom: "muno",
-            coinDecimals: 6,
-            coinGeckoId: "union",
-        },
-    ],
-    gasPriceStep: {
-        low: 0.0025,
-        average: 0.025,
-        high: 0.04,
-    },
-    stakeCurrency: {
-        coinDenom: "UNO",
-        coinMinimalDenom: "muno",
-        coinDecimals: 6,
-        coinGeckoId: "union",
-    },
-    theme: {
-        primaryColor: '#fff',
-        gradient:
-            'linear-gradient(180deg, rgba(255, 255, 255, 0.32) 0%, rgba(255, 255, 255, 0) 100%)',
-    },
-    image: "https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets/uniontestnet/images/union.png",
+  ],
+  gasPriceStep: {
+    low: 0.0025,
+    average: 0.025,
+    high: 0.04,
+  },
+  stakeCurrency: {
+    coinDenom: "UNO",
+    coinMinimalDenom: "muno",
+    coinDecimals: 6,
+    coinGeckoId: "union",
+  },
+  theme: {
+    primaryColor: '#fff',
+    gradient:
+      'linear-gradient(180deg, rgba(255, 255, 255, 0.32) 0%, rgba(255, 255, 255, 0) 100%)',
+  },
+  image: "https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets/uniontestnet/images/union.png",
 }
--- app/src/lib/wallet/cosmos/config.ts
+++ app/src/lib/wallet/cosmos/config.ts
@@ -1,7 +1,7 @@
-import { get } from "svelte/store"
-import { sleep } from "$lib/utilities/index.ts"
-import { persisted } from "svelte-persisted-store"
-import type { ChainWalletStore } from "$lib/wallet/types"
+import {get} from "svelte/store"
+import {sleep} from "$lib/utilities/index.ts"
+import {persisted} from "svelte-persisted-store"
+import type {ChainWalletStore} from "$lib/wallet/types"
 import {keplrChainInfo, leapChainInfo} from "$lib/wallet/cosmos/chain-info.ts";
 
 export const cosmosWalletsInformation = [
@@ -33,7 +33,7 @@ function createCosmosStore(
   }
 ) {
   console.log("[cosmosStore] previousState", previousState)
-  const { subscribe, set, update } = persisted("cosmos-store", previousState, {
+  const {subscribe, set, update} = persisted("cosmos-store", previousState, {
     syncTabs: true,
     storage: "session"
   })
@@ -41,56 +41,49 @@ function createCosmosStore(
     set,
     update,
     subscribe,
-      connect: async (walletId: string) => {
-          if (!walletId || (walletId !== "keplr" && walletId !== "leap")) return
-          update(v => ({...v, connectionStatus: "connecting", connectedWallet: walletId}))
-
-          const walletApi = window[walletId];
-
-          if (!walletApi) {
-              alert(`Please install ${walletId} wallet`)
-              return update(v => ({...v, connectionStatus: "disconnected"}))
-          }
-
-          const chainInfoMap = {
-              keplr: keplrChainInfo,
-              leap: leapChainInfo
-          };
-
-          const chainInfo = chainInfoMap[walletId];
-
-          if (!chainInfo) {
-              alert('Chain information is missing for the selected wallet.');
-              return update(v => ({ ...v, connectionStatus: "disconnected" }));
-          }
-
-          try {
-              await walletApi.experimentalSuggestChain(chainInfo);
-              await walletApi.enable(["union-testnet-8"])
-          } catch (e) {
-              return update(v => ({ ...v, connectionStatus: "disconnected" }));
-          }
-
-          const account = await walletApi.getKey("union-testnet-8")
-          update(v => ({
-              ...v,
-              connectionStatus: "connected",
-              address: account?.bech32Address,
-              connectedWallet: walletId
-          }))
-          await sleep(2_000)
-      },
+    connect: async (walletId: string) => {
+      if (!walletId || (walletId !== "keplr" && walletId !== "leap")) return
+      update(v => ({...v, connectionStatus: "connecting", connectedWallet: walletId}))
+      const walletApi = window[walletId];
+      if (!walletApi) {
+        alert(`Please install ${walletId} wallet`)
+        return update(v => ({...v, connectionStatus: "disconnected"}))
+      }
+      const chainInfoMap = {
+        keplr: keplrChainInfo,
+        leap: leapChainInfo
+      };
+      const chainInfo = chainInfoMap[walletId];
+      if (!chainInfo) {
+        alert('Chain information is missing for the selected wallet.');
+        return update(v => ({...v, connectionStatus: "disconnected"}));
+      }
+      try {
+        await walletApi.experimentalSuggestChain(chainInfo);
+        await walletApi.enable(["union-testnet-8"])
+      } catch (e) {
+        return update(v => ({...v, connectionStatus: "disconnected"}));
+      }
+      const account = await walletApi.getKey("union-testnet-8")
+      update(v => ({
+        ...v,
+        connectionStatus: "connected",
+        address: account?.bech32Address,
+        connectedWallet: walletId
+      }))
+      await sleep(2_000)
+    },
     disconnect: async () => {
-      const cosmosWalletId = get({ subscribe }).connectedWallet as CosmosWalletId
-      console.log({ cosmosWalletId })
+      const cosmosWalletId = get({subscribe}).connectedWallet as CosmosWalletId
+      console.log({cosmosWalletId})
       console.log("[cosmos] cosmosDisconnectClick", get(cosmosStore))
       if (cosmosWalletId && cosmosWalletId === "keplr" && window[cosmosWalletId]) {
         await window[cosmosWalletId]?.disable("union-testnet-8")
-        update(v => ({ ...v, connectedWallet: "none", connectionStatus: "disconnected" }))
+        update(v => ({...v, connectedWallet: "none", connectionStatus: "disconnected"}))
       }
       if (cosmosWalletId && cosmosWalletId === "leap" && window[cosmosWalletId]) {
         await window[cosmosWalletId]?.disconnect("union-testnet-8")
-        update(v => ({ ...v, connectedWallet: "none", connectionStatus: "disconnected" }))
+        update(v => ({...v, connectedWallet: "none", connectionStatus: "disconnected"}))
       }
     }
   }
