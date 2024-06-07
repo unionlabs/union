cce6062d7dabb89d3562489ddb14f9aa9cc50861
omar <omaraziz.dev@proton.me>
2024-06-07 01:55:35 -0700

    fix: fmt and spellcheck

--- app/src/lib/wallet/cosmos/chain-info.ts
+++ app/src/lib/wallet/cosmos/chain-info.ts
@@ -1,13 +1,13 @@
-import type {ChainInfo as KeplrChainInfo} from "@keplr-wallet/types";
-import type {ChainInfo as LeapChainInfo} from "@leapwallet/types";
+import type { ChainInfo as KeplrChainInfo } from "@keplr-wallet/types"
+import type { ChainInfo as LeapChainInfo } from "@leapwallet/types"
 
 //This exist according to docs
 interface LeapExtendedInfo extends LeapChainInfo {
   theme: {
-    primaryColor: string;
-    gradient: string;
-  };
-  image: string;
+    primaryColor: string
+    gradient: string
+  }
+  image: string
 }
 
 //todo handle this for main-net
@@ -17,7 +17,7 @@ export const keplrChainInfo: KeplrChainInfo = {
   rest: "https://api.testnet.bonlulu.uno",
   rpc: "https://rpc.testnet.bonlulu.uno",
   bip44: {
-    coinType: 118,
+    coinType: 118
   },
   bech32Config: {
     bech32PrefixAccAddr: "union",
@@ -25,15 +25,15 @@ export const keplrChainInfo: KeplrChainInfo = {
     bech32PrefixValAddr: "unionvaloper",
     bech32PrefixValPub: "unionvaloperpub",
     bech32PrefixConsAddr: "unionvalcons",
-    bech32PrefixConsPub: "unionvalconspub",
+    bech32PrefixConsPub: "unionvalconspub"
   },
   currencies: [
     {
       coinDenom: "UNO",
       coinMinimalDenom: "muno",
       coinDecimals: 6,
-      coinGeckoId: "cosmos",
-    },
+      coinGeckoId: "cosmos"
+    }
   ],
   feeCurrencies: [
     {
@@ -44,16 +44,16 @@ export const keplrChainInfo: KeplrChainInfo = {
       gasPriceStep: {
         low: 0.0025,
         average: 0.025,
-        high: 0.04,
-      },
-    },
+        high: 0.04
+      }
+    }
   ],
   stakeCurrency: {
     coinDenom: "UNO",
     coinMinimalDenom: "muno",
     coinDecimals: 6,
-    coinGeckoId: "union",
-  },
+    coinGeckoId: "union"
+  }
 }
 
 //todo handle this for main-net
@@ -63,7 +63,7 @@ export const leapChainInfo: LeapExtendedInfo = {
   rest: "https://api.testnet.bonlulu.uno",
   rpc: "https://rpc.testnet.bonlulu.uno",
   bip44: {
-    coinType: 118,
+    coinType: 118
   },
   bech32Config: {
     bech32PrefixAccAddr: "union",
@@ -71,39 +71,39 @@ export const leapChainInfo: LeapExtendedInfo = {
     bech32PrefixValAddr: "unionvaloper",
     bech32PrefixValPub: "unionvaloperpub",
     bech32PrefixConsAddr: "unionvalcons",
-    bech32PrefixConsPub: "unionvalconspub",
+    bech32PrefixConsPub: "unionvalconspub"
   },
   currencies: [
     {
       coinDenom: "UNO",
       coinMinimalDenom: "muno",
       coinDecimals: 6,
-      coinGeckoId: "cosmos",
-    },
+      coinGeckoId: "cosmos"
+    }
   ],
   feeCurrencies: [
     {
       coinDenom: "UNO",
       coinMinimalDenom: "muno",
       coinDecimals: 6,
-      coinGeckoId: "union",
-    },
+      coinGeckoId: "union"
+    }
   ],
   gasPriceStep: {
     low: 0.0025,
     average: 0.025,
-    high: 0.04,
+    high: 0.04
   },
   stakeCurrency: {
     coinDenom: "UNO",
     coinMinimalDenom: "muno",
     coinDecimals: 6,
-    coinGeckoId: "union",
+    coinGeckoId: "union"
   },
   theme: {
-    primaryColor: '#fff',
-    gradient:
-      'linear-gradient(180deg, rgba(255, 255, 255, 0.32) 0%, rgba(255, 255, 255, 0) 100%)',
+    primaryColor: "#fff",
+    gradient: "linear-gradient(180deg, rgba(255, 255, 255, 0.32) 0%, rgba(255, 255, 255, 0) 100%)"
   },
-  image: "https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets/uniontestnet/images/union.png",
+  image:
+    "https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets/uniontestnet/images/union.png"
 }
