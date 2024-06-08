57fe68146b6958ac3f72dbf1592da0b05bdbe277
Lukas <Lukas.nyberg@gmail.com>
2024-06-08 17:30:14 +0200

    chore(app): remove old schemas

--- app/src/routes/faucet/schema.ts
+++ app/src/routes/faucet/schema.ts
@@ -1,12 +1,3 @@
-import { string, regex, object, pipe, type InferOutput } from "valibot"
-
 export const unionAddressRegex = /^union[a-z0-9]{39}$/
-
-export const faucetFormSchema = object({
-  address: pipe(string(), regex(unionAddressRegex, "Invalid Union address"))
-})
-
-export type FaucetSchema = typeof faucetFormSchema
-export type FaucetForm = InferOutput<typeof faucetFormSchema>
 
 export type Message = { status: "error" | "success" | "warning"; text: string }
