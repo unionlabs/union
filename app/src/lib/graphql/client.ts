8ba221559f2b17ff718f5d678d3128b14822ecd3
Lukas <Lukas.nyberg@gmail.com>
2024-06-09 00:29:38 +0200

    fix(app): save progress

---
+++ app/src/lib/graphql/client.ts
@@ -0,0 +1,29 @@
+import { MutationCache, QueryClient } from "@tanstack/svelte-query"
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
+import { browser } from "$app/environment"
+
+export function createQueryClient() {
+  const queryClient: QueryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        enabled: browser,
+        gcTime: 1000 * 60 * 60 * 24,
+        refetchOnReconnect: () => !queryClient.isMutating()
+      }
+    },
+    mutationCache: new MutationCache({
+      onSettled: () => {
+        if (queryClient.isMutating() === 1) {
+          return queryClient.invalidateQueries()
+        }
+      }
+    })
+  })
+
+  const localStoragePersister = createSyncStoragePersister({
+    key: "SVELTE_QUERY",
+    storage: typeof window !== "undefined" ? window.localStorage : undefined // Use local storage if in browser
+  })
+
+  return { queryClient, localStoragePersister }
+}
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,71 +1,53 @@
 <script lang="ts">
-import "$lib/polyfill.ts"
-import "$styles/index.css"
-import { onMount } from "svelte"
-import { ModeWatcher } from "mode-watcher"
-import { browser } from "$app/environment"
-import { onNavigate } from "$app/navigation"
-import { page, navigating } from "$app/stores"
-import { shortcut } from "@svelte-put/shortcut"
-import { cosmosStore } from "$lib/wallet/cosmos"
-import Footer from "$lib/components/footer.svelte"
-import { Toaster } from "$lib/components/ui/sonner"
-import Header from "$lib/components/header/header.svelte"
-import { updateTheme } from "$lib/utilities/update-theme.ts"
-import OnlineStatus from "$lib/components/online-status.svelte"
-import { partytownSnippet } from "@builder.io/partytown/integration"
-import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools"
-import PreloadingIndicator from "$lib/components/preloading-indicator.svelte"
-import { QueryClient, MutationCache, notifyManager } from "@tanstack/svelte-query"
-import { PersistQueryClientProvider } from "@tanstack/svelte-query-persist-client"
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
+  import "$lib/polyfill.ts"
+  import "$styles/index.css"
+  import { onMount } from "svelte"
+  import { ModeWatcher } from "mode-watcher"
+  import { browser } from "$app/environment"
+  import { onNavigate } from "$app/navigation"
+  import { page, navigating } from "$app/stores"
+  import { shortcut } from "@svelte-put/shortcut"
+  import { cosmosStore } from "$lib/wallet/cosmos"
+  import Footer from "$lib/components/footer.svelte"
+  import { Toaster } from "$lib/components/ui/sonner"
+  import Header from "$lib/components/header/header.svelte"
+  import { updateTheme } from "$lib/utilities/update-theme.ts"
+  import OnlineStatus from "$lib/components/online-status.svelte"
+  import { partytownSnippet } from "@builder.io/partytown/integration"
+  import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools"
+  import PreloadingIndicator from "$lib/components/preloading-indicator.svelte"
+  import { notifyManager } from "@tanstack/svelte-query"
+  import { PersistQueryClientProvider } from "@tanstack/svelte-query-persist-client"
+  import { createQueryClient } from "$lib/graphql/client.ts";
 
-if (browser) notifyManager.setScheduler(window.requestAnimationFrame)
+  const { queryClient, localStoragePersister } = createQueryClient();
+  if (browser) notifyManager.setScheduler(window.requestAnimationFrame)
 
-$: updateTheme({ path: $page.url.pathname, activeTheme: "dark" })
+  $: updateTheme({ path: $page.url.pathname, activeTheme: "dark" })
 
-onMount(() => {
-  /* fix for iOS Safari viewport zooming on input focus */
-  if (navigator.userAgent.indexOf("iPhone") === -1) return
-  const metaElement = document.querySelector("meta[name=viewport]")
-  if (!metaElement) return
-  metaElement.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1")
-})
-
-onMount(() => {
-  const lastConnectedWallet = $cosmosStore["connectedWallet"] as "leap" | "keplr"
-  if (
-    lastConnectedWallet &&
-    window[lastConnectedWallet] &&
-    ["leap", "keplr"].includes(lastConnectedWallet)
-  )
-    return cosmosStore.connect(lastConnectedWallet)
-
-  if (window?.keplr) cosmosStore.connect("keplr")
-  else if (window?.leap) cosmosStore.connect("leap")
-})
-
-const queryClient: QueryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      enabled: browser,
-      gcTime: 1_000 * 60 * 60 * 24, // 24 hours
-      refetchOnReconnect: () => !queryClient.isMutating()
-    }
-  },
-  mutationCache: new MutationCache({
-    onSettled: () => {
-      if (queryClient.isMutating() === 1) {
-        return queryClient.invalidateQueries()
-      }
-    }
+  onMount(() => {
+    /* fix for iOS Safari viewport zooming on input focus */
+    if (navigator.userAgent.indexOf("iPhone") === -1) return
+    const metaElement = document.querySelector("meta[name=viewport]")
+    if (!metaElement) return
+    metaElement.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1")
   })
-})
 
-const localStoragePersister = createSyncStoragePersister({
-  key: "SVELTE_QUERY",
-  storage: browser ? window.localStorage : undefined // or window.sessionStorage
-})
+  onMount(() => {
+    const lastConnectedWallet = $cosmosStore["connectedWallet"] as "leap" | "keplr"
+    if (
+      lastConnectedWallet &&
+      window[lastConnectedWallet] &&
+      ["leap", "keplr"].includes(lastConnectedWallet)
+    )
+      return cosmosStore.connect(lastConnectedWallet)
+
+    if (window?.keplr) cosmosStore.connect("keplr")
+    else if (window?.leap) cosmosStore.connect("leap")
+  })
+
+  onNavigate(navigation => console.info("Navigating to", navigation.to?.route.id))
+
 </script>
 
 <svelte:head>
