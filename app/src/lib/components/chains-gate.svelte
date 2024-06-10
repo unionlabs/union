793a016c79a86d3f90d017cf43ae1b9b611f108f
cor <cor@pruijs.dev>
2024-06-10 12:29:45 +0200

    feat(app): add chains-gate

---
+++ app/src/lib/components/chains-gate.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+import { chainsQuery } from "$lib/queries/chains";
+import { type Readable, derived } from "svelte/store";
+let chains = chainsQuery();
+
+type Chain = {
+  chain_id: string,
+  display_name: string,
+  rpc_type: "evm" | "cosmos",
+  rpcs: Array<{
+    type: string,
+    url: string
+  }>,
+  addr_prefix: string
+};
+const EMPTY_CHAINS: Array<Chain> = [];
+
+let checkedChains: Readable<Array<Chain>> = derived(chains, ($chains) => {
+  // this will never happen, but is needed to satisy svelte's prop type checker
+  if (!$chains.isSuccess) return EMPTY_CHAINS;
+
+  return $chains.data.map((chain) => {
+    let display_name = "";
+
+    if (chain.display_name === null) {
+      console.error("no display_name for chain", chain); 
+    } else {
+      display_name = chain.display_name;
+    }
+
+    let rpc_type: "evm" | "cosmos" = "cosmos";
+    if (chain.rpc_type !== "evm" && chain.rpc_type !== "cosmos") {
+      console.error("invalid rpc type for chain", chain); 
+    } else {
+      rpc_type = chain.rpc_type;
+    }
+
+    let addr_prefix = "";
+    if (chain.addr_prefix === null) {
+      console.error("no addr_prefix for chain", chain); 
+    } else {
+      addr_prefix = chain.addr_prefix;
+    }
+
+    return {
+      chain_id: chain.chain_id,
+      display_name,
+      rpc_type,
+      rpcs: chain.rpcs,
+      addr_prefix
+    };
+  });
+
+});
+</script>
+
+{#if $chains.isLoading}
+  Loading chains...
+{:else if $chains.isSuccess}
+  <slot chains={$checkedChains}/>
+{/if}
