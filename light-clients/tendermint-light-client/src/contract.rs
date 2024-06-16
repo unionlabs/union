a079eba72e43701667045414fa1331c329aeb1bc
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-06-16 09:25:24 -0500

    fix: convert Binary to Vec

--- light-clients/tendermint-light-client/src/contract.rs
+++ light-clients/tendermint-light-client/src/contract.rs
@@ -30,15 +30,15 @@ pub fn instantiate(
     save_proto_consensus_state::<TendermintLightClient>(
         deps.branch(),
         ProtoConsensusState {
-            data: msg.consensus_state,
+            data: msg.consensus_state.to_vec(),
         },
         &client_state.latest_height,
     );
     save_proto_client_state::<TendermintLightClient>(
         deps,
         ProtoClientState {
-            data: msg.client_state,
-            checksum: msg.checksum,
+            data: msg.client_state.to_vec(),
+            checksum: msg.checksum.to_vec(),
             latest_height: Some(client_state.latest_height.into()),
         },
     );
