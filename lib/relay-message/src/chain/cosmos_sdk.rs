ffbd33a766bce441e65611142e14ff919eda952d
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-06-16 08:30:16 -0500

    fix: post rebase cleanup

--- lib/relay-message/src/chain/cosmos_sdk.rs
+++ lib/relay-message/src/chain/cosmos_sdk.rs
@@ -1,8 +1,7 @@
 use std::{fmt::Debug, marker::PhantomData};
 
 use chain_utils::cosmos_sdk::{BroadcastTxCommitError, CosmosSdkChain, CosmosSdkChainExt};
-use prost::Message;
-use queue_msg::{data, fetch, seq, wait, Op, QueueMsg};
+use queue_msg::{data, fetch, seq, wait, Op};
 use tracing::{debug, info};
 use unionlabs::{
     encoding::{Decode, DecodeAs, Encode, Proto},
