c244620ee67f8548b5aaeb809c42776070fba484
cor <cor@pruijs.dev>
2024-06-16 21:53:00 +0200

    feat(app): toaster style

--- app/src/lib/components/ui/sonner/sonner.svelte
+++ app/src/lib/components/ui/sonner/sonner.svelte
@@ -10,7 +10,7 @@ type $$Props = SonnerProps
 	class="toaster group"
 	toastOptions={{
 		classes: {
-			toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+			toast: "group toast rounded-none group-[.toaster]:bg-card group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
 			description: "group-[.toast]:text-muted-foreground",
 			actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
 			cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
