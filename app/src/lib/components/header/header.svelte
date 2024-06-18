f3893b0a24681b082a00e532832f8d3e80218645
cor <cor@pruijs.dev>
2024-06-18 16:52:08 +0200

    feat(app): style improvement

--- app/src/lib/components/header/header.svelte
+++ app/src/lib/components/header/header.svelte
@@ -23,7 +23,7 @@ import { routes } from "$lib/components/navigation/index.ts"
       alt="Union Logo"
       class="size-full max-w-32 h-12 select-none invert dark:invert-0"
     />
-    <Badge variant="default" class="tracking-widest mb-0.5 ml-1">testnet</Badge>
+    <Badge variant="default" class="font-condensed mb-0.5 ml-1 uppercase">Testnet</Badge>
   </Button>
   <nav class="hidden md:flex items-center justify-end space-x-0 sm:space-x-2 w-full">
     {#each Object.entries(routes) as [name, { draft, path }], index (name)}
