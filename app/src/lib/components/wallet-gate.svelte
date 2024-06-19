f73f77443677babbbfc642d8505ca78c980955be
Lukas <Lukas.nyberg@gmail.com>
2024-06-19 14:48:25 +0200

    feat(app): add wallet slot

--- app/src/lib/components/wallet-gate.svelte
+++ app/src/lib/components/wallet-gate.svelte
@@ -54,10 +54,13 @@ let confirmedUserAddr: Readable<UserAddresses> = derived(userAddr, $userAddr => 
 })
 </script>
 
-
-{#if !$userAddr }
-  <div>Connect your wallets to continue.</div>
-{:else if $userAddr}
+{#if $userAddr}
   <slot userAddr={$confirmedUserAddr} />
+{:else}
+  <slot name="disconnected">
+    <span>Connect your wallets to continue</span>
+  </slot>
 {/if}
+
+
 
