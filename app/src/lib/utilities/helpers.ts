4934a6728a96d474c43ce7250352fbac57137a5b
Lukas <Lukas.nyberg@gmail.com>
2024-06-20 14:09:15 +0200

    chore(app): rename

--- app/src/lib/utilities/helpers.ts
+++ app/src/lib/utilities/helpers.ts
@@ -1,9 +1,9 @@
 import type { Chain } from "$lib/types.ts"
 
-export const findAsset = (chain: Chain, denom: string) => {
-  for (const asset of chain.assets) {
-    if (asset.denom === denom) {
-      return asset
+export const findAsset = (chain: Chain, denom: any) => {
+  for (const supportedAsset of chain.assets) {
+    if (supportedAsset.denom === denom) {
+      return supportedAsset
     }
   }
   return undefined
