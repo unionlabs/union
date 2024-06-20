2875071610621b6efc637b0b72add3261bda8fd9
cor <cor@pruijs.dev>
2024-06-20 21:39:50 +0200

    feat(app): title on mobile explorer overview

--- app/src/routes/explorer/+layout.svelte
+++ app/src/routes/explorer/+layout.svelte
@@ -76,8 +76,11 @@ $: mainExplorerPage = $page.route.id?.split("/").length <= 3
 
 
 <!-- mobile layout !-->
-<div class="flex flex-row divide-x overflow-x-none max-w-full w-full">
+<div class="flex flex-row sm:divide-x overflow-x-none max-w-full w-full">
   <nav class={cn("bg-muted h-full overflow-y-auto", explorerRoute === null ? "flex-1" : "hidden sm:block")}>
+    <h2 class="sm:hidden ml-4 mt-6 mb-3 text-2xl font-bold uppercase font-supermolot">
+      Explorer
+    </h2>
     <Menu tableRoutes={data.tables} isCollapsed={false} />
   </nav>
   <main class={cn("overflow-auto flex-1 w-full", explorerRoute === null ? "hidden sm:block" : "")}>
@@ -89,70 +92,14 @@ $: mainExplorerPage = $page.route.id?.split("/").length <= 3
       <span class="uppercase">{$page.route.id?.split('/').at(-2)}</span>
     </a>
 
-    <slot/>
+    <div class="p-2 pt-0 sm:p-6">
+      <div class={cn($page.route.id?.split('/').length !== 2 ? "" : "hidden")}>
+        <h2 class="text-4xl font-extrabold font-expanded sm:!font-extra-expanded uppercase font-supermolot">
+          {explorerRoute?.replaceAll('-', ' ')}
+        </h2>
+        <p class="pb-4 -mt-1 text-muted-foreground">{'>'} {explorerPageDescription}</p>
+      </div>
+      <slot />
+    </div>
   </main>
 </div>
-
-
-<!-- desktop layout !-->
-<!--
-<main class={cn('hidden sm:flex flex-1 overflow-hidden', mainExplorerPage ? 'flex-row' : 'flex-col')}>
-  {#if mainExplorerPage}
-    <Resizable.PaneGroup
-      class="w-full"
-      autoSaveId="explorer"
-      direction="horizontal"
-      {onLayoutChange}
-    >
-      <Resizable.Pane
-        {onExpand}
-        {onCollapse}
-        maxSize={14}
-        minSize={14}
-        collapsible={true}
-        collapsedSize={4.5}
-        bind:pane={leftPane}
-        defaultSize={leftSize}
-        class={cn(
-          isCollapsed ? 'min-w-13 max-w-13' : 'min-w-[200px] w-[250px]',
-          'w-full border-r bg-muted',
-        )}
-      >
-        <Menu tableRoutes={data.tables} {isCollapsed} />
-      </Resizable.Pane>
-      <Resizable.Handle
-        withHandle
-        class="relative flex w-4 max-w-4 items-center justify-center bg-background"
-      >
-        <div class="h-full w-12 items-center justify-center rounded-sm border bg-muted">
-          <GripVerticalIcon />
-        </div>
-      </Resizable.Handle>
-      <Resizable.Pane defaultSize={rightSize} class="rounded-lg p-0">
-        <ScrollArea orientation="both" class="size-full flex-1">
-          <div class="py-6 pr-4 pl-2">
-            <h2 class="text-4xl font-extrabold font-extra-expanded uppercase font-supermolot">
-              {explorerRoute?.replaceAll('-', ' ')}
-            </h2>
-            <p class="pb-4 text-muted-foreground">{'>'} {explorerPageDescription}</p>
-            <slot />
-          </div>
-        </ScrollArea>
-      </Resizable.Pane>
-    </Resizable.PaneGroup>
-  {:else}
-    <a
-      class="font-bold font- text-lg p-4 flex flex-row gap-2 items-center font-supermolot"
-      href={$page.route.id?.split('/').slice(0, 3).join('/')}
-    >
-      <ArrowLeftIcon />
-      <span class="uppercase">{$page.route.id?.split('/').at(2)}</span>
-    </a>
-    <ScrollArea class="flex-1" orientation="both">
-      <div class="p-4 sm:p-6 flex items-center justify-center">
-        <slot />
-      </div>
-    </ScrollArea>
-  {/if}
-</main>
-!-->
