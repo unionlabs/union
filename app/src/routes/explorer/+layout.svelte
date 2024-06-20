09678f0f928eab774f80010f800149e168e1c036
cor <cor@pruijs.dev>
2024-06-20 22:03:21 +0200

    feat(app): mobile explorer alignment

--- app/src/routes/explorer/+layout.svelte
+++ app/src/routes/explorer/+layout.svelte
@@ -79,7 +79,7 @@ $: mainExplorerPage = $page.route.id?.split("/").length <= 3
 <!-- mobile layout !-->
 <div class="flex flex-row sm:divide-x overflow-x-none max-w-full w-full">
   <nav class={cn("sm:bg-muted h-full overflow-y-auto", explorerRoute === null ? "flex-1 sm:flex-none" : "hidden sm:block")}>
-    <h2 class="sm:hidden ml-4 mt-6 mb-3 text-2xl font-bold uppercase font-supermolot">
+    <h2 class="sm:hidden ml-3 mt-6 mb-3 text-2xl font-bold font-supermolot">
       Explorer
     </h2>
     <Menu tableRoutes={data.tables} isCollapsed={false} />
