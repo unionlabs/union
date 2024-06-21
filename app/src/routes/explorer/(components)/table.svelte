1852ce29b6e79a906b9d4388b08d9d832ace0266
omar <omaraziz.dev@proton.me>
2024-06-20 19:52:53 -0700

    feat: table filters

--- app/src/routes/explorer/(components)/table.svelte
+++ app/src/routes/explorer/(components)/table.svelte
@@ -1,76 +1,101 @@
-<script lang="ts" generics="T extends object" >
-import { derived, get } from "svelte/store"
-import {
-  flexRender,
-  type ColumnDef,
-  getCoreRowModel,
-  type TableOptions,
-  createSvelteTable,
-  getFilteredRowModel,
-  getPaginationRowModel
-} from "@tanstack/svelte-table"
-import { writable, type Readable } from "svelte/store"
-import { cn } from "$lib/utilities/shadcn.ts"
-import * as Table from "$lib/components/ui/table"
-import { createVirtualizer } from "@tanstack/svelte-virtual"
-import * as Card from "$lib/components/ui/card/index.ts"
+<script lang="ts">
+  import {
+    flexRender,
+    type FilterFn,
+    type ColumnDef,
+    getCoreRowModel,
+    type TableOptions,
+    createSvelteTable,
+    getFilteredRowModel,
+    getPaginationRowModel,
+  } from '@tanstack/svelte-table'
+  import { derived } from 'svelte/store'
+  import type { MaybePromise } from 'valibot'
+  import { cn } from '$lib/utilities/shadcn.ts'
+  import * as Table from '$lib/components/ui/table'
+  import { writable, type Readable } from 'svelte/store'
+  import * as Card from '$lib/components/ui/card/index.ts'
+  import Input from '$lib/components/ui/input/input.svelte'
+  import { createVirtualizer } from '@tanstack/svelte-virtual'
+  import type { FormInputEvent } from '$lib/components/ui/input'
 
-export let columns: Array<ColumnDef<any>>
-// https://github.com/TanStack/table/issues/4241
-// @ts-ignore
-export let dataStore: Readable<Array<any>>
-export let onClick: ((tr: unknown) => void) | undefined = undefined
+  type DataRow = $$Generic
 
-const options = writable<TableOptions<any>>({
-  data: $dataStore,
-  enableHiding: true,
-  enableFilters: true,
-  // https://github.com/TanStack/table/issues/4241
-  // @ts-ignore
-  columns,
-  autoResetPageIndex: true,
-  enableColumnFilters: true,
-  enableColumnResizing: true,
-  enableMultiRowSelection: true,
-  getCoreRowModel: getCoreRowModel(),
-  getFilteredRowModel: getFilteredRowModel(),
-  getPaginationRowModel: getPaginationRowModel()
-})
+  export let enableFiltering = false
+  export let tableName: string | undefined = undefined
+  export let globalFilter: string | undefined = undefined
+  export let fuzzyFilter: FilterFn<DataRow> | undefined = undefined
+  export let columns: Array<ColumnDef<DataRow>>
+  export let dataStore: Readable<Array<DataRow>>
+  export let onClick: undefined | ((row: DataRow) => MaybePromise<void>) = undefined
 
-let virtualListElement: HTMLDivElement
+  const options = writable<TableOptions<DataRow>>({
+    columns,
+    data: $dataStore,
+    enableHiding: true,
+    enableFilters: true,
+    autoResetPageIndex: true,
+    enableColumnFilters: true,
+    enableColumnResizing: true,
+    enableMultiRowSelection: true,
+    enableGlobalFilter: !!globalFilter,
+    globalFilterFn: fuzzyFilter,
+    filterFns: fuzzyFilter ? { fuzzy: fuzzyFilter } : undefined,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  })
 
-const table = createSvelteTable(options)
-const rows = derived(table, $t => $t.getRowModel().rows)
+  let virtualListElement: HTMLDivElement
 
-$: virtualizer = createVirtualizer<HTMLDivElement, HTMLTableRowElement>({
-  overscan: 20,
-  count: $rows.length,
-  estimateSize: () => 34,
-  getScrollElement: () => virtualListElement
-})
+  const table = createSvelteTable(options)
+  const rows = derived(table, $t => $t.getRowModel().rows)
 
-$: dataStore.subscribe(() => {
-  if (!$dataStore) return
-  $table.setPageSize($dataStore.length)
-  options.update(options => ({ ...options, data: $dataStore as unknown as Array<T> }))
-})
+  const handleKeyUp = (event: FormInputEvent<KeyboardEvent>) => {
+    // @ts-expect-error
+    $table.setGlobalFilter(String(event?.target['value']))
+  }
+
+  $: virtualizer = createVirtualizer<HTMLDivElement, HTMLTableRowElement>({
+    overscan: 20,
+    count: $rows.length,
+    estimateSize: () => 34,
+    getScrollElement: () => virtualListElement,
+  })
+
+  $: dataStore.subscribe(() => {
+    if (!$dataStore) return
+    $table.setPageSize($dataStore.length)
+    options.update(options => ({ ...options, data: $dataStore as unknown as Array<DataRow> }))
+  })
 </script>
 
+{#if enableFiltering}
+  <Input
+    type="text"
+    on:keyup={handleKeyUp}
+    bind:value={globalFilter}
+    placeholder={`Search ${tableName || 'for any column'}`}
+    class={cn('border w-full py-1 px-2.5', 'focus:outline-none focus-visible:ring-0')}
+  />
+{/if}
 <Card.Root>
-  <div bind:this={virtualListElement} >
+  <div bind:this={virtualListElement}>
     <Table.Root>
       <Table.Header>
         {#each $table.getHeaderGroups() as headerGroup (headerGroup.id)}
           <Table.Row>
             {#each headerGroup.headers as header (header.id)}
-              <Table.Head
-                colspan={header.colSpan}
-                class={cn(`w-[${header.getSize()}px] whitespace-nowrap`)}
-              >
+              {#if !header.id.endsWith('hidden')}
+                <Table.Head
+                  colspan={header.colSpan}
+                  class={cn(`w-[${header.getSize()}px] whitespace-nowrap`)}
+                >
                   <svelte:component
                     this={flexRender(header.column.columnDef.header, header.getContext())}
                   />
-              </Table.Head>
+                </Table.Head>
+              {/if}
             {/each}
           </Table.Row>
         {/each}
@@ -78,17 +103,20 @@ $: dataStore.subscribe(() => {
       <Table.Body class={cn(`h-[${$virtualizer.getTotalSize()}px]] whitespace-nowrap`)}>
         {#each $virtualizer.getVirtualItems() as row, index (row.index)}
           <Table.Row
-            class={cn(onClick !== undefined ? 'cursor-pointer' : '',
+            class={cn(
+              onClick !== undefined ? 'cursor-pointer' : '',
               index % 2 === 0 ? 'bg-secondary/10' : 'bg-transparent',
             )}
-             on:click={onClick !== undefined ? (() => onClick($rows[row.index].original)) : undefined}
+            on:click={onClick !== undefined ? () => onClick($rows[row.index].original) : undefined}
           >
             {#each $rows[row.index].getVisibleCells() as cell, index (cell.id)}
-              <Table.Cell>
-                <svelte:component
-                  this={flexRender(cell.column.columnDef.cell, cell.getContext())}
-                />
-              </Table.Cell>
+              {#if !cell.id.endsWith('hidden')}
+                <Table.Cell>
+                  <svelte:component
+                    this={flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  />
+                </Table.Cell>
+              {/if}
             {/each}
           </Table.Row>
         {/each}
--- app/src/routes/explorer/channels/+page.svelte
+++ app/src/routes/explorer/channels/+page.svelte
@@ -1,75 +1,104 @@
 <script lang="ts">
-import request from "graphql-request"
-import { channelsQuery } from "$lib/graphql/documents/channels.ts"
-import { createQuery } from "@tanstack/svelte-query"
-import { URLS } from "$lib/constants"
-import Table from "../(components)/table.svelte"
-import { flexRender, type ColumnDef } from "@tanstack/svelte-table"
-import { writable } from "svelte/store"
-import CellStatus from "../(components)/cell-status.svelte"
+  import request from 'graphql-request'
+  import { URLS } from '$lib/constants'
+  import { writable } from 'svelte/store'
+  import Table from '../(components)/table.svelte'
+  import { createQuery } from '@tanstack/svelte-query'
+  import { rankItem } from '@tanstack/match-sorter-utils'
+  import CellStatus from '../(components)/cell-status.svelte'
+  import { channelsQuery } from '$lib/graphql/documents/channels.ts'
+  import { flexRender, type ColumnDef, type FilterFn } from '@tanstack/svelte-table'
 
-$: channels = createQuery({
-  queryKey: ["channels"],
-  refetchInterval: 5_000,
-  queryFn: async () => request(URLS.GRAPHQL, channelsQuery, {})
-})
+  $: channels = createQuery({
+    queryKey: ['channels'],
+    refetchInterval: 5_000,
+    queryFn: async () => request(URLS.GRAPHQL, channelsQuery, {}),
+  })
 
-$: channelsData = $channels?.data?.v0_channel_map ?? []
+  $: channelsData = $channels?.data?.v0_channel_map ?? []
 
-type Channel = (typeof channelsData)[number]
+  type DataRow = (typeof channelsData)[number]
 
-$: channelsStore = writable<Array<Channel>>(channelsData as Array<Channel>)
-$: if (channels) {
-  channelsStore.update(channels => channels)
-}
-
-const columns: Array<ColumnDef<{ chain_id: string }>> = [
-  {
-    accessorKey: "from_chain_id",
-    header: () => "From Chain ID",
-    size: 200,
-    cell: info => info.getValue()
-  },
-  {
-    accessorKey: "from_connection_id",
-    header: () => "From Connection ID",
-    size: 200,
-    cell: info => info.getValue()
-  },
-  {
-    accessorKey: "from_channel_id",
-    header: () => "From Channel ID",
-    size: 200,
-    cell: info => info.getValue()
-  },
-  {
-    accessorKey: "to_chain_id",
-    header: () => "To Chain ID",
-    size: 200,
-    cell: info => info.getValue()
-  },
-  {
-    accessorKey: "to_connection_id",
-    header: () => "To Connection ID",
-    size: 200,
-    cell: info => info.getValue()
-  },
-  {
-    accessorKey: "to_channel_id",
-    header: () => "To Channel ID",
-    size: 200,
-    cell: info => info.getValue()
-  },
-  {
-    accessorKey: "status",
-    header: () => "Status",
-    size: 200,
-    cell: info =>
-      flexRender(CellStatus, {
-        value: info.getValue()
-      })
+  $: channelsStore = writable<Array<DataRow>>(channelsData as Array<DataRow>)
+  $: if (channels) {
+    channelsStore.update(channels => channels)
   }
-]
+
+  let globalFilter = ''
+  const fuzzyFilter = ((row, columnId, value, addMeta) => {
+    const itemRank = rankItem(row.getValue(columnId), value)
+    addMeta({ itemRank })
+    return itemRank.passed
+  }) satisfies FilterFn<DataRow>
+
+  const columns: Array<ColumnDef<DataRow>> = [
+    {
+      size: 200,
+      accessorKey: 'from_chain_id',
+      header: () => 'From Chain ID',
+      filterFn: 'includesString',
+      accessorFn: row => row.from_chain_id,
+      cell: info => info.getValue(),
+    },
+    {
+      size: 200,
+      accessorKey: 'from_connection_id',
+      header: () => 'From Connection ID',
+      filterFn: 'includesString',
+      accessorFn: row => row.from_connection_id,
+      cell: info => info.getValue(),
+    },
+    {
+      size: 200,
+      accessorKey: 'from_channel_id',
+      header: () => 'From DataRow ID',
+      filterFn: 'includesString',
+      accessorFn: row => row.from_channel_id,
+      cell: info => info.getValue(),
+    },
+    {
+      size: 200,
+      accessorKey: 'to_chain_id',
+      header: () => 'To Chain ID',
+      filterFn: 'includesString',
+      accessorFn: row => row.to_chain_id,
+      cell: info => info.getValue(),
+    },
+    {
+      size: 200,
+      accessorKey: 'to_connection_id',
+      header: () => 'To Connection ID',
+      filterFn: 'includesString',
+      accessorFn: row => row.to_connection_id,
+      cell: info => info.getValue(),
+    },
+    {
+      size: 200,
+      accessorKey: 'to_channel_id',
+      header: () => 'To DataRow ID',
+      filterFn: 'includesString',
+      accessorFn: row => row.to_channel_id,
+      cell: info => info.getValue(),
+    },
+    {
+      size: 200,
+      accessorKey: 'status',
+      header: () => 'Status',
+      filterFn: 'includesString',
+      accessorFn: row => row.status,
+      cell: info =>
+        flexRender(CellStatus, {
+          value: info.getValue(),
+        }),
+    },
+  ]
 </script>
 
-<Table bind:dataStore={channelsStore} {columns} />
+<Table
+  {columns}
+  {fuzzyFilter}
+  {globalFilter}
+  tableName="Channels"
+  enableFiltering={true}
+  bind:dataStore={channelsStore}
+/>
