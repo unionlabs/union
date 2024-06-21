9a03a4430c2e85bdddf7ffc968a8bc09dfb5eff8
omar <omaraziz.dev@proton.me>
2024-06-20 23:33:48 -0700

    feat: style search bar

--- app/src/routes/explorer/(components)/table.svelte
+++ app/src/routes/explorer/(components)/table.svelte
@@ -1,86 +1,93 @@
 <script lang="ts">
-import {
-  flexRender,
-  type FilterFn,
-  type ColumnDef,
-  getCoreRowModel,
-  type TableOptions,
-  createSvelteTable,
-  getFilteredRowModel,
-  getPaginationRowModel
-} from "@tanstack/svelte-table"
-import { derived } from "svelte/store"
-import type { MaybePromise } from "valibot"
-import { cn } from "$lib/utilities/shadcn.ts"
-import * as Table from "$lib/components/ui/table"
-import { writable, type Readable } from "svelte/store"
-import * as Card from "$lib/components/ui/card/index.ts"
-import Input from "$lib/components/ui/input/input.svelte"
-import { createVirtualizer } from "@tanstack/svelte-virtual"
-import type { FormInputEvent } from "$lib/components/ui/input"
+  import {
+    flexRender,
+    type FilterFn,
+    type ColumnDef,
+    getCoreRowModel,
+    type TableOptions,
+    createSvelteTable,
+    getFilteredRowModel,
+    getPaginationRowModel,
+  } from '@tanstack/svelte-table'
+  import { derived } from 'svelte/store'
+  import type { MaybePromise } from 'valibot'
+  import { cn } from '$lib/utilities/shadcn.ts'
+  import Search from 'virtual:icons/lucide/search'
+  import * as Table from '$lib/components/ui/table'
+  import { writable, type Readable } from 'svelte/store'
+  import * as Card from '$lib/components/ui/card/index.ts'
+  import Input from '$lib/components/ui/input/input.svelte'
+  import { createVirtualizer } from '@tanstack/svelte-virtual'
+  import type { FormInputEvent } from '$lib/components/ui/input'
 
-type DataRow = $$Generic
+  type DataRow = $$Generic
 
-export let tableName: string | undefined = undefined
-export let globalFilter: string | undefined = undefined
-export let fuzzyFilter: FilterFn<DataRow> | undefined = undefined
-export let columns: Array<ColumnDef<DataRow>>
-export let dataStore: Readable<Array<DataRow>>
-export let onClick: undefined | ((row: DataRow) => MaybePromise<void>) = undefined
+  export let tableName: string | undefined = undefined
+  export let globalFilter: string | undefined = undefined
+  export let fuzzyFilter: FilterFn<DataRow> | undefined = undefined
+  export let columns: Array<ColumnDef<DataRow>>
+  export let dataStore: Readable<Array<DataRow>>
+  export let onClick: undefined | ((row: DataRow) => MaybePromise<void>) = undefined
 
-const options = writable<TableOptions<DataRow>>({
-  columns,
-  data: $dataStore,
-  enableHiding: true,
-  enableFilters: true,
-  autoResetPageIndex: true,
-  enableColumnFilters: true,
-  enableColumnResizing: true,
-  enableMultiRowSelection: true,
-  enableGlobalFilter: true,
-  globalFilterFn: fuzzyFilter,
-  filterFns: fuzzyFilter ? { fuzzy: fuzzyFilter } : undefined,
-  getCoreRowModel: getCoreRowModel(),
-  getFilteredRowModel: getFilteredRowModel(),
-  getPaginationRowModel: getPaginationRowModel()
-})
+  const options = writable<TableOptions<DataRow>>({
+    columns,
+    data: $dataStore,
+    enableHiding: true,
+    enableFilters: true,
+    autoResetPageIndex: true,
+    enableColumnFilters: true,
+    enableColumnResizing: true,
+    enableMultiRowSelection: true,
+    enableGlobalFilter: true,
+    globalFilterFn: fuzzyFilter,
+    filterFns: fuzzyFilter ? { fuzzy: fuzzyFilter } : undefined,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  })
 
-let virtualListElement: HTMLDivElement
+  let virtualListElement: HTMLDivElement
 
-const table = createSvelteTable(options)
-const rows = derived(table, $t => $t.getRowModel().rows)
+  const table = createSvelteTable(options)
+  const rows = derived(table, $t => $t.getRowModel().rows)
 
-const handleKeyUp = (event: FormInputEvent<KeyboardEvent>) => {
-  // @ts-expect-error
-  $table.setGlobalFilter(String(event?.target["value"]))
-}
+  const handleKeyUp = (event: FormInputEvent<KeyboardEvent>) => {
+    // @ts-expect-error
+    $table.setGlobalFilter(String(event?.target['value']))
+  }
 
-$: virtualizer = createVirtualizer<HTMLDivElement, HTMLTableRowElement>({
-  overscan: 20,
-  count: $rows.length,
-  estimateSize: () => 34,
-  getScrollElement: () => virtualListElement
-})
+  $: virtualizer = createVirtualizer<HTMLDivElement, HTMLTableRowElement>({
+    overscan: 20,
+    count: $rows.length,
+    estimateSize: () => 34,
+    getScrollElement: () => virtualListElement,
+  })
 
-$: dataStore.subscribe(() => {
-  if (!$dataStore) return
-  $table.setPageSize($dataStore.length)
-  options.update(options => ({ ...options, data: $dataStore as unknown as Array<DataRow> }))
-})
+  $: dataStore.subscribe(() => {
+    if (!$dataStore) return
+    $table.setPageSize($dataStore.length)
+    options.update(options => ({ ...options, data: $dataStore as unknown as Array<DataRow> }))
+  })
 </script>
 
-<Input
-  type="text"
-  autocorrect="off"
-  autocomplete="off"
-  spellcheck="false"
-  autocapitalize="off"
-  on:keyup={handleKeyUp}
-  bind:value={globalFilter}
-  placeholder={`Search ${tableName || 'for any column'}`}
-  class={cn('border w-full py-1 px-2.5', 'focus:outline-none focus-visible:ring-0')}
-/>
-
+<div class="relative w-full">
+  <Search class="absolute left-2.5 top-3 size-4 text-muted-foreground" />
+  <Input
+    type="text"
+    autocorrect="off"
+    autocomplete="off"
+    spellcheck="false"
+    autocapitalize="off"
+    on:keyup={handleKeyUp}
+    bind:value={globalFilter}
+    placeholder={`Search ${tableName || 'for any column'}`}
+    class={cn(
+      'bg-white/35',
+      'pl-8 pr-2.5 border border-b-transparent w-full py-1 outline-none ring-0 ring-offset-0 ring-offset-transparent',
+      'focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:outline-none focus:ring-offset-0 focus-visible:ring-offset-0',
+    )}
+  />
+</div>
 <Card.Root>
   <div bind:this={virtualListElement}>
     <Table.Root>
@@ -109,7 +116,9 @@ $: dataStore.subscribe(() => {
               onClick !== undefined ? 'cursor-pointer' : '',
               index % 2 === 0 ? 'bg-secondary/10' : 'bg-transparent',
             )}
-            on:click={onClick !== undefined ? () => onClick($rows[row.index].original) : undefined}
+            on:click|once={onClick !== undefined
+              ? () => onClick($rows[row.index].original)
+              : undefined}
           >
             {#each $rows[row.index].getVisibleCells() as cell, index (cell.id)}
               {#if !cell.id.endsWith('hidden')}
--- app/src/routes/explorer/+layout.svelte
+++ app/src/routes/explorer/+layout.svelte
@@ -1,60 +1,12 @@
 <script lang="ts">
-import { onMount } from "svelte"
 import { page } from "$app/stores"
 import { onNavigate } from "$app/navigation"
 import { cn } from "$lib/utilities/shadcn.ts"
 import type { LayoutData } from "./$types.ts"
 import Menu from "./(components)/menu.svelte"
-import * as Resizable from "$lib/components/ui/resizable"
 import ArrowLeftIcon from "virtual:icons/lucide/arrow-left"
-import GripVerticalIcon from "virtual:icons/tabler/grip-vertical"
-import { ScrollArea } from "$lib/components/ui/scroll-area/index.ts"
 
 export let data: LayoutData
-
-let windowSize = { width: window.innerWidth, height: window.innerHeight }
-
-const handleResize = () => {
-  requestAnimationFrame(() => {
-    windowSize = { width: window.innerWidth, height: window.innerHeight }
-  })
-}
-
-let isCollapsed = false
-let leftPane: Resizable.PaneAPI
-$: [leftSize, rightSize] = [14, 88]
-
-onMount(() => {
-  window.addEventListener("resize", handleResize)
-  return () => {
-    window.removeEventListener("resize", handleResize)
-  }
-})
-
-$: {
-  try {
-    if (windowSize?.width < 900) {
-      isCollapsed = true
-    } else {
-      isCollapsed = false
-    }
-    // biome-ignore lint/suspicious/noEmptyBlockStatements: <explanation>
-  } catch {}
-}
-
-const onLayoutChange: Resizable.PaneGroupProps["onLayoutChange"] = sizes => {
-  document.cookie = `PaneForge:layout=${JSON.stringify(sizes)}`
-}
-
-const onCollapse: Resizable.PaneProps["onExpand"] = () => {
-  isCollapsed = true
-  document.cookie = `PaneForge:collapsed=${true}`
-}
-
-const onExpand: Resizable.PaneProps["onExpand"] = () => {
-  isCollapsed = false
-  document.cookie = `PaneForge:collapsed=${false}`
-}
 
 let explorerRoute = $page.route.id?.split("/").at(2) ?? null
 $: explorerPageDescription =
@@ -65,16 +17,11 @@ onNavigate(navigation => {
     explorerRoute = navigation.to?.route.id?.split("/").at(2) ?? null
   }
 })
-
-// @ts-expect-error
-$: mainExplorerPage = $page.route.id?.split("/").length <= 3
 </script>
 
 <svelte:head>
   <title>Union - Explorer</title>
 </svelte:head>
-
-
 
 <!-- mobile layout !-->
 <div class="flex flex-row sm:divide-x overflow-x-none max-w-full w-full">
@@ -96,12 +43,12 @@ $: mainExplorerPage = $page.route.id?.split("/").length <= 3
       <span class="uppercase">{$page.route.id?.split('/').at(-2)}</span>
     </a>
 
-    <div class="p-2 pt-0 sm:p-6 flex flex-col flex-1">
+    <div class="p-2 pt-0 sm:px-6 sm:py-4 flex flex-col flex-1">
       <div class={cn($page.route.id?.split('/').length === 3 ? "" : "hidden")}>
         <h2 class="text-4xl font-extrabold font-expanded sm:!font-extra-expanded uppercase font-supermolot">
           {explorerRoute?.replaceAll('-', ' ')}
         </h2>
-        <p class="pb-4 -mt-1 text-muted-foreground">{'>'} {explorerPageDescription}</p>
+        <p class="pb-2.5 -mt-1 text-muted-foreground">{'>'} {explorerPageDescription}</p>
       </div>
       <slot />
     </div>
