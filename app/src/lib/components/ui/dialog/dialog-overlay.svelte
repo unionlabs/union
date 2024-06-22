bb86cb09e61a1a9e07da040c5c7c0eb110cb62c4
cor <cor@pruijs.dev>
2024-06-22 16:24:28 +0200

    feat(app): add crt effect
    gh pr create

--- app/src/lib/components/ui/dialog/dialog-overlay.svelte
+++ app/src/lib/components/ui/dialog/dialog-overlay.svelte
@@ -16,6 +16,6 @@ export { className as class }
 <DialogPrimitive.Overlay
   {transition}
   {transitionConfig}
-  class={cn('fixed inset-0 z-50 bg-background/80 backdrop-blur-sm', className)}
+  class={cn('fixed inset-0 z-40 bg-muted/60 backdrop-blur-sm', className)}
   {...$$restProps}
 />
