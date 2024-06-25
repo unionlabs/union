c3773e776f05223531b928f3ced135f47ea338e1
cor <cor@pruijs.dev>
2024-06-26 00:14:45 +0200

    feat(app): use chainsgate for connections table

--- app/src/routes/explorer/connections/+page.svelte
+++ app/src/routes/explorer/connections/+page.svelte
@@ -1,58 +1,8 @@
 <script lang="ts">
-import request from "graphql-request"
-import { connectionsQuery } from "$lib/graphql/documents/connections.ts"
-import { createQuery } from "@tanstack/svelte-query"
-import { URLS } from "$lib/constants"
-import Table from "../(components)/table.svelte"
-import { flexRender, type ColumnDef } from "@tanstack/svelte-table"
-import { derived } from "svelte/store"
-import CellOriginConnection from "../(components)/cell-origin-connection.svelte"
-import { raise } from "$lib/utilities"
-
-
-let connections = createQuery({
-  queryKey: ["connections"],
-  refetchInterval: 5_000,
-  queryFn: async () => { 
-    const response = await request(URLS.GRAPHQL, connectionsQuery, {})
-    if (!response.v0_connection_map) raise("error fetching transfers")
-
-    return response.v0_connection_map.map(connection => ({
-      source: {
-        chain_display_name: connection.from_chain_id ?? "unknown",
-        chain_id: connection.from_chain_id ?? "unknown",
-        connection_id: connection.from_connection_id ?? "unknown",
-        client_id: connection.from_client_id ?? "unknown",
-      },
-      destination: {
-        chain_display_name: connection.to_chain_id ?? "unknown",
-        chain_id: connection.to_chain_id ?? "unknown",
-        connection_id: connection.to_connection_id ?? "unknown",
-        client_id: connection.to_client_id ?? "unknown",
-      },
-      status: connection.status
-    }));
-  }
-})
-
-let connectionsData = derived(connections, ($connections) =>  $connections.data ?? [] )
-
-const columns: Array<ColumnDef<{ chain_id: string }>> = [
-  {
-    accessorKey: "source",
-    header: () => "Source",
-    size: 200,
-    cell: info => flexRender(CellOriginConnection, { value: info.getValue() })
-  },
-  {
-    accessorKey: "destination",
-    header: () => "Destination",
-    size: 200,
-    cell: info => flexRender(CellOriginConnection, { value: info.getValue() })
-  }
-]
+import ChainsGate from "$lib/components/chains-gate.svelte"
+import TableConnections from "../(components)/table-connections.svelte";
 </script>
 
-{#if $connections.data }
-  <Table bind:dataStore={connectionsData} {columns} />
-{/if}
+<ChainsGate let:chains>
+  <TableConnections {chains}/>
+</ChainsGate>
