9d3ddf3ae151b67569413978210ac2adf07a9335
cor <cor@pruijs.dev>
2024-06-26 21:47:04 +0200

    fix(site): forty one

--- app/src/lib/stores/user.ts
+++ app/src/lib/stores/user.ts
@@ -12,4 +12,4 @@ function persistStore<T>(key: string, initial: T) {
   return store
 }
 
-export const showUnsupported = persistStore("show-unsupported", false)
+export const showUnsupported = persistStore("show-unsupported", true)
