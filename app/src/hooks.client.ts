47442b22d9b4c68d5eb0cf36349301a215daf0b0
omar <omaraziz.dev@proton.me>
2024-06-27 03:35:17 -0700

    feat: sentry feedback and bug widget

--- app/src/hooks.client.ts
+++ app/src/hooks.client.ts
@@ -1,20 +1,27 @@
 import * as Sentry from "@sentry/sveltekit"
 import type { HandleClientError } from "@sveltejs/kit"
-
-if (import.meta.env.MODE === "production") {
-  Sentry.init({
-    enabled: import.meta.env.MODE === "production",
-    dsn: "https://b410cea864cbfaefea5fc8b18e40ae4f@o4506911891783680.ingest.us.sentry.io/4507500708954112",
-    tracesSampleRate: 1,
-    replaysOnErrorSampleRate: 1,
-    replaysSessionSampleRate: 0.1,
-    integrations: [
-      Sentry.replayIntegration(),
-      Sentry.breadcrumbsIntegration(),
-      Sentry.extraErrorDataIntegration()
-    ]
-  })
-}
+console.info(Sentry.feedbackIntegration.arguments)
+Sentry.init({
+  enabled: import.meta.env.MODE === "production",
+  dsn: "https://b410cea864cbfaefea5fc8b18e40ae4f@o4506911891783680.ingest.us.sentry.io/4507500708954112",
+  tracesSampleRate: 1,
+  replaysOnErrorSampleRate: 1,
+  replaysSessionSampleRate: 0.1,
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.breadcrumbsIntegration(),
+    Sentry.extraErrorDataIntegration(),
+    Sentry.feedbackIntegration({
+      showBranding: false,
+      colorScheme: "system",
+      submitButtonLabel: "Submit",
+      formTitle: "Feedback / Bug Report",
+      buttonLabel: "Feedback / Bug Report",
+      messagePlaceholder:
+        "Please describe the issue you encountered or provide feedback on how we can improve."
+    })
+  ]
+})
 
 // biome-ignore lint/suspicious/useAwait: no need
 export const handleError = (async ({ error, event, status, message, ...context }) => {
