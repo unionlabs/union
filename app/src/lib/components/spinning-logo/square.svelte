211a945a4f6cc39988d56cc3051c585cde85d8c4
cor <cor@pruijs.dev>
2024-07-02 17:41:22 +0200

    feat(app): in flux spinning animation

---
+++ app/src/lib/components/spinning-logo/square.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let size: number;
+  export let x: number;
+  export let y: number;
+</script>
+
+<svg class="absolute" style={`top: ${y}; left: ${x}`} width={`${size}px`} height={`${size}px`} viewBox={`0 0 ${size} ${size}`} fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect x=0 y=0 width={size} height={size} stroke="black"/>
+</svg>
