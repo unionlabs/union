932638f96ba1cb09e39b774ac5098563e2f61ff9
Lukas <Lukas.nyberg@gmail.com>
2024-07-03 10:34:23 +0200

    fix(app): scroll

--- app/src/routes/explorer/(components)/stats-bar.svelte
+++ app/src/routes/explorer/(components)/stats-bar.svelte
@@ -3,23 +3,26 @@
   import { packetCountQuery, transferCountQuery, transfersPerDayQuery } from "$lib/queries/stats.ts";
   import PixelGraph from "../(components)/pixel-graph.svelte"
   import { userTime } from "$lib/utilities/user-time.ts";
+  import { ScrollArea } from "$lib/components/ui/scroll-area";
 
   $: transfersPerDayData = transfersPerDayQuery()
   $: packetCountData = packetCountQuery()
-  $: transferCountData= transferCountQuery()
+  $: transferCountData = transferCountQuery()
 
   $: console.log($packetCountData)
   $: console.log($transferCountData)
 </script>
 
-<div class="bg-muted border-b flex">
-  <StatsBarStat label={"Total Messages"} value={155_300_677}/>
-  <StatsBarStat label="Total Packets" value={123_325_332} />
-  <StatsBarStat label="Metrics" value={$userTime}>
-    {#if $transfersPerDayData.data}
-      <div class="ml-5 flex items-end">
-        <PixelGraph data={$transfersPerDayData.data}/>
-      </div>
-    {/if}
-  </StatsBarStat>
-</div>
+  <div class="bg-muted border-b flex">
+    <div class="w-full flex overflow-x-auto">
+      <StatsBarStat label={"Total Messages"} value={155_300_677}/>
+      <StatsBarStat label="Total Packets" value={123_325_332}/>
+      <StatsBarStat label="Metrics" value={$userTime}>
+        {#if $transfersPerDayData.data}
+          <div class="ml-6 flex items-end">
+            <PixelGraph data={$transfersPerDayData.data}/>
+          </div>
+        {/if}
+      </StatsBarStat>
+    </div>
+  </div>
