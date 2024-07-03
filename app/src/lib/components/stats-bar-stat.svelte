622cd75d1934faec34c083a9d7e331bc281a2d5d
Lukas <Lukas.nyberg@gmail.com>
2024-07-03 10:34:01 +0200

    fix(app): padding acc to figma

--- app/src/lib/components/stats-bar-stat.svelte
+++ app/src/lib/components/stats-bar-stat.svelte
@@ -3,7 +3,7 @@
     export let value: number | string;
   </script>
   
-  <div class="uppercase w-fit flex px-6 py-5 border-r">
+  <div class="uppercase w-fit flex px-6 py-4 border-r">
     <div class="flex flex-col divide-y">
       <h3 class="text-sm pb-2">{label}</h3>
       <div class="text-xl pt-2 font-bold">{value}</div>
