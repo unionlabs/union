0599ec1b2e7fbd623b6a793d21c67e082ca6575a
cor <cor@pruijs.dev>
2024-07-04 21:42:43 +0200

    fix(app): 25 transfers per page

--- app/src/lib/graphql/documents/transfers.ts
+++ app/src/lib/graphql/documents/transfers.ts
@@ -2,7 +2,7 @@ import { graphql } from "gql.tada"
 
 export const allTransfersQueryDocument = graphql(/* GraphQL */ `
     query AllTransfersQuery @cached(ttl: 5) {
-    v0_transfers(limit: 100, order_by: {source_timestamp: desc}) {
+    v0_transfers(limit: 25, order_by: {source_timestamp: desc}) {
       sender
       source_chain_id
       source_transaction_hash
