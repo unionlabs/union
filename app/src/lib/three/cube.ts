b64f57dce33833c37a6ba80ea4c640f46584b9c6
cor <cor@pruijs.dev>
2024-07-05 17:20:27 +0200

    fix(app): format

--- app/src/lib/three/cube.ts
+++ app/src/lib/three/cube.ts
@@ -1,7 +1,7 @@
 import * as THREE from "three"
 
 export function createCube(cubeWidth: number, strokeWidth: number): THREE.Group {
-  const boxGeometry = new THREE.BoxGeometry(cubeWidth, cubeWidth, cubeWidth);
+  const boxGeometry = new THREE.BoxGeometry(cubeWidth, cubeWidth, cubeWidth)
   const edgesGeometry = new THREE.EdgesGeometry(boxGeometry)
 
   const createTubeLine = (points, thickness) => {
