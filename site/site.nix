b5f2ae7dba7840004a3def8889abc5d792c5c1e0
omar <omaraziz.dev@proton.me>
2024-07-08 15:52:23 -0700

    chore: update deps

--- site/site.nix
+++ site/site.nix
@@ -7,8 +7,13 @@
     in
     {
       packages = {
+<<<<<<< HEAD
         site = mkCi false (unstablePkgs.buildNpmPackage {
           npmDepsHash = "sha256-iacabyQyragOOO1AsWc0+N14e9cyl2p3Kq+egRaSGYc=";
+=======
+        site = unstablePkgs.buildNpmPackage {
+          npmDepsHash = "sha256-IQVZ2SaabqwerS9zIwB7qLhg2JUWy5+xdXYn8uFVNCg=";
+>>>>>>> 2fddec7ce (chore: update deps)
           src = ./.;
           srcs = [ ./. ./../evm/. ./../networks/genesis/. ./../versions/. ];
           sourceRoot = "site";
