a7c5bcbdcf66ef2650a1dbf9bbcfab6529e12e79
Lukas <Lukas.nyberg@gmail.com>
2024-07-16 19:11:49 +0200

    fix(app): text color

--- app/src/routes/explorer/(components)/cell-duration-text.svelte
+++ app/src/routes/explorer/(components)/cell-duration-text.svelte
@@ -7,4 +7,4 @@ export let value: Date | undefined
 </script>
 
 
-<div class="text-muted-foreground">{#if value && value !== undefined}{toIsoString(new Date(value)).split('T')[1].slice(0, 5)}{:else}in transit{/if}</div>
+<div>{#if value && value !== undefined}{toIsoString(new Date(value)).split('T')[1].slice(0, 5)}{:else}in transit{/if}</div>
