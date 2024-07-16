bd1db698b392769c5ad8e55f9d49f9d91a2eec4e
omar <omaraziz.dev@proton.me>
2024-07-16 07:16:41 -0700

    chore: update hash

--- site/site.nix
+++ site/site.nix
@@ -7,13 +7,8 @@
     in
     {
       packages = {
-<<<<<<< HEAD
         site = mkCi false (unstablePkgs.buildNpmPackage {
-          npmDepsHash = "sha256-iacabyQyragOOO1AsWc0+N14e9cyl2p3Kq+egRaSGYc=";
-=======
-        site = unstablePkgs.buildNpmPackage {
           npmDepsHash = "sha256-IQVZ2SaabqwerS9zIwB7qLhg2JUWy5+xdXYn8uFVNCg=";
->>>>>>> 2fddec7ce (chore: update deps)
           src = ./.;
           srcs = [ ./. ./../evm/. ./../networks/genesis/. ./../versions/. ];
           sourceRoot = "site";
@@ -45,19 +40,6 @@
               export PUPPETEER_SKIP_DOWNLOAD=1 
               npm install
               npm run dev
-            '';
-          };
-        };
-
-        site-check = {
-          type = "app";
-          program = pkgs.writeShellApplication {
-            name = "site-check";
-            runtimeInputs = combinedDeps;
-            text = ''
-              ${ensureAtRepositoryRoot}
-              cd site/
-              npm run check
             '';
           };
         };
