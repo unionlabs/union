8f7e4cd72654020505bf7d1dd61254b63488ad68
Lukas <Lukas.nyberg@gmail.com>
2024-07-19 01:14:07 +0200

    feat(app): bind copy function

--- app/src/routes/explorer/(components)/cell-origin-channel.svelte
+++ app/src/routes/explorer/(components)/cell-origin-channel.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
-import { cn } from "$lib/utilities/shadcn.ts"
-import * as Tooltip from "$lib/components/ui/tooltip"
-import { truncate } from "$lib/utilities/format.ts"
+  import { cn } from "$lib/utilities/shadcn.ts"
+  import * as Tooltip from "$lib/components/ui/tooltip"
+  import { truncate } from "$lib/utilities/format.ts"
+  import { copy } from "$lib/utilities/copy.ts";
 
-export let value: {
-  chain_display_name: string
-  chain_id: string
-  connection_id: string
-  channel_id: string
-  port_id: string
-}
+
+  export let value: {
+    chain_display_name: string
+    chain_id: string
+    connection_id: string
+    channel_id: string
+    port_id: string
+  }
 </script>
 
 <div {...$$restProps} class={cn("flex flex-col ")}>
-  <div class="font-bold">{value.chain_display_name}</div>
-  <div>{value.chain_id}</div>
-  <div>{value.connection_id}</div>
-  <div>{value.channel_id}</div>
+  <div class="font-bold" use:copy>{value.chain_display_name}<span></span></div>
+  <div use:copy>{value.chain_id}</div>
+  <div use:copy>{value.connection_id}</div>
+  <div use:copy>{value.channel_id}</div>
   <Tooltip.Root>
     <Tooltip.Trigger>
-      <div class="text-start">{truncate(value.port_id, 10)}</div>
+      <div class="text-start" use:copy={value.port_id}>{truncate(value.port_id, 10)}</div>
     </Tooltip.Trigger>
     <Tooltip.Content>
       {value.port_id}
