3a6abb19957e2bd3c386dc67ba5d81e7477f7ac3
omar <omaraziz.dev@proton.me>
2024-07-23 01:15:10 -0700

    chore: add required dev deps

--- site/stackbit.config.ts
+++ site/stackbit.config.ts
@@ -1,17 +1,31 @@
 import { defineStackbitConfig } from "@stackbit/types"
 import { ContentfulContentSource } from "@stackbit/cms-contentful"
 
+/**
+ * https://docs.netlify.com/visual-editor/content-sources/contentful/#prerequisites
+ */
+
 export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
-  nodeVersion: "18",
   ssgName: "astro",
+  nodeVersion: "22.5.1",
+  stackbitVersion: "~0.6.0",
+  presetReferenceBehavior: "duplicateContents",
   contentSources: [
     new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID,
       previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
       accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-      environment: process.env.CONTENTFUL_ENVIRONMENT || "master"
+      environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+      useWebhookForContentUpdates: true // default is false
     })
   ],
-  postInstallCommand: "npm i --no-save @stackbit/types @stackbit/cms-contentful"
+  import: {
+    uploadAssets: true,
+    type: "contentful",
+    assetsDirectory: "contentful",
+    contentFile: "contentful/export.json",
+    spaceIdEnvVar: "CONTENTFUL_SPACE_ID",
+    previewTokenEnvVar: "CONTENTFUL_PREVIEW_TOKEN",
+    deliveryTokenEnvVar: "CONTENTFUL_DELIVERY_TOKEN"
+  }
 })
