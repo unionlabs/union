dcbb276aa4fbc5c8dd8c877cc2c880c34b4f80f4
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2024-07-23 19:03:25 -0500

    fix: fix biome-lint error

--- site/stackbit.config.ts
+++ site/stackbit.config.ts
@@ -10,7 +10,7 @@ export default defineStackbitConfig({
       spaceId: process.env.CONTENTFUL_SPACE_ID,
       previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
       accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-      environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+      environment: process.env.CONTENTFUL_ENVIRONMENT || "master"
     })
   ],
   postInstallCommand: "npm i --no-save @stackbit/types @stackbit/cms-contentful"
