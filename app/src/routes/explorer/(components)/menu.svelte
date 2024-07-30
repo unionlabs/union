c23c5f123bb24d5ebfa143f9f15610094c9b38fc
omar <omaraziz.dev@proton.me>
2024-07-30 10:43:26 -0700

    fix: pagination click away

--- app/src/routes/explorer/(components)/menu.svelte
+++ app/src/routes/explorer/(components)/menu.svelte
@@ -7,20 +7,16 @@ import * as Tooltip from "$lib/components/ui/tooltip/index.ts"
 
 export let tableRoutes: LayoutData["tables"]
 
-const devBorder = 0 && "outline outline-[1px] outline-pink-200/40"
-
 export let isCollapsed = false
 
-$: selectedTable = $page.route.id?.split("/").at(-1) || "blocks"
+$: selectedTable = $page.route.id?.split("/").at(-1) || "transfers"
 
 let alwaysCollapsedWidth = 580
 let alwaysCollapsed = innerWidth <= alwaysCollapsedWidth
 </script>
 
 <nav
-  class={cn(
-    'rounded-none flex flex-col items-start h-full dark:bg-background'
-  )}
+  class={cn("rounded-none flex flex-col items-start h-full dark:bg-background")}
 >
   {#each tableRoutes as { route, icon }}
     {#if isCollapsed}
@@ -31,12 +27,16 @@ let alwaysCollapsed = innerWidth <= alwaysCollapsedWidth
             variant="link"
             builders={[builder]}
             href={`/explorer/${route}`}
+            data-sveltekit-reload={selectedTable === "transfers" ? true : "off"}
             class={cn(
-              selectedTable === route ? 'bg-muted' : 'bg-transparent',
-              'px-1 text-center mb-2 w-full flex py-0 hover:no-underline hover:bg-muted',
+              selectedTable === route ? "bg-muted" : "bg-transparent",
+              "px-1 text-center mb-2 w-full flex py-0 hover:no-underline hover:bg-muted"
             )}
           >
-            <svelte:component this={icon} class="min-w-[14px] block size-5 text-muted-foreground" />
+            <svelte:component
+              this={icon}
+              class="min-w-[14px] block size-5 text-muted-foreground"
+            />
             <span class="sr-only">{route}</span>
           </Button>
         </Tooltip.Trigger>
@@ -53,14 +53,17 @@ let alwaysCollapsed = innerWidth <= alwaysCollapsedWidth
         title={route}
         variant="link"
         href={`/explorer/${route}`}
+        data-sveltekit-reload={route === "transfers" ? true : "off"}
         class={cn(
-          'mb-2 w-full flex justify-start gap-x-1',
-          selectedTable === route ? 'bg-foreground text-primary-foreground' : 'bg-transparent',
+          "mb-2 w-full flex justify-start gap-x-1",
+          selectedTable === route
+            ? "bg-foreground text-primary-foreground"
+            : "bg-transparent"
         )}
       >
         <span>[</span>
-        <svelte:component this={icon} class={'size-3.5'} />
-        <div>{route.replaceAll('-', ' ')}</div>
+        <svelte:component this={icon} class={"size-3.5"} />
+        <div>{route.replaceAll("-", " ")}</div>
         <span>]</span>
       </Button>
     {/if}
--- app/src/routes/explorer/+layout.ts
+++ app/src/routes/explorer/+layout.ts
@@ -1,19 +1,22 @@
 import type { LayoutLoad } from "./$types.ts"
-import type { SvelteComponent } from "svelte"
 import { getCurrentISODateTime } from "$lib/utilities/date.ts"
-
-import TransfersIcon from "$lib/components/union-icons/color/icon-transfers-color.svelte"
-// import BlocksIcon from "$lib/components/union-icons/color/icon-blocks-color.svelte"
-import ConnectionIcon from "$lib/components/union-icons/color/icon-connection-color.svelte"
-import ChannelsIcon from "$lib/components/union-icons/color/icon-channel-color.svelte"
 import IndexIcon from "$lib/components/union-icons/color/icon-index-color.svelte"
-// import PacketIcon from "$lib/components/union-icons/color/icon-packet-color.svelte"
+import ChannelsIcon from "$lib/components/union-icons/color/icon-channel-color.svelte"
+import TransfersIcon from "$lib/components/union-icons/color/icon-transfers-color.svelte"
+import ConnectionIcon from "$lib/components/union-icons/color/icon-connection-color.svelte"
 
-const tables = ["blocks", "packets", "channels", "connections"] as const
+const tables = [
+  //
+  // "blocks",
+  "transfers",
+  "connections",
+  "channels",
+  "index-status"
+] as const
 
 export interface Table {
   route: (typeof tables)[number]
-  icon: typeof SvelteComponent
+  icon: typeof TransfersIcon
   description: string
 }
 
@@ -36,12 +39,15 @@ export const load = (loadEvent => ({
       icon: ConnectionIcon,
       description: "Confirmed IBC Connections based on on-chain four-way handshake events."
     },
-    { route: "channels", icon: ChannelsIcon, description: "Open IBC Channels" },
-    // { route: "packets", icon: PacketIcon, description: "Packets sent through Union" },
+    {
+      route: "channels",
+      icon: ChannelsIcon,
+      description: "Open IBC Channels"
+    },
     {
       route: "index-status",
       icon: IndexIcon,
       description: "Statuses of Hubble indices for connected chains"
     }
-  ] as Array<Table>
+  ] satisfies Array<Table>
 })) satisfies LayoutLoad
