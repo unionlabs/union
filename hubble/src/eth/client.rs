501822a61d57eca417ee865d81416c8343354f68
PablitoAmaral <pablitoamaral@gmail.com>
2024-08-06 10:37:14 +0100

    feat(hubble): migrate from ethers to alloy

--- hubble/src/eth/client.rs
+++ hubble/src/eth/client.rs
@@ -1,39 +1,40 @@
 #![allow(clippy::disallowed_types)]
-use ethers::{
-    core::types::BlockId,
-    providers::{Http, Middleware, Provider, ProviderError},
-    types::{Block, BlockNumber, Filter, Log, TransactionReceipt, TxHash, U256},
+use alloy::{
+    eips::{BlockId, BlockNumberOrTag},
+    providers::{Provider, RootProvider},
+    rpc::types::{Block, BlockTransactionsKind, Filter, Log, TransactionReceipt},
+    transports::{
+        http::{Client, Http},
+        RpcError, TransportErrorKind,
+    },
 };
 
 use crate::race_client::RaceClient;
 
-impl RaceClient<Provider<Http>> {
-    pub async fn get_chainid(&self) -> Result<U256, ProviderError> {
-        self.race(|c| c.get_chainid()).await
+impl RaceClient<RootProvider<Http<Client>>> {
+    pub async fn get_chain_id(&self) -> Result<u64, RpcError<TransportErrorKind>> {
+        self.race(|c| c.get_chain_id()).await
     }
-
     #[allow(dead_code)]
-    pub async fn get_block_receipts<T>(
+    pub async fn get_block_receipts(
         &self,
-        block: T,
-    ) -> Result<Vec<TransactionReceipt>, ProviderError>
-    where
-        T: Into<BlockNumber> + Send + Sync,
-    {
-        let block = block.into();
+        block: BlockNumberOrTag,
+    ) -> Result<Option<Vec<TransactionReceipt>>, RpcError<TransportErrorKind>> {
         self.race(|c| c.get_block_receipts(block)).await
     }
 
-    pub async fn get_block<T>(&self, id: T) -> Result<Option<Block<TxHash>>, ProviderError>
-    where
-        T: Into<BlockId> + Send + Sync,
-    {
-        let id = id.into();
-        self.race_some(|c| c.get_block(id)).await
+    pub async fn get_block(
+        &self,
+        id: BlockId,
+        kind: BlockTransactionsKind,
+    ) -> Result<Option<Block>, RpcError<TransportErrorKind>> {
+        self.race_some(|c| c.get_block(id, kind)).await
     }
-
     #[allow(dead_code)]
-    pub async fn get_logs(&self, filter: &Filter) -> Result<Vec<Log>, ProviderError> {
+    pub async fn get_logs(
+        &self,
+        filter: &Filter,
+    ) -> Result<Vec<Log>, RpcError<TransportErrorKind>> {
         self.race(|c| c.get_logs(filter)).await
     }
 }
