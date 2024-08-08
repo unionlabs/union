6766399ca096cc6c61d7591740e93daf6c0190aa
cor <cor@pruijs.dev>
2024-08-08 15:16:11 +0200

    feat(app): style segments of cosmos addresses

--- app/src/lib/components/address-multichain.svelte
+++ app/src/lib/components/address-multichain.svelte
@@ -6,27 +6,54 @@ import { Badge } from "$lib/components/ui/badge/index.ts"
 export let address: { address: string; normalizedAddress: string }
 export let chains: Array<Chain>
 
-// let addressChain = chains.find(c => c.chain_id === chainId)
-// let otherChains: Array<Chain> = chains.filter(c => c.chain_id !== chainId)
-let cosmosChains: Array<Chain> = chains.filter(c => c.rpc_type === "cosmos" && c.enabled_staging)
+const addressChain = chains.find(c => address.address.startsWith(c.addr_prefix)) as Chain
+const cosmosChains: Array<Chain> = chains.filter(c => c.rpc_type === "cosmos" && c.enabled_staging)
+
+const rpc_type = address.address.startsWith("0x") ? "evm" : "cosmos"
 
 // @ts-ignore
 const fromHexString = hexString =>
   Uint8Array.from(hexString.match(/.{1,2}/g).map(byte => Number.parseInt(byte, 16)))
+
+const otherCosmosAddresses: Array<{ address: string; chain: Chain }> = chains
+  .filter(c => c.rpc_type === "cosmos")
+  .map(c => ({
+    address: rawToBech32(c.addr_prefix, fromHexString(address.normalizedAddress)),
+    chain: c
+  }))
+  .filter(pair => pair.address !== address.address)
+
+const allCosmosAddresses = [
+  { address: address.address, chain: addressChain },
+  ...otherCosmosAddresses
+].map(pair => {
+  let prefix = pair.chain.addr_prefix
+  let body = pair.address.replace(prefix, "").slice(0, -6)
+  let checksum = pair.address.slice(-6)
+
+  return {
+    prefix,
+    body,
+    checksum,
+    ...pair
+  }
+})
 </script>
 
 <div>
-{#if address.address.startsWith("0x")}
+{#if addressChain?.rpc_type === "evm"}
   <div class="text-lg font-bold flex items-center gap-2">{address.address}<Badge>EVM</Badge></div>
 {:else}
-  <div class="text-lg font-bold flex items-center gap-2">{address.address}<Badge>Cosmos</Badge></div>
-  {#each cosmosChains as cosmosChain}
-    {#if !address.address.startsWith(cosmosChain.addr_prefix)}
-    <div>
-      {rawToBech32(cosmosChain.addr_prefix, fromHexString(address.normalizedAddress))}
-    </div>
-    {/if}
+<Badge>Cosmos</Badge>
+<ul>
+  {#each allCosmosAddresses as cosmosAddress}
+    <li class="text-lg first:font-bold">
+      <span class="text-muted-foreground mr-1">{cosmosAddress.prefix}</span>{cosmosAddress.body}<span class="ml-1 text-muted-foreground">{cosmosAddress.checksum}</span>
+    </li>
   {/each}
+</ul>
+
+  
 {/if}
 </div>
 
