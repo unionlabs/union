54807222fc860eca492af4ab07c6d4dc6648752f
cor <cor@pruijs.dev>
2024-08-16 15:15:52 +0200

    feat(app): swap navigation item

--- app/src/lib/components/navigation/index.ts
+++ app/src/lib/components/navigation/index.ts
@@ -2,6 +2,7 @@ import Navigation from "./navigation.svelte"
 
 const routes = {
   transfer: { draft: false, path: "/transfer" },
+  swap: { draft: false, path: "/swap" },
   faucet: { draft: false, path: "/faucet" },
   explorer: { draft: false, path: "/explorer" },
   transfers: { draft: true, path: "/transfers" }
