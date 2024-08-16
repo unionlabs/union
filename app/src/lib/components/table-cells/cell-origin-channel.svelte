dfb95e32b99722c87549b322c38e30ec81f46c20
cor <cor@pruijs.dev>
2024-08-16 16:53:37 +0200

    fix(app): cell origins show what's available

--- app/src/lib/components/table-cells/cell-origin-channel.svelte
+++ app/src/lib/components/table-cells/cell-origin-channel.svelte
@@ -3,18 +3,18 @@ import { cn } from "$lib/utilities/shadcn.ts"
 import CellCopy from "./cell-copy.svelte"
 
 export let value: {
-  chain_display_name: string
-  chain_id: string
-  connection_id: string
-  channel_id: string
-  port_id: string
+  chain_display_name: string | undefined
+  chain_id: string | undefined
+  connection_id: string | undefined
+  channel_id: string | undefined
+  port_id: string | undefined
 }
 </script>
 
 <div {...$$restProps} class={cn("flex flex-col ")}>
-  <CellCopy value={value.chain_display_name}/>
-  <CellCopy value={value.chain_id}/>
-  <CellCopy value={value.connection_id}/>
-  <CellCopy value={value.channel_id}/>
-  <CellCopy trunc={10} value={value.port_id}/>
+  {#if value.chain_display_name}<CellCopy value={value.chain_display_name}/>{/if}
+  {#if value.chain_id}<CellCopy value={value.chain_id}/>{/if}
+  {#if value.connection_id}<CellCopy value={value.connection_id}/>{/if}
+  {#if value.channel_id}<CellCopy value={value.channel_id}/>{/if}
+  {#if value.port_id}<CellCopy trunc={10} value={value.port_id}/>{/if}
 </div>
