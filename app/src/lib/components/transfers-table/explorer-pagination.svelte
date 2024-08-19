fbf92f7487fe3aaa2787470e7b2851af5a1b7ef2
cor <cor@pruijs.dev>
2024-08-19 11:08:41 +0200

    refactor(app): pagination cleanup

--- app/src/lib/components/transfers-table/explorer-pagination.svelte
+++ app/src/lib/components/transfers-table/explorer-pagination.svelte
@@ -4,30 +4,25 @@ import { Label } from "$lib/components/ui/label"
 import ChevronLeft from "virtual:icons/lucide/chevron-left"
 import Button from "$lib/components/ui/button/button.svelte"
 import ChevronRight from "virtual:icons/lucide/chevron-right"
-import * as Pagination from "$lib/components/ui/pagination/index.ts"
 import { derived, type Readable } from "svelte/store"
 import { toast } from "svelte-sonner"
 import { encodeTimestampSearchParam, decodeTimestampSearchParam } from "$lib/timestamps"
 import { goto } from "$app/navigation"
 import { page } from "$app/stores"
 
+export let explorerItems: Readable<Array<{ timestamp: any }>>
+
 let timestamp: Readable<string | null> = derived(page, $page => {
   const urlTimestamp = $page.url.searchParams.get("timestamp")
   if (!urlTimestamp) return null
   return decodeTimestampSearchParam(urlTimestamp)
 })
-export let explorerItems: Readable<Array<{ timestamp: any }>>
-
 $: live = $timestamp === null
+
 const onOlderPage = () => {
   const stamp = $explorerItems.at(-1)?.timestamp
+  if (!stamp) return toast.error("Invalid older timestamp")
 
-  if (!stamp) {
-    toast.error("Invalid older timestamp")
-    return
-  }
-
-  // timestamp.set(stamp)
   goto(encodeTimestampSearchParam(stamp), {
     replaceState: true,
     state: { timestamp: stamp }
@@ -36,13 +31,8 @@ const onOlderPage = () => {
 
 const onNewerPage = () => {
   const stamp = $explorerItems.at(0)?.timestamp
+  if (!stamp) return toast.error("Invalid newer timestamp")
 
-  if (!stamp) {
-    toast.error("Invalid newer timestamp")
-    return
-  }
-
-  // timestamp.set(stamp)
   goto(encodeTimestampSearchParam(stamp), {
     replaceState: true,
     state: { timestamp: stamp }
@@ -54,64 +44,40 @@ const onCurrentClick = () => {
 }
 </script>
 
-<Pagination.Root
-  let:pages
-  siblingCount={2000}
-  perPage={20}
-  class="w-auto"
-  count={20}
->
-  <Pagination.Content
-    class="py-2 text-sm uppercase font-supermolot w-full flex gap-x-1"
+<div class="flex items-center gap-4 mt-4">
+  <Button
+    size="sm"
+    variant="default"
+    on:click={onCurrentClick}
+    disabled={live}
+    title={live ? "Already on the newest page" : "Go to the first page"}
   >
-    <div class={cn("flex flex-row uppercase")}>
-      <Button
-        size="sm"
-        variant="default"
-        on:click={(event) => {
-          onCurrentClick()
-        }}
-        disabled={live}
-        title={live ? "Already on the newest page" : "Go to the first page"}
-        class="hover:bg-accent hover:text-black"
-      >
-        {live ? "live" : "current"}
-      </Button>
-    </div>
-    {#if $timestamp}
-      <Pagination.Item>
-        <Pagination.PrevButton
-          disabled={status === "pending" || live}
-          title={live ? "Already on the newest page" : "Go to the previous page"}
-          on:click={onNewerPage}
-          class={cn(
-            "text-sm",
-            status === "pending" 
-              ? "cursor-not-allowed disabled"
-              : ""
-          )}
-        >
-          <ChevronLeft class="size-6" />
-          <span class="text-sm uppercase font-supermolot">
-            {live ? "newest" : "Newer"}
-          </span>
-        </Pagination.PrevButton>
-      </Pagination.Item>
-      <Label class="w-full">
-        <time class="font-normal text-sm uppercase font-mono my-auto w-full">
-          {$timestamp}
-        </time>
-      </Label>
-    {/if}
-    <Pagination.Item>
-      <Pagination.NextButton
-        disabled={status === "pending"}
-        on:click={onOlderPage}
-        class="pr-0"
-      >
-        <span class="text-sm uppercase font-supermolot">Older</span>
-        <ChevronRight class="size-6" />
-      </Pagination.NextButton>
-    </Pagination.Item>
-  </Pagination.Content>
-</Pagination.Root>
+    {live ? "live" : "current"}
+  </Button>
+  {#if $timestamp}
+    <Button
+      size="sm"
+      variant="secondary"
+      title={live ? "Already on the newest page" : "Go to the previous page"}
+      on:click={onNewerPage}
+      class="pl-2"
+    >
+      <ChevronLeft class="size-6" />
+      Newer
+    </Button>
+    <Label class="w-full">
+      <time class="font-normal text-sm uppercase font-mono w-full">
+        {$timestamp}
+      </time>
+    </Label>
+  {/if}
+  <Button
+    size="sm"
+    variant="secondary"
+    on:click={onOlderPage}
+    class="pr-2"
+  >
+    Older
+    <ChevronRight class="size-6" />
+  </Button>
+</div>
