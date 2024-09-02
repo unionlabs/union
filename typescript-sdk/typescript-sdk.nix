1ce8e3c0d5e771eb9fe1345d2b1454e191ae5e4c
o-az <omaraziz.dev@proton.me>
2024-09-01 22:16:13 -0700

    fix: remove nix package for ts sdk

--- typescript-sdk/typescript-sdk.nix
+++ typescript-sdk/typescript-sdk.nix
@@ -4,15 +4,6 @@
       packageJSON = lib.importJSON ./package.json;
     in
     {
-      packages = {
-        typescript-sdk = unstablePkgs.buildNpmPackage {
-          npmDepsHash = "sha256-6FK5jmTOxqEiLoy+T5ajD+ldzwXtVgSjkD6KPBO1dRk=";
-          src = ./.;
-          pname = packageJSON.name;
-          version = packageJSON.version;
-          doDist = false;
-        };
-      };
       apps = {
         ts-sdk-check = {
           type = "app";
