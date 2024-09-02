146bfd877b50f9f12ca9464960c5c05ca2b6cfa6
Lukas <Lukas.nyberg@gmail.com>
2024-09-02 10:28:47 +0200

    fix(app): change icon approach due to tree-shake

--- app/src/routes/explorer/+layout.ts
+++ app/src/routes/explorer/+layout.ts
@@ -1,11 +1,16 @@
 import type { LayoutLoad } from "./$types.ts"
-import { UnionIcons } from "$lib/components/union-icons/union-icons.ts"
+import IndexIcon from "$lib/components/union-icons/color/icon-index-color.svelte"
+import ChannelsIcon from "$lib/components/union-icons/color/icon-channel-color.svelte"
+import TransfersIcon from "$lib/components/union-icons/color/icon-transfers-color.svelte"
+import ConnectionIcon from "$lib/components/union-icons/color/icon-connection-color.svelte"
+import PacketIcon from "$lib/components/union-icons/color/icon-packet-color.svelte"
+
 
 const tables = ["channels", "transfers", "packets", "connections", "index-status"] as const
 
 export interface Table {
   route: (typeof tables)[number]
-  icon: typeof UnionIcons.transfers.variants.color
+  icon: typeof IndexIcon
   description: string
 }
 
@@ -14,27 +19,27 @@ export const load = (loadEvent => ({
   tables: [
     {
       route: "transfers",
-      icon: UnionIcons.transfers.variants.color,
+      icon: TransfersIcon,
       description: "All transfers"
     },
     {
       route: "packets",
-      icon: UnionIcons.packet.variants.color,
+      icon: PacketIcon,
       description: "All packets"
     },
     {
       route: "connections",
-      icon: UnionIcons.connection.variants.color,
+      icon: ConnectionIcon,
       description: "Confirmed IBC Connections based on on-chain four-way handshake events."
     },
     {
       route: "channels",
-      icon: UnionIcons.channel.variants.color,
+      icon: ChannelsIcon,
       description: "Open IBC Channels"
     },
     {
       route: "index-status",
-      icon: UnionIcons.index.variants.color,
+      icon: IndexIcon,
       description: "Statuses of Hubble indices for connected chains"
     }
   ] as const satisfies Array<Table>
