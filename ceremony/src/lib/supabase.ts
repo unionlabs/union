56558e1b028e958dafab47431ef875f527b9bb3d
Lukas <Lukas.nyberg@gmail.com>
2024-09-03 14:23:47 +0200

    fix(ceremony): env client side

--- ceremony/src/lib/supabase.ts
+++ ceremony/src/lib/supabase.ts
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_ANON_KEY, SUPABASE_URL } from "$env/static/private";
+
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
