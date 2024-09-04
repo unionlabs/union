3eb84c613e3bd8fbd72d3edee1aa5b995489659a
benluelo <git@luelo.dev>
2024-09-04 06:34:07 -0400

    wip: fix

--- voyager/voyager.nix
+++ voyager/voyager.nix
@@ -60,7 +60,7 @@
             done
           '';
         };
-        voyager-dev = mkCi false voyager-dev.packages.voyager-dev;
+        voyager-dev = mkCi false voyager-dev.packages.voyager;
       } // voyager-modules.packages;
       checks = voyager.checks // voyager-modules.checks;
     };
