7df814d5ba04e7de2b1074bbf80a1b17d3bc030d
cor <cor@pruijs.dev>
2024-09-10 17:05:06 +0200

    fix(site): type errors

--- site/src/lib/types.ts
+++ site/src/lib/types.ts
@@ -1,9 +1,16 @@
 import type { EntryFieldTypes } from "contentful"
+import { type ImageMetadata } from 'astro';
 
 export type MaybePromise<T> = T | Promise<T>
 
 export type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
+}
+
+export type NamedImage = {
+  name: string, 
+  url: string, 
+  logo: ImageMetadata
 }
 
 export interface BlogPost {
