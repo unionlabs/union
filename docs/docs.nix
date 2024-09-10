2ca8352906a0d856cb989b21b4cbfbb5594ec198
o-az <omaraziz.dev@proton.me>
2024-09-10 05:22:16 -0700

    chore: fmt

--- docs/docs.nix
+++ docs/docs.nix
@@ -51,8 +51,16 @@
             runtimeInputs = combinedDeps;
             text = ''
               ${ensureAtRepositoryRoot}
+              biome check docs --error-on-warnings --write --unsafe
+              
+              nix fmt
+
               cd docs/
+
               npm_config_yes=true npx astro check
+              npm_config_yes=true npx astro build
+
+              nix build .\#checks.${pkgs.system}.spellcheck --print-build-logs
             '';
           };
         };
