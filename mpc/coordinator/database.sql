71a9a53a78f61ade6d422feef30ae28eee99d4fb
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-09-11 11:54:27 +0200

    feat(mpc): more view for averages and string search_path

--- mpc/coordinator/database.sql
+++ mpc/coordinator/database.sql
@@ -45,16 +45,16 @@ ALTER VIEW current_queue SET (security_invoker = on);
 
 CREATE OR REPLACE FUNCTION min_score() RETURNS INTEGER AS $$
 BEGIN
-  RETURN (SELECT COALESCE(MIN(score) - 1, 1000000) FROM queue);
+  RETURN (SELECT COALESCE(MIN(score) - 1, 1000000) FROM public.queue);
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 CREATE OR REPLACE FUNCTION set_initial_score_trigger() RETURNS TRIGGER AS $$
 BEGIN
-  NEW.score := min_score();
+  NEW.score := public.min_score();
   RETURN NEW;
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 CREATE TRIGGER queue_set_initial_score
 BEFORE INSERT
@@ -80,6 +80,7 @@ CREATE TABLE contribution_status(
 ALTER TABLE contribution_status ENABLE ROW LEVEL SECURITY;
 ALTER TABLE contribution_status ADD FOREIGN KEY (id) REFERENCES queue(id);
 CREATE UNIQUE INDEX idx_contribution_status_id_expire ON contribution_status(id, expire);
+CREATE UNIQUE INDEX idx_contribution_status_id_started ON contribution_status(id, started);
 
 CREATE POLICY view_all
   ON contribution_status
@@ -88,6 +89,7 @@ CREATE POLICY view_all
     USING (
       true
     );
+
 
 ----------------------------
 -- Contribution Submitted --
@@ -103,7 +105,7 @@ ALTER TABLE contribution_submitted ADD FOREIGN KEY (id) REFERENCES contribution_
 ALTER TABLE contribution_submitted ADD FOREIGN KEY (object_id) REFERENCES storage.objects(id);
 
 CREATE INDEX idx_contribution_submitted_object ON contribution_submitted(object_id);
-CREATE INDEX idx_contribution_submitted_id_created_at ON contribution_submitted(id, created_at);
+CREATE UNIQUE INDEX idx_contribution_submitted_id_created_at ON contribution_submitted(id, created_at);
 
 CREATE POLICY view_all
   ON contribution_submitted
@@ -127,6 +129,7 @@ ALTER TABLE contribution ENABLE ROW LEVEL SECURITY;
 ALTER TABLE contribution ADD FOREIGN KEY (id) REFERENCES contribution_submitted(id);
 CREATE UNIQUE INDEX idx_contribution_seq ON contribution(seq);
 CREATE UNIQUE INDEX idx_contribution_seq_success ON contribution(success, seq);
+CREATE UNIQUE INDEX idx_contribution_id_created_at ON contribution(id, created_at);
 
 CREATE POLICY view_all
   ON contribution
@@ -139,10 +142,10 @@ CREATE POLICY view_all
 -- The next contributor is the one with the higest score that didn't contribute yet.
 CREATE OR REPLACE FUNCTION set_next_contributor_trigger() RETURNS TRIGGER AS $$
 BEGIN
-  CALL set_next_contributor();
+  CALL public.set_next_contributor();
   RETURN NEW;
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 -- Rotate the current contributor whenever a contribution is done.
 CREATE TRIGGER contribution_added
@@ -150,6 +153,22 @@ AFTER INSERT
 ON contribution
 FOR EACH ROW
 EXECUTE FUNCTION set_next_contributor_trigger();
+
+CREATE OR REPLACE VIEW current_verification_average AS (
+  SELECT AVG(c.created_at - cs.created_at) AS verification_average
+  FROM contribution c
+  INNER JOIN contribution_submitted cs ON (c.id = cs.id)
+);
+
+ALTER VIEW current_verification_average SET (security_invoker = on);
+
+CREATE OR REPLACE VIEW current_contribution_average AS (
+  SELECT AVG(cs.created_at - c.started) AS contribution_average
+  FROM contribution_status c
+  INNER JOIN contribution_submitted cs ON (c.id = cs.id)
+);
+
+ALTER VIEW current_contribution_average SET (security_invoker = on);
 
 -- Current contributor is the highest score in the queue with the contribution
 -- not done yet and it's status expired without payload submitted.
@@ -207,34 +226,34 @@ ALTER VIEW current_payload_id SET (security_invoker = on);
 
 CREATE OR REPLACE PROCEDURE set_next_contributor() AS $$
 BEGIN
-  IF (SELECT COUNT(*) FROM current_contributor_id) = 0 THEN
-    INSERT INTO contribution_status(id) SELECT q.id FROM queue q WHERE q.score = (
+  IF (SELECT COUNT(*) FROM public.current_contributor_id) = 0 THEN
+    INSERT INTO public.contribution_status(id) SELECT q.id FROM public.queue q WHERE q.score = (
       SELECT MAX(qq.score)
-      FROM queue qq
+      FROM public.queue qq
       WHERE NOT EXISTS (
-        SELECT c.id FROM contribution c WHERE c.id = qq.id
+        SELECT c.id FROM public.contribution c WHERE c.id = qq.id
       ) AND NOT EXISTS (
-        SELECT cs.expire FROM contribution_status cs WHERE cs.id = qq.id AND cs.expire < now()
+        SELECT cs.expire FROM public.contribution_status cs WHERE cs.id = qq.id AND cs.expire < now()
       )
     );
   END IF;
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 CREATE OR REPLACE FUNCTION can_upload(name varchar) RETURNS BOOLEAN AS $$
 BEGIN
   RETURN (
     -- User must be the current contributor.
-    (SELECT cci.id FROM current_contributor_id cci) = auth.uid()
+    (SELECT cci.id FROM public.current_contributor_id cci) = auth.uid()
     AND
     -- User is only allowed to submit the expected payload.
-    storage.filename(name) = (SELECT q.payload_id::text FROM queue q WHERE q.id = auth.uid())
+    storage.filename(name) = (SELECT q.payload_id::text FROM public.queue q WHERE q.id = auth.uid())
     AND
     -- Do not allow the user to interact with the file after its been submitted.
-    NOT EXISTS (SELECT * FROM contribution_submitted cs WHERE cs.id = auth.uid())
+    NOT EXISTS (SELECT * FROM public.contribution_submitted cs WHERE cs.id = auth.uid())
   );
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 CREATE POLICY allow_authenticated_contributor_upload_insert
   ON storage.objects
@@ -258,16 +277,16 @@ CREATE OR REPLACE FUNCTION can_download(name varchar) RETURNS BOOLEAN AS $$
 BEGIN
   RETURN (
     -- User must be the current contributor.
-    (SELECT cci.id FROM current_contributor_id cci) = auth.uid()
+    (SELECT cci.id FROM public.current_contributor_id cci) = auth.uid()
     AND
     -- User is only allowed to download the last verified contribution.
-    storage.filename(name) = (SELECT cpi.payload_id::text FROM current_payload_id cpi)
+    storage.filename(name) = (SELECT cpi.payload_id::text FROM public.current_payload_id cpi)
     AND
     -- Do not allow the user to interact with the file after its contribution has been submitted.
-    NOT EXISTS (SELECT * FROM contribution_submitted cs WHERE cs.id = auth.uid())
+    NOT EXISTS (SELECT * FROM public.contribution_submitted cs WHERE cs.id = auth.uid())
   );
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 CREATE POLICY allow_authenticated_contributor_download
   ON storage.objects
@@ -281,16 +300,16 @@ CREATE POLICY allow_authenticated_contributor_download
 
 CREATE OR REPLACE PROCEDURE set_contribution_submitted(queue_id uuid, object_id uuid) AS $$
 BEGIN
-  INSERT INTO contribution_submitted(id, object_id) VALUES(queue_id, object_id);
+  INSERT INTO public.contribution_submitted(id, object_id) VALUES(queue_id, object_id);
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 -- Phase 2 contribution payload is constant size
 CREATE OR REPLACE FUNCTION expected_payload_size() RETURNS INTEGER AS $$
 BEGIN
   RETURN 306032532;
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 -- Metadata pushed on upload.
 -- {
@@ -310,15 +329,15 @@ BEGIN
   IF (NEW.metadata IS NOT NULL) THEN
     file_size := (NEW.metadata->>'size')::integer;
     CASE
-      WHEN file_size = expected_payload_size()
-        THEN CALL set_contribution_submitted(uuid(NEW.owner_id), NEW.id);
+      WHEN file_size = public.expected_payload_size()
+        THEN CALL public.set_contribution_submitted(uuid(NEW.owner_id), NEW.id);
       ELSE
         RAISE EXCEPTION 'invalid file size, name: %, got: %, expected: %, meta: %', NEW.name, file_size, expected_payload_size(), NEW.metadata;
     END CASE;
   END IF;
   RETURN NEW;
 END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql SET search_path = '';
 
 -- Rotate the current contributor whenever a contribution is done.
 CREATE TRIGGER contribution_payload_uploaded
@@ -336,7 +355,7 @@ BEGIN
   INSERT INTO public.queue(id) VALUES(NEW.id);
   RETURN NEW;
 END
-$$ LANGUAGE plpgsql SET search_path = '';
+$$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = '';
 
 CREATE TRIGGER user_join_queue
   AFTER INSERT
