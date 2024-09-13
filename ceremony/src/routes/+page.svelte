e872a3a0d6a9daaac6c88a796f9dfebe09ebc37e
Lukas <Lukas.nyberg@gmail.com>
2024-09-13 20:17:24 +0200

    chore(ceremony): format

--- ceremony/src/routes/+page.svelte
+++ ceremony/src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-  import {user} from "$lib/stores/user.svelte.ts"
-  import H1 from "$lib/components/typography/H1.svelte";
-  import {ContributorState} from "$lib/stores/state.svelte.ts";
-  import H4 from "$lib/components/typography/H4.svelte";
-  import Ceremony from "$lib/components/Ceremony.svelte";
-  import {AddressForm, type ValidState} from "$lib/components/address/index.ts"
+import { user } from "$lib/stores/user.svelte.ts"
+import H1 from "$lib/components/typography/H1.svelte"
+import { ContributorState } from "$lib/stores/state.svelte.ts"
+import H4 from "$lib/components/typography/H4.svelte"
+import Ceremony from "$lib/components/Ceremony.svelte"
+import { AddressForm, type ValidState } from "$lib/components/address/index.ts"
 
-  let addressValidState: ValidState = $state("PENDING")
-  let contributor: ContributorState = new ContributorState()
+let addressValidState: ValidState = $state("PENDING")
+let contributor: ContributorState = new ContributorState()
 
-  $effect(() => {
-    console.info(`ADDRESS VALIDITY STATE: ${addressValidState}`)
-    const userId = user.session?.user.id
-    if (userId) contributor.setUserId(userId)
-  })
+$effect(() => {
+  console.info(`ADDRESS VALIDITY STATE: ${addressValidState}`)
+  const userId = user.session?.user.id
+  if (userId) contributor.setUserId(userId)
+})
 </script>
 
 
