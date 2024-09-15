1fa5016d09c89f4f465ee8a1b16c889acce302cd
Lukas <Lukas.nyberg@gmail.com>
2024-09-15 20:46:10 +0200

    fix(ceremony): make it interactive

--- ceremony/src/routes/+layout.svelte
+++ ceremony/src/routes/+layout.svelte
@@ -86,7 +86,7 @@ $effect(() => {
 <!--<Navbar/>-->
 <canvas
         id="canvas3d"
-        class=" w-full h-auto inset-0 absolute -z-10 canvas-fade"
+        class=" w-full h-auto inset-0 absolute z-0 canvas-fade"
         class:loaded={!loading}
 ></canvas>
 
