ef9288cfcbaa154362075fccfbdfafe3912c819e
Lukas <Lukas.nyberg@gmail.com>
2024-09-16 13:04:28 +0200

    fix(ceremony): ceremony changes

--- ceremony/src/lib/components/Counter/index.svelte
+++ ceremony/src/lib/components/Counter/index.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-import Zero from "./numbers/zero.svelte"
-import One from "./numbers/one.svelte"
-import Two from "./numbers/two.svelte"
-import Three from "./numbers/three.svelte"
-import Four from "./numbers/four.svelte"
-import Five from "./numbers/five.svelte"
-import Six from "./numbers/six.svelte"
-import Seven from "./numbers/seven.svelte"
-import Eight from "./numbers/eight.svelte"
-import Nine from "./numbers/nine.svelte"
-import H4 from "$lib/components/typography/H4.svelte"
+// import Zero from "./numbers/zero.svelte"
+// import One from "./numbers/one.svelte"
+// import Two from "./numbers/two.svelte"
+// import Three from "./numbers/three.svelte"
+// import Four from "./numbers/four.svelte"
+// import Five from "./numbers/five.svelte"
+// import Six from "./numbers/six.svelte"
+// import Seven from "./numbers/seven.svelte"
+// import Eight from "./numbers/eight.svelte"
+// import Nine from "./numbers/nine.svelte"
+// import H4 from "$lib/components/typography/H4.svelte"
+// import H1 from "$lib/components/typography/H1.svelte"
+// import Text from "$lib/components/typography/Text.svelte"
+import H2 from "$lib/components/typography/H2.svelte"
+import H3 from "$lib/components/typography/H3.svelte"
+import H1 from "$lib/components/typography/H1.svelte"
 
 type Props = {
   targetTimestamp: number
@@ -21,7 +26,7 @@ let hours = $state("00")
 let minutes = $state("00")
 let seconds = $state("00")
 
-const components = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+// const components = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 
 let interval: ReturnType<typeof setInterval>
 
@@ -56,26 +61,25 @@ $effect(() => {
 })
 </script>
 
-<div class="pointer-events-none p-8 bg-gradient-to-t from-transparent via-black/70 to-transparent backdrop-blur w-full flex items-center justify-center flex-col min-h-48">
-  <div class="flex flex-col md:flex-row  justify-center items-center gap-8">
-    {@render pair(hours, 'hours')}
-    {@render pair(minutes, 'minutes')}
-    {@render pair(seconds, 'seconds')}
+<div class="flex flex-col h-svh items-center justify-center gap-3">
+  <H1></H1>
+  <H2>Ceremony set to begin</H2>
+  <H3>2024-09-20 | <span class="text-union-accent-500">10:00 AM</span> (CET)</H3>
+  <div class="flex gap-2 justify-center">
+    {@render pair(hours, 'h')}
+    {@render pair(minutes, 'm')}
+    {@render pair(seconds, 's')}
   </div>
 </div>
+s
 
 {#snippet pair(time: string, timeType: string)}
-<div>
-  <div class="flex">
-    {#each time.split('') as digit, index (index + time)}
-      <div class="w-20 flex items-center justify-center text-white rounded mb-2">
-        {#if /^[0-9]$/.test(digit)}
-          {@const Component = components[parseInt(digit)]}
-          <Component/>
-        {/if}
-      </div>
-    {/each}
-  </div>
-  <H4>{timeType}</H4>
+<div class="flex">
+  {#each time.split('') as digit, index (index + time)}
+    <div class="flex text-white font-supermolot text-2xl font-semibold">
+      <div>{digit}</div>
+    </div>
+  {/each}
+  <div class="!text-union-accent-500 self-end uppercase">{timeType}</div>
 </div>
 {/snippet}
