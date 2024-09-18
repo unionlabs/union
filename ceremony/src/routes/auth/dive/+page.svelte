f2aa3d8dc0ee825214080736b137c32e467268e0
Lukas <Lukas.nyberg@gmail.com>
2024-09-18 11:54:40 +0200

    feat(ceremony): add button for google

--- ceremony/src/routes/auth/dive/+page.svelte
+++ ceremony/src/routes/auth/dive/+page.svelte
@@ -1,24 +1,35 @@
 <script lang="ts">
-import H1 from "$lib/components/typography/H1.svelte"
 import Button from "$lib/components/Button.svelte"
 import Spinner from "$lib/components/Spinner.svelte"
 import { page } from "$app/stores"
-import { auth } from "$lib/utils/auth.ts"
+import { supabase } from "$lib/supabase/client.ts"
 
-let loading = false
+async function diveIn(provider: "github" | "google") {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: provider,
+    options: {
+      redirectTo: `${$page.url.origin}/`
+    }
+  })
 
-async function login() {
-  loading = true
-  await auth(`${$page.url.origin}/`)
+  if (error) {
+    console.error("Error signing up with GitHub:", error.message)
+  }
 }
 </script>
 
-<div class="p-8  bg-gradient-to-t from-transparent via-black/50 to-transparent backdrop-blur w-full flex items-center flex-col">
-  <Button onclick={login}>
-    <span>Dive in using GitHub</span>
-    {#if loading}
-      <Spinner class="size-4"/>
-    {/if}
-  </Button>
+{#snippet Dive(provider)}
+<Button onclick={() => diveIn(provider)}>
+  <span>Dive in using {provider}</span>
+</Button>
+{/snippet}
+
+<div class="p-8 flex items-center flex-col gap-4">
+  {@render Dive('github')}
+  {@render Dive('google')}
 </div>
+
+<!--{#if loading}-->
+<!--  <Spinner class="size-4"/>-->
+<!--{/if}-->
 
