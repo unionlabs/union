a54b75c04d8ed4b1b1f695ba11bca38f22aed5db
o-az <omaraziz.dev@proton.me>
2024-09-19 01:42:55 -0700

    feat: graphiql playground

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,12 +1,15 @@
 import { loadEnv } from "vite"
 import vue from "@astrojs/vue"
+import react from "@astrojs/react"
 import svelte from "@astrojs/svelte"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
+import starlight from "@astrojs/starlight"
 import { defineConfig } from "astro/config"
-import { starlightConfig } from "./starlight.config.ts"
+import starlightUtils from "@lorenzo_lewis/starlight-utils"
 import { markdownConfiguration } from "./markdown.config.ts"
-import starlightLinksValidatorPlugin from "starlight-links-validator"
+import starlightHeadingBadges from "starlight-heading-badges"
+import starlightLinksValidator from "starlight-links-validator"
 
 const SITE_URL = "https://docs.union.build"
 
@@ -49,14 +52,165 @@ export default defineConfig({
   prefetch: { prefetchAll: true, defaultStrategy: "viewport" },
   redirects: { "/logo": "/union-logo.zip" },
   integrations: [
-    starlightConfig,
-    starlightLinksValidatorPlugin(),
+    starlight({
+      title: "Union",
+      tagline: "Connecting blockchains trustlessly",
+      description:
+        "Union is a hyper-efficient, zero-knowledge interoperability layer that connects Appchains, Layer 1, and Layer 2 networks.",
+      favicon: "/favicon.svg",
+      lastUpdated: true,
+      editLink: {
+        baseUrl: "https://github.com/unionlabs/union/edit/main/docs/"
+      },
+      social: {
+        github: "https://github.com/unionlabs",
+        discord: "https://discord.union.build",
+        "x.com": "https://x.com/union_build"
+      },
+      defaultLocale: "root",
+      locales: { root: { label: "English", lang: "en" } },
+      logo: {
+        alt: "Union Logo",
+        dark: "./src/assets/union-logo/union-logo-transparent.svg",
+        light: "./src/assets/union-logo/union-logo-white-transparent.svg"
+      },
+      expressiveCode: false,
+      head: [
+        {
+          tag: "meta",
+          attrs: {
+            name: "description",
+            content: "The Modular ZK Interoperability Layer"
+          }
+        },
+        {
+          tag: "meta",
+          attrs: { property: "og:image", content: "/og.png" }
+        },
+        {
+          tag: "meta",
+          attrs: { property: "twitter:image", content: "/og.png" }
+        },
+        {
+          tag: "script",
+          attrs: { src: "/scripts/anchor-targets.js" }
+        }
+      ],
+      sidebar: [
+        {
+          label: "DOCS",
+          items: [
+            {
+              label: "Introduction",
+              link: "/"
+            },
+            {
+              label: "Architecture",
+              autogenerate: {
+                directory: "/architecture"
+              }
+            },
+            {
+              label: "Concepts",
+              autogenerate: {
+                directory: "/concepts"
+              }
+            },
+            {
+              label: "Infrastructure",
+              items: [
+                {
+                  label: "Node Operators",
+                  collapsed: true,
+                  autogenerate: {
+                    directory: "/infrastructure/node-operators"
+                  }
+                }
+              ]
+            },
+            {
+              label: "Integrations",
+              items: [
+                {
+                  label: "Getting Started",
+                  link: "/integrations/getting-started"
+                },
+                {
+                  label: "TypeScript SDK",
+                  link: "/integrations/typescript",
+                  badge: { variant: "note", text: "new" }
+                },
+                {
+                  label: "API",
+                  collapsed: true,
+                  autogenerate: {
+                    directory: "/integrations/api"
+                  }
+                },
+                {
+                  label: "CometBFT",
+                  collapsed: true,
+                  autogenerate: {
+                    directory: "/integrations/cometbft"
+                  }
+                }
+              ]
+            },
+            {
+              label: "Joining the Testnet",
+              collapsed: true,
+              autogenerate: {
+                directory: "/joining-testnet"
+              }
+            }
+          ]
+        },
+        {
+          label: "API",
+          items: [
+            { label: "GraphQL", link: "/reference/graphql" }
+            // { label: "OpenAPI", link: "/reference/openapi" }
+          ]
+        }
+        // {
+        //   label: "leadingNavLinks",
+        //   items: [
+        //     { label: "Blog", link: "https://blog.union.build" },
+        //     { label: "Testnet App", link: "https://app.union.build" }
+        //   ]
+        // }
+      ],
+      plugins: [
+        starlightUtils({
+          multiSidebar: {
+            switcherStyle: "horizontalList"
+          }
+          // navLinks: {
+          //   leading: {
+          //     useSidebarLabelled: "leadingNavLinks"
+          //   }
+          // }
+        }),
+        starlightHeadingBadges(),
+        starlightLinksValidator()
+      ],
+      customCss: [
+        "./src/styles/index.css",
+        "./src/styles/fonts.css",
+        "./src/styles/tailwind.css",
+        "./src/styles/twoslash.css",
+        "./src/styles/starlight.css",
+        "./node_modules/katex/dist/katex.min.css",
+        "./node_modules/@shikijs/twoslash/style-rich.css"
+      ]
+    }),
     sitemap(),
     tailwind({
       applyBaseStyles: false,
       configFile: "tailwind.config.ts"
     }),
     svelte(),
-    vue({ jsx: true, devtools: true })
+    vue({ jsx: true, devtools: true }),
+    react({ experimentalReactChildren: true })
   ]
 })
--- docs/docs.nix
+++ docs/docs.nix
@@ -9,7 +9,7 @@
     {
       packages = {
         docs = mkCi false (unstablePkgs.buildNpmPackage {
-          npmDepsHash = "sha256-kWiBtd+T9TLhtl4anOnGuBrT6TBVxFcgUncOUlLbfqI=";
+          npmDepsHash = "sha256-9lKkB+iwN+0mZYk6VskpIabzTJ6oXMjRG0YZZOrn+rw=";
           src = ./.;
           srcs = [ ./. ./../evm/. ./../networks/genesis/. ./../versions/. ];
           sourceRoot = "docs";
