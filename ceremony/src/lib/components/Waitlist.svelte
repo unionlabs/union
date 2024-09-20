fe70703b08e784e66d1d9edc1110974cfe162fa2
Lukas <Lukas.nyberg@gmail.com>
2024-09-20 21:11:32 +0200

    fix(ceremony): fix waitlist

--- ceremony/src/lib/components/Waitlist.svelte
+++ ceremony/src/lib/components/Waitlist.svelte
@@ -4,6 +4,7 @@ import Code from "$lib/components/Code.svelte"
 import Text from "$lib/components/typography/Text.svelte"
 import H1 from "$lib/components/typography/H1.svelte"
 import type { ContributorState } from "$lib/stores/state.svelte.ts"
+import {getNumberSuffix} from "$lib/utils/utils.ts";
 
 type Props = {
   contributor: ContributorState
@@ -13,8 +14,8 @@ let { contributor }: Props = $props()
 </script>
 
 <H1 class="mb-4">You're on the waitlist </H1>
-<Text class="mb-2">When the ceremony opens to the public you will have position <span>{contributor.waitListPosition}</span> of public queue.</Text>
-<Text class="mb-12">You will be emailed 12-18 hours before the public phase begins.</Text>
+<Text class="mb-2">When the ceremony opens to the public, you will be <span class="text-union-accent-500">{contributor.waitListPosition}{getNumberSuffix(contributor.waitListPosition)}</span> in the public queue.</Text>
+<Text class="mb-12">You will receive an email 12-18 hours before the public phase begins.</Text>
 <H4 class="mb-4">Received an invite?</H4>
 <Text class="mb-4">You can skip the waitlist and join now.</Text>
 <form class="flex flex-col items-center">
