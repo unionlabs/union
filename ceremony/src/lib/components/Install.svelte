2374ba4dfffabb3f7748effaf117a24e1e7dd18a
Lukas <Lukas.nyberg@gmail.com>
2024-09-20 18:55:43 +0200

    fix(ceremony): button

--- ceremony/src/lib/components/Install.svelte
+++ ceremony/src/lib/components/Install.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-import Text from "$lib/components/typography/Text.svelte"
-import H1 from "$lib/components/typography/H1.svelte"
-import type { ContributorState } from "$lib/stores/state.svelte.ts"
-import Button from "$lib/components/Button.svelte"
-import { toast } from "svelte-sonner"
+  import Text from "$lib/components/typography/Text.svelte"
+  import H1 from "$lib/components/typography/H1.svelte"
+  import type {ContributorState} from "$lib/stores/state.svelte.ts"
+  import Button from "$lib/components/Button.svelte"
+  import {toast} from "svelte-sonner"
 
-type Props = {
-  contributor: ContributorState
-}
-let { contributor }: Props = $props()
+  type Props = {
+    contributor: ContributorState
+  }
+  let {contributor}: Props = $props()
 
-let command =
-  "docker pull ghcr.io/unionlabs/union/mpc-client:latest && docker run -p 4919:4919 -it ghcr.io/unionlabs/union/mpc-client:latest"
+  let command =
+    "docker pull ghcr.io/unionlabs/union/mpc-client:latest && docker run -p 4919:4919 -it ghcr.io/unionlabs/union/mpc-client:latest"
 
-const copy = () => {
-  navigator.clipboard.writeText(command)
-  toast.success("Copied to clipboard", { position: "bottom-right" })
-}
+  const copy = () => {
+    navigator.clipboard.writeText(command)
+    toast.success("Copied to clipboard", {position: "bottom-right"})
+  }
 </script>
 
 {#if contributor}
@@ -35,7 +35,9 @@ const copy = () => {
       and paste the command to start the MPC client then return here.
     </Text>
     <div class="max-w-4xl p-8">
-      <code onclick={copy} class="cursor-pointer text-sm sm:text-base inline-flex text-left items-center space-x-4 bg-black text-white p-4 pl-6 font-mono border-white border">
+      <button onclick={copy}>
+        <code
+              class="cursor-pointer text-sm sm:text-base inline-flex text-left items-center space-x-4 bg-black text-white p-4 pl-6 font-mono border-white border">
           <span class="flex gap-4">
               <span class="shrink-0 text-union-accent-500 select-none">
                   $
@@ -44,8 +46,6 @@ const copy = () => {
                   <span>{command}</span>
               </span>
           </span>
-
-        <Button onclick={copy} class="!bg-transparent text-neutral-500">
           <svg class="shrink-0 h-5 w-5" xmlns="http://www.w3.org/2000/svg"
                viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path d="M8 2a1 1 0 000 2h2a1 1 0 100-2H8z"></path>
@@ -53,8 +53,9 @@ const copy = () => {
                     d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z">
             </path>
           </svg>
-        </Button>
-      </code>
+
+        </code>
+      </button>
     </div>
     <Text>Is it running but still see this page? We support Chrome, FireFox and Brave. <br>For Brave disable the shields
       in the address bar.
