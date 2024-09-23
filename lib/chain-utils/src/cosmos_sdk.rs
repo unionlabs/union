b4b0c17c31773895d9990831954bbbf6c7d4e19d
benluelo <git@luelo.dev>
2024-09-23 22:59:31 +0100

    feat(voyager): tendermint modules

--- lib/chain-utils/src/cosmos_sdk.rs
+++ lib/chain-utils/src/cosmos_sdk.rs
@@ -889,7 +889,7 @@ pub mod cosmos_sdk_error {
         )
 
         // https://github.com/cosmos/ibc-go/blob/5ef01dae21dd60df22715b8e99bf641087a98879/modules/capability/types/errors.go
-        #[err(name = Capability, codespace = "capability")]
+        #[err(name = CapabilityError, codespace = "capability")]
         var (
             ErrInvalidCapabilityName    = errorsmod.Register(ModuleName, 2, "capability name not valid")
             ErrNilCapability            = errorsmod.Register(ModuleName, 3, "provided capability is nil")
@@ -898,6 +898,43 @@ pub mod cosmos_sdk_error {
             ErrCapabilityNotOwned       = errorsmod.Register(ModuleName, 6, "capability not owned by module")
             ErrCapabilityNotFound       = errorsmod.Register(ModuleName, 7, "capability not found")
             ErrCapabilityOwnersNotFound = errorsmod.Register(ModuleName, 8, "owners not found for capability")
+        )
+        // https://github.com/cosmos/ibc-go/blob/7f89b7dd8796eca1bfe07f8a7833f3ce2d7a8e04/modules/core/02-client/types/errors.go
+        // IBC client sentinel errors
+        #[err(name = ClientError, codespace = "client")]
+        var (
+            ErrClientExists                           = errorsmod.Register(SubModuleName, 2, "light client already exists")
+            ErrInvalidClient                          = errorsmod.Register(SubModuleName, 3, "light client is invalid")
+            ErrClientNotFound                         = errorsmod.Register(SubModuleName, 4, "light client not found")
+            ErrClientFrozen                           = errorsmod.Register(SubModuleName, 5, "light client is frozen due to misbehaviour")
+            ErrInvalidClientMetadata                  = errorsmod.Register(SubModuleName, 6, "invalid client metadata")
+            ErrConsensusStateNotFound                 = errorsmod.Register(SubModuleName, 7, "consensus state not found")
+            ErrInvalidConsensus                       = errorsmod.Register(SubModuleName, 8, "invalid consensus state")
+            ErrClientTypeNotFound                     = errorsmod.Register(SubModuleName, 9, "client type not found")
+            ErrInvalidClientType                      = errorsmod.Register(SubModuleName, 10, "invalid client type")
+            ErrRootNotFound                           = errorsmod.Register(SubModuleName, 11, "commitment root not found")
+            ErrInvalidHeader                          = errorsmod.Register(SubModuleName, 12, "invalid client header")
+            ErrInvalidMisbehaviour                    = errorsmod.Register(SubModuleName, 13, "invalid light client misbehaviour")
+            ErrFailedClientStateVerification          = errorsmod.Register(SubModuleName, 14, "client state verification failed")
+            ErrFailedClientConsensusStateVerification = errorsmod.Register(SubModuleName, 15, "client consensus state verification failed")
+            ErrFailedConnectionStateVerification      = errorsmod.Register(SubModuleName, 16, "connection state verification failed")
+            ErrFailedChannelStateVerification         = errorsmod.Register(SubModuleName, 17, "channel state verification failed")
+            ErrFailedPacketCommitmentVerification     = errorsmod.Register(SubModuleName, 18, "packet commitment verification failed")
+            ErrFailedPacketAckVerification            = errorsmod.Register(SubModuleName, 19, "packet acknowledgement verification failed")
+            ErrFailedPacketReceiptVerification        = errorsmod.Register(SubModuleName, 20, "packet receipt verification failed")
+            ErrFailedNextSeqRecvVerification          = errorsmod.Register(SubModuleName, 21, "next sequence receive verification failed")
+            ErrSelfConsensusStateNotFound             = errorsmod.Register(SubModuleName, 22, "self consensus state not found")
+            ErrUpdateClientFailed                     = errorsmod.Register(SubModuleName, 23, "unable to update light client")
+            ErrInvalidRecoveryClient                  = errorsmod.Register(SubModuleName, 24, "invalid recovery client")
+            ErrInvalidUpgradeClient                   = errorsmod.Register(SubModuleName, 25, "invalid client upgrade")
+            ErrInvalidHeight                          = errorsmod.Register(SubModuleName, 26, "invalid height")
+            ErrInvalidSubstitute                      = errorsmod.Register(SubModuleName, 27, "invalid client state substitute")
+            ErrInvalidUpgradeProposal                 = errorsmod.Register(SubModuleName, 28, "invalid upgrade proposal")
+            ErrClientNotActive                        = errorsmod.Register(SubModuleName, 29, "client state is not active")
+            ErrFailedMembershipVerification           = errorsmod.Register(SubModuleName, 30, "membership verification failed")
+            ErrFailedNonMembershipVerification        = errorsmod.Register(SubModuleName, 31, "non-membership verification failed")
+            ErrRouteNotFound                          = errorsmod.Register(SubModuleName, 32, "light client module route not found")
+            ErrClientTypeNotSupported                 = errorsmod.Register(SubModuleName, 33, "client type not supported")
         )
     }
 }
