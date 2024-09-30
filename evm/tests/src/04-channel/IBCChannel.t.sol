7e87654b8e5af67f12c1c5fc1bb60fc3dc132c0c
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-09-30 18:29:04 +0200

    feat(evm): ibc channel tests

--- evm/tests/src/04-channel/IBCChannel.t.sol
+++ evm/tests/src/04-channel/IBCChannel.t.sol
@@ -54,14 +54,27 @@ contract IBCChannelTests is Test {
         handler.connectionOpenConfirm(msgConfirm_);
     }
 
-    function test_channelOpenInit_ok(IBCMsgs.MsgChannelOpenInit memory msg_)
-        public
-    {
+    function test_channelOpenInit_ok(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        address relayer
+    ) public {
         vm.pauseGasMetering();
-        msg_.portId = address(module);
-        msg_.channel.state = IBCChannelState.Init;
-        msg_.channel.connectionId = connectionId;
-        msg_.channel.ordering = IBCChannelOrder.Unordered;
+        IBCMsgs.MsgChannelOpenInit memory msg_ = IBCMsgs.MsgChannelOpenInit({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.Init,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            relayer: relayer
+        });
         vm.expectEmit();
         emit IBCChannelLib.ChannelOpenInit(
             msg_.portId,
@@ -73,5 +86,508 @@ contract IBCChannelTests is Test {
         );
         vm.resumeGasMetering();
         handler.channelOpenInit(msg_);
+    }
+
+    function test_channelOpenInit_invalidState(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenInit memory msg_ = IBCMsgs.MsgChannelOpenInit({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.Unspecified,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            relayer: relayer
+        });
+        vm.expectRevert(IBCChannelLib.ErrInvalidChannelState.selector);
+        handler.channelOpenInit(msg_);
+    }
+
+    function test_channelOpenInit_invalidOrdering(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenInit memory msg_ = IBCMsgs.MsgChannelOpenInit({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.Init,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unspecified,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            relayer: relayer
+        });
+        vm.expectRevert(IBCChannelLib.ErrInvalidChannelOrdering.selector);
+        handler.channelOpenInit(msg_);
+    }
+
+    function test_channelOpenInit_channelClaimed(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenInit memory msg_ = IBCMsgs.MsgChannelOpenInit({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.Init,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            relayer: relayer
+        });
+        uint32 channelId = handler.channelOpenInit(msg_);
+        assertEq(handler.channelOwner(channelId), address(module));
+    }
+
+    function test_channelOpenInit_commitmentSaved(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        address relayer
+    ) public {
+        IBCChannel memory channel = IBCChannel({
+            state: IBCChannelState.Init,
+            connectionId: connectionId,
+            ordering: IBCChannelOrder.Unordered,
+            version: version,
+            counterparty: IBCChannelCounterparty({
+                portId: counterpartyPortId,
+                channelId: counterpartyChannelId
+            })
+        });
+        IBCMsgs.MsgChannelOpenInit memory msg_ = IBCMsgs.MsgChannelOpenInit({
+            portId: address(module),
+            channel: channel,
+            relayer: relayer
+        });
+        uint32 channelId = handler.channelOpenInit(msg_);
+        assertEq(
+            handler.commitments(IBCCommitment.channelCommitmentKey(channelId)),
+            keccak256(abi.encode(channel))
+        );
+    }
+
+    function test_channelOpenTry_ok(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        vm.pauseGasMetering();
+        IBCMsgs.MsgChannelOpenTry memory msg_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.TryOpen,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        vm.expectEmit();
+        emit IBCChannelLib.ChannelOpenTry(
+            msg_.portId,
+            IBCChannelLib.normalizePortId(msg_.portId),
+            0,
+            msg_.channel.counterparty.portId,
+            msg_.channel.counterparty.channelId,
+            msg_.channel.connectionId,
+            msg_.counterpartyVersion
+        );
+        vm.resumeGasMetering();
+        handler.channelOpenTry(msg_);
+    }
+
+    function test_channelOpenTry_invalidState(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenTry memory msg_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.Unspecified,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        vm.expectRevert(IBCChannelLib.ErrInvalidChannelState.selector);
+        handler.channelOpenTry(msg_);
+    }
+
+    function test_channelOpenTry_invalidOrdering(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenTry memory msg_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.TryOpen,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unspecified,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        vm.expectRevert(IBCChannelLib.ErrInvalidChannelOrdering.selector);
+        handler.channelOpenTry(msg_);
+    }
+
+    function test_channelOpenTry_invalidProof(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenTry memory msg_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.TryOpen,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        vm.expectRevert(IBCChannelLib.ErrInvalidProof.selector);
+        handler.channelOpenTry(msg_);
+    }
+
+    function test_channelOpenTry_channelClaimed(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenTry memory msg_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.TryOpen,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        uint32 channelId = handler.channelOpenTry(msg_);
+        assertEq(handler.channelOwner(channelId), address(module));
+    }
+
+    function test_channelOpenTry_commitmentSaved(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCChannel memory channel = IBCChannel({
+            state: IBCChannelState.TryOpen,
+            connectionId: connectionId,
+            ordering: IBCChannelOrder.Unordered,
+            version: version,
+            counterparty: IBCChannelCounterparty({
+                portId: counterpartyPortId,
+                channelId: counterpartyChannelId
+            })
+        });
+        IBCMsgs.MsgChannelOpenTry memory msg_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: channel,
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        uint32 channelId = handler.channelOpenTry(msg_);
+        assertEq(
+            handler.commitments(IBCCommitment.channelCommitmentKey(channelId)),
+            keccak256(abi.encode(channel))
+        );
+    }
+
+    function test_channelOpenInitOpenAck_ok(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        vm.pauseGasMetering();
+        IBCMsgs.MsgChannelOpenInit memory msgInit_ = IBCMsgs.MsgChannelOpenInit({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.Init,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            relayer: relayer
+        });
+        uint32 channelId = handler.channelOpenInit(msgInit_);
+        IBCMsgs.MsgChannelOpenAck memory msgAck_ = IBCMsgs.MsgChannelOpenAck({
+            portId: address(module),
+            channelId: channelId,
+            counterpartyVersion: counterpartyVersion,
+            counterpartyChannelId: counterpartyChannelId,
+            proofTry: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        vm.expectEmit();
+        emit IBCChannelLib.ChannelOpenAck(
+            address(module),
+            IBCChannelLib.normalizePortId(address(module)),
+            0,
+            counterpartyPortId,
+            counterpartyChannelId,
+            connectionId
+        );
+        vm.resumeGasMetering();
+        handler.channelOpenAck(msgAck_);
+    }
+
+    function test_channelOpenInitOpenAck_invalidState(
+        uint32 channelId,
+        uint32 counterpartyChannelId,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenAck memory msgAck_ = IBCMsgs.MsgChannelOpenAck({
+            portId: address(module),
+            channelId: channelId,
+            counterpartyVersion: counterpartyVersion,
+            counterpartyChannelId: counterpartyChannelId,
+            proofTry: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        vm.expectRevert(IBCChannelLib.ErrInvalidChannelState.selector);
+        handler.channelOpenAck(msgAck_);
+    }
+
+    function test_channelOpenInitOpenAck_commitmentSaved(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCChannel memory channel = IBCChannel({
+            state: IBCChannelState.Init,
+            connectionId: connectionId,
+            ordering: IBCChannelOrder.Unordered,
+            version: version,
+            counterparty: IBCChannelCounterparty({
+                portId: counterpartyPortId,
+                channelId: counterpartyChannelId
+            })
+        });
+        IBCMsgs.MsgChannelOpenInit memory msgInit_ = IBCMsgs.MsgChannelOpenInit({
+            portId: address(module),
+            channel: channel,
+            relayer: relayer
+        });
+        uint32 channelId = handler.channelOpenInit(msgInit_);
+        IBCMsgs.MsgChannelOpenAck memory msgAck_ = IBCMsgs.MsgChannelOpenAck({
+            portId: address(module),
+            channelId: channelId,
+            counterpartyVersion: counterpartyVersion,
+            counterpartyChannelId: counterpartyChannelId,
+            proofTry: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        channel.version = counterpartyVersion;
+        channel.state = IBCChannelState.Open;
+        lightClient.pushValidMembership();
+        handler.channelOpenAck(msgAck_);
+        assertEq(
+            handler.commitments(IBCCommitment.channelCommitmentKey(channelId)),
+            keccak256(abi.encode(channel))
+        );
+    }
+
+    function test_channelOpenTryOpenConfirm_ok(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        vm.pauseGasMetering();
+        IBCMsgs.MsgChannelOpenTry memory msgTry_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: IBCChannel({
+                state: IBCChannelState.TryOpen,
+                connectionId: connectionId,
+                ordering: IBCChannelOrder.Unordered,
+                version: version,
+                counterparty: IBCChannelCounterparty({
+                    portId: counterpartyPortId,
+                    channelId: counterpartyChannelId
+                })
+            }),
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        uint32 channelId = handler.channelOpenTry(msgTry_);
+        IBCMsgs.MsgChannelOpenConfirm memory msgConfirm_ = IBCMsgs
+            .MsgChannelOpenConfirm({
+            portId: address(module),
+            channelId: channelId,
+            proofAck: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        vm.expectEmit();
+        emit IBCChannelLib.ChannelOpenConfirm(
+            address(module),
+            IBCChannelLib.normalizePortId(address(module)),
+            0,
+            counterpartyPortId,
+            counterpartyChannelId,
+            connectionId
+        );
+        vm.resumeGasMetering();
+        handler.channelOpenConfirm(msgConfirm_);
+    }
+
+    function test_channelOpenTryOpenConfirm_invalidState(
+        uint32 channelId,
+        address relayer
+    ) public {
+        IBCMsgs.MsgChannelOpenConfirm memory msgConfirm_ = IBCMsgs
+            .MsgChannelOpenConfirm({
+            portId: address(module),
+            channelId: channelId,
+            proofAck: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        vm.expectRevert(IBCChannelLib.ErrInvalidChannelState.selector);
+        handler.channelOpenConfirm(msgConfirm_);
+    }
+
+    function test_channelOpenTryOpenConfirm_commitmentSaved(
+        bytes32 counterpartyPortId,
+        uint32 counterpartyChannelId,
+        bytes32 version,
+        bytes32 counterpartyVersion,
+        address relayer
+    ) public {
+        IBCChannel memory channel = IBCChannel({
+            state: IBCChannelState.TryOpen,
+            connectionId: connectionId,
+            ordering: IBCChannelOrder.Unordered,
+            version: version,
+            counterparty: IBCChannelCounterparty({
+                portId: counterpartyPortId,
+                channelId: counterpartyChannelId
+            })
+        });
+        IBCMsgs.MsgChannelOpenTry memory msgTry_ = IBCMsgs.MsgChannelOpenTry({
+            portId: address(module),
+            channel: channel,
+            counterpartyVersion: counterpartyVersion,
+            proofInit: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        uint32 channelId = handler.channelOpenTry(msgTry_);
+        IBCMsgs.MsgChannelOpenConfirm memory msgConfirm_ = IBCMsgs
+            .MsgChannelOpenConfirm({
+            portId: address(module),
+            channelId: channelId,
+            proofAck: hex"",
+            proofHeight: 0,
+            relayer: relayer
+        });
+        lightClient.pushValidMembership();
+        handler.channelOpenConfirm(msgConfirm_);
+        channel.state = IBCChannelState.Open;
+        assertEq(
+            handler.commitments(IBCCommitment.channelCommitmentKey(channelId)),
+            keccak256(abi.encode(channel))
+        );
     }
 }
