ab54769c3c6c8bed7cfdd365b45272cf6d4b9493
Lukas <Lukas.nyberg@gmail.com>
2024-10-01 22:12:27 +0200

    chore(ceremony): format

--- ceremony/vite.config.ts
+++ ceremony/vite.config.ts
@@ -1,13 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
 
-export default defineConfig((config) => {
-  const isProduction = config.mode === 'production';
+export default defineConfig(config => {
+  const isProduction = config.mode === "production"
 
   return {
     plugins: [sveltekit()],
     esbuild: {
       drop: isProduction ? ["console", "debugger"] : []
-    },
+    }
   }
 })
