e38afba9aa0638f73997fd57291075b9585c4597
Lukas <Lukas.nyberg@gmail.com>
2024-10-02 20:09:15 +0200

    fix(ceremony): add image

--- ceremony/src/routes/+layout.svelte
+++ ceremony/src/routes/+layout.svelte
@@ -76,9 +76,42 @@ const handleError = async (e: Event) => {
   const errorId = await sendWindowErrorLog(e)
   console.error(generateUserErrorMessage(errorId))
 }
+
+const imagePath = "https://ceremony.union.build/images/ceremony-og.png"
 </script>
 
 <svelte:window on:error={handleError} on:unhandledrejection={handleRejection} />
+
+<svelte:head>
+  <title>Union Ceremony</title>
+  <meta name="description" content="Ceremony to generate trustworthy cryptographic keys for securing the Union zero-knowledge system."/>
+
+  <meta property="og:title" content="Union Ceremony"/>
+  <meta property="og:description" content="Ceremony to generate trustworthy cryptographic keys for securing the Union zero-knowledge system."/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:url" content="https://ceremony.union.build"/>
+  <meta property="og:site_name" content="Union Ceremony"/>
+  <meta property="og:locale" content="en_US"/>
+  <meta property="og:image" content="https://ceremony.union.build/images/ceremony-og.png"/>
+  <meta property="og:image:secure_url" content="https://ceremony.union.build/images/ceremony-og.png"/>
+  <meta property="og:image:type" content="image/png"/>
+  <meta property="og:image:width" content="1200"/>
+  <meta property="og:image:height" content="630"/>
+  <meta property="og:image:alt" content="Union Ceremony event banner"/>
+
+  <meta name="twitter:title" content="Union Ceremony"/>
+  <meta name="twitter:description" content="Ceremony to generate trustworthy cryptographic keys for securing the Union zero-knowledge system."/>
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:site" content="@union_build"/>
+  <meta name="twitter:creator" content="@union_build"/>
+  <meta name="twitter:image" content="https://ceremony.union.build/images/ceremony-og.png"/>
+  <meta name="twitter:image:alt" content="Union Ceremony event banner"/>
+  <meta name="twitter:image:width" content="1200"/>
+  <meta name="twitter:image:height" content="630"/>
+
+  <link rel="canonical" href="https://ceremony.union.build"/>
+  <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+</svelte:head>
 
 {#if showBootSequence}
   <video
