4bdfdf20a97e848d17a17c3d93c9f957c874ebc4
Lukas <Lukas.nyberg@gmail.com>
2024-10-11 18:21:30 +0200

    fix(ceremony): change and move command

--- ceremony/src/lib/constants/index.ts
+++ ceremony/src/lib/constants/index.ts
@@ -1,1 +1,3 @@
 export const AXIOM_KEY = "xaat-c2ba1f52-0af4-4814-81d1-996c285912c8"
+export const COMMAND =
+  "mkdir -p ceremony && docker pull ghcr.io/unionlabs/union/mpc-client:v1.1 && docker run -v $(pwd)/ceremony:/ceremony -w /ceremony -p 4919:4919 --rm -it ghcr.io/unionlabs/union/mpc-client:v1.1"
