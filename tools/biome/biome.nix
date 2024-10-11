adb31504b35a90a246a5116d584898dfc64cc90e
o-az <omaraziz.dev@proton.me>
2024-10-10 21:46:41 -0700

    chore: biome 1.9.3

--- tools/biome/biome.nix
+++ tools/biome/biome.nix
@@ -29,7 +29,7 @@
 
       rustToolchain = rust.mkNightly { targets = [ CARGO_BUILD_TARGET ]; };
 
-      BIOME_VERSION = "1.9.2";
+      BIOME_VERSION = "1.9.3";
 
       biome = (crane.lib.overrideToolchain rustToolchain).buildPackage {
         inherit CARGO_BUILD_TARGET BIOME_VERSION;
