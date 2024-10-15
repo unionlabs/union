92c93f642c1f8bf3abf334810cded3f96aece1fa
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2024-10-15 14:32:00 +0200

    fix(tokenfactory): upgrade to appmodule v2

--- uniond/x/tokenfactory/module.go
+++ uniond/x/tokenfactory/module.go
@@ -13,12 +13,12 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"cosmossdk.io/core/registry"
-	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
 
-	abci "github.com/cometbft/cometbft/abci/types"
+	"cosmossdk.io/core/appmodule"
+	"cosmossdk.io/core/registry"
+	"google.golang.org/grpc"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -33,75 +33,16 @@ import (
 )
 
 var (
-	_ module.AppModule = AppModule{}
-	_ module.AppModule = AppModuleBasic{}
+	_ appmodule.AppModule             = AppModule{}
+	_ appmodule.HasGenesis            = AppModule{}
+	_ appmodule.HasRegisterInterfaces = AppModule{}
+
+	_ module.HasAminoCodec  = AppModule{}
+	_ module.HasGRPCGateway = AppModule{}
 )
 
 // ConsensusVersion defines the current x/tokenfactory module consensus version.
 const ConsensusVersion = 1
-
-// ----------------------------------------------------------------------------
-// AppModuleBasic
-// ----------------------------------------------------------------------------
-
-// AppModuleBasic implements the AppModuleBasic interface for the capability module.
-type AppModuleBasic struct{}
-
-func (AppModuleBasic) IsAppModule() {}
-
-func (AppModuleBasic) IsOnePerModuleType() {}
-
-func NewAppModuleBasic() AppModuleBasic {
-	return AppModuleBasic{}
-}
-
-// Name returns the x/tokenfactory module's name.
-func (AppModuleBasic) Name() string {
-	return types.ModuleName
-}
-
-func (AppModuleBasic) RegisterLegacyAminoCodec(registrar registry.AminoRegistrar) {
-	types.RegisterLegacyAminoCodec(registrar)
-}
-
-// RegisterInterfaces registers the module's interface types
-func (a AppModuleBasic) RegisterInterfaces(reg registry.InterfaceRegistrar) {
-	types.RegisterInterfaces(reg)
-}
-
-// DefaultGenesis returns the x/tokenfactory module's default genesis state.
-func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
-	return cdc.MustMarshalJSON(types.DefaultGenesis())
-}
-
-// ValidateGenesis performs genesis state validation for the x/tokenfactory module.
-func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, _ client.TxEncodingConfig, bz json.RawMessage) error {
-	var genState types.GenesisState
-	if err := cdc.UnmarshalJSON(bz, &genState); err != nil {
-		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
-	}
-
-	return genState.Validate()
-}
-
-// RegisterRESTRoutes registers the capability module's REST service handlers.
-func (AppModuleBasic) RegisterRESTRoutes(_ client.Context, _ *mux.Router) {
-}
-
-// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
-func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)) //nolint:errcheck
-}
-
-// GetTxCmd returns the x/tokenfactory module's root tx command.
-func (a AppModuleBasic) GetTxCmd() *cobra.Command {
-	return cli.GetTxCmd()
-}
-
-// GetQueryCmd returns the x/tokenfactory module's root query command.
-func (AppModuleBasic) GetQueryCmd() *cobra.Command {
-	return cli.GetQueryCmd()
-}
 
 // ----------------------------------------------------------------------------
 // AppModule
@@ -109,37 +50,77 @@ func (AppModuleBasic) GetQueryCmd() *cobra.Command {
 
 // AppModule implements the AppModule interface for the capability module.
 type AppModule struct {
-	AppModuleBasic
-
+	cdc           codec.Codec
 	keeper        keeper.Keeper
 	accountKeeper types.AccountKeeper
 	bankKeeper    types.BankKeeper
 }
 
 func NewAppModule(
+	cdc codec.Codec,
 	keeper keeper.Keeper,
 	accountKeeper types.AccountKeeper,
 	bankKeeper types.BankKeeper,
 ) AppModule {
 	return AppModule{
-		AppModuleBasic: NewAppModuleBasic(),
-		keeper:         keeper,
-		accountKeeper:  accountKeeper,
-		bankKeeper:     bankKeeper,
+		cdc:           cdc,
+		keeper:        keeper,
+		accountKeeper: accountKeeper,
+		bankKeeper:    bankKeeper,
 	}
 }
 
-// IsOnePerModuleType implements the depinject.OnePerModuleType interface.
-func (am AppModule) IsOnePerModuleType() { // marker
-}
+func (AppModule) IsAppModule() {}
 
-// IsAppModule implements the appmodule.AppModule interface.
-func (am AppModule) IsAppModule() { // marker
+func (AppModule) IsOnePerModuleType() {}
+
+func NewAppModuleBasic() AppModule {
+	return AppModule{}
 }
 
 // Name returns the x/tokenfactory module's name.
-func (am AppModule) Name() string {
-	return am.AppModuleBasic.Name()
+func (AppModule) Name() string {
+	return types.ModuleName
+}
+
+func (AppModule) RegisterGRPCGatewayRoutes(clientCtx client.Context, serveMux *runtime.ServeMux) {
+	err := types.RegisterQueryHandlerClient(context.Background(), serveMux, types.NewQueryClient(clientCtx))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (AppModule) RegisterLegacyAminoCodec(registrar registry.AminoRegistrar) {
+	types.RegisterLegacyAminoCodec(registrar)
+}
+
+// RegisterInterfaces registers the module's interface types
+func (a AppModule) RegisterInterfaces(reg registry.InterfaceRegistrar) {
+	types.RegisterInterfaces(reg)
+}
+
+// DefaultGenesis returns the x/tokenfactory module's default genesis state.
+func (am AppModule) DefaultGenesis() json.RawMessage {
+	return am.cdc.MustMarshalJSON(types.DefaultGenesis())
+}
+
+// ValidateGenesis performs genesis state validation for the wasm module.
+func (am AppModule) ValidateGenesis(bz json.RawMessage) error {
+	var gs types.GenesisState
+	if err := am.cdc.UnmarshalJSON(bz, &gs); err != nil {
+		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
+	}
+	return gs.Validate()
+}
+
+// GetTxCmd returns the x/tokenfactory module's root tx command.
+func (a AppModule) GetTxCmd() *cobra.Command {
+	return cli.GetTxCmd()
+}
+
+// GetQueryCmd returns the x/tokenfactory module's root query command.
+func (AppModule) GetQueryCmd() *cobra.Command {
+	return cli.GetQueryCmd()
 }
 
 // QuerierRoute returns the x/tokenfactory module's query routing key.
@@ -147,50 +128,38 @@ func (AppModule) QuerierRoute() string { return types.QuerierRoute }
 
 // RegisterServices registers a GRPC query service to respond to the
 // module-specific GRPC queries.
-func (am AppModule) RegisterServices(cfg module.Configurator) {
-	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
-	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
-
-	// If migration required
-	// m := keeper.NewMigrator(am.keeper, am.legacySubspace)
+func (am AppModule) RegisterServices(registrar grpc.ServiceRegistrar) error {
+	types.RegisterMsgServer(registrar, keeper.NewMsgServerImpl(am.keeper))
+	types.RegisterQueryServer(registrar, am.keeper)
+	return nil
 }
 
 // RegisterInvariants registers the x/tokenfactory module's invariants.
 func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
-// InitGenesis performs the x/tokenfactory module's genesis initialization. It
-// returns no validator updates.
-func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) []abci.ValidatorUpdate {
-	var genState types.GenesisState
-	cdc.MustUnmarshalJSON(gs, &genState)
-
-	am.keeper.InitGenesis(ctx, genState)
-
-	return []abci.ValidatorUpdate{}
+// InitGenesis performs genesis initialization for the wasm module. It returns
+// no validator updates.
+func (am AppModule) InitGenesis(ctx context.Context, bz json.RawMessage) error {
+	var gs types.GenesisState
+	err := am.cdc.UnmarshalJSON(bz, &gs)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal genesis state: %s", err)
+	}
+	am.keeper.InitGenesis(ctx, &gs)
+	return nil
 }
 
-// ExportGenesis returns the x/tokenfactory module's exported genesis state as raw
-// JSON bytes.
-func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
-	genState := am.keeper.ExportGenesis(ctx)
-	return cdc.MustMarshalJSON(genState)
+// ExportGenesis returns the exported genesis state as raw bytes for the wasm
+// module.
+func (am AppModule) ExportGenesis(ctx context.Context) (json.RawMessage, error) {
+	gs := am.keeper.ExportGenesis(ctx)
+	return am.cdc.MarshalJSON(gs)
 }
 
 // ConsensusVersion implements ConsensusVersion.
 func (AppModule) ConsensusVersion() uint64 {
 	return ConsensusVersion
 }
-
-// // BeginBlock executes all ABCI BeginBlock logic respective to the tokenfactory module.
-// func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
-
-// // EndBlock executes all ABCI EndBlock logic respective to the tokenfactory module. It
-// // returns no validator updates.
-// func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-// 	return []abci.ValidatorUpdate{}
-// }
-
-// ____________________________________________________________________________
 
 // AppModuleSimulation functions
 func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
