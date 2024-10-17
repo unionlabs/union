825faa2d52189b0de83660d5c7eefe867b6d6f5f
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-10-17 15:48:27 -0500

    chore: hahahahahahahahahaha

--- galoisd/vendor/github.com/consensys/gnark/test/flag_unset.go
+++
@@ -1,8 +0,0 @@
-//go:build !release_checks && !prover_checks
-
-package test
-
-const (
-	proverTestFlag  = false
-	releaseTestFlag = false
-)
--- galoisd/vendor/github.com/tunabay/go-bitarray/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021 Hirotsuna Mizuno. All rights reserved.
-// Use of this source code is governed by the MIT license that can be found in
-// the LICENSE file.
-
-/*
-Package bitarray provides data types and functions for manipurating bit arrays,
-aka bit strings, of arbitrary number of bits.
-*/
-package bitarray
