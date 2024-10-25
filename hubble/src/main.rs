9461caac89810f9c821e9bbcc207c6b65bd44a3b
jurriaan <jurriaan@pruijs.nl>
2024-10-25 09:52:59 +0200

    fix(hubble): debug ice - minimal change on  3d62619

--- hubble/src/main.rs
+++ hubble/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(more_qualified_paths)]
 #![feature(try_blocks)]
-#![allow(clippy::needless_lifetimes, clippy::manual_async_fn)]
+#![allow(clippy::manual_async_fn, clippy::needless_lifetimes)]
 #![feature(async_closure)]
 
 use std::time::Duration;
