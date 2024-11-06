ef9f713e6f9e91a644248689f2f1bd27580563c9
o-az <omaraziz.dev@proton.me>
2024-11-06 15:38:52 -0800

    fix: no timeout check

--- site/public/scripts/scroll-to-anchor.js
+++ site/public/scripts/scroll-to-anchor.js
@@ -2,18 +2,24 @@
  * This script is used to scroll to anchors in the blog post page.
  */
 
+/** @param {string} selector */
+const isElementLoaded = async selector => {
+  while (document.querySelector(selector) === null) {
+    await new Promise(resolve => requestAnimationFrame(resolve))
+  }
+  return document.querySelector(selector)
+}
+
 window.addEventListener(
   "DOMContentLoaded",
-  _event => {
-    setTimeout(() => {
-      try {
-        const url = new URL(window.location.href)
-        const anchorWrapper = document.querySelector(CSS.escape(url.hash).slice(1))
-        if (anchorWrapper) anchorWrapper?.scrollIntoView({ behavior: "smooth" })
-      } catch {
-        /* empty */
-      }
-    }, 500)
+  async _event => {
+    try {
+      const url = new URL(window.location.href)
+      const anchorWrapper = await isElementLoaded(CSS.escape(url.hash).slice(1))
+      if (anchorWrapper) anchorWrapper?.scrollIntoView({ behavior: "smooth" })
+    } catch {
+      /* empty */
+    }
   },
   { once: true }
 )
