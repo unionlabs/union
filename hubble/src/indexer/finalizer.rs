fd581794a9370a9c67b8b7969ac9b992aa2e8cb5
jurriaan <jurriaan@pruijs.nl>
2024-11-08 12:00:00 +0100

    feat(hubble): configurable reorg handling

--- hubble/src/indexer/finalizer.rs
+++ hubble/src/indexer/finalizer.rs
@@ -1,4 +1,4 @@
-use std::{cmp::min, time::Duration};
+use std::cmp::min;
 
 use color_eyre::eyre::Report;
 use tokio::time::sleep;
@@ -6,7 +6,7 @@ use tracing::{debug, info, info_span, trace, warn, Instrument};
 
 use super::{
     api::{BlockRange, FetcherClient, IndexerError},
-    postgres::get_next_block_to_refresh,
+    postgres::get_next_block_to_monitor,
     Indexer,
 };
 use crate::indexer::{
@@ -36,11 +36,19 @@ impl<T: FetcherClient> Indexer<T> {
                             reference
                         );
 
-                        if block_range_to_finalize.start_inclusive <= reference.height {
+                        // consider the block to be finalized if it's >= than the consensus height, considering the finalization delay blocks.
+                        let consensus_height_with_safety_margin = reference
+                            .height
+                            .saturating_sub(self.finalizer_config.delay_blocks as u64);
+
+                        let some_blocks_needs_to_be_finalized = block_range_to_finalize
+                            .start_inclusive
+                            <= consensus_height_with_safety_margin;
+                        if some_blocks_needs_to_be_finalized {
                             // find the end of the range to finalize
                             let end_of_chunk = block_range_to_finalize.start_inclusive
                                 + self.chunk_size as BlockHeight;
-                            let end_until_finalized = reference.height + 1;
+                            let end_until_finalized = consensus_height_with_safety_margin + 1;
                             let end_until_last_tracked_block =
                                 block_range_to_finalize.end_exclusive;
 
@@ -57,9 +65,13 @@ impl<T: FetcherClient> Indexer<T> {
                                 block_range_to_finalize, range_to_finalize
                             );
 
-                            self.finalize_blocks(&last_finalized, range_to_finalize)
-                                .instrument(info_span!("finalize"))
-                                .await?;
+                            self.finalize_blocks(
+                                &last_finalized,
+                                range_to_finalize,
+                                consensus_height_with_safety_margin,
+                            )
+                            .instrument(info_span!("finalize"))
+                            .await?;
                         } else {
                             trace!(
                                 "{}: nothing to finalize (before finalized {})",
@@ -68,39 +80,49 @@ impl<T: FetcherClient> Indexer<T> {
                             );
                         }
 
-                        if let Some(height) = self.next_block_to_refresh(reference.height).await? {
-                            let range_to_refresh = (height
+                        if let Some(height) = self
+                            .next_block_to_monitor(consensus_height_with_safety_margin)
+                            .await?
+                        {
+                            let range_to_monitor = (height
                                 ..(min(
                                     height + self.chunk_size as BlockHeight,
                                     block_range_to_finalize.end_exclusive,
                                 )))
                                 .into();
-                            info!("{}: finalizing", range_to_refresh);
+                            debug!(
+                                "{}: monitoring: {}",
+                                block_range_to_finalize, range_to_monitor
+                            );
 
-                            self.finalize_blocks(&last_finalized, range_to_refresh)
-                                .instrument(info_span!("monitor"))
-                                .await?;
+                            self.finalize_blocks(
+                                &last_finalized,
+                                range_to_monitor,
+                                consensus_height_with_safety_margin,
+                            )
+                            .instrument(info_span!("monitor"))
+                            .await?;
                         } else {
                             trace!("{}: nothing to update", block_range_to_finalize);
+
+                            if !some_blocks_needs_to_be_finalized {
+                                info!("idle finalize run => retry later");
+                                sleep(self.finalizer_config.retry_later_sleep).await;
+                            }
                         }
                     }
                     Err(IndexerError::NoBlock(_)) => {
                         info!("no finalized height => retry later");
-                        sleep(Duration::from_millis(1000)).await;
+                        sleep(self.finalizer_config.retry_later_sleep).await;
                     }
                     Err(error) => {
                         warn!("error fetching finalized height ({}) => retry later", error);
-                        sleep(Duration::from_millis(1000)).await;
+                        sleep(self.finalizer_config.retry_later_sleep).await;
                     }
-                }
-
-                if block_range_to_finalize.len() < self.chunk_size as u64 {
-                    info!("not much to finalize => retry later");
-                    sleep(Duration::from_millis(1000)).await;
                 }
             } else {
                 info!("nothing to finalize => retry later");
-                sleep(Duration::from_millis(1000)).await;
+                sleep(self.finalizer_config.retry_later_sleep).await;
             }
         }
     }
@@ -109,12 +131,11 @@ impl<T: FetcherClient> Indexer<T> {
         &self,
         last_finalized: &T::BlockHandle,
         block_range: BlockRange,
+        height_considered_to_be_finalized: BlockHeight,
     ) -> Result<(), Report> {
-        let last_finalized_reference = last_finalized.reference();
-
         last_finalized
             .fetch_range_expect_all(block_range.clone(), FetchMode::Lazy, |block| {
-                self.finalize_block(block, last_finalized_reference.height)
+                self.finalize_block(block, height_considered_to_be_finalized)
             })
             .instrument(info_span!("finalize"))
             .await?;
@@ -134,10 +155,7 @@ impl<T: FetcherClient> Indexer<T> {
 
         let mut tx = self.pg_pool.begin().await?;
 
-        // consider the block to be finalized if it's >= than the consensus height, considering the finalization delay blocks.
-        let is_finalized = last_finalized_height >= reference.height
-            && last_finalized_height - reference.height
-                >= self.finalizer_config.delay_blocks as u64;
+        let is_finalized = last_finalized_height >= reference.height;
 
         if let Some(old_hash) = match is_finalized {
             true => delete_block_status(&mut tx, self.indexer_id.clone(), reference.height).await?,
@@ -195,13 +213,18 @@ impl<T: FetcherClient> Indexer<T> {
         Ok(result)
     }
 
-    async fn next_block_to_refresh(
+    async fn next_block_to_monitor(
         &self,
         consensus_height: BlockHeight,
     ) -> Result<Option<BlockHeight>, Report> {
         let mut tx = self.pg_pool.begin().await?;
-        let result =
-            get_next_block_to_refresh(&mut tx, self.indexer_id.clone(), consensus_height).await?;
+        let result = get_next_block_to_monitor(
+            &mut tx,
+            self.indexer_id.clone(),
+            consensus_height,
+            self.finalizer_config.min_duration_between_monitor_checks,
+        )
+        .await?;
         tx.commit().await?;
 
         Ok(result)
