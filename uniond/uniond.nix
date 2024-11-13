8999a4d6797d010d267027edf58859176ff70ef2
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2024-11-13 10:04:56 -0600

    chore(uniond): update vendor hash

--- uniond/uniond.nix
+++ uniond/uniond.nix
@@ -76,7 +76,7 @@
                     (nix-filter.matchExt "md")
                   ];
                 };
-                vendorHash = "sha256-1VFtDDDjhDn/WBy7rW0DOHd/R3/wle7zCNqV+a3hk8w=";
+                vendorHash = "sha256-P+/F+JkEDcnQtTxLTesMGYWv4O33/poZ0iirsyJPUWU=";
                 doCheck = true;
                 meta.mainProgram = "uniond";
               }
