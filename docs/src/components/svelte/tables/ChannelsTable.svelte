0ba64a5dce68cee8404d9dc5a2baf89b497ccc63
o-az <omaraziz.dev@proton.me>
2024-11-18 10:04:26 -0800

    chore: add loading

--- docs/src/components/svelte/tables/ChannelsTable.svelte
+++ docs/src/components/svelte/tables/ChannelsTable.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-let promise = $state(fetchChannels())
+  let promise = $state(fetchChannels())
 
-async function fetchChannels() {
-  const response = await fetch("https://development.graphql.union.build/v1/graphql", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      query: /* GraphQL */ `
+  async function fetchChannels() {
+    const response = await fetch('https://development.graphql.union.build/v1/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
           query v1UcsPathsQuery {
             data: v1_ucs1_paths {
               channel_id
@@ -22,34 +22,35 @@ async function fetchChannels() {
               }
             }
           }
-        `
+        `,
+      }),
     })
-  })
-  console.info(new Date().toISOString())
-  const json = await response.json()
-  // @ts-expect-error
-  const dataArray = json.data.data
-  return {
-    data: {
-      headers: ["channel", "source label", "source chain", "dest. label", "dest. chain"],
-      // @ts-expect-error
-      rows: dataArray.map(item => [
-        item.channel_id.split("-").at(-1),
-        item.source_chain.display_name,
-        `${item.source_chain.rpc_type}/${item.source_chain.chain_id}`,
-        item.destination_chain.display_name,
-        `${item.destination_chain.rpc_type}/${item.destination_chain.chain_id}`
-      ])
+    const json = await response.json()
+    // @ts-expect-error
+    const dataArray = json.data.data
+    return {
+      data: {
+        headers: ['channel', 'source label', 'source chain', 'dest. label', 'dest. chain'],
+        // @ts-expect-error
+        rows: dataArray.map(item => [
+          item.channel_id.split('-').at(-1),
+          item.source_chain.display_name,
+          `${item.source_chain.rpc_type}/${item.source_chain.chain_id}`,
+          item.destination_chain.display_name,
+          `${item.destination_chain.rpc_type}/${item.destination_chain.chain_id}`,
+        ]),
+      },
     }
   }
-}
 </script>
 
 <div class="px-4 sm:px-6 lg:px-8 flex justify-center w-full">
   <div class="mt-8 flow-root">
     <div class="overflow-x-auto">
       <div class="inline-block min-w-full align-middle border rounded-sm border-neutral-400">
-        {#await promise then data}
+        {#await promise}
+          <p>Loading...</p>
+        {:then data}
           {@const { headers, rows } = data.data}
           <table class="divide-y divide-gray-300 tabular-nums">
             <thead class="w-full min-w-full">
