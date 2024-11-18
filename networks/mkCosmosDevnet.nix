d07d8de2c6e7e8277bdd5ac2262618626dc29ac7
cor <cor@pruijs.dev>
2024-11-18 11:34:05 +0000

    feat(devnet): add cpu profile

--- networks/mkCosmosDevnet.nix
+++ networks/mkCosmosDevnet.nix
@@ -849,6 +849,7 @@ let
 
                 ${nodeBin} \
                   start \
+                  --cpu-profile CPU_PROFILE \
                   --home home \
                   $$params \
                   --rpc.laddr              tcp://0.0.0.0:26657 \
