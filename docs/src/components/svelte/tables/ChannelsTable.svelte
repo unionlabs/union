7ade6ec8a3ce83a5ce457171551db8de2725b6ef
o-az <omaraziz.dev@proton.me>
2024-11-20 05:02:32 -0800

    feat: pagination

--- docs/src/components/svelte/tables/ChannelsTable.svelte
+++ docs/src/components/svelte/tables/ChannelsTable.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 import { dedent } from "ts-dedent"
 import { cn } from "#/lib/shadcn.ts"
-import { stringIsJSON } from "#/lib/utilities.ts"
 import jsonSvg from "#/assets/icons/json.svg?raw"
-import { Button } from "#/components/svelte/ui/button"
 import { highlightCode } from "#/lib/highlight-code.ts"
+import { splitArray, stringIsJSON } from "#/lib/utilities.ts"
 import * as Table from "#/components/svelte/ui/table/index.ts"
 import * as Pagination from "#/components/svelte/ui/pagination/index.ts"
 
-const promise = $state(fetchChannels())
-
+let pageNumber = $state(0)
 let toggleRowIcon = $state(jsonSvg)
+const promise = $state(fetchChannels())
 
 async function fetchChannels() {
   const response = await fetch("https://development.graphql.union.build/v1/graphql", {
@@ -19,7 +18,7 @@ async function fetchChannels() {
     body: JSON.stringify({
       query: /* GraphQL */ `
           query ChannelsForDocs {
-            v1_channels {
+            data: v1_channels {
               source_chain {
                 display_name
                 chain_id
@@ -36,28 +35,29 @@ async function fetchChannels() {
   })
   const json = await response.json()
   // @ts-expect-error
-  const dataArray = json.data.v1_channels
+  const dataArray = json.data.data
+  // @ts-expect-error
+  const rows = dataArray.map(item => [
+    item.source_chain.display_name,
+    item.source_connection_id?.split("-")?.at(-1),
+    item.source_channel_id?.split("-")?.at(-1),
+    item.status,
+    item.version
+  ])
 
   return {
     data: {
-      headers: ["chain", "connection", "channel", "status", "version"],
-      // @ts-expect-error
-      rows: dataArray.map(item => {
-        return [
-          item.source_chain.display_name,
-          item.source_connection_id,
-          item.source_channel_id,
-          item.status,
-          item.version
-        ]
-      })
+      headers: ["chain", "conn. #", "channel #", "status", "version"],
+      rows: splitArray({ array: rows, n: 6 }),
+      total: rows.length
     }
   }
 }
 
 async function attachContent(event: MouseEvent, rowIndex: number, version: unknown) {
-  const eventTarget = event.target as HTMLElement
+  let eventTarget = event.target?.closest("button") as HTMLElement
   if (!eventTarget.innerHTML) return
+
   const jsonSnippetElement = document.querySelector(`td[data-row-index="${rowIndex}"]`)
   if (!jsonSnippetElement) return
   const previousUncleElement = jsonSnippetElement.parentElement
@@ -90,12 +90,15 @@ async function attachContent(event: MouseEvent, rowIndex: number, version: unkno
 {#await promise}
   <div>Loading...</div>
 {:then data}
-  {@const { headers, rows } = data.data}
-  <Pagination.Root count={rows.length} perPage={16}>
+  {@const { headers, rows: rowsChunks, total } = data.data}
+  {@const [perPage, count] = [Number(rowsChunks.at(0)?.length), total]}
+  {@const rows = rowsChunks.at(pageNumber - 1 < 0 ? 0 : pageNumber - 1) as Array<string>}
+
+  <Pagination.Root {count} {perPage}>
     {#snippet children({ pages, currentPage })}
       <Pagination.Content>
         <Pagination.Item>
-          <Pagination.PrevButton />
+          <Pagination.PrevButton onclick={_ => (pageNumber = currentPage)} />
         </Pagination.Item>
         {#each pages as page (page.key)}
           {#if page.type === 'ellipsis'}
@@ -104,24 +107,32 @@ async function attachContent(event: MouseEvent, rowIndex: number, version: unkno
             </Pagination.Item>
           {:else}
             <Pagination.Item isVisible={currentPage === page.value}>
-              <Pagination.Link {page} isActive={currentPage === page.value}>
+              <Pagination.Link
+                {page}
+                isActive={currentPage === page.value}
+                onclick={_ => (pageNumber = page.value)}
+              >
                 {page.value}
               </Pagination.Link>
             </Pagination.Item>
           {/if}
         {/each}
         <Pagination.Item>
-          <Pagination.NextButton />
+          <Pagination.NextButton onclick={_ => (pageNumber = currentPage)} />
         </Pagination.Item>
       </Pagination.Content>
     {/snippet}
   </Pagination.Root>
-  <Table.Root class="w-full border rounded-sm">
+  <Table.Root class="w-full border border-neutral-500 rounded-sm">
     <Table.Header class="w-full">
       <Table.Row class="w-full">
         {#each headers as header, index}
           <Table.Head
-            class={cn(index === 0 && 'w-[100px]', index === headers.length - 1 && 'text-right')}
+            class={cn(
+              'text-nowrap uppercase',
+              index === 0 && 'w-[100px]',
+              index === headers.length - 1 && 'text-right',
+            )}
           >
             {header}
           </Table.Head>
@@ -130,40 +141,40 @@ async function attachContent(event: MouseEvent, rowIndex: number, version: unkno
     </Table.Header>
     <Table.Body class="w-full">
       {#each rows as row, rowIndex}
-        <Table.Row class={cn(['w-full'])}>
+        <Table.Row class={cn('w-full border-neutral-500')}>
           {#each row as cell, cellIndex}
             {@const lastColumn = cellIndex === row.length - 1}
             {#if lastColumn}
-              <Table.Cell class={cn('text-right')}>
+              <Table.Cell class={cn('text-right text-nowrap border-neutral-500')}>
                 {@const isJSON = stringIsJSON(cell)}
                 {#if isJSON}
                   {@const version = JSON.parse(cell)}
-                  <Button
-                    size="icon"
-                    variant="ghost"
-                    class="bg-transparent hover:bg-background/30 hover:cursor-pointer"
-                    onclick={async event => {
-                      event.preventDefault()
-                      await attachContent(event, rowIndex, version)
-                    }}
+                  <button
+                    onclick={event => attachContent(event, rowIndex, version)}
+                    class="bg-transparent hover:bg-background/30 hover:cursor-pointer size-8"
                   >
                     {@html toggleRowIcon}
-                  </Button>
+                  </button>
                 {:else}
                   {cell}
                 {/if}
               </Table.Cell>
             {:else}
-              <Table.Cell class={cn(cellIndex === 0 && 'font-medium w-[135px]')}>
+              <Table.Cell
+                class={cn(
+                  'border-neutral-500',
+                  cellIndex === 0 && 'font-medium w-[135px] text-nowrap',
+                )}
+              >
                 {cell}
               </Table.Cell>
             {/if}
           {/each}
         </Table.Row>
-        <Table.Row>
+        <Table.Row class="border-none">
           <Table.Cell
             colspan={5}
-            class="p-0 border-t-transparent"
+            class="p-0 border-transparent"
             data-json-snippet
             data-row-index={rowIndex}
           ></Table.Cell>
@@ -178,6 +189,10 @@ async function attachContent(event: MouseEvent, rowIndex: number, version: unkno
 <style lang="postcss">
   :global(.sl-markdown-content table:not(:where(.not-content *))) {
     display: table;
+  }
+
+  :global(.sl-markdown-content th:not(:where(.not-content *))) {
+    border-bottom: 0.8px solid #a1a1ab;
   }
 
   :global(li::marker) {
--- docs/src/components/svelte/ui/table/table-cell.svelte
+++ docs/src/components/svelte/ui/table/table-cell.svelte
@@ -13,7 +13,7 @@ let {
 
 <td
 	bind:this={ref}
-	class={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+	class={cn("p-3 align-middle [&:has([role=checkbox])]:pr-0", className)}
 	{...restProps}
 >
 	{@render children?.()}
