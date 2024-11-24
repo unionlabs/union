0cb9eb7e87176117139b55193e747b99618460bf
aeryz <abdullaheryz@protonmail.com>
2024-11-24 22:00:40 +0300

    chore(eth-lc): add ethabi feature
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- cosmwasm/union-ibc/light-clients/ethereum/types/src/consensus_state.rs
+++ cosmwasm/union-ibc/light-clients/ethereum/types/src/consensus_state.rs
@@ -1,7 +1,4 @@
-use core::array::TryFromSliceError;
-
-use alloy::sol_types::SolValue;
-use unionlabs::{bls::BlsPublicKey, hash::H256, impl_ethabi_via_try_from_into};
+use unionlabs::{bls::BlsPublicKey, hash::H256};
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -18,63 +15,73 @@ pub struct ConsensusState {
     pub next_sync_committee: BlsPublicKey,
 }
 
-alloy::sol! {
-    struct SolConsensusState {
-        uint64 slot;
-        bytes32 state_root;
-        bytes32 storage_root;
-        uint64 timestamp;
-        bytes current_sync_committee;
-        bytes next_sync_committee;
-    }
-}
+#[cfg(feature = "ethabi")]
+pub mod ethabi {
+    use core::array::TryFromSliceError;
 
-impl From<ConsensusState> for SolConsensusState {
-    fn from(value: ConsensusState) -> Self {
-        Self {
-            slot: value.slot,
-            state_root: value.state_root.get().into(),
-            storage_root: value.storage_root.get().into(),
-            timestamp: value.timestamp,
-            current_sync_committee: value.current_sync_committee.0.into(),
-            next_sync_committee: value.next_sync_committee.0.into(),
+    use alloy::sol_types::SolValue;
+    use unionlabs::impl_ethabi_via_try_from_into;
+
+    use super::*;
+
+    impl_ethabi_via_try_from_into!(ConsensusState => SolConsensusState);
+
+    alloy::sol! {
+        struct SolConsensusState {
+            uint64 slot;
+            bytes32 state_root;
+            bytes32 storage_root;
+            uint64 timestamp;
+            bytes current_sync_committee;
+            bytes next_sync_committee;
         }
     }
-}
 
-impl_ethabi_via_try_from_into!(ConsensusState => SolConsensusState);
+    impl From<ConsensusState> for SolConsensusState {
+        fn from(value: ConsensusState) -> Self {
+            Self {
+                slot: value.slot,
+                state_root: value.state_root.get().into(),
+                storage_root: value.storage_root.get().into(),
+                timestamp: value.timestamp,
+                current_sync_committee: value.current_sync_committee.0.into(),
+                next_sync_committee: value.next_sync_committee.0.into(),
+            }
+        }
+    }
 
-#[derive(Debug, thiserror::Error)]
-pub enum TryFromEthAbiError {
-    #[error("invalid current sync committee")]
-    CurrentSyncCommittee(#[source] TryFromSliceError),
-    #[error("invalid next sync committee")]
-    NextSyncCommittee(#[source] TryFromSliceError),
-}
+    #[derive(Debug, thiserror::Error)]
+    pub enum TryFromEthAbiError {
+        #[error("invalid current sync committee")]
+        CurrentSyncCommittee(#[source] TryFromSliceError),
+        #[error("invalid next sync committee")]
+        NextSyncCommittee(#[source] TryFromSliceError),
+    }
 
-impl TryFrom<SolConsensusState> for ConsensusState {
-    type Error = TryFromEthAbiError;
+    impl TryFrom<SolConsensusState> for ConsensusState {
+        type Error = TryFromEthAbiError;
 
-    fn try_from(value: SolConsensusState) -> Result<Self, Self::Error> {
-        Ok(Self {
-            slot: value.slot,
-            state_root: H256::new(value.state_root.0),
-            storage_root: H256::new(value.storage_root.0),
-            timestamp: value.timestamp,
-            current_sync_committee: BlsPublicKey(
-                value
-                    .current_sync_committee
-                    .as_ref()
-                    .try_into()
-                    .map_err(TryFromEthAbiError::CurrentSyncCommittee)?,
-            ),
-            next_sync_committee: BlsPublicKey(
-                value
-                    .next_sync_committee
-                    .as_ref()
-                    .try_into()
-                    .map_err(TryFromEthAbiError::NextSyncCommittee)?,
-            ),
-        })
+        fn try_from(value: SolConsensusState) -> Result<Self, Self::Error> {
+            Ok(Self {
+                slot: value.slot,
+                state_root: H256::new(value.state_root.0),
+                storage_root: H256::new(value.storage_root.0),
+                timestamp: value.timestamp,
+                current_sync_committee: BlsPublicKey(
+                    value
+                        .current_sync_committee
+                        .as_ref()
+                        .try_into()
+                        .map_err(TryFromEthAbiError::CurrentSyncCommittee)?,
+                ),
+                next_sync_committee: BlsPublicKey(
+                    value
+                        .next_sync_committee
+                        .as_ref()
+                        .try_into()
+                        .map_err(TryFromEthAbiError::NextSyncCommittee)?,
+                ),
+            })
+        }
     }
 }
