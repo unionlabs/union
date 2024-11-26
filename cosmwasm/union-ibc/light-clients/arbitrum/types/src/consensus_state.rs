142af82241ba4160c9aa806f303a03300c65724f
aeryz <abdullaheryz@protonmail.com>
2024-11-26 21:30:37 +0300

    chore(arb-lc): add `SolConsensusState`
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- cosmwasm/union-ibc/light-clients/arbitrum/types/src/consensus_state.rs
+++ cosmwasm/union-ibc/light-clients/arbitrum/types/src/consensus_state.rs
@@ -6,3 +6,38 @@ pub struct ConsensusState {
     pub ibc_storage_root: H256,
     pub timestamp: u64,
 }
+
+#[cfg(feature = "ethabi")]
+pub mod ethabi {
+    use alloy::sol_types::SolValue;
+    use unionlabs::impl_ethabi_via_try_from_into;
+
+    use super::*;
+
+    impl_ethabi_via_try_from_into!(ConsensusState => SolConsensusState);
+
+    alloy::sol! {
+        struct SolConsensusState {
+            bytes32 ibc_storage_root;
+            uint64 timestamp;
+        }
+    }
+
+    impl From<ConsensusState> for SolConsensusState {
+        fn from(value: ConsensusState) -> SolConsensusState {
+            Self {
+                ibc_storage_root: value.ibc_storage_root.get().into(),
+                timestamp: value.timestamp,
+            }
+        }
+    }
+
+    impl From<SolConsensusState> for ConsensusState {
+        fn from(value: SolConsensusState) -> ConsensusState {
+            Self {
+                ibc_storage_root: H256::new(value.ibc_storage_root.0),
+                timestamp: value.timestamp,
+            }
+        }
+    }
+}
