d19bea7450a3348a822cafd686483e546f809bfd
aeryz <abdullaheryz@protonmail.com>
2024-12-02 11:21:01 +0300

    chore: move `light-clients/*/types` under `lib`
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- cosmwasm/union-ibc/light-clients/arbitrum/types/src/consensus_state.rs
+++
@@ -1,8 +0,0 @@
-use unionlabs::hash::H256;
-
-#[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct ConsensusState {
-    pub ibc_storage_root: H256,
-    pub timestamp: u64,
-}
---
+++ lib/arbitrum-light-client-types/src/client_state.rs
@@ -0,0 +1,20 @@
+use unionlabs::{
+    bounded::BoundedU32, hash::H160, ibc::core::client::height::Height, id::ClientId, uint::U256,
+};
+
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct ClientState {
+    pub l1_client_id: ClientId,
+    pub chain_id: U256,
+    pub l1_latest_slot: u64,
+    pub l1_contract_address: H160,
+    pub l1_next_node_num_slot: U256,
+    pub l1_nodes_slot: U256,
+    // this is a u32 because protobuf sucks
+    pub l1_next_node_num_slot_offset_bytes: BoundedU32<0, 24>,
+    // TODO: Rename this in the protos
+    pub l1_nodes_confirm_data_offset: U256,
+    pub frozen_height: Height,
+    pub l2_ibc_contract_address: H160,
+}
