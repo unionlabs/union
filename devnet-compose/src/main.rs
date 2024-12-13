6e38e73f909e9cc6baf8ab789c9f90ce3dc674fa
itsasecret <87604944+tudorpintea999@users.noreply.github.com>
2024-12-13 14:17:07 +0200

    Update main.rs

--- devnet-compose/src/main.rs
+++ devnet-compose/src/main.rs
@@ -15,7 +15,9 @@ mod process_compose;
 mod theme;
 mod voyager;
 
+// Configuration Constants
 const LOGS_BASE_PATH: &str = "./.devnet/logs/";
+const CONFIG_PATH: &str = "./voyager-config.json";
 
 pub fn log_path(process_name: &str) -> String {
     format!("{LOGS_BASE_PATH}{process_name}.log")
@@ -31,26 +33,19 @@ pub enum Network {
 
 impl Network {
     fn to_process(self) -> Process {
-        Process {
-            name: self.network_id().clone(),
-            command: format!("nix run .#{}", self.network_id()),
-            is_daemon: None,
-            disabled: None,
-            depends_on: None,
-            liveliness_probe: None,
-            readiness_probe: Some(Probe::http_get(self.probe_port(), "/block?height=2")),
-            log_configuration: LogConfiguration::default(),
-            log_location: log_path(&self.network_id()),
-            shutdown: ShutdownConfig::default(),
-            availability: Some(RestartPolicy::always(10)),
-        }
+        ProcessBuilder::new(
+            &self.network_id(),
+            &format!("nix run .#{}", self.network_id()),
+        )
+        .readiness_probe(Probe::http_get(self.probe_port(), "/block?height=2"))
+        .availability(RestartPolicy::always(10))
+        .build()
     }
 
     fn network_id(&self) -> String {
         format!("devnet-{}", self.to_string().to_lowercase())
     }
 
-    // TODO: Put this in a config file
     fn probe_port(&self) -> usize {
         match self {
             Network::Union => 26657,
@@ -61,7 +56,6 @@ impl Network {
     }
 
     fn cometbls_light_client_config(&self) -> String {
-        // TODO: this is a bit hacky, well need better Network types rather than an assertion here.
         assert!(
             self != &Network::Union,
             "Tried to get cometbls client id on union"
@@ -70,11 +64,14 @@ impl Network {
             "./.devnet/homes/{}/code-ids/cometbls_light_client",
             self.to_string().to_lowercase()
         ))
-        .unwrap_or_else(|_| panic!("could not find code-id for cometbls_light_client on {self}"));
+        .unwrap_or_else(|_| panic!(
+            "could not find code-id for cometbls_light_client on {self}"
+        ));
 
-        let cometbls_lightclient_checksum = cometbls_lightclient_checksum.trim().to_string();
-
-        format!("'{{\"checksum\":\"0x{cometbls_lightclient_checksum}\"}}'")
+        format!(
+            "'{{\"checksum\":\"0x{}\"}}'",
+            cometbls_lightclient_checksum.trim()
+        )
     }
 }
 
@@ -85,7 +82,6 @@ pub struct DevnetConfig {
 }
 
 pub fn connection_to_process((net_a, net_b): &(Network, Network)) -> Process {
-    use Network::*;
     let name = format!(
         "connection-{}-{}",
         net_a.to_string().to_lowercase(),
@@ -93,48 +89,41 @@ pub fn connection_to_process((net_a, net_b): &(Network, Network)) -> Process {
     );
 
     let (client_a_config, client_b_config) = match (net_a, net_b) {
-        (Union, n) => ("null".to_string(), n.cometbls_light_client_config()),
-        (n, Union) => (n.cometbls_light_client_config(), "null".to_string()),
+        (Network::Union, n) => ("null".to_string(), n.cometbls_light_client_config()),
+        (n, Network::Union) => (n.cometbls_light_client_config(), "null".to_string()),
         (_, _) => ("null".to_string(), "null".to_string()),
     };
 
-    Process {
-        name: name.clone(),
-        disabled: None,
-        is_daemon: Some(true),
-        command: format!("set -o pipefail; nix run .#voyager -- queue enqueue \"$(nix run -L .#voyager -- -c ./voyager-config.json handshake {} {} --client-a-config {} --client-b-config {} --create-clients --open-connection --connection-ordering unordered --init-fetch)\"",net_a.network_id(), net_b.network_id(), client_a_config, client_b_config ),
-
-        log_configuration: LogConfiguration::default(),
-        log_location: log_path(&name),
-        depends_on: Some(HashMap::from([
-            (net_a.to_process().name,ProcessDependency::healthy()),
-            (net_b.to_process().name,ProcessDependency::healthy()),
-            (voyager::relay_process(&[]).name,ProcessDependency::healthy())
-        ])),
-        liveliness_probe: None,
-        readiness_probe: None, // TODO
-        shutdown: ShutdownConfig::default(),
-        availability: None,
-    }
+    ProcessBuilder::new(
+        &name,
+        &format!(
+            "set -o pipefail; nix run .#voyager -- queue enqueue \"$(nix run -L .#voyager -- -c {CONFIG_PATH} handshake {} {} --client-a-config {} --client-b-config {} --create-clients --open-connection --connection-ordering unordered --init-fetch)\"",
+            net_a.network_id(), net_b.network_id(), client_a_config, client_b_config
+        ),
+    )
+    .is_daemon(true)
+    .depends_on(HashMap::from([
+        (net_a.to_process().name, ProcessDependency::healthy()),
+        (net_b.to_process().name, ProcessDependency::healthy()),
+        (voyager::relay_process(&[]).name, ProcessDependency::healthy()),
+    ]))
+    .build()
 }
 
 impl DevnetConfig {
     pub fn to_process_compose(&self) -> Project {
         let mut project = Project::default();
 
-        // Add a devnet for each network
         for network in self.networks.clone() {
             project.add_process(network.to_process());
         }
 
         if !self.connections.is_empty() {
-            // There are connections, so we need voyager running with applied migrations
             project.add_process(voyager::queue_process());
             project.add_process(voyager::migrations_process());
             project.add_process(voyager::relay_process(&self.networks));
 
             if self.networks.contains(&Network::Union) {
-                // There are connections to Union, so we need to prove Union consensus
                 project.add_process(galois::download_circuit_process());
                 project.add_process(galois::galoisd_process());
             }
@@ -148,73 +137,72 @@ impl DevnetConfig {
     }
 }
 
-fn main() {
-    cliclack::set_theme(theme::UnionTheme);
-    let _ = cliclack::clear_screen();
-
-    intro(style(" Union Devnet Compose ").on_cyan().black().bold()).unwrap();
-    let networks: Vec<Network> = multiselect("Which networks do you want to include?")
+fn setup_network_selection() -> Vec<Network> {
+    multiselect("Which networks do you want to include?")
         .initial_values(vec![])
-        .item(Union, "Union", "recommended")
-        .item(Osmosis, "Osmosis", "")
-        .item(Stargaze, "Stargaze", "")
-        .item(Simd, "Simd", "")
+        .item(Network::Union, "Union", "recommended")
+        .item(Network::Osmosis, "Osmosis", "")
+        .item(Network::Stargaze, "Stargaze", "")
+        .item(Network::Simd, "Simd", "")
         .interact()
-        .unwrap();
+        .unwrap()
+}
 
-    let mut connections: Vec<(Network, Network)> = Vec::new();
+fn setup_connections(networks: &[Network]) -> Vec<(Network, Network)> {
     if networks.len() > 1 {
-        let connection_options: Vec<((Network, Network), String, String)> = networks
-            .clone()
-            .into_iter()
+        let connection_options = networks
+            .iter()
             .combinations(2)
-            .map(|mut combo| (combo.remove(0), combo.remove(0)))
-            .map(|combo @ (net_a, net_b)| {
+            .map(|combo| {
+                let (net_a, net_b) = (combo[0], combo[1]);
                 (
-                    combo,
-                    format!("{net_a} <-> {net_b}").to_string(),
-                    if net_a == Union || net_b == Union {
+                    (*net_a, *net_b),
+                    format!("{net_a} <-> {net_b}"),
+                    if *net_a == Network::Union || *net_b == Network::Union {
                         "Will include Galois services".to_string()
                     } else {
                         "".to_string()
                     },
                 )
             })
-            .collect();
-        connections = multiselect("Which IBC connections do you want to set up?")
+            .collect::<Vec<_>>();
+
+        multiselect("Which IBC connections do you want to set up?")
             .items(&connection_options)
             .required(false)
             .interact()
-            .unwrap();
+            .unwrap_or_default()
+    } else {
+        Vec::new()
     }
+}
 
-    let info_text = format!("Tips:\n - Run {} in a second terminal tab to view logs.\n - You can restart single processes in the interface with ctrl+r.\n - You can view the generated process composition at `process-compose.yml` in the repo root.\n - Processes are designed to be overridden for a fast dev feedback cycle.\n   For example, add a `process-compose.override.yml` to the repo root with the following contents\n   to use a cargo debug build of voyager instead of the nix build.", style("`nix run .#devnet-logs`").cyan().bold()) +
-    r##"
-    {
-      "processes": {
-        "voyager-relay" : {
-          "command" : "RUST_LOG=info cargo run -p voyager"
-        }
-      }
-    }
-    "##;
+fn save_project_to_file(project: &Project) {
+    let project_str = serde_json::to_string_pretty(project).expect("failed to serialize project");
+    fs::write("process-compose.yml", project_str).expect("failed to write process-compose.yml");
+}
+
+fn main() {
+    cliclack::set_theme(theme::UnionTheme);
+    let _ = cliclack::clear_screen();
+
+    intro(style(" Union Devnet Compose ").on_cyan().black().bold()).unwrap();
+    let networks = setup_network_selection();
+    let connections = setup_connections(&networks);
 
     cliclack::note(
         style(" Devnet generated! ").on_cyan().black().bold(),
-        info_text,
+        format!(
+            "Tips:\n - Run {} in a second terminal tab to view logs.\n - Restart single processes with ctrl+r.\n - View the generated process composition in process-compose.yml.\n",
+            style("`nix run .#devnet-logs`").cyan().bold()
+        ),
     )
     .unwrap();
 
-    use Network::*;
-    let config = DevnetConfig {
-        networks,
-        connections,
-    };
-
+    let config = DevnetConfig { networks, connections };
     let project = config.to_process_compose();
-    let project = serde_json::to_string_pretty(&project).expect("failed to serialize project");
 
-    fs::write("process-compose.yml", project).expect("failed to write contents");
+    save_project_to_file(&project);
 
     let answer = cliclack::confirm("Ready to launch the devnet?")
         .initial_value(true)
