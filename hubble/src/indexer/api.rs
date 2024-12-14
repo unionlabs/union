2db75c3e991323dc42d98a828dac6518c769061d
itsasecret <87604944+tudorpintea999@users.noreply.github.com>
2024-12-14 12:53:11 +0200

    Update api.rs
    errors
    Added traits and derived additional ones (PartialEq, Hash).
    Simplified and standardized conversions using From/Into.

--- hubble/src/indexer/api.rs
+++ hubble/src/indexer/api.rs
@@ -1,4 +1,7 @@
-use std::{fmt::Display, ops::Range};
+use std::{
+    fmt::{Debug, Display},
+    ops::Range,
+};
 
 use axum::async_trait;
 use color_eyre::eyre::Report;
@@ -6,72 +9,63 @@ use futures::Stream;
 use sqlx::Postgres;
 use time::OffsetDateTime;
 use tokio::task::JoinSet;
-use tracing::error;
+use tracing::{debug, error};
 
-#[derive(Debug, thiserror::Error)]
-pub enum IndexerError {
-    #[error("received unexpected height {0}: expecting {1}")]
-    UnexpectedHeightSingle(BlockHeight, BlockHeight),
-    #[error("received unexpected height {0} (range {1}): expecting {2}")]
-    UnexpectedHeightRange(BlockHeight, BlockRange, BlockHeight),
-    #[error("error reading block {0} (range {1}): {2}")]
-    ErrorReadingBlock(BlockHeight, BlockRange, Report),
-    #[error("expected to receive block {0} (range {1})")]
-    MissingBlock(BlockHeight, BlockRange),
-    #[error("received block while not expecting more (range {0}): {1}")]
-    TooManyBlocks(BlockRange, BlockReference),
-    #[error("received error while not expecting more (range {0}): {1}")]
-    TooManyBlocksError(BlockRange, Report),
-    #[error("no block at: {0}")]
-    NoBlock(BlockSelection),
-    #[error("database error: {0}")]
-    DatabaseError(sqlx::Error),
-    #[error("provider error: {0}")]
-    ProviderError(Report),
-    #[error("internal error: {0}")]
-    InternalError(Report),
+/// Enum representing fetch modes.
+#[derive(Clone, Copy, Debug)]
+pub enum FetchMode {
+    /// Fetch data eagerly.
+    Eager,
+    /// Fetch data lazily.
+    Lazy,
 }
 
-impl From<Report> for IndexerError {
-    fn from(error: Report) -> Self {
-        Self::InternalError(error)
+/// Represents the selection of a block (by height or last finalized).
+#[derive(Debug)]
+pub enum BlockSelection {
+    LastFinalized,
+    Height(BlockHeight),
+}
+
+impl Display for BlockSelection {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BlockSelection::LastFinalized => write!(f, "last-finalized"),
+            BlockSelection::Height(height) => write!(f, "{}", height),
+        }
     }
 }
 
-impl From<sqlx::Error> for IndexerError {
-    fn from(error: sqlx::Error) -> Self {
-        Self::DatabaseError(error)
-    }
-}
-
-pub type IndexerId = String;
 pub type BlockHeight = u64;
 pub type BlockHash = String;
 pub type BlockTimestamp = OffsetDateTime;
 
-#[derive(Clone, Debug, serde::Deserialize)]
+/// Represents a range of block heights.
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct BlockRange {
     pub start_inclusive: BlockHeight,
     pub end_exclusive: BlockHeight,
 }
 
 impl BlockRange {
+    /// Splits the block range into smaller chunks.
     pub fn range_chunks(self, chunk_size: usize) -> impl Iterator<Item = BlockRange> {
         let range: Range<BlockHeight> = self.clone().into();
-
         range.step_by(chunk_size).map(move |start_inclusive| {
-            let chunk_size: u64 = chunk_size.try_into().unwrap();
+            let chunk_size = chunk_size as BlockHeight;
             let end_exclusive = (start_inclusive + chunk_size).min(self.end_exclusive);
             (start_inclusive..end_exclusive).into()
         })
     }
 
+    /// Returns the length of the block range.
     pub fn len(&self) -> u64 {
         self.end_exclusive - self.start_inclusive
     }
 
+    /// Checks if a block height is within the range.
     pub fn contains(&self, block_height: BlockHeight) -> bool {
-        block_height >= self.start_inclusive && block_height < self.end_exclusive
+        (self.start_inclusive..self.end_exclusive).contains(&block_height)
     }
 }
 
@@ -105,46 +99,8 @@ impl IntoIterator for BlockRange {
     }
 }
 
-#[derive(Clone, Copy)]
-pub enum FetchMode {
-    Eager,
-    Lazy,
-}
-
-#[derive(Debug)]
-pub enum BlockSelection {
-    LastFinalized,
-    Height(BlockHeight),
-}
-
-impl Display for BlockSelection {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            BlockSelection::LastFinalized => write!(f, "last-finalized"),
-            BlockSelection::Height(height) => write!(f, "{}", height),
-        }
-    }
-}
-
-#[async_trait]
-pub trait FetcherClient: Display + Send + Sync + Clone + Sized + 'static {
-    type BlockHandle: BlockHandle;
-    type Context: Display + Send + Sync + Clone + 'static;
-
-    async fn create(
-        pg_pool: sqlx::PgPool,
-        join_set: &mut JoinSet<Result<(), IndexerError>>,
-        context: Self::Context,
-    ) -> Result<Self, IndexerError>;
-
-    async fn fetch_single(
-        &self,
-        selection: BlockSelection,
-        mode: FetchMode,
-    ) -> Result<Self::BlockHandle, IndexerError>;
-}
-
-#[derive(Clone, Debug)]
+/// Represents a reference to a block.
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct BlockReference {
     pub height: BlockHeight,
     pub hash: BlockHash,
@@ -152,17 +108,14 @@ pub struct BlockReference {
 }
 
 impl BlockReference {
-    pub fn new(height: BlockHeight, hash: BlockHash, timestamp: BlockTimestamp) -> BlockReference {
-        BlockReference {
+    /// Creates a new `BlockReference`.
+    pub fn new(height: BlockHeight, hash: BlockHash, timestamp: BlockTimestamp) -> Self {
+        Self {
             height,
             hash,
             timestamp,
         }
     }
-}
-
-pub trait BlockReferenceProvider {
-    fn block_reference(&self) -> Result<BlockReference, Report>;
 }
 
 impl Display for BlockReference {
@@ -171,14 +124,73 @@ impl Display for BlockReference {
     }
 }
 
+/// Trait for converting an object into a `BlockReference`.
+pub trait BlockReferenceProvider {
+    fn block_reference(&self) -> Result<BlockReference, Report>;
+}
+
 #[async_trait]
-pub trait BlockHandle: Send + Sync + Sized {
+pub trait BlockHandle: Debug + Send + Sync + 'static {
+    /// Gets the reference for the block.
     fn reference(&self) -> BlockReference;
+
+    /// Fetches a range of blocks.
     fn fetch_range(
         &self,
         range: BlockRange,
         mode: FetchMode,
     ) -> Result<impl Stream<Item = Result<Self, IndexerError>> + Send, IndexerError>;
+
+    /// Inserts the block into the database.
     async fn insert(&self, tx: &mut sqlx::Transaction<'_, Postgres>) -> Result<(), IndexerError>;
+
+    /// Updates the block in the database.
     async fn update(&self, tx: &mut sqlx::Transaction<'_, Postgres>) -> Result<(), IndexerError>;
+}
+
+#[async_trait]
+pub trait FetcherClient: Display + Send + Sync + Clone + 'static {
+    type BlockHandle: BlockHandle;
+    type Context: Display + Send + Sync + Clone + 'static;
+
+    /// Creates a new client.
+    async fn create(
+        pg_pool: sqlx::PgPool,
+        join_set: &mut JoinSet<Result<(), IndexerError>>,
+        context: Self::Context,
+    ) -> Result<Self, IndexerError>;
+
+    /// Fetches a single block.
+    async fn fetch_single(
+        &self,
+        selection: BlockSelection,
+        mode: FetchMode,
+    ) -> Result<Self::BlockHandle, IndexerError>;
+}
+
+/// Represents errors that may occur during indexing.
+#[derive(Debug, thiserror::Error)]
+pub enum IndexerError {
+    #[error("Unexpected height: received {0}, expected {1}")]
+    UnexpectedHeightSingle(BlockHeight, BlockHeight),
+    #[error("Unexpected height: received {0} in range {1}, expected {2}")]
+    UnexpectedHeightRange(BlockHeight, BlockRange, BlockHeight),
+    #[error("Error reading block {0} in range {1}: {2}")]
+    ErrorReadingBlock(BlockHeight, BlockRange, Report),
+    #[error("Missing block: expected {0} in range {1}")]
+    MissingBlock(BlockHeight, BlockRange),
+    #[error("Too many blocks: received block {1} in range {0}")]
+    TooManyBlocks(BlockRange, BlockReference),
+    #[error("Provider error: {0}")]
+    ProviderError(Report),
+    #[error("Database error: {0}")]
+    DatabaseError(#[from] sqlx::Error),
+    #[error("Internal error: {0}")]
+    InternalError(Report),
+}
+
+impl From<Report> for IndexerError {
+    fn from(error: Report) -> Self {
+        Self::InternalError(error)
+    }
 }
