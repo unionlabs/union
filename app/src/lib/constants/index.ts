e1eab74d3b1ec218c50eec9174a08893077f725c
cor <cor@pruijs.dev>
2025-01-07 17:52:06 +0000

    fix(app): delete invalid endpoints

--- app/src/lib/constants/index.ts
+++ app/src/lib/constants/index.ts
@@ -28,27 +28,8 @@ export const URLS = () => {
       // REST: "https://api.testnet.bonlulu.uno"
       REST: "https://rest.testnet-9.union.build/"
     },
-    SEPOLIA: {
-      RPC: "https://rpc.ankr.com/eth_sepolia/6c72c8d164912bed4694cb882fc4ca55574126511a4f5f66828a53fa2448a20a",
-      REST: null
-    }
   }
 }
 
 export const CHAINS = ["SEPOLIA", "UNION"] as const
 export type Chain = (typeof CHAINS)[number]
-
-export const CHAIN = {
-  UNION: {
-    ID: "union-testnet-9",
-    NAME: "Union Testnet"
-  },
-  SEPOLIA: {
-    ID: sepolia.id.toString(),
-    NAME: sepolia.name
-  }
-} satisfies Record<Chain, { ID: string; NAME: string }>
-
-export const CHAIN_URLS = {
-  [CHAIN.UNION.ID]: URLS().UNION
-}
