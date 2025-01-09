7af1f798b28aab86f5b32e3c9e422462c36d7155
aeryz <abdullaheryz@protonmail.com>
2025-01-09 15:24:20 +0300

    chore: add babylon network binary
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -51,6 +51,15 @@
       url = "github:cosmos/ics23";
       flake = false;
     };
+    # for chains that require blst (babylon)
+    blst = {
+      url = "github:supranational/blst?rev=3dd0f804b1819e5d03fb22ca2e6fac105932043a";
+      flake = false;
+    };
+    bls-eth-go = {
+      url = "git+https://github.com/herumi/bls-eth-go-binary?ref=refs/tags/v1.33.0&submodules=1";
+      flake = false;
+    };
     cosmosproto = {
       url = "github:cosmos/cosmos-proto?ref=refs/tags/v1.0.0-beta.5";
       flake = false;
@@ -75,6 +84,10 @@
     };
     osmosis = {
       url = "github:osmosis-labs/osmosis/v24.0.0-rc0";
+      flake = false;
+    };
+    babylon = {
+      url = "github:babylonlabs-io/babylon/v1.0.0-rc.3";
       flake = false;
     };
 
@@ -215,6 +228,7 @@
         ./tools/tools.nix
         ./tools/wasm-light-client.nix
         ./tools/libwasmvm/libwasmvm.nix
+        ./tools/libblst/libblst.nix
         ./tools/tidy/tidy.nix
         ./tools/rust/rust.nix
         ./tools/rust/crane.nix
@@ -228,6 +242,7 @@
         ./networks/simulation/simd.nix
         ./networks/stargaze.nix
         ./networks/osmosis.nix
+        ./networks/babylon.nix
         ./e2e/all-tests.nix
         ./e2e/e2e.nix
         ./devnet-compose/devnet-compose.nix
