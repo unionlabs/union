0b931f28e42b3fdede6e66dac96ab2c02b76edc8
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2025-01-11 15:33:22 +0100

    feat(zkgm): optimized calldata commitment when possible

--- evm/contracts/apps/ucs/03-zkgm/Zkgm.sol
+++ evm/contracts/apps/ucs/03-zkgm/Zkgm.sol
@@ -757,7 +757,7 @@ contract UCS03Zkgm is
         bytes calldata ack,
         address relayer
     ) external virtual override onlyIBC {
-        bytes32 packetHash = IBCPacketLib.commitPacketMemory(ibcPacket);
+        bytes32 packetHash = IBCPacketLib.commitPacket(ibcPacket);
         IBCPacket memory parent = inFlightPacket[packetHash];
         // Specific case of forwarding where the ack is threaded back directly.
         if (parent.timeoutTimestamp != 0 || parent.timeoutHeight != 0) {
@@ -934,7 +934,7 @@ contract UCS03Zkgm is
         IBCPacket calldata ibcPacket,
         address relayer
     ) external virtual override onlyIBC {
-        bytes32 packetHash = IBCPacketLib.commitPacketMemory(ibcPacket);
+        bytes32 packetHash = IBCPacketLib.commitPacket(ibcPacket);
         IBCPacket memory parent = inFlightPacket[packetHash];
         // Specific case of forwarding where the failure is threaded back directly.
         if (parent.timeoutTimestamp != 0 || parent.timeoutHeight != 0) {
