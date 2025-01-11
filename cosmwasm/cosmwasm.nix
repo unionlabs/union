e8fbb9c739885b3a142c448e9b36511120d97593
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2025-01-11 15:30:16 +0100

    feat(ucs01): deprecated and removed from cargo

--- cosmwasm/cosmwasm.nix
+++ cosmwasm/cosmwasm.nix
@@ -6,12 +6,6 @@
       ucs02-nft = crane.buildWasmContract {
         crateDirFromRoot = "cosmwasm/ucs02-nft";
       };
-      ucs01-relay = crane.buildWasmContract {
-        crateDirFromRoot = "cosmwasm/ucs01-relay";
-      };
-      ucs01-relay-api = crane.buildWorkspaceMember {
-        crateDirFromRoot = "cosmwasm/ucs01-relay-api";
-      };
       ucs00-pingpong = crane.buildWasmContract {
         crateDirFromRoot = "cosmwasm/ucs00-pingpong";
       };
@@ -37,15 +31,12 @@
           inherit cw721-base;
         }
         // ucs02-nft.packages
-        // ucs01-relay.packages
         // ucs00-pingpong.packages
         // ibc-union.packages
         // multicall.packages
         // ibc-union-ucs03-zkgm.packages;
       checks =
         ucs02-nft.checks
-        // ucs01-relay.checks
-        // ucs01-relay-api.checks
         // ucs00-pingpong.checks
         // ibc-union-ucs03-zkgm.checks;
     };
