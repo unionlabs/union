5f5bd71f477ad6bea63323e4fdc0a84d11194791
cor <cor@pruijs.dev>
2025-01-15 20:40:20 +0000

    feat(app): show basic ibc-union transfer details

--- app/src/lib/components/transfer-details.svelte
+++ app/src/lib/components/transfer-details.svelte
@@ -63,11 +63,11 @@ let processedTransfers = derived(
     return $transfers.data.map(transfer => {
       let tx = structuredClone(transfer)
 
-      let hop_chain_id: string | null = null
-      let hop_chain_destination_connection_id: string | null = null
-      let hop_chain_destination_channel_id: string | null = null
-      let hop_chain_source_connection_id: string | null = null
-      let hop_chain_source_channel_id: string | null = null
+      // let hop_chain_id: string | null = null
+      // let hop_chain_destination_connection_id: string | null = null
+      // let hop_chain_destination_channel_id: string | null = null
+      // let hop_chain_source_connection_id: string | null = null
+      // let hop_chain_source_channel_id: string | null = null
 
       // overwrite destination and receiver if to last forward
 
@@ -105,11 +105,11 @@ let processedTransfers = derived(
       // }
 
       return {
-        hop_chain_id,
-        hop_chain_destination_connection_id,
-        hop_chain_destination_channel_id,
-        hop_chain_source_connection_id,
-        hop_chain_source_channel_id,
+        // hop_chain_id,
+        // hop_chain_destination_connection_id,
+        // hop_chain_destination_channel_id,
+        // hop_chain_source_connection_id,
+        // hop_chain_source_channel_id,
         // @ts-ignore
         transfer_day: toIsoString(new Date(tx.source_timestamp)).split("T")[0],
         ...tx
@@ -388,6 +388,145 @@ let processedTransfers = derived(
 !-->
 
 <pre>
-{JSON.stringify($transfers.data, null, 2)}
+{JSON.stringify($processedTransfers, null, 2)}
 </pre>
+
+{#if $processedTransfers !== null && $processedTransfers.length > 0}
+  <div class="flex flex-col w-full items-center gap-6">
+    {#each $processedTransfers as transfer, transferIndex}
+      {@const sourceExplorer = chains
+        .find((c) => c.chain_id === transfer.source_chain_id)
+        ?.explorers?.at(0)}
+      {@const destinationExplorer = chains
+        .find((c) => c.chain_id === transfer.destination_chain_id)
+        ?.explorers?.at(0)}
+
+      <!--
+    <pre>{JSON.stringify($transfers.data, null, 2)}</pre>
+    !-->
+
+      <Card.Root
+        class="flex flex-col w-full lg:w-auto max-w-full overflow-y-hidden overflow-x-auto justify-self-center dark:bg-muted"
+      >
+        <Card.Header
+          class="font-bold text-md text-center break-words text-muted-foreground flex flex-row gap-2 justify-center"
+        >
+          TRANSFER <Truncate
+            value={transfer.packet_send_transaction_hash}
+            type="hash"
+          />
+        </Card.Header>
+        <Card.Content class="flex flex-col gap-8">
+          <section class="mt-6">
+            {#if transfer.tokens}
+              <ul
+                class="text-foreground text-center uppercase condenced font-bold text-3xl sm:text-4xl"
+              >
+                {#each transfer.tokens as token}
+                  {#if token.asset}
+                    <li>
+                      <Truncate
+                        value={formatUnits(token.amount, token.asset.decimals)}
+                        type="full"
+                      />
+                      <Truncate
+                        value={token.asset.display_symbol}
+                        type="address"
+                      />
+                    </li>
+                  {:else}
+                    <li>
+                      <Truncate value={token.amount} type="full" />
+                      <Truncate value={token.denom} type="address" />
+                    </li>
+                  {/if}
+                {/each}
+              </ul>
+            {:else}
+              No assets in transfer
+            {/if}
+          </section>
+
+          <section>
+            <PacketPath packet={transfer} {chains}/>
+          </section>
+          <section class="flex flex-col lg:flex-row gap-8">
+            <div class=" lex-col text-muted-foreground">
+              <DetailsHeading>
+                Sender
+              </DetailsHeading>
+              {#if sourceExplorer !== undefined}
+                <a
+                  href={`/explorer/address/${transfer.sender}`}
+                  class="block text-sm underline break-words"
+                  >{transfer.sender}
+                </a>{:else}<p class="text-sm break-words">
+                  {transfer.sender}
+                </p>{/if}
+              <p
+                class={cn(
+                  "text-[10px] break-words",
+                  transfer.normalized_sender
+                    ? "text-black dark:text-muted-foreground"
+                    : "text-transparent"
+                )}
+              >
+                raw: {transfer.normalized_sender}
+              </p>
+            </div>
+            <div class="flex-1 lg:text-right flex-col text-muted-foreground">
+              <DetailsHeading>
+                Receiver
+              </DetailsHeading>
+              {#if destinationExplorer !== undefined}
+                <a
+                  href={`/explorer/address/${transfer.receiver}`}
+                  class="block text-sm underline break-words"
+                  >{transfer.receiver}
+                </a>{:else}<p class="text-sm break-words">
+                  {transfer.receiver}
+                </p>{/if}
+              <p
+                class={cn(
+                  "text-[10px] break-words",
+                  transfer.normalized_receiver
+                    ? "text-black dark:text-muted-foreground"
+                    : "text-transparent"
+                )}
+              >
+                raw: {transfer.normalized_receiver}
+              </p>
+            </div>
+          </section>
+        </Card.Content>
+        <!--
+        <Card.Footer class="items-start flex flex-col w-full gap-4">
+          <div class="mt-6 font-bold text-md">{transfer.transfer_day}</div>
+          <!-- bit of a hack, pTrace is used to check if there is a trace, and if there is, we show the steps !-->
+          <!--
+          {@const pTrace = $processedTransfers?.at(transferIndex) ?? null}
+          {@const ts = derived(
+            tracesSteps,
+            ($tracesSteps) => $tracesSteps?.at(transferIndex) ?? []
+          )}
+          {#if pTrace}
+            <Stepper steps={ts} />
+          {:else}
+            <LoadingLogo />
+          {/if}
+        </Card.Footer>
+        !-->
+      </Card.Root>
+      <!--
+      <div class="text-transparent hover:text-muted-foreground transition text-xs overflow-hidden">
+        {#if !(source.slice(0, 2) === "0x")}0x{/if}{source.toLowerCase()}
+      </div>
+      !-->
+    {/each}
+  </div>
+{:else if $transfers.isLoading}
+  <LoadingLogo class="size-16" />
+{:else if $transfers.isError}
+  Error loading transfer data
+{/if}
 
