6d7d7d7c80b4827bf5f882df4e532e48b6f75b5c
cor <cor@pruijs.dev>
2025-01-21 14:00:11 +0000

    feat(app): show token details in transfer details

--- app/src/lib/components/token.svelte
+++ app/src/lib/components/token.svelte
@@ -1,65 +1,72 @@
 <script lang="ts">
-import type { Chain, TokenInfo } from "$lib/types"
-import TokenQualityLevel from "$lib/components/token-quality-level.svelte"
-import Truncate from "./truncate.svelte"
-import ArrowLeftIcon from "virtual:icons/lucide/arrow-left"
-import { toDisplayName } from "$lib/utilities/chains.ts"
-import { formatUnits } from "viem"
+  import type { Chain, TokenInfo } from "$lib/types";
+  import TokenQualityLevel from "$lib/components/token-quality-level.svelte";
+  import Truncate from "./truncate.svelte";
+  import ArrowRightIcon from "virtual:icons/lucide/arrow-right";
+  import { toDisplayName } from "$lib/utilities/chains.ts";
+  import { formatUnits } from "viem";
 
-export let chains: Array<Chain>
-export let chainId: string
-export let denom: string
-export let amount: string | number | bigint | null = null
+  export let chains: Array<Chain>;
+  export let chainId: string;
+  export let denom: string;
+  export let amount: string | number | bigint | null = null;
 
-let chain = chains.find(c => c.chain_id === chainId) ?? null
-let graphqlToken = chain?.tokens.find(t => t.denom === denom) ?? null
+  let chain = chains.find((c) => c.chain_id === chainId) ?? null;
+  let graphqlToken = chain?.tokens.find((t) => t.denom === denom) ?? null;
 
-let token: TokenInfo = (() => {
-  let graphqlToken = chain?.tokens.find(t => t.denom === denom) ?? null
+  let token: TokenInfo = (() => {
+    let graphqlToken = chain?.tokens.find((t) => t.denom === denom) ?? null;
 
-  if (graphqlToken?.representations && graphqlToken.representations.length > 0) {
-    let fullRepresentations = graphqlToken.representations.filter(
-      repr => repr.decimals != null && repr.name != null && repr.symbol != null
-    ) as Array<
-      {
-        decimals: number
-        name: string
-        symbol: string
-      } & (typeof graphqlToken.representations)[number]
-    >
+    if (
+      graphqlToken?.representations &&
+      graphqlToken.representations.length > 0
+    ) {
+      let fullRepresentations = graphqlToken.representations.filter(
+        (repr) =>
+          repr.decimals != null && repr.name != null && repr.symbol != null,
+      ) as Array<
+        {
+          decimals: number;
+          name: string;
+          symbol: string;
+        } & (typeof graphqlToken.representations)[number]
+      >;
 
-    if (fullRepresentations.length > 0) {
-      return {
-        quality_level: "GRAPHQL",
-        denom,
-        primaryRepresentation: fullRepresentations[0],
-        representations: fullRepresentations,
-        wrapping: graphqlToken.wrapping
+      if (fullRepresentations.length > 0) {
+        return {
+          quality_level: "GRAPHQL",
+          denom,
+          primaryRepresentation: fullRepresentations[0],
+          representations: fullRepresentations,
+          wrapping: graphqlToken.wrapping,
+        };
       }
     }
-  }
 
-  return {
-    quality_level: "NONE",
-    denom
-  }
-})()
+    return {
+      quality_level: "NONE",
+      denom,
+    };
+  })();
 </script>
 
 <div class="flex gap-1 items-center">
-  <TokenQualityLevel level={token.quality_level}/>
+  <TokenQualityLevel level={token.quality_level} />
   {#if token.quality_level === "GRAPHQL"}
     {#if amount !== null}
       {formatUnits(BigInt(amount), token.primaryRepresentation.decimals)}
     {/if}
     <div class="font-bold">{token.primaryRepresentation.symbol}</div>
     <div class="text-muted-foreground text-xs flex gap-1 items-center">
-    {#each token.wrapping as wrapping}
-       <ArrowLeftIcon/>{toDisplayName(wrapping.wrapped_chain.chain_id, chains)}
-    {/each}
+      {#each token.wrapping as wrapping}
+        <ArrowRightIcon />{toDisplayName(
+          wrapping.wrapped_chain.chain_id,
+          chains,
+        )}
+      {/each}
     </div>
   {:else}
     {amount}
-    <b><Truncate value={token.denom} type = "address"/></b>
+    <b><Truncate value={token.denom} type="address" /></b>
   {/if}
 </div>
--- app/src/lib/components/transfer-details.svelte
+++ app/src/lib/components/transfer-details.svelte
@@ -1,66 +1,72 @@
 <script lang="ts">
-import { page } from "$app/stores"
-import request from "graphql-request"
-import { transfersBySourceHashBaseQueryDocument } from "$lib/graphql/queries/transfer-details.ts"
-import DetailsHeading from "$lib/components/details-heading.svelte"
-import { createQuery } from "@tanstack/svelte-query"
-import { URLS } from "$lib/constants"
-import * as Card from "$lib/components/ui/card/index.ts"
-import { toIsoString } from "$lib/utilities/date"
-import LoadingLogo from "$lib/components/loading-logo.svelte"
-import { derived } from "svelte/store"
-import { raise } from "$lib/utilities"
-import Trace from "$lib/components/trace.svelte"
-import type { Chain } from "$lib/types"
-import { submittedTransfers } from "$lib/stores/submitted-transfers"
-import { cn } from "$lib/utilities/shadcn"
-import Truncate from "$lib/components/truncate.svelte"
-import { formatUnits } from "viem"
-import PacketPath from "./packet-path.svelte"
+  import { page } from "$app/stores";
+  import request from "graphql-request";
+  import { transfersBySourceHashBaseQueryDocument } from "$lib/graphql/queries/transfer-details.ts";
+  import DetailsHeading from "$lib/components/details-heading.svelte";
+  import { createQuery } from "@tanstack/svelte-query";
+  import { URLS } from "$lib/constants";
+  import * as Card from "$lib/components/ui/card/index.ts";
+  import { toIsoString } from "$lib/utilities/date";
+  import LoadingLogo from "$lib/components/loading-logo.svelte";
+  import { derived } from "svelte/store";
+  import { raise } from "$lib/utilities";
+  import Trace from "$lib/components/trace.svelte";
+  import type { Chain } from "$lib/types";
+  import { submittedTransfers } from "$lib/stores/submitted-transfers";
+  import { cn } from "$lib/utilities/shadcn";
+  import Truncate from "$lib/components/truncate.svelte";
+  import { formatUnits } from "viem";
+  import PacketPath from "./packet-path.svelte";
+  import Token from "./token.svelte";
 
-const source = $page.params.source
-export let chains: Array<Chain>
+  const source = $page.params.source;
+  export let chains: Array<Chain>;
 
-let transfers = createQuery({
-  queryKey: ["transfers-by-source-base", source],
-  refetchInterval: query => (query.state.data?.length === 0 ? 1_000 : false), // fetch every second until we have the transaction
-  placeholderData: (previousData, _) => previousData,
-  queryFn: async () => {
-    console.log("querying")
-    const response = await request(URLS().GRAPHQL, transfersBySourceHashBaseQueryDocument, {
-      source_transaction_hash: source
-    })
+  let transfers = createQuery({
+    queryKey: ["transfers-by-source-base", source],
+    refetchInterval: (query) =>
+      query.state.data?.length === 0 ? 1_000 : false, // fetch every second until we have the transaction
+    placeholderData: (previousData, _) => previousData,
+    queryFn: async () => {
+      console.log("querying");
+      const response = await request(
+        URLS().GRAPHQL,
+        transfersBySourceHashBaseQueryDocument,
+        {
+          source_transaction_hash: source,
+        },
+      );
 
-    if (
-      response.v1_ibc_union_fungible_asset_orders === undefined ||
-      response.v1_ibc_union_fungible_asset_orders === null
-    )
-      raise("error fetching transfers")
+      if (
+        response.v1_ibc_union_fungible_asset_orders === undefined ||
+        response.v1_ibc_union_fungible_asset_orders === null
+      )
+        raise("error fetching transfers");
 
-    return response.v1_ibc_union_fungible_asset_orders
-  }
-})
+      return response.v1_ibc_union_fungible_asset_orders;
+    },
+  });
 
-let processedTransfers = derived(
-  [transfers, submittedTransfers],
-  ([$transfers, $submittedTransfers]) => {
-    if ($transfers.data === undefined || $transfers.data.length === 0) {
-      if ($submittedTransfers[source] === undefined) {
-        return null
+  let processedTransfers = derived(
+    [transfers, submittedTransfers],
+    ([$transfers, $submittedTransfers]) => {
+      if ($transfers.data === undefined || $transfers.data.length === 0) {
+        if ($submittedTransfers[source] === undefined) {
+          return null;
+        }
+        return [$submittedTransfers[source]];
       }
-      return [$submittedTransfers[source]]
-    }
-    return $transfers.data.map(transfer => {
-      let tx = structuredClone(transfer)
-      return {
-        transfer_day: tx.packet_send_timestamp
-          ? toIsoString(new Date(tx.packet_send_timestamp)).split("T")[0]
-          : null,
-        ...tx
-      }
-    })
-  }
-)
+      return $transfers.data.map((transfer) => {
+        let tx = structuredClone(transfer);
+        return {
+          transfer_day: tx.packet_send_timestamp
+            ? toIsoString(new Date(tx.packet_send_timestamp)).split("T")[0]
+            : null,
+          ...tx,
+        };
+      });
+    },
+  );
 </script>
 
 {#if $processedTransfers !== null && $processedTransfers.length > 0}
@@ -73,131 +79,106 @@ let processedTransfers = derived(
         .find((c) => c.chain_id === transfer.destination_chain_id)
         ?.explorers?.at(0)}
       {#if transfer.source_chain_id !== null && transfer.destination_chain_id !== null}
-      <Card.Root
-        class="flex flex-col w-full lg:w-auto max-w-full overflow-y-hidden overflow-x-auto justify-self-center dark:bg-muted"
-      >
-        <Card.Header
-          class="font-bold text-md text-center break-words text-muted-foreground flex flex-row gap-2 justify-center"
+        <Card.Root
+          class="flex flex-col w-full lg:w-auto max-w-full overflow-y-hidden overflow-x-auto justify-self-center dark:bg-muted"
         >
-          TRANSFER <Truncate
-            value={transfer.packet_send_transaction_hash}
-            type="hash"
-          />
-        </Card.Header>
-        <Card.Content class="flex flex-col gap-8">
-          <section class="flex justify-between">
-            <div>
-            <h2 class="font-supermolot uppercase md:font-expanded text-2xl font-extrabold text-foreground whitespace-nowrap">
-              {#if transfer.base_amount}
-              {@const base_amount = BigInt(transfer.base_amount)}
-                <Truncate
-                  value={("base_token_details" in transfer && transfer.base_token_details?.decimals) ? formatUnits(base_amount, transfer.base_token_details.decimals) : base_amount}
-                  type="full"
-                />
-              {/if}
-              <Truncate
-                value={("base_token_details" in transfer && transfer.base_token_details?.display_symbol) ? transfer.base_token_details.display_symbol : transfer.base_token}
-                type="address"
-              />
-            </h2>
-            <p class="text-muted-foreground text-sm break-words">
-            <Truncate
-              value={transfer.base_token}
-              type="address"
-            /> | {#if "base_token_details" in transfer && transfer.base_token_details}{transfer.base_token_details?.origin}{:else}NO DETAILS{/if}
-            </p>
-
-            </div>
-
-            
-            <div class="flex flex-col items-end">
-            {#if "quote_token" in transfer}
-            <h2 class="font-supermolot uppercase md:font-expanded text-2xl font-extrabold text-foreground whitespace-nowrap">
-              {#if "quote_amount" in transfer && transfer.quote_amount}
-              {@const quote_amount = BigInt(transfer.quote_amount)}
-                <Truncate
-                  value={("quote_token_details" in transfer && transfer.quote_token_details?.decimals) ? formatUnits(quote_amount, transfer.quote_token_details.decimals) : quote_amount}
-                  type="full"
-                />
-              {/if}
-            <Truncate
-                value={(transfer.quote_token_details && transfer.quote_token_details.display_symbol) ? transfer.quote_token_details.display_symbol : transfer.quote_token}
-              type="address"
+          <Card.Header
+            class="font-bold text-md text-center break-words text-muted-foreground flex flex-row gap-2 justify-center"
+          >
+            TRANSFER <Truncate
+              value={transfer.packet_send_transaction_hash}
+              type="hash"
             />
-            </h2>
-            <p class="text-muted-foreground text-sm break-words">
-            <Truncate
-              value={transfer.quote_token}
-              type="address"
-            /> | {#if  "quote_token_details" in transfer && "quote_token_details" in transfer && transfer.quote_token_details}{transfer.quote_token_details?.origin}{:else}NO DETAILS{/if}
+          </Card.Header>
+          <Card.Content class="flex flex-col gap-8">
+            <section class="flex justify-between">
+              <div>
+                <div class="block">
+                  {#if transfer.base_amount && transfer.base_token}
+                    <Token
+                      amount={transfer.base_amount}
+                      denom={transfer.base_token}
+                      chainId={transfer.source_chain_id}
+                      {chains}
+                    />
+                  {/if}
+                  {#if "quote_amount" in transfer && transfer.quote_amount && "quote_token" in transfer && transfer.quote_token}
+                    <Token
+                      amount={transfer.quote_amount}
+                      denom={transfer.quote_token}
+                      chainId={transfer.destination_chain_id}
+                      {chains}
+                    />
+                  {/if}
+                </div>
+              </div>
+            </section>
 
-            </p>
+            <section>
+              <!-- typescript is stupid here, as source_chain_id and destination_chain_id have already established not to be null. !-->
+              <PacketPath packet={transfer} {chains} />
+            </section>
+            <section class="flex flex-col lg:flex-row gap-8">
+              <div class="flex-col text-muted-foreground">
+                <DetailsHeading>Sender</DetailsHeading>
+                {#if sourceExplorer !== undefined}
+                  <a
+                    href={`/explorer/address/${transfer.sender}`}
+                    class="block text-sm underline break-words"
+                    ><Truncate
+                      class="underline"
+                      value={transfer.sender}
+                      type="address"
+                    />
+                  </a>{:else}<p class="text-sm break-words">
+                    <Truncate value={transfer.sender} type="address" />
+                  </p>{/if}
+                <p
+                  class={cn(
+                    "text-[10px] break-words",
+                    "normalized_sender" in transfer &&
+                      transfer.normalized_sender
+                      ? "text-black dark:text-muted-foreground"
+                      : "text-transparent",
+                  )}
+                ></p>
+              </div>
+              <div class="flex-1 lg:text-right flex-col text-muted-foreground">
+                <DetailsHeading>Receiver</DetailsHeading>
+                {#if destinationExplorer !== undefined}
+                  <a
+                    href={`/explorer/address/${transfer.receiver}`}
+                    class="block text-sm underline break-words"
+                    ><Truncate
+                      class="underline"
+                      value={transfer.receiver}
+                      type="address"
+                    />
+                  </a>{:else}<p class="text-sm break-words">
+                    <Truncate value={transfer.receiver} type="address" />
+                  </p>{/if}
+                <p
+                  class={cn(
+                    "text-[10px] break-words",
+                    "normalized_receiver" in transfer &&
+                      transfer.normalized_receiver
+                      ? "text-black dark:text-muted-foreground"
+                      : "text-transparent",
+                  )}
+                ></p>
+              </div>
+            </section>
+          </Card.Content>
+          <Card.Footer class="items-start flex flex-col w-full gap-4">
+            <div class="font-bold text-md">{transfer.transfer_day}</div>
+            {#if transfer.traces}
+              <Trace traces={transfer.traces} {chains} />
+            {:else}
+              <LoadingLogo />
             {/if}
-            </div>
-            
-          </section>
-
-          <section>
-            <!-- typescript is stupid here, as source_chain_id and destination_chain_id have already established not to be null. !-->
-            <PacketPath packet={transfer} {chains}/>
-          </section>
-          <section class="flex flex-col lg:flex-row gap-8">
-            <div class="flex-col text-muted-foreground">
-              <DetailsHeading>
-                Sender
-              </DetailsHeading>
-              {#if sourceExplorer !== undefined}
-                <a
-                  href={`/explorer/address/${transfer.sender}`}
-                  class="block text-sm underline break-words"
-                  ><Truncate class="underline" value={transfer.sender} type="address"/>
-                </a>{:else}<p class="text-sm break-words">
-                  <Truncate value={transfer.sender} type="address"/>
-                </p>{/if}
-              <p
-                class={cn(
-                  "text-[10px] break-words",
-                  "normalized_sender" in transfer && transfer.normalized_sender
-                    ? "text-black dark:text-muted-foreground"
-                    : "text-transparent"
-                )}
-              >
-              </p>
-            </div>
-            <div class="flex-1 lg:text-right flex-col text-muted-foreground">
-              <DetailsHeading>
-                Receiver
-              </DetailsHeading>
-              {#if destinationExplorer !== undefined}
-                <a
-                  href={`/explorer/address/${transfer.receiver}`}
-                  class="block text-sm underline break-words"
-                  ><Truncate class="underline" value={transfer.receiver} type="address"/>
-                </a>{:else}<p class="text-sm break-words">
-                  <Truncate value={transfer.receiver} type="address"/>
-                </p>{/if}
-              <p
-                class={cn(
-                  "text-[10px] break-words",
-                  "normalized_receiver" in transfer && transfer.normalized_receiver
-                    ? "text-black dark:text-muted-foreground"
-                    : "text-transparent"
-                )}
-              >
-              </p>
-            </div>
-          </section>
-        </Card.Content>
-        <Card.Footer class="items-start flex flex-col w-full gap-4">
-          <div class="font-bold text-md">{transfer.transfer_day}</div>
-          {#if transfer.traces}
-            <Trace traces={transfer.traces} {chains}/>
-          {:else}
-            <LoadingLogo />
-          {/if}
-        </Card.Footer>
-      </Card.Root>
-      <!--
+          </Card.Footer>
+        </Card.Root>
+        <!--
       <div class="text-transparent hover:text-muted-foreground transition text-xs overflow-hidden">
         {#if !(source.slice(0, 2) === "0x")}0x{/if}{source.toLowerCase()}
       </div>
@@ -210,4 +191,3 @@ let processedTransfers = derived(
 {:else if $transfers.isError}
   Error loading transfer data
 {/if}
-
