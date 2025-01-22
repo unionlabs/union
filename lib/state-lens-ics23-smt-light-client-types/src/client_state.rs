94dca4439337515db0ce7e626bba8d520d8712e7
aeryz <abdullaheryz@protonmail.com>
2025-01-22 09:17:56 +0300

    chore(aptos-move): regen types and rename ics23move to ics23smt

--- lib/state-lens-ics23-smt-light-client-types/src/client_state.rs
+++ lib/state-lens-ics23-smt-light-client-types/src/client_state.rs
@@ -10,6 +10,6 @@ pub struct Extra {
     /// timestamp = consensus_state[timestamp_offset:timestamp_offset+8]
     pub timestamp_offset: u16,
     /// the offset at which we extract the bytes32 storage root (of the ibc contract on the l2) from the l2 consensus state
-    /// storage_root = consensus_state[storage_root_offset:storage_root_offset+32]
-    pub storage_root_offset: u16,
+    /// state_root = consensus_state[state_root_offset:state_root_offset+32]
+    pub state_root_offset: u16,
 }
