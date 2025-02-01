a18dc43aa2de4bde1ab29bc1341f94a36c08ae89
o-az <omaraziz.dev@proton.me>
2025-02-01 01:42:39 -0800

    fix: typo

--- typescript-sdk/src/cosmos/client.ts
+++ typescript-sdk/src/cosmos/client.ts
@@ -79,7 +79,7 @@ export const createCosmosClient = (parameters: CosmosClientParameters) =>
         ]
       })
     },
-    c20IncreaseAllowance: async ({
+    cw20IncreaseAllowance: async ({
       contractAddress,
       amount,
       spender,
