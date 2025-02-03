345c7f417741b49b0d769165ff48ee06cfb0865d
Lukas <Lukas.nyberg@gmail.com>
2025-02-03 16:41:13 +0100

    chore(app): fmt

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -5,11 +5,11 @@ export const trailingSlash = "ignore"
 import { fetchFeatures } from "$lib/queries/features"
 
 export const load = async ({ url }) => {
-  let environment = 'development'
-  if (url.host.startsWith('staging')) {
-    environment = 'staging'
-  } else if (url.host.startsWith('app')) {
-    environment = 'production'
+  let environment = "development"
+  if (url.host.startsWith("staging")) {
+    environment = "staging"
+  } else if (url.host.startsWith("app")) {
+    environment = "production"
   }
 
   const features = await fetchFeatures(environment.toUpperCase())
