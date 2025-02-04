839aa6c4bb77ae5376b3c247ce7327c34a79de5f
cor <cor@pruijs.dev>
2025-02-04 13:51:37 +0000

    fix(app): don't throw in cosmos balances

--- app/src/lib/queries/balance/cosmos.ts
+++ app/src/lib/queries/balance/cosmos.ts
@@ -1,5 +1,5 @@
 import * as v from "valibot"
-import { raise } from "$lib/utilities"
+import { err, ok, ResultAsync } from "neverthrow"
 
 const cosmosBalancesResponseSchema = v.object({
   balances: v.array(
@@ -10,32 +10,33 @@ const cosmosBalancesResponseSchema = v.object({
   )
 })
 
-export async function getCosmosChainBalances({
+const fetchJson = (url: string) => {
+  return ResultAsync.fromPromise(
+    fetch(url).then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error for url ${url} status: ${response.status}`)
+      }
+      return response.json()
+    }),
+    e =>
+      new Error(`Failed to fetch data from url ${url} with error: ${(e as Error).message}`, {
+        cause: e
+      })
+  )
+}
+
+export function getCosmosChainBalances({
   url,
   walletAddress
-}: { url: string; walletAddress: string }) {
-  let json: undefined | unknown
-
-  try {
-    url = url.startsWith("https") ? url : `https://${url}`
-    const response = await fetch(`${url}/cosmos/bank/v1beta1/balances/${walletAddress}`)
-    if (!response.ok) raise("invalid response")
-
-    json = await response.json()
-  } catch (error) {
-    if (error instanceof Error) {
-      raise(`error fetching balances from /cosmos/bank: ${error.message}`)
-    }
-    raise(`unknown error while fetching from /cosmos/bank: ${JSON.stringify(error)}`)
-  }
-
-  const result = v.safeParse(cosmosBalancesResponseSchema, json)
-
-  if (!result.success) raise(`error parsing result ${JSON.stringify(result.issues)}`)
-  return result.output.balances.map(x => ({
-    address: x.denom,
-    symbol: x.denom,
-    balance: BigInt(x.amount),
-    decimals: 0
-  }))
+}: { url: string; walletAddress: string }): ResultAsync<
+  v.InferOutput<typeof cosmosBalancesResponseSchema>,
+  Error
+> {
+  url = url.startsWith("https") ? url : `https://${url}`
+  return fetchJson(`${url}/cosmos/bank/v1beta1/balances/${walletAddress}`).andThen(json => {
+    const result = v.safeParse(cosmosBalancesResponseSchema, json)
+    return result.success
+      ? ok(result.output)
+      : err(new Error("Validation failed:", { cause: result.issues }))
+  })
 }
