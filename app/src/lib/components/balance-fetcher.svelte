be2d49d0185a09d9dce91bda85717a4790a6056b
cor <cor@pruijs.dev>
2025-02-05 18:09:38 +0000

    fix(app): more finegrained balance purging

--- app/src/lib/components/balance-fetcher.svelte
+++ app/src/lib/components/balance-fetcher.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
-import type { Chain, UserAddresses } from "$lib/types"
-import { balances, deleteBalancesForRpcType, queryBalances } from "$lib/stores/balances.ts"
+import type { Chain } from "$lib/types"
+import { deleteBalancesForRpcType, queryBalances } from "$lib/stores/balances.ts"
 import { onMount } from "svelte"
 import { userAddrCosmos } from "$lib/wallet/cosmos"
 import { userAddrEvm } from "$lib/wallet/evm"
 import { userAddressAptos } from "$lib/wallet/aptos"
-import { derived, type Readable } from "svelte/store"
 
 export let chains: Array<Chain>
 
-let userAddress: Readable<UserAddresses> = derived(
-  [userAddrCosmos, userAddrEvm, userAddressAptos],
-  ([$userAddrCosmos, $userAddrEvm, $userAddressAptos]) => ({
-    evm: $userAddrEvm,
-    cosmos: $userAddrCosmos,
-    aptos: $userAddressAptos
+onMount(() => {
+  const unsubscribe = userAddrEvm.subscribe(evmAddr => {
+    if (!evmAddr) {
+      deleteBalancesForRpcType(chains, "evm")
+      return
+    }
+    chains
+      .filter(chain => chain.rpc_type === "evm")
+      .forEach(chain => queryBalances(chain, evmAddr.canonical))
   })
-)
+  return unsubscribe
+})
 
 onMount(() => {
-  const unsubscribe = userAddress.subscribe(addr => {
-    console.log("new address", addr)
-
-    // wipe balances on wallet disconnect
-    if (!addr.evm) deleteBalancesForRpcType(chains, "evm")
-    if (!addr.cosmos) deleteBalancesForRpcType(chains, "evm")
-    if (!addr.aptos) deleteBalancesForRpcType(chains, "aptos")
-
-    // foreach chain with a connected wallet, fetch balances.
+  const unsubscribe = userAddrCosmos.subscribe(cosmosAddr => {
+    if (!cosmosAddr) {
+      deleteBalancesForRpcType(chains, "cosmos")
+      return
+    }
     chains
-      .filter(chain => addr[chain.rpc_type])
-      .forEach(chain => queryBalances(chain, addr[chain.rpc_type]?.canonical as string))
+      .filter(chain => chain.rpc_type === "cosmos")
+      .forEach(chain => queryBalances(chain, cosmosAddr.canonical))
+  })
+  return unsubscribe
+})
+
+onMount(() => {
+  const unsubscribe = userAddressAptos.subscribe(aptosAddr => {
+    if (!aptosAddr) {
+      deleteBalancesForRpcType(chains, "aptos")
+      return
+    }
+    chains
+      .filter(chain => chain.rpc_type === "aptos")
+      .forEach(chain => queryBalances(chain, aptosAddr.canonical))
   })
   return unsubscribe
 })
