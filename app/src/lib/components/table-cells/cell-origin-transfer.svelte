4a82b12e1191c9c31467d60a38f8cd636e963904
cor <cor@pruijs.dev>
2025-02-06 16:57:43 +0000

    feat(app): show chain statuses in transfer list

--- app/src/lib/components/table-cells/cell-origin-transfer.svelte
+++ app/src/lib/components/table-cells/cell-origin-transfer.svelte
@@ -4,22 +4,18 @@ import { truncate } from "$lib/utilities/format"
 import * as Tooltip from "$lib/components/ui/tooltip"
 import type { Chain } from "$lib/types"
 import Address from "../address.svelte"
+import ChainDetails from "$lib/chain-details.svelte"
 
 export let chains: Array<Chain>
 export let value: {
   chainId: string
   address: string
 }
-
-const chainDisplayName =
-  chains.find(chain => chain.chain_id === value.chainId)?.display_name ??
-  value.chainId ??
-  "unknown chain"
 </script>
 
 <div class={cn("flex flex-col items-start")} {...$$restProps}>
     <div class="font-bold">
-        {chainDisplayName}
+      <ChainDetails {chains} chainId={value.chainId}/>
     </div>
-    <Address address={value.address} chainId={value.chainId} {chains}/>
+    <span class="text-muted-foreground"><Address address={value.address} chainId={value.chainId} {chains}/><span>
 </div>
