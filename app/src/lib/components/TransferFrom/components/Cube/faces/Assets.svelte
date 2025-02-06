5c0293109f7bfda78606991cc43bd6974683311e
Lukas <Lukas.nyberg@gmail.com>
2025-02-06 17:16:21 +0100

    fix(app): remove test

--- app/src/lib/components/TransferFrom/components/Cube/faces/Assets.svelte
+++ app/src/lib/components/TransferFrom/components/Cube/faces/Assets.svelte
@@ -60,12 +60,4 @@ $: filteredTokens =
         <Token chainId={$rawIntents.source} denom={token.denom} amount={token.balance} {chains}/>
       </button>
     {/each}
-    {#each filteredTokens as token}
-      <button
-              class="px-2 py-1 hover:bg-neutral-400 dark:hover:bg-neutral-800 text-md flex justify-start items-center"
-              on:click={() => setAsset(token.denom)}
-      >
-        <Token chainId={$rawIntents.source} denom={token.denom} amount={token.balance} {chains}/>
-      </button>
-    {/each}
   </div>
