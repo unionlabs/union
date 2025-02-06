b8168a14f7bf700c5b537e34450a07732c36b749
Lukas <Lukas.nyberg@gmail.com>
2025-02-06 17:15:10 +0100

    fix(app): assets scroll

--- app/src/lib/components/TransferFrom/components/Cube/faces/Assets.svelte
+++ app/src/lib/components/TransferFrom/components/Cube/faces/Assets.svelte
@@ -51,7 +51,15 @@ $: filteredTokens =
     >✕</button>
   </div>
 
-  <div class="flex flex-col overflow-y-auto">
+  <div class="flex flex-col overflow-y-auto flex-1">
+    {#each filteredTokens as token}
+      <button
+              class="px-2 py-1 hover:bg-neutral-400 dark:hover:bg-neutral-800 text-md flex justify-start items-center"
+              on:click={() => setAsset(token.denom)}
+      >
+        <Token chainId={$rawIntents.source} denom={token.denom} amount={token.balance} {chains}/>
+      </button>
+    {/each}
     {#each filteredTokens as token}
       <button
               class="px-2 py-1 hover:bg-neutral-400 dark:hover:bg-neutral-800 text-md flex justify-start items-center"
