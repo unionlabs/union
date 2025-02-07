19ce903bd84994245468a2fb9d06c31652dee3ae
aeryz <abdullaheryz@protonmail.com>
2025-02-07 19:53:46 +0300

    chore: disable native-token-minter
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- cosmwasm/cosmwasm.nix
+++ cosmwasm/cosmwasm.nix
@@ -211,9 +211,9 @@
         crateDirFromRoot = "cosmwasm/multicall";
       };
 
-      native-token-minter = crane.buildWasmContract {
-        crateDirFromRoot = "cosmwasm/native-token-minter";
-      };
+      # native-token-minter = crane.buildWasmContract {
+      #   crateDirFromRoot = "cosmwasm/native-token-minter";
+      # };
 
       cw20-token-minter = crane.buildWasmContract {
         crateDirFromRoot = "cosmwasm/cw20-token-minter";
@@ -283,7 +283,7 @@
             bytecode-base
             cw721-base
             ucs03-zkgm
-            native-token-minter
+            # native-token-minter
             cw20-token-minter
             ibc-union
             multicall
