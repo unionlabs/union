1fa87c2841035d0624032080a61cb7e9e12e6d05
Lukas <Lukas.nyberg@gmail.com>
2025-02-15 18:23:00 +0100

    fix(app): adjust query

--- app/src/lib/graphql/queries/connections.ts
+++ app/src/lib/graphql/queries/connections.ts
@@ -1,34 +1,34 @@
-import { graphql } from "gql.tada"
+import {graphql} from "gql.tada"
 
 export const connectionsQuery = graphql(/* GraphQL */ `
-query ConnectionsQuery($limit: Int = 100) @cached(ttl: 30) {
-  v1_ibc_union_connections(
-    order_by: [
-    {status: asc}, 
-    {source_chain_id: asc}, 
-    {source_client_id: asc}, 
-    {source_connection_id: asc},
-    {destination_chain_id: asc}, 
-    {destination_client_id: asc}, 
-    {destination_connection_id: asc}
-  ], 
-  limit: $limit, 
-  where: { status: {_eq: "CONFIRM"}, }) {
-    source_chain_id
-    destination_chain_id
-    source_client_id
-    source_connection_id
-    source_chain {
-      enabled
-      display_name
+    query ConnectionsQuery($limit: Int = 100) @cached(ttl: 30) {
+        v1_ibc_union_channel_recommendations(
+            order_by: [
+                {source_chain_id: asc},
+                {source_client_id: asc},
+                {source_connection_id: asc},
+                {destination_chain_id: asc},
+                {destination_client_id: asc},
+                {destination_connection_id: asc}
+            ],
+            limit: $limit
+        ) {
+            source_chain_id
+            destination_chain_id
+            source_client_id
+            source_connection_id
+            source_chain {
+                enabled
+                display_name
+            }
+            destination_client_id
+            destination_connection_id
+            destination_chain {
+                enabled
+                display_name
+            }
+            status
+        }
     }
-    destination_client_id
-    destination_connection_id
-    destination_chain {
-      enabled
-      display_name
-    }
-    status
-  }
-}
 `)
+
