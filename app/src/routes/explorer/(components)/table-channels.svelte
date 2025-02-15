3b1b825af9e9d72a865d0f8b599eeb5f0d1893ff
Lukas <Lukas.nyberg@gmail.com>
2025-02-15 15:45:44 +0100

    fix(app): channels interval

--- app/src/routes/explorer/(components)/table-channels.svelte
+++ app/src/routes/explorer/(components)/table-channels.svelte
@@ -15,7 +15,7 @@ import { page } from "$app/stores"
 export let chains: Array<Chain>
 let channels = createQuery({
   queryKey: ["channels"],
-  refetchInterval: 5_000,
+  refetchInterval: 60_000,
   retryDelay: attempt => Math.min(attempt > 1 ? 2 ** attempt * 1000 : 1000, 30 * 1000),
   queryFn: async () => request(URLS().GRAPHQL, channelsQuery, {}),
   select: data =>
