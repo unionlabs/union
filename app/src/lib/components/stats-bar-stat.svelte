8f1dafdf477a9e939dc4b8e55c75d68e80fa551b
cor <cor@pruijs.dev>
2025-02-16 17:48:01 +0000

    feat(app): refined stats bar

--- app/src/lib/components/stats-bar-stat.svelte
+++ app/src/lib/components/stats-bar-stat.svelte
@@ -16,11 +16,11 @@ function formatValue(value: number | string): string {
 </script>
 
 <div class={cn("capitalize  flex px-6 py-4 border-none", $deviceWidth >= 888 ? "w-fit" : "w-full")}>
-  <div class="text-xl pt-2 font-bold divide-y" on:copy={(event) => {
+  <div class="text-xl font-bold" on:copy={(event) => {
       event?.clipboardData?.setData('text/plain', value.toString());
       event.preventDefault();
     }}>
-    <h3 class="text-sm pb-2">{label}</h3>
+    <h3 class="text-xs pt-2 text-muted-foreground">{label}</h3>
     {#if !value}
       <div class="flex justify-start">
         <LoadingDots class="size-6"/>
