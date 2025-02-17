cc64603c87c499a34c642e32310d3468534cb427
Lukas <Lukas.nyberg@gmail.com>
2025-02-17 20:45:27 +0100

    feat(app): new dev settings

---
+++ app/src/lib/components/DevSettings/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import TracesSwitch from "$lib/components/DevSettings/TracesSwitch.svelte"
+import DetailsSwitch from "$lib/components/DevSettings/DetailsSwitch.svelte"
+</script>
+
+<div class="flex flex-col space-y-2 mt-auto">
+  <h3 class="uppercase font-supermolot font-bold text-xl">Developer settings</h3>
+  <div class="flex flex-col space-y-2">
+    <TracesSwitch />
+    <DetailsSwitch />
+  </div>
+</div>
