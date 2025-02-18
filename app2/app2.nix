27a0d9244ac21472adae46c3ad099c981073c858
cor <cor@pruijs.dev>
2025-02-18 11:25:17 +0000

    feat(app2): add Effect

--- app2/app2.nix
+++ app2/app2.nix
@@ -19,7 +19,7 @@ _: {
     {
       packages = {
         app2 = jsPkgs.buildNpmPackage {
-          npmDepsHash = "sha256-nNc5XsabH4dVwv8/mrpA5vw2m8HEJkrqPiHrHfUA8q0=";
+          npmDepsHash = "sha256-WuArQDucZyZ63KC/9/Yq2XVnCdYjI6ssOm4M+bNmjUg=";
           src = ./.;
           sourceRoot = "app2";
           npmFlags = [ "--legacy-peer-deps" ];
--- app2/src/routes/+page.svelte
+++ app2/src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+import { Effect } from "effect"
+
+const task = Effect.sync(() => {
+  console.log("hi")
+})
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button on:click={() => Effect.runSync(task)}>Run effect</button>
