c37ad85eb89100001843ad94e22745859f840a6b
cor <cor@pruijs.dev>
2025-02-20 12:15:11 +0000

    feat(app2): more advanced effect

--- app2/src/routes/+page.svelte
+++ app2/src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-import { Effect } from "effect"
+import { Console, Effect, Fiber, Schedule } from "effect"
 
-const task = Effect.sync(() => {
-  console.log("hi")
-})
+let number = $state(0)
+const program = Effect.repeat(
+  Effect.sync(() => {
+    console.log(number)
+    number += 1
+  }),
+  Schedule.spaced("200 millis")
+)
+const fiber = Effect.runFork(program)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button on:click={() => Effect.runSync(task)}>Run effect</button>
+<button onclick={() => Effect.runFork(Fiber.interrupt(fiber))}>Run effect</button>
+{number}
