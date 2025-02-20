a65294221239e6c294269788688df00919b6561b
Lukas <Lukas.nyberg@gmail.com>
2025-02-20 11:10:25 +0100

    chore(sdk): pre commit

--- typescript-sdk/src/abi/convert.ts
+++ typescript-sdk/src/abi/convert.ts
@@ -1,23 +1,23 @@
-import fs from 'fs';
+import fs from "node:fs"
 
 // ts-node convert.ts
 // Function to convert JSON file to TypeScript variable after running hubble command
 function convertJsonToTs(jsonPath: string, variableName: string): string {
-  const jsonContent = fs.readFileSync(jsonPath, 'utf-8');
-  const obj = JSON.parse(jsonContent);
+  const jsonContent = fs.readFileSync(jsonPath, "utf-8")
+  const obj = JSON.parse(jsonContent)
   return JSON.stringify(obj, null, 2)
-    .replace(/"([^"]+)":/g, '$1:')
+    .replace(/"([^"]+)":/g, "$1:")
     .replace(/^/, `export const ${variableName} = `)
-    .replace(/}$/, '};');
+    .replace(/}$/, "};")
 }
 
 const files = [
-  { path: './app.ucs01.json', name: 'ucs01Abi' },
-  { path: './app.ucs02.json', name: 'ucs02Abi' },
-  { path: './app.ucs03.json', name: 'ucs03Abi' },
-];
+  { path: "./app.ucs01.json", name: "ucs01Abi" },
+  { path: "./app.ucs02.json", name: "ucs02Abi" },
+  { path: "./app.ucs03.json", name: "ucs03Abi" }
+]
 
 files.forEach(file => {
-  const tsContent = convertJsonToTs(file.path, file.name);
-  fs.writeFileSync(`${file.name}.ts`, tsContent);
-});
+  const tsContent = convertJsonToTs(file.path, file.name)
+  fs.writeFileSync(`${file.name}.ts`, tsContent)
+})
