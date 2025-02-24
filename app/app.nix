1554698bb4f577bea48b9a37052eebffa0258dbd
Lukas <Lukas.nyberg@gmail.com>
2025-02-24 23:48:08 +0100

    feat(app): use turnstile

--- app/app.nix
+++ app/app.nix
@@ -19,7 +19,7 @@ _: {
     {
       packages = {
         app = jsPkgs.buildNpmPackage {
-          npmDepsHash = "sha256-p/tie5TJYuSpBOph87ZP6QXnPPp9wjveiPKytVfKYaQ=";
+          npmDepsHash = "sha256-6VXC6YAFhxPuoI03OojOURqYHPzrrhJAMFXnLfGl+yc";
           src = ./.;
           sourceRoot = "app";
           npmFlags = [
