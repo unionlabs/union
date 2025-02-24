3fe0db913c8aca4435ce9e466d1c038cd324e795
cor <cor@pruijs.dev>
2025-02-24 20:44:13 +0000

    feat(app2): fetch data and errors at the same time

--- app2/src/routes/+page.svelte
+++ app2/src/routes/+page.svelte
@@ -1,40 +1,74 @@
 <script lang="ts">
-import { Effect, Fiber, Random, Schedule } from "effect"
+import { Console, Context, Option, Effect, Fiber, pipe, Random, Schedule } from "effect"
 import { FetchHttpClient, HttpClient } from "@effect/platform"
+import { onMount } from "svelte"
 
-let posts: "loading" | unknown = $state("loading")
+let responseData: Option.Option<unknown> = $state(Option.none())
+let responseError: Option.Option<unknown> = $state(Option.none())
+
+class SvelteStore extends Context.Tag("SvelteStore")<
+  SvelteStore,
+  { readonly next: Effect.Effect<number> }
+>() {}
 
 const fetcher = Effect.gen(function* () {
   const client = yield* HttpClient.HttpClient
   yield* Effect.log("fetching data")
-  // yield* Effect.sleep("3 seconds")
   const r = yield* Random.next
   const response = yield* client.get(
-    r > 0.3 ? "https://rpc.testnet-9.union.build/block" : "thisisnotavalidurl"
+    r > 0.3 ? "https://rpc.testnet-9.union.build/block" : "https://thisisnotavalidurl.com"
   )
   const json = yield* response.json
   yield* Effect.log("fetched data")
+  return json
+})
 
-  posts = json
-
-  return response
-}).pipe(
-  Effect.catchAll(error => Effect.logError(error.message)),
+const fetcherPipeline = pipe(
+  fetcher,
+  Effect.tapBoth({
+    onSuccess: data =>
+      Effect.sync(() => {
+        responseData = Option.some(data)
+      }),
+    onFailure: error =>
+      Effect.sync(() => {
+        responseError = Option.some(error)
+      })
+  }),
+  Effect.catchAll(_ => Effect.succeed(null)),
   Effect.scoped,
   Effect.provide(FetchHttpClient.layer)
 )
 
 const program = Effect.repeat(
-  fetcher,
+  fetcherPipeline,
   Schedule.addDelay(Schedule.repeatForever, () => "2 seconds")
 )
 
-const fiber = Effect.runFork(program)
-Effect.runPromiseExit(fiber)
+let fiber
+
+const stop = Effect.gen(function* () {
+  yield* Fiber.interrupt(fiber)
+  responseData = Option.none()
+})
+onMount(() => {
+  fiber = Effect.runFork(program)
+  return () => Effect.runPromise(stop)
+})
 </script>
 
-<button class="bg-red-500" onclick={() => {Effect.runPromise(Fiber.interrupt(fiber))}}> stop the fetcher </button>
+<button class="bg-red-500" onclick={() => {Effect.runPromise(stop)}}> stop the fetcher </button>
 
-<pre class="font-mono">
-  {JSON.stringify(posts, null, 2)}
-</pre>
+
+{#if Option.isSome(responseData)}
+  <pre class="font-mono">
+    {JSON.stringify(responseData.value.result.block_id.hash, null, 2)}
+  </pre>
+{:else}
+  Loading... yeah wahooo
+{/if}
+{#if Option.isSome(responseError)}
+  <pre class="font-mono bg-red-500">
+    {JSON.stringify(responseError.value, null, 2)}
+  </pre>
+{/if}
