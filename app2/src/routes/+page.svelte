60a76c6dbf4f31dc7a5217dac55464532b158ffe
cor <cor@pruijs.dev>
2025-02-25 01:23:08 +0000

    refactor(app2): current block hash component

--- app2/src/routes/+page.svelte
+++ app2/src/routes/+page.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
-import { Option } from "effect"
-import { block } from "$lib/stores/block.svelte"
+import CurrentBlockHash from "$lib/components/current-block-hash.svelte"
 </script>
 
-{#if Option.isSome(block.data)}
-  <div class="font-mono">
-    {block.data.value.result.block_id.hash}
-  </div>
-  {#if Option.isSome(block.error)}
-    There was an error refetching this data. the data you see is stale
-    <pre class="font-mono bg-red-500">
-      {JSON.stringify(block.error.value, null, 2)}
-    </pre>
-  {/if}
-{:else}
-  Loading...
-  {#if Option.isSome(block.error)}
-    There was an error fetching this data. We will retry automatically. Error:
-    <pre class="font-mono bg-red-500">
-      {JSON.stringify(block.error.value, null, 2)}
-    </pre>
-  {/if}
-{/if}
+<CurrentBlockHash/>
+<CurrentBlockHash/>
+
