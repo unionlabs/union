d0e3300c185a2ce551cb05b56a6e3da8a17969e3
cor <cor@pruijs.dev>
2025-02-25 18:08:05 +0000

    feat(app2): add sidebar

---
+++ app2/src/lib/components/layout/Sidebar/index.svelte
@@ -0,0 +1,32 @@
+<script>
+import ChainList from "$lib/components/ChainList.svelte"
+</script>
+
+<div class="p-4 flex flex-col gap-4">
+  <section>
+    <h2 class="font-bold text-xl m2-4">Transfer</h2>
+    <ul>
+      <li>Transfer</li>
+      <li>Swap</li>
+      <li>Stake</li>
+    </ul>
+  </section>
+  <section>
+    <h2 class="font-bold text-xl m2-4">Explorer</h2>
+    <ul>
+      <li>Transfers</li>
+      <li>Packets</li>
+      <li>Connections</li>
+      <li>Channels</li>
+    </ul>
+  </section>
+
+  <section>
+    <h2 class="font-bold text-xl m2-4">Explorer</h2>
+    <ul>
+      <li>Transfers</li>
+      <li>Connections</li>
+      <li>Channels</li>
+    </ul>
+  </section>
+</div>
--- app2/src/routes/+layout.svelte
+++ app2/src/routes/+layout.svelte
@@ -5,6 +5,7 @@ import { Effect, Fiber, Option } from "effect"
 import { block } from "$lib/stores/block.svelte"
 import { chainsQuery } from "$lib/queries/chains.svelte"
 import { chains } from "$lib/stores/chains.svelte"
+import Sidebar from "$lib/components/layout/Sidebar/index.svelte"
 
 let { children } = $props()
 
@@ -20,8 +21,26 @@ onMount(() => {
 })
 </script>
 
-{@render children()}
+<div class="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] min-h-[100svh] w-full">
+  <header class="col-span-2 fixed top-0 left-0 right-0 h-16 z-10 dark:bg-zinc-800 shadow">
+    <div class="px-4 h-full flex items-center">
+      Header Content
+    </div>
+  </header>
+  
+  <aside class="fixed top-16 left-0 bottom-0 w-64 dark:bg-zinc-800 shadow overflow-auto">
+    <Sidebar/>
+  </aside>
+  
+  <!-- Main content area: Has margin to clear fixed elements -->
+  <main class="col-start-2 mt-16 ml-64">
+    <!-- Main scrollable content -->
+    {@render children()}
+  </main>
+</div>
 
+<!--
+<Sidebar/>
 {#if Option.isSome(chains.error)}
   <div class="bg-red-500">
     <h2 class="text-xl font-bold">Error updating chains</h2>
@@ -29,3 +48,4 @@ onMount(() => {
     <div>{chains.error.value.cause}</div>
   </div>
 {/if}
+!-->
