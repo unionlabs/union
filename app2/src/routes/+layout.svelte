35c01b27151c05ea8c4ac5585bdb280af6fee23a
cor <cor@pruijs.dev>
2025-02-25 18:34:56 +0000

    feat(app): show global app errors

--- app2/src/routes/+layout.svelte
+++ app2/src/routes/+layout.svelte
@@ -6,6 +6,7 @@ import { block } from "$lib/stores/block.svelte"
 import { chainsQuery } from "$lib/queries/chains.svelte"
 import { chains } from "$lib/stores/chains.svelte"
 import Sidebar from "$lib/components/layout/Sidebar/index.svelte"
+import AppErrors from "$lib/components/layout/AppErrors/index.svelte"
 
 let { children } = $props()
 
@@ -21,21 +22,14 @@ onMount(() => {
 })
 </script>
 
-<div class="grid grid-cols-[auto_1fr] min-h-[100svh] w-full">
+<div class="grid grid-cols-[auto_1fr] min-h-[100svh] w-screen">
   <aside class="fixed top-0 left-0 bottom-0 w-64 dark:bg-zinc-800 shadow overflow-auto">
     <Sidebar/>
   </aside>
   
-  <!-- Main content area: Has margin to clear fixed elements -->
-  <main class="col-start-2 ml-64">
-    <!-- Show any app-wide errors here -->
-    {#if Option.isSome(chains.error)}
-      <div class="bg-red-500">
-        <h2 class="text-xl font-bold">Error updating chains</h2>
-        <div>{chains.error.value}</div>
-        <div>{chains.error.value.cause}</div>
-      </div>
-    {/if}
+  <!-- Main content area: Has margin to clear fixed sidebar -->
+  <main class="col-start-2 ml-64 max-w-[calc(100vw - calc(var(--spacing)*64))] overflow-hidden">
+    <AppErrors/>
     {@render children()}
   </main>
 </div>
