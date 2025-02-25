55387ea443f31c7a624332fc3a5e2a31e861da7c
cor <cor@pruijs.dev>
2025-02-25 16:43:52 +0000

    feat(app2): use Schema.Class

--- app2/src/lib/schemas/chain.ts
+++ app2/src/lib/schemas/chain.ts
@@ -1,9 +1,9 @@
 import { Schema } from "effect"
 
-export const Chain = Schema.Struct({
+class Chain extends Schema.Class<Chain>("Chain")({
   chain_id: Schema.String,
   display_name: Schema.String,
   addr_prefix: Schema.String
-})
+}) {}
 
 export const Chains = Schema.Array(Chain)
