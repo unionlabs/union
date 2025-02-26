f2f570fb4ed87bf048998ee791377a82995cbb0f
Lukas <Lukas.nyberg@gmail.com>
2025-02-26 19:30:00 +0100

    fix(app2): remove hardcoded address

--- app2/src/lib/stores/wallets.svelte.ts
+++ app2/src/lib/stores/wallets.svelte.ts
@@ -1,14 +1,12 @@
 import { Option } from "effect"
-import {
+import type {
   AddressEvmCanonical,
-  type AddressAptosCanonical,
-  type AddressCosmosCanonical
+  AddressAptosCanonical,
+  AddressCosmosCanonical
 } from "$lib/schema/address"
 
 class WalletsStore {
-  evmAddress: Option.Option<typeof AddressEvmCanonical.Type> = $state(
-    Option.some(AddressEvmCanonical.make("0xe6831e169d77a861a0e71326afa6d80bcc8bc6aa"))
-  )
+  evmAddress: Option.Option<typeof AddressEvmCanonical.Type> = $state(Option.none())
   cosmosAddress: Option.Option<typeof AddressCosmosCanonical.Type> = $state(Option.none())
   aptosAddress: Option.Option<typeof AddressAptosCanonical.Type> = $state(Option.none())
 }
