e9a28445590685e15df01c0a9166f846c4eac5d8
Lukas <Lukas.nyberg@gmail.com>
2025-02-26 19:26:49 +0100

    fix(app2): svelte config

--- app2/svelte.config.js
+++ app2/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static"
+import adapterStatic from "@sveltejs/adapter-static"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,12 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapterStatic({
+      strict: true,
+      pages: "build",
+      assets: "build",
+      fallback: "index.html"
+    })
   }
 }
 
