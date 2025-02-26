acd286bfa1160ce5ffbc2567514eca891adc8001
cor <cor@pruijs.dev>
2025-02-26 23:04:31 +0000

    feat(app2): load transfers with skeletons

--- app2/src/routes/explorer/transfers/+page.svelte
+++ app2/src/routes/explorer/transfers/+page.svelte
@@ -18,24 +18,28 @@ onMount(() => {
 
 
 <Sections>
+  <h1 class="font-bold text-4xl">Transfers</h1>
   <Card class="overflow-auto p-0 divide-y divide-zinc-800">
     {#if Option.isSome(transferList.data) && Option.isSome(chains.data)}
       {@const chainss = chains.data.value}
       {#each transferList.data.value as transfer}
         {@const sourceChain = getChain(chainss, transfer.source_chain_id)}
         {@const destinationChain = getChain(chainss, transfer.destination_chain_id)}
-        <div class="flex gap-8 px-4 py-2">
-          <div>
+        <div class="flex gap-8 px-4 py-2 flex-cols-3">
+          <div class="flex-1">
             <p class="uppercase text-zinc-500 text-xs font-semibold">from</p>
             {#if Option.isSome(sourceChain)}
               <ChainComponent chain={sourceChain.value}/>
             {/if}
           </div>
-          <div>
+          <div class="flex-1">
             <p class="uppercase text-zinc-500 text-xs font-semibold">to</p>
             {#if Option.isSome(destinationChain)}
               <ChainComponent chain={destinationChain.value}/>
             {/if}
+          </div>
+          <div class="flex-1">
+            yeah
           </div>
         </div>
       {/each}
@@ -43,7 +47,10 @@ onMount(() => {
         <ErrorComponent error={transferList.error.value}/>
       {/if}
     {:else}
-      Loading...
+      {#each [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
+        <div class="h-[57px] p-4">
+        </div>
+      {/each}
       {#if Option.isSome(transferList.error)}
         <ErrorComponent error={transferList.error.value}/>
       {/if}
