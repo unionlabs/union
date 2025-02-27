44fffd53a8427b682f43daeeddb7ea1cc69fb552
cor <cor@pruijs.dev>
2025-02-27 16:58:40 +0000

    fix(app2): stylable card

--- app2/src/lib/components/ui/Card.svelte
+++ app2/src/lib/components/ui/Card.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
+import { cn } from "$lib/utils"
+import type { HTMLAttributes } from "svelte/elements"
 import type { Snippet } from "svelte"
 
-// Card.svelte
-type Props = {
+type Props = HTMLAttributes<HTMLDivElement> & {
   children: Snippet
   class?: string
 }
 
-// Define props with default
-const { children, class: className = "" }: Props = $props()
+const { children, class: className = "", ...rest }: Props = $props()
+
+const classes = cn(
+  // Base styles
+  "rounded p-4 border shadow-sm",
+  "dark:border-zinc-700 dark:bg-zinc-900",
+  // Additional classes passed as props
+  className
+)
 </script>
 
-<div class="rounded border dark:border-zinc-700 dark:bg-zinc-900 shadow-sm {className}">
+<div
+  class={classes}
+  {...rest}
+>
   {@render children()}
 </div>
