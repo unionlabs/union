d67d814c1e35b103222a1f612115f4f859cdac51
cor <cor@pruijs.dev>
2025-03-03 14:01:58 +0000

    feat(app2): switch chains, use connectorClient for tx submission

--- app2/src/lib/services/transfer.ts
+++ app2/src/lib/services/transfer.ts
@@ -9,7 +9,12 @@ import type {
 } from "viem"
 import { createPublicClient, createWalletClient, http } from "viem"
 import { sepolia } from "viem/chains"
-import { custom, getConnectorClient, switchChain, waitForTransactionReceipt } from "@wagmi/core"
+import {
+  custom,
+  getConnectorClient,
+  switchChain as wagmiSwitchChain,
+  waitForTransactionReceipt
+} from "@wagmi/core"
 import { wagmiConfig, type ConfiguredChainId } from "$lib/wallet/evm/wagmi-config"
 
 export class CreateWalletClientError extends Data.TaggedError("CreateWalletClientError")<{
@@ -33,17 +38,25 @@ export class SendTransactionError extends Data.TaggedError("SendTransactionError
 export class SwitchChainError extends Data.TaggedError("SwitchChainError")<{
   cause: SwitchChainErrorType
 }> {}
+export class ConnectorClientError extends Data.TaggedError("ConnectorClientError")<{
+  cause: Error
+}> {}
 
 export type SubmitTransferError = SendTransactionError | CreateWalletClientError
 
-export const switchToChain = (chainId: ConfiguredChainId) =>
+export const switchChain = (chainId: ConfiguredChainId) =>
   Effect.tryPromise({
-    try: () => switchChain(wagmiConfig, { chainId }),
+    try: () => wagmiSwitchChain(wagmiConfig, { chainId }),
     catch: err => new SwitchChainError({ cause: err as SwitchChainErrorType })
   })
 
 export const submitTransfer = (transactionArgs: SendTransactionParameters) =>
   Effect.gen(function* () {
+    const connectorClient = yield* Effect.tryPromise({
+      try: () => getConnectorClient(wagmiConfig),
+      catch: err => new ConnectorClientError({ cause: err as Error })
+    })
+
     const publicClient = yield* Effect.try({
       try: () =>
         createPublicClient({
@@ -57,10 +70,11 @@ export const submitTransfer = (transactionArgs: SendTransactionParameters) =>
       try: () =>
         createWalletClient({
           chain: sepolia,
-          transport: () => window.ethereum
+          transport: custom(connectorClient.transport)
         }),
       catch: err => new CreateWalletClientError({ cause: err as CreateWalletClientErrorType })
     })
+
     const hash = yield* Effect.tryPromise({
       try: () => walletClient.sendTransaction(transactionArgs),
       catch: err => new SendTransactionError({ cause: err as SendTransactionErrorType })
