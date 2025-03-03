414870d37b5383981e7795e3cc9684ea87735345
cor <cor@pruijs.dev>
2025-03-03 00:37:06 +0000

    feat(app2): improve error types

--- app2/src/lib/services/transfer.ts
+++ app2/src/lib/services/transfer.ts
@@ -1,14 +1,18 @@
 import { Data, Effect } from "effect"
-import type { SendTransactionParameters } from "viem"
+import type {
+  CreateWalletClientErrorType,
+  SendTransactionErrorType,
+  SendTransactionParameters
+} from "viem"
 import { createWalletClient } from "viem"
 import { sepolia } from "viem/chains"
 
 export class CreateWalletClientError extends Data.TaggedError("CreateWalletClientError")<{
-  cause: Error
+  cause: CreateWalletClientErrorType
 }> {}
 
 export class SendTransactionError extends Data.TaggedError("SendTransactionError")<{
-  cause: Error
+  cause: SendTransactionErrorType
 }> {}
 
 export type SubmitTransferError = SendTransactionError | CreateWalletClientError
@@ -21,12 +25,12 @@ export const submitTransfer = (transactionArgs: SendTransactionParameters) =>
           chain: sepolia,
           transport: () => window.ethereum
         }),
-      catch: err => new CreateWalletClientError({ cause: err as Error })
+      catch: err => new CreateWalletClientError({ cause: err as CreateWalletClientErrorType })
     })
 
     const hash = yield* Effect.tryPromise({
       try: () => walletClient.sendTransaction(transactionArgs),
-      catch: err => new SendTransactionError({ cause: err as Error })
+      catch: err => new SendTransactionError({ cause: err as SendTransactionErrorType })
     })
     return hash
   })
