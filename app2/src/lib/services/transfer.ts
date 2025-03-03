33f968630a39e454019d84c40341a6e54e978b93
cor <cor@pruijs.dev>
2025-03-03 10:49:19 +0000

    feat(app2): wait on transfer receipt

--- app2/src/lib/services/transfer.ts
+++ app2/src/lib/services/transfer.ts
@@ -1,14 +1,26 @@
 import { Data, Effect } from "effect"
 import type {
+  CreatePublicClientErrorType,
   CreateWalletClientErrorType,
   SendTransactionErrorType,
-  SendTransactionParameters
+  SendTransactionParameters,
+  WaitForTransactionReceiptErrorType
 } from "viem"
-import { createWalletClient } from "viem"
+import { createPublicClient, createWalletClient, http } from "viem"
 import { sepolia } from "viem/chains"
 
 export class CreateWalletClientError extends Data.TaggedError("CreateWalletClientError")<{
   cause: CreateWalletClientErrorType
+}> {}
+
+export class WaitForTransactionReceiptError extends Data.TaggedError(
+  "WaitForTransactionReceiptError"
+)<{
+  cause: WaitForTransactionReceiptErrorType
+}> {}
+
+export class CreatePublicClientError extends Data.TaggedError("CreatePublicClientError")<{
+  cause: CreatePublicClientErrorType
 }> {}
 
 export class SendTransactionError extends Data.TaggedError("SendTransactionError")<{
@@ -19,6 +31,15 @@ export type SubmitTransferError = SendTransactionError | CreateWalletClientError
 
 export const submitTransfer = (transactionArgs: SendTransactionParameters) =>
   Effect.gen(function* () {
+    const publicClient = yield* Effect.try({
+      try: () =>
+        createPublicClient({
+          chain: sepolia,
+          transport: http()
+        }),
+      catch: err => new CreatePublicClientError({ cause: err as CreatePublicClientErrorType })
+    })
+
     const walletClient = yield* Effect.try({
       try: () =>
         createWalletClient({
@@ -27,10 +48,16 @@ export const submitTransfer = (transactionArgs: SendTransactionParameters) =>
         }),
       catch: err => new CreateWalletClientError({ cause: err as CreateWalletClientErrorType })
     })
-
     const hash = yield* Effect.tryPromise({
       try: () => walletClient.sendTransaction(transactionArgs),
       catch: err => new SendTransactionError({ cause: err as SendTransactionErrorType })
     })
-    return hash
+
+    const receipt = yield* Effect.tryPromise({
+      try: () => publicClient.waitForTransactionReceipt({ hash }),
+      catch: err =>
+        new WaitForTransactionReceiptError({ cause: err as WaitForTransactionReceiptErrorType })
+    })
+
+    return receipt
   })
