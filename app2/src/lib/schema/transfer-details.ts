15a73efd58898e197c5444ff58bd9fd8de8a5655
jurriaan <jurriaan@pruijs.nl>
2025-03-04 13:39:09 +0100

    feat(apps): use v2 api - transfer details

--- app2/src/lib/schema/transfer-details.ts
+++ app2/src/lib/schema/transfer-details.ts
@@ -1,5 +1,5 @@
 import { Schema } from "effect"
-import { ChainId } from "$lib/schema/chain"
+import { ChainReference } from "$lib/schema/chain"
 import { Hex } from "$lib/schema/hex"
 import { PacketTrace } from "$lib/schema/packet-trace"
 import { ConnectionId } from "$lib/schema/connection"
@@ -7,13 +7,13 @@ import { ChannelId } from "$lib/schema/channel"
 import { TokenRawDenom, TokenRawAmount } from "$lib/schema/token"
 
 export class TransferDetails extends Schema.Class<TransferDetails>("TransferDetails")({
-  sender_normalized: Hex,
-  source_chain_id: ChainId,
+  sender_canonical: Hex,
+  source_chain: ChainReference,
   source_connection_id: ConnectionId,
   source_channel_id: ChannelId,
   packet_send_transaction_hash: Schema.String,
-  receiver_normalized: Hex,
-  destination_chain_id: ChainId,
+  receiver_canonical: Hex,
+  destination_chain: ChainReference,
   destination_connection_id: ConnectionId,
   destination_channel_id: ChannelId,
   packet_send_timestamp: Schema.DateTimeUtc,
