db08d5559dfcdc25a5128bdce1edcb8419c1e058
Kaan Caglan <caglankaan@gmail.com>
2025-03-06 18:49:06 +0300

    chore(app): wip stargaze faucet added
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- app/src/routes/faucet/+page.svelte
+++ app/src/routes/faucet/+page.svelte
@@ -1,155 +1,161 @@
 <script lang="ts">
-import request from "graphql-request"
-import { onDestroy, onMount } from "svelte"
-import { cn } from "$lib/utilities/shadcn.ts"
-import { URLS } from "$lib/constants/index.ts"
-import { Label } from "$lib/components/ui/label"
-import { writable, type Writable } from "svelte/store"
-import Truncate from "$lib/components/truncate.svelte"
-import * as Card from "$lib/components/ui/card/index.ts"
-import { Input } from "$lib/components/ui/input/index.ts"
-import { Button } from "$lib/components/ui/button/index.ts"
-import SpinnerSVG from "$lib/components/spinner-svg.svelte"
-import WalletGateCosmos from "$lib/components/wallet-gate-cosmos.svelte"
-import ChainsGate from "$lib/components/chains-gate.svelte"
-import { cosmosStore } from "$/lib/wallet/cosmos/config.ts"
-import type { DiscriminatedUnion } from "$lib/utilities/types.ts"
-import { faucetUnoMutation2 } from "$lib/graphql/queries/faucet.ts"
-import { isValidCosmosAddress } from "$lib/wallet/utilities/validate.ts"
-import { createCosmosSdkAddressRegex } from "$lib/utilities/address.ts"
-import { Turnstile } from "svelte-turnstile"
+  import request from "graphql-request";
+  import { onDestroy, onMount } from "svelte";
+  import { cn } from "$lib/utilities/shadcn.ts";
+  import { URLS } from "$lib/constants/index.ts";
+  import { Label } from "$lib/components/ui/label";
+  import { writable, type Writable } from "svelte/store";
+  import Truncate from "$lib/components/truncate.svelte";
+  import * as Card from "$lib/components/ui/card/index.ts";
+  import { Input } from "$lib/components/ui/input/index.ts";
+  import StargazeFaucet from "./(components)/stargaze-faucet.svelte";
+  import { Button } from "$lib/components/ui/button/index.ts";
+  import SpinnerSVG from "$lib/components/spinner-svg.svelte";
+  import WalletGateCosmos from "$lib/components/wallet-gate-cosmos.svelte";
+  import ChainsGate from "$lib/components/chains-gate.svelte";
+  import { cosmosStore } from "$/lib/wallet/cosmos/config.ts";
+  import type { DiscriminatedUnion } from "$lib/utilities/types.ts";
+  import { faucetUnoMutation2 } from "$lib/graphql/queries/faucet.ts";
+  import { isValidCosmosAddress } from "$lib/wallet/utilities/validate.ts";
+  import { createCosmosSdkAddressRegex } from "$lib/utilities/address.ts";
+  import { Turnstile } from "svelte-turnstile";
 
-// import ExternalFaucets from "./(components)/external-faucets.svelte"
-// import DydxFaucet from "./(components)/dydx-faucet.svelte"
-// import StrideFaucet from "./(components)/stride-faucet.svelte"
+  // import ExternalFaucets from "./(components)/external-faucets.svelte"
+  // import DydxFaucet from "./(components)/dydx-faucet.svelte"
+  // import StrideFaucet from "./(components)/stride-faucet.svelte"
 
-type FaucetState = DiscriminatedUnion<
-  "kind",
-  {
-    IDLE: {}
-    VERIFYING: {}
-    VERIFIED: {}
-    SUBMITTING: { captchaToken: string }
-    RESULT_OK: { message: string }
-    RESULT_ERR: { error: string }
-    VERIFICATION_FAILED: { error: string }
-  }
->
+  type FaucetState = DiscriminatedUnion<
+    "kind",
+    {
+      IDLE: {};
+      VERIFYING: {};
+      VERIFIED: {};
+      SUBMITTING: { captchaToken: string };
+      RESULT_OK: { message: string };
+      RESULT_ERR: { error: string };
+      VERIFICATION_FAILED: { error: string };
+    }
+  >;
 
-let address = ""
-let turnstileToken = ""
-let resetTurnstile: () => void
-let showTurnstile = false
+  let address = "";
+  let turnstileToken = "";
+  let resetTurnstile: () => void;
+  let showTurnstile = false;
 
-onMount(() => {
-  address = $cosmosStore.address ?? ""
-})
+  onMount(() => {
+    address = $cosmosStore.address ?? "";
+  });
 
-const unsubscribe = cosmosStore.subscribe(v => {
-  if (address !== v.address) {
-    address = v.address ?? ""
-  }
-})
+  const unsubscribe = cosmosStore.subscribe((v) => {
+    if (address !== v.address) {
+      address = v.address ?? "";
+    }
+  });
 
-onDestroy(() => {
-  unsubscribe()
-})
+  onDestroy(() => {
+    unsubscribe();
+  });
 
-const resetInput = () => {
-  address = $cosmosStore.address ?? ""
-}
+  const resetInput = () => {
+    address = $cosmosStore.address ?? "";
+  };
 
-let unoFaucetState: Writable<FaucetState> = writable({ kind: "IDLE" })
+  let unoFaucetState: Writable<FaucetState> = writable({ kind: "IDLE" });
 
-const verifyWithTurnstile = () => {
-  if ($unoFaucetState.kind === "IDLE") {
-    showTurnstile = true
-    unoFaucetState.set({ kind: "VERIFYING" })
-    resetTurnstile?.() // Trigger verification
-  }
-}
+  const verifyWithTurnstile = () => {
+    if ($unoFaucetState.kind === "IDLE") {
+      showTurnstile = true;
+      unoFaucetState.set({ kind: "VERIFYING" });
+      resetTurnstile?.(); // Trigger verification
+    }
+  };
 
-const requestUnoFromFaucet = async () => {
-  if ($unoFaucetState.kind === "VERIFIED") {
-    unoFaucetState.set({ kind: "SUBMITTING", captchaToken: turnstileToken })
-  }
+  const requestUnoFromFaucet = async () => {
+    if ($unoFaucetState.kind === "VERIFIED") {
+      unoFaucetState.set({ kind: "SUBMITTING", captchaToken: turnstileToken });
+    }
 
-  if ($unoFaucetState.kind === "SUBMITTING") {
-    try {
-      const result = await request(URLS().GRAPHQL, faucetUnoMutation2, {
-        chainId: "union-testnet-9",
-        denom: "muno",
-        address,
-        captchaToken: $unoFaucetState.captchaToken
-      })
-      if (result.send === null) {
+    if ($unoFaucetState.kind === "SUBMITTING") {
+      try {
+        const result = await request(URLS().GRAPHQL, faucetUnoMutation2, {
+          chainId: "union-testnet-9",
+          denom: "muno",
+          address,
+          captchaToken: $unoFaucetState.captchaToken,
+        });
+        if (result.send === null) {
+          unoFaucetState.set({
+            kind: "RESULT_ERR",
+            error: "Empty faucet response",
+          });
+          turnstileToken = "";
+          showTurnstile = false;
+          return;
+        }
+
+        if (result.send.startsWith("ERROR")) {
+          console.error(result.send);
+          unoFaucetState.set({
+            kind: "RESULT_ERR",
+            error: `Error from faucet`,
+          });
+          turnstileToken = "";
+          showTurnstile = false;
+          return;
+        }
+
+        unoFaucetState.set({
+          kind: "RESULT_OK",
+          message: result.send,
+        });
+        turnstileToken = "";
+        showTurnstile = false;
+      } catch (error) {
         unoFaucetState.set({
           kind: "RESULT_ERR",
-          error: "Empty faucet response"
-        })
-        turnstileToken = ""
-        showTurnstile = false
-        return
+          error: `Faucet error: ${error}`,
+        });
+        turnstileToken = "";
+        showTurnstile = false;
+        return;
       }
-
-      if (result.send.startsWith("ERROR")) {
-        console.error(result.send)
-        unoFaucetState.set({ kind: "RESULT_ERR", error: `Error from faucet` })
-        turnstileToken = ""
-        showTurnstile = false
-        return
-      }
-
-      unoFaucetState.set({
-        kind: "RESULT_OK",
-        message: result.send
-      })
-      turnstileToken = ""
-      showTurnstile = false
-    } catch (error) {
-      unoFaucetState.set({
-        kind: "RESULT_ERR",
-        error: `Faucet error: ${error}`
-      })
-      turnstileToken = ""
-      showTurnstile = false
-      return
     }
-  }
-}
+  };
 
-const resetVerification = () => {
-  if ($unoFaucetState.kind === "VERIFICATION_FAILED") {
-    turnstileToken = ""
-    showTurnstile = false
-    unoFaucetState.set({ kind: "IDLE" })
-  }
-}
+  const resetVerification = () => {
+    if ($unoFaucetState.kind === "VERIFICATION_FAILED") {
+      turnstileToken = "";
+      showTurnstile = false;
+      unoFaucetState.set({ kind: "IDLE" });
+    }
+  };
 
-const handleTurnstileCallback = (
-  e: CustomEvent<{ token: string; preClearanceObtained: boolean }>
-) => {
-  turnstileToken = e.detail.token
-  if ($unoFaucetState.kind === "VERIFYING") {
-    unoFaucetState.set({ kind: "VERIFIED" })
-  }
-}
+  const handleTurnstileCallback = (
+    e: CustomEvent<{ token: string; preClearanceObtained: boolean }>,
+  ) => {
+    turnstileToken = e.detail.token;
+    if ($unoFaucetState.kind === "VERIFYING") {
+      unoFaucetState.set({ kind: "VERIFIED" });
+    }
+  };
 
-const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
-  if ($unoFaucetState.kind === "VERIFYING") {
-    unoFaucetState.set({
-      kind: "VERIFICATION_FAILED",
-      error: `Verification error: ${e.detail.code}`
-    })
-  }
-}
+  const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
+    if ($unoFaucetState.kind === "VERIFYING") {
+      unoFaucetState.set({
+        kind: "VERIFICATION_FAILED",
+        error: `Verification error: ${e.detail.code}`,
+      });
+    }
+  };
 </script>
 
 <svelte:head>
   <title>Union | Faucet</title>
 </svelte:head>
 
-<main class="flex flex-col gap-6 items-center overflow-y-scroll max-h-full py-6 px-3 sm:px-6 w-full dark:bg-muted">
+<main
+  class="flex flex-col gap-6 items-center overflow-y-scroll max-h-full py-6 px-3 sm:px-6 w-full dark:bg-muted"
+>
   <Card.Root class="w-full max-w-lg">
     <Card.Header>
       <Card.Title>UNO Drip Faucet</Card.Title>
@@ -161,14 +167,14 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
       {#if $unoFaucetState.kind === "RESULT_OK"}
         <p>
           Tokens sent: <a
-                href={`https://explorer.testnet-9.union.build/union/tx/${$unoFaucetState.message}`}
-        >
-          <Truncate
-                  class="underline"
-                  value={$unoFaucetState.message}
-                  type="hash"
-          />
-        </a>
+            href={`https://explorer.testnet-9.union.build/union/tx/${$unoFaucetState.message}`}
+          >
+            <Truncate
+              class="underline"
+              value={$unoFaucetState.message}
+              type="hash"
+            />
+          </a>
         </p>
       {:else if $unoFaucetState.kind === "RESULT_ERR"}
         <p class="mb-4">
@@ -181,11 +187,11 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
         <p class="mt-4 break-words text-xs">{$unoFaucetState.error}</p>
       {:else}
         <form
-                action="?"
-                method="POST"
-                class="flex flex-col w-full gap-4"
-                name="faucet-form"
-                on:submit|preventDefault
+          action="?"
+          method="POST"
+          class="flex flex-col w-full gap-4"
+          name="faucet-form"
+          on:submit|preventDefault
         >
           <div>
             <Label for="address">Address</Label>
@@ -193,22 +199,23 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
               <div class="w-full">
                 <div class="relative w-full mb-2">
                   <Input
-                          autocapitalize="none"
-                          autocomplete="off"
-                          autocorrect="off"
-                          bind:value={address}
-                          id="address"
-                          pattern={createCosmosSdkAddressRegex({ prefix: "union" }).source}
-                          placeholder="union14ea6..."
-                          required={true}
-                          minlength={44}
-                          maxlength={44}
-                          spellcheck="false"
-                          name="wallet-address"
-                          type="text"
-                          data-1p-ignore={true}
-                          data-lpignore={true}
-                          class="disabled:opacity-100 disabled:bg-black/20"
+                    autocapitalize="none"
+                    autocomplete="off"
+                    autocorrect="off"
+                    bind:value={address}
+                    id="address"
+                    pattern={createCosmosSdkAddressRegex({ prefix: "union" })
+                      .source}
+                    placeholder="union14ea6..."
+                    required={true}
+                    minlength={44}
+                    maxlength={44}
+                    spellcheck="false"
+                    name="wallet-address"
+                    type="text"
+                    data-1p-ignore={true}
+                    data-lpignore={true}
+                    class="disabled:opacity-100 disabled:bg-black/20"
                   />
                 </div>
                 <div class="flex justify-between px-1">
@@ -225,17 +232,15 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
                             />
                         </p>
                         !-->
-                        <p slot="disconnected">
-                          Connect cosmos wallet
-                        </p>
+                        <p slot="disconnected">Connect cosmos wallet</p>
                       </WalletGateCosmos>
                     </ChainsGate>
                   </div>
                   {#if address !== $cosmosStore.address}
                     <button
-                            type="button"
-                            on:click={resetInput}
-                            class="text-xs text-muted-foreground hover:text-primary transition"
+                      type="button"
+                      on:click={resetInput}
+                      class="text-xs text-muted-foreground hover:text-primary transition"
                     >
                       Reset
                     </button>
@@ -246,25 +251,27 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
           </div>
           {#if showTurnstile}
             <Turnstile
-                    siteKey="0x4AAAAAAA-eVs5k0b8Q1dl5"
-                    on:callback={handleTurnstileCallback}
-                    on:error={handleTurnstileError}
-                    theme="auto"
-                    size="normal"
-                    bind:reset={resetTurnstile}
+              siteKey="0x4AAAAAAA-eVs5k0b8Q1dl5"
+              on:callback={handleTurnstileCallback}
+              on:error={handleTurnstileError}
+              theme="auto"
+              size="normal"
+              bind:reset={resetTurnstile}
             />
           {/if}
           <div class="flex flex-row items-center gap-4">
             {#if $unoFaucetState.kind === "IDLE" || $unoFaucetState.kind === "VERIFYING"}
               <Button
-                      type="button"
-                      on:click={(event) => {
-                  event.preventDefault()
-                  verifyWithTurnstile()
+                type="button"
+                on:click={(event) => {
+                  event.preventDefault();
+                  verifyWithTurnstile();
                 }}
-                      disabled={!isValidCosmosAddress(address, ["union"]) ||
+                disabled={!isValidCosmosAddress(address, ["union"]) ||
                   $unoFaucetState.kind === "VERIFYING"}
-                      class={cn("min-w-[110px] disabled:cursor-not-allowed disabled:opacity-50")}
+                class={cn(
+                  "min-w-[110px] disabled:cursor-not-allowed disabled:opacity-50",
+                )}
               >
                 Verify
                 {#if $unoFaucetState.kind === "VERIFYING"}
@@ -275,13 +282,15 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
               </Button>
             {:else if $unoFaucetState.kind === "VERIFIED" || $unoFaucetState.kind === "SUBMITTING"}
               <Button
-                      type="button"
-                      on:click={(event) => {
-                  event.preventDefault()
-                  requestUnoFromFaucet()
+                type="button"
+                on:click={(event) => {
+                  event.preventDefault();
+                  requestUnoFromFaucet();
                 }}
-                      disabled={$unoFaucetState.kind === "SUBMITTING"}
-                      class={cn("min-w-[110px] disabled:cursor-not-allowed disabled:opacity-50")}
+                disabled={$unoFaucetState.kind === "SUBMITTING"}
+                class={cn(
+                  "min-w-[110px] disabled:cursor-not-allowed disabled:opacity-50",
+                )}
               >
                 Submit
                 {#if $unoFaucetState.kind === "SUBMITTING"}
@@ -292,12 +301,14 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
               </Button>
             {:else if $unoFaucetState.kind === "VERIFICATION_FAILED"}
               <Button
-                      type="button"
-                      on:click={(event) => {
-                  event.preventDefault()
-                  resetVerification()
+                type="button"
+                on:click={(event) => {
+                  event.preventDefault();
+                  resetVerification();
                 }}
-                      class={cn("min-w-[110px] disabled:cursor-not-allowed disabled:opacity-50")}
+                class={cn(
+                  "min-w-[110px] disabled:cursor-not-allowed disabled:opacity-50",
+                )}
               >
                 Reset
               </Button>
@@ -312,8 +323,9 @@ const handleTurnstileError = (e: CustomEvent<{ code: string }>) => {
     </Card.Content>
   </Card.Root>
 
-<!--  <DydxFaucet />-->
-<!--  <StrideFaucet />-->
+  <!--  <DydxFaucet />-->
+  <!--  <StrideFaucet />-->
+  <StargazeFaucet />
   <!--
   <ChainsGate let:chains>
     <ExternalFaucets {chains} />
