4ddc45a051e22e8afd0adb3440c4a29f598f2a91
Lukas <Lukas.nyberg@gmail.com>
2025-03-12 20:44:40 +0100

    feat(app2): cosm client

--- app2/src/lib/services/cosmos/clients.ts
+++ app2/src/lib/services/cosmos/clients.ts
@@ -1,8 +1,48 @@
 import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate"
 import { Effect } from "effect"
-import {cosmosStore} from "$lib/wallet/cosmos";
+import {cosmosStore, type CosmosWalletId} from "$lib/wallet/cosmos";
+import type {CosmosWallet} from "$lib/services/cosmos/types.ts";
+import type {Chain} from "$lib/schema/chain.ts";
+import {getCosmosOfflineSigner} from "$lib/services/transfer-cosmos/offline-signer.ts";
+import {GasPrice, SigningStargateClient} from "@cosmjs/stargate";
+import {StargateClientError} from "$lib/services/transfer-cosmos";
+import {getGasPriceForChain} from "$lib/services/cosmos/chain-info";
 
-export const getCosmWasmClient = (rpc: URL | string) =>
+export const getCosmosClient = (
+  chain: Chain,
+  connectedWallet: CosmosWalletId
+) =>
+  Effect.gen(function* () {
+    if (!chain.rpcs) {
+      yield* Effect.fail(new StargateClientError({ cause: "No rpcs" }))
+      return
+    }
+
+    const offlineSigner = yield* getCosmosOfflineSigner(chain, connectedWallet)
+
+    if (!offlineSigner) {
+      yield* Effect.fail(new StargateClientError({ cause: "Offline signer is undefined" }))
+      return
+    }
+
+    const gasPriceInfo = yield* Effect.mapError(
+      getGasPriceForChain(chain, connectedWallet),
+      error => new StargateClientError({ cause: `Failed to get gas price: ${error.cause}` })
+    )
+
+    const gasPrice = GasPrice.fromString(`${gasPriceInfo.amount}${gasPriceInfo.denom}`)
+
+    return yield* Effect.tryPromise({
+      try: () => SigningStargateClient.connectWithSigner(
+        chain.getRpcUrl('rpc').toString(),
+        offlineSigner,
+        { gasPrice }
+      ),
+      catch: err => new StargateClientError({ cause: String(err) })
+    })
+  })
+
+export const getCosmosPublicClient = (rpc: URL | string) =>
   Effect.tryPromise({
     try: () => {
       const rpcString = typeof rpc === "string" ? rpc : rpc.toString()
@@ -12,11 +52,7 @@ export const getCosmWasmClient = (rpc: URL | string) =>
       new Error(`Failed to create CosmWasm client with RPC ${rpc} ${err}`, { cause: err })
   })
 
-
-//Move into schema?
-type CosmosWallet = Window['keplr'] | Window['leap']
-
-export const getWalletClient = (): Effect.Effect<CosmosWallet, Error, never> =>
+export const getCosmosWalletClient = (): Effect.Effect<CosmosWallet, Error, never> =>
   Effect.try({
     try: () => {
       const { connectedWallet, connectionStatus } = cosmosStore
---
+++ app2/src/lib/services/cosmos/types.ts
@@ -0,0 +1,108 @@
+
+//Move into schema?
+
+export type CosmosWallet = Window['keplr'] | Window['leap']
+
+export interface StdSignature {
+  readonly pub_key: {
+    readonly type: string
+    readonly value: any
+  }
+  readonly signature: string
+}
+
+export interface AminoMsg {
+  readonly type: string
+  readonly value: any
+}
+
+export interface SignDoc {
+  /**
+   * body_bytes is protobuf serialization of a TxBody that matches the
+   * representation in TxRaw.
+   */
+  bodyBytes: Uint8Array
+  /**
+   * auth_info_bytes is a protobuf serialization of an AuthInfo that matches the
+   * representation in TxRaw.
+   */
+  authInfoBytes: Uint8Array
+  /**
+   * chain_id is the unique identifier of the chain this transaction targets.
+   * It prevents signed transactions from being used on another chain by an
+   * attacker
+   */
+  chainId: string
+  /** account_number is the account number of the account in state */
+  accountNumber: bigint
+}
+
+/** Cosmos chain coin type. */
+export type Coin = { denom: string; amount: string }
+
+export interface StdFee {
+  readonly amount: ReadonlyArray<Coin>
+  readonly gas: string
+  /** The granter address that is used for paying with feegrants */
+  readonly granter?: string
+  /** The fee payer address. The payer must have signed the transaction. */
+  readonly payer?: string
+}
+
+export interface StdSignDoc {
+  readonly chain_id: string
+  readonly account_number: string
+  readonly sequence: string
+  readonly fee: StdFee
+  readonly msgs: ReadonlyArray<AminoMsg>
+  readonly memo: string
+  readonly timeout_height?: string
+}
+
+export type Algo = "secp256k1" | "ed25519" | "sr25519"
+export interface AccountData {
+  readonly address: string
+  readonly algo: Algo
+  readonly pubkey: Uint8Array
+}
+
+/** Offline signer is the account for a Cosmos chain. */
+export type OfflineSigner =
+  | {
+  /** getAccounts returns the list of accounts available on this signer. */
+  readonly getAccounts: () => Promise<ReadonlyArray<AccountData>>
+  readonly signDirect: (
+    signerAddress: string,
+    signDoc: SignDoc
+  ) => Promise<{
+    /**
+     * The sign doc that was signed.
+     * This may be different from the input signDoc when the signer modifies it as part of the signing process.
+     */
+    readonly signed: SignDoc
+    readonly signature: StdSignature
+  }>
+}
+  | {
+  readonly getAccounts: () => Promise<ReadonlyArray<AccountData>>
+  /**
+   * Request signature from whichever key corresponds to provided bech32-encoded address. Rejects if not enabled.
+   *
+   * The signer implementation may offer the user the ability to override parts of the signDoc. It must
+   * return the doc that was signed in the response.
+   *
+   * @param signerAddress The address of the account that should sign the transaction
+   * @param signDoc The content that should be signed
+   */
+  readonly signAmino: (
+    signerAddress: string,
+    signDoc: StdSignDoc
+  ) => Promise<{
+    /**
+     * The sign doc that was signed.
+     * This may be different from the input signDoc when the signer modifies it as part of the signing process.
+     */
+    readonly signed: StdSignDoc
+    readonly signature: StdSignature
+  }>
+}
