afae636ede10f7ddd928080140bfa68903c2a53c
Lukas <Lukas.nyberg@gmail.com>
2025-03-12 17:39:08 +0100

    feat(app2): getWalletClient cosmos

--- app2/src/lib/services/cosmos/clients.ts
+++ app2/src/lib/services/cosmos/clients.ts
@@ -1,5 +1,6 @@
 import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate"
 import { Effect } from "effect"
+import {cosmosStore} from "$lib/wallet/cosmos";
 
 export const getCosmWasmClient = (rpc: URL | string) =>
   Effect.tryPromise({
@@ -9,4 +10,27 @@ export const getCosmWasmClient = (rpc: URL | string) =>
     },
     catch: err =>
       new Error(`Failed to create CosmWasm client with RPC ${rpc} ${err}`, { cause: err })
+  })
+
+
+//Move into schema?
+type CosmosWallet = Window['keplr'] | Window['leap']
+
+export const getWalletClient = (): Effect.Effect<CosmosWallet, Error, never> =>
+  Effect.try({
+    try: () => {
+      const { connectedWallet, connectionStatus } = cosmosStore
+      if (connectionStatus === "connected" && connectedWallet) {
+        // Type assertion to help TypeScript understand this will be a cosmos wallet
+        const wallet = window[connectedWallet as keyof Window] as CosmosWallet
+        if (!wallet) {
+          throw new Error(`Wallet ${connectedWallet} not found`)
+        }
+        return wallet
+      }
+
+      throw new Error("Wallet not connected")
+    },
+    catch: (err) =>
+      new Error(`Failed to get cosmos wallet client`, { cause: err })
   })
