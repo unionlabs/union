b30df96990ddf29318dfe854d3582eac5b113698
cor <cor@pruijs.dev>
2025-03-18 13:05:37 +0000

    feat(ts-sdk): expose all subabis

--- ts-sdk/src/evm/abi/index.ts
+++ ts-sdk/src/evm/abi/index.ts
@@ -7,22 +7,21 @@ const packetComponentAbis = Option.fromNullable(
   ucs03abi.find(a => "name" in a && a.name === "ensureExported")
 ).pipe(Option.map(a => a.inputs))
 
-export const fungibleAssetOrderAbi = packetComponentAbis.pipe(
-  Option.flatMap(a =>
-    Option.fromNullable(a.find(as => as.internalType === "struct FungibleAssetOrder"))
-  ),
-  Option.map(a => a.components),
-  Option.getOrThrow
-)
+const getStructComponents = (structName: string) =>
+  packetComponentAbis.pipe(
+    Option.flatMap(a =>
+      Option.fromNullable(a.find(as => as.internalType === `struct ${structName}`))
+    ),
+    Option.map(a => a.components),
+    Option.getOrThrow
+  )
 
-export const instructionAbi = packetComponentAbis.pipe(
-  Option.flatMap(a => Option.fromNullable(a.find(as => as.internalType === "struct Instruction"))),
-  Option.map(a => a.components),
-  Option.getOrThrow
-)
-
-export const zkgmPacketAbi = packetComponentAbis.pipe(
-  Option.flatMap(a => Option.fromNullable(a.find(as => as.internalType === "struct ZkgmPacket"))),
-  Option.map(a => a.components),
-  Option.getOrThrow
-)
+export const fungibleAssetOrderAbi = getStructComponents("FungibleAssetOrder")
+export const instructionAbi = getStructComponents("Instruction")
+export const zkgmPacketAbi = getStructComponents("ZkgmPacket")
+export const forwardAbi = getStructComponents("Forward")
+export const multiplexAbi = getStructComponents("Multiplex")
+export const batchAbi = getStructComponents("Batch")
+export const ackAbi = getStructComponents("Ack")
+export const batchAckAbi = getStructComponents("BatchAck")
+export const fungibleAssetOrderAckAbi = getStructComponents("FungibleAssetOrderAck")
