eef4416a4b4798874098cccf18f3dae7006f1a3e
cor <cor@pruijs.dev>
2025-03-18 13:52:10 +0000

    feat(app2): batch submit attempt

--- app2/src/routes/transfer/ts-packets/+page.svelte
+++ app2/src/routes/transfer/ts-packets/+page.svelte
@@ -1,34 +1,6 @@
 <script lang="ts">
-import { ucs03ZkgmAbi } from "$lib/abi/ucs03"
-import { Option } from "effect"
-import { encodeAbiParameters, createPublicClient, http, getContract, parseAbi } from "viem"
-
-const packetComponentAbis = Option.fromNullable(
-  ucs03ZkgmAbi.find(a => "name" in a && a.name === "ensureExported")
-).pipe(Option.map(a => a.inputs))
-
-// biome-ignore lint/style/noNonNullAssertion: we know this will be included bc the abi is const
-const fungibleAssetOrderAbi = packetComponentAbis.pipe(
-  Option.flatMap(a =>
-    Option.fromNullable(a.find(as => as.internalType === "struct FungibleAssetOrder"))
-  ),
-  Option.map(a => a.components),
-  Option.getOrUndefined
-)!
-
-// biome-ignore lint/style/noNonNullAssertion: we know this will be included bc the abi is const
-const instructionAbi = packetComponentAbis.pipe(
-  Option.flatMap(a => Option.fromNullable(a.find(as => as.internalType === "struct Instruction"))),
-  Option.map(a => a.components),
-  Option.getOrUndefined
-)!
-
-// biome-ignore lint/style/noNonNullAssertion: we know this will be included bc the abi is const
-const zkgmPacketAbi = packetComponentAbis.pipe(
-  Option.flatMap(a => Option.fromNullable(a.find(as => as.internalType === "struct ZkgmPacket"))),
-  Option.map(a => a.components),
-  Option.getOrUndefined
-)!
+import { encodeAbiParameters } from "viem"
+import { batchAbi, fungibleAssetOrderAbi } from "@unionlabs/sdk/evm/abi"
 
 const assetOrder = encodeAbiParameters(fungibleAssetOrderAbi, [
   "0xE6831e169d77a861A0E71326AFA6d80bCC8Bc6aA",
@@ -42,8 +14,17 @@ const assetOrder = encodeAbiParameters(fungibleAssetOrderAbi, [
   "0x74d5b8eacfeb0dadaaf66403f40e304b3ef968b3",
   4n
 ])
+
+const batch = encodeAbiParameters(batchAbi, [
+  [
+    { version: 1, opcode: 3, operand: assetOrder },
+    { version: 1, opcode: 3, operand: assetOrder }
+  ]
+])
 </script>
 
 <div class="font-mono break-words">{JSON.stringify(assetOrder, null, 2)}</div>
+
+<div class="font-mono break-words">{JSON.stringify(batch, null, 2)}</div>
 
 
