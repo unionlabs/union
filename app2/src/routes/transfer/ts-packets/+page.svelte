bb54ed8e32d6e2750422cb0f3e3232de5cc2ce3f
cor <cor@pruijs.dev>
2025-03-18 14:39:25 +0000

    feat(app2): order constructors

--- app2/src/routes/transfer/ts-packets/+page.svelte
+++ app2/src/routes/transfer/ts-packets/+page.svelte
@@ -2,7 +2,23 @@
 import { encodeAbiParameters } from "viem"
 import { batchAbi, fungibleAssetOrderAbi } from "@unionlabs/sdk/evm/abi"
 
-const assetOrder = encodeAbiParameters(fungibleAssetOrderAbi, [
+const fungibleAssetOrder = (
+  operand: Parameters<typeof encodeAbiParameters<typeof fungibleAssetOrderAbi>>[1]
+) => ({
+  opcode: 3,
+  version: 1,
+  operand: encodeAbiParameters(fungibleAssetOrderAbi, operand)
+})
+
+const batch = (
+  instructions: Array<{ version: number; opcode: number; operand: `0x${string}` }>
+) => ({
+  opcode: 2,
+  version: 0,
+  operand: encodeAbiParameters(batchAbi, [instructions])
+})
+
+const exampleOrder = fungibleAssetOrder([
   "0xE6831e169d77a861A0E71326AFA6d80bCC8Bc6aA",
   "0xE6831e169d77a861A0E71326AFA6d80bCC8Bc6aA",
   "0x74d5b8eacfeb0dadaaf66403f40e304b3ef968b3",
@@ -15,16 +31,11 @@ const assetOrder = encodeAbiParameters(fungibleAssetOrderAbi, [
   4n
 ])
 
-const batch = encodeAbiParameters(batchAbi, [
-  [
-    { version: 1, opcode: 3, operand: assetOrder },
-    { version: 1, opcode: 3, operand: assetOrder }
-  ]
-])
+const exampleBatch = batch([exampleOrder, exampleOrder])
 </script>
 
-<div class="font-mono break-words">{JSON.stringify(assetOrder, null, 2)}</div>
+<div class="font-mono break-words">{JSON.stringify(exampleOrder, null, 2)}</div>
 
-<div class="font-mono break-words">{JSON.stringify(batch, null, 2)}</div>
+<div class="font-mono break-words">{JSON.stringify(exampleBatch, null, 2)}</div>
 
 
