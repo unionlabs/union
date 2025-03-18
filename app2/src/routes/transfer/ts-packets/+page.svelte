6a0e301a0c3a9cdbc4f6fafde720bb0d1386d1eb
cor <cor@pruijs.dev>
2025-03-18 12:12:29 +0000

    fix(app2): cleanup

--- app2/src/routes/transfer/ts-packets/+page.svelte
+++ app2/src/routes/transfer/ts-packets/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
 import { ucs03ZkgmAbi } from "$lib/abi/ucs03"
-import { generateSalt } from "$lib/services/shared"
-import { Effect, Option, pipe } from "effect"
+import { Option } from "effect"
 import { encodeAbiParameters, createPublicClient, http, getContract, parseAbi } from "viem"
-import { mainnet } from "viem/chains"
 
-const packetAbis = Option.fromNullable(
+const packetComponentAbis = Option.fromNullable(
   ucs03ZkgmAbi.find(a => "name" in a && a.name === "ensureExported")
 ).pipe(Option.map(a => a.inputs))
 
 // biome-ignore lint/style/noNonNullAssertion: we know this will be included bc the abi is const
-const fungibleAssetOrderAbi = packetAbis.pipe(
+const fungibleAssetOrderAbi = packetComponentAbis.pipe(
   Option.flatMap(a =>
     Option.fromNullable(a.find(as => as.internalType === "struct FungibleAssetOrder"))
   ),
@@ -19,42 +17,33 @@ const fungibleAssetOrderAbi = packetAbis.pipe(
 )!
 
 // biome-ignore lint/style/noNonNullAssertion: we know this will be included bc the abi is const
-const instructionAbi = packetAbis.pipe(
+const instructionAbi = packetComponentAbis.pipe(
   Option.flatMap(a => Option.fromNullable(a.find(as => as.internalType === "struct Instruction"))),
   Option.map(a => a.components),
   Option.getOrUndefined
 )!
 
 // biome-ignore lint/style/noNonNullAssertion: we know this will be included bc the abi is const
-const zkgmPacketAbi = packetAbis.pipe(
+const zkgmPacketAbi = packetComponentAbis.pipe(
   Option.flatMap(a => Option.fromNullable(a.find(as => as.internalType === "struct ZkgmPacket"))),
   Option.map(a => a.components),
   Option.getOrUndefined
 )!
 
-const zkgmInstruction = Effect.gen(function* () {
-  const salt = yield* generateSalt
-
-  const assetOrder = encodeAbiParameters(fungibleAssetOrderAbi, [
-    "0xE6831e169d77a861A0E71326AFA6d80bCC8Bc6aA",
-    "0xE6831e169d77a861A0E71326AFA6d80bCC8Bc6aA",
-    "0x74d5b8eacfeb0dadaaf66403f40e304b3ef968b3",
-    4n,
-    "muno",
-    "muno",
-    18,
-    0n,
-    "0x74d5b8eacfeb0dadaaf66403f40e304b3ef968b3",
-    4n
-  ])
-  // const instruction = encodeAbiParameters(instructionAbi, [1, 3, assetOrder])
-
-  return assetOrder
-})
-
-const pt = Effect.runSync(zkgmInstruction)
+const assetOrder = encodeAbiParameters(fungibleAssetOrderAbi, [
+  "0xE6831e169d77a861A0E71326AFA6d80bCC8Bc6aA",
+  "0xE6831e169d77a861A0E71326AFA6d80bCC8Bc6aA",
+  "0x74d5b8eacfeb0dadaaf66403f40e304b3ef968b3",
+  4n,
+  "muno",
+  "muno",
+  18,
+  0n,
+  "0x74d5b8eacfeb0dadaaf66403f40e304b3ef968b3",
+  4n
+])
 </script>
 
-<div class="font-mono break-words">{JSON.stringify(pt, null, 2)}</div>
+<div class="font-mono break-words">{JSON.stringify(assetOrder, null, 2)}</div>
 
 
