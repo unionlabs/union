75c3cc4645da6bbb29d493dd9c1b22d0ea721661
cor <cor@pruijs.dev>
2025-03-20 01:37:20 +0000

    feat(ts-sdk): cosmos -> cosmos fungible asset order example

--- ts-sdk/examples/create-fungible-asset-order.ts
+++ ts-sdk/examples/create-fungible-asset-order.ts
@@ -7,7 +7,8 @@ import { CosmosDestinationConfig } from "../src/cosmos/quote-token.js"
 import {
   createEvmToEvmFungibleAssetOrder,
   createEvmToCosmosFungibleAssetOrder,
-  createCosmosToEvmFungibleAssetOrder
+  createCosmosToEvmFungibleAssetOrder,
+  createCosmosToCosmosFungibleAssetOrder
 } from "../src/evm/ucs03/fungible-asset-order.js"
 
 import {
@@ -77,7 +78,7 @@ Effect.runPromiseExit(
   })
 ).then(exit => console.log("Cosmos to EVM", JSON.stringify(exit, null, 2)))
 
-// // Example 3: EVM to Cosmos transfer
+// Example 3: EVM to Cosmos transfer
 Effect.runPromiseExit(
   Effect.gen(function* () {
     const client = yield* createCosmWasmClient("https://rpc.elgafar-1.stargaze-apis.com")
@@ -103,3 +104,27 @@ Effect.runPromiseExit(
     )
   })
 ).then(exit => console.log("EVM to Cosmos", JSON.stringify(exit, null, 2)))
+
+// Example 4: Cosmos to Cosmos transfer
+Effect.runPromiseExit(
+  Effect.gen(function* () {
+    // Create CosmWasm clients for source and destination chains
+    const sourceClient = yield* createCosmWasmClient("https://rpc.elgafar-1.stargaze-apis.com")
+    const destClient = yield* createCosmWasmClient("https://rpc.testnet-9.union.build")
+
+    return yield* createCosmosToCosmosFungibleAssetOrder({
+      sender: "stars1qcvavxpxw3t8d9j7mwaeq9wgytkf5vwputv5x4",
+      receiver: "union1d95n4r6dnrfrps59szhl8mk7yqewsuzyw0zh5q", // Example cosmos address
+      baseToken: "stars1qrde534d4jwk44dn7w7gu9e2rayutr7kqx8lfjhsk3rd7z9rzxhq2gh3lr", // WETH on stargaze
+      baseAmount: BigInt(1000000), // 1 token
+      quoteAmount: BigInt(1000000) // Expected amount on destination chain
+    }).pipe(
+      Effect.provideService(CosmWasmClientSource, { client: sourceClient }),
+      Effect.provideService(CosmWasmClientDestination, { client: destClient }),
+      Effect.provideService(CosmosDestinationConfig, {
+        ucs03address: "union1x2jzeup7uwfxjxxrtfna2ktcugltntgu6kvc0eeayk0d82l247cqz669ee",
+        channelId: 4
+      })
+    )
+  })
+).then(exit => console.log("Cosmos to Cosmos", JSON.stringify(exit, null, 2)))
