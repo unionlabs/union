bc693f504a037d683519818534ea0514b7615cd0
Lukas <Lukas.nyberg@gmail.com>
2025-03-21 02:52:17 +0100

    feat(app2): add more icons

---
+++ app2/src/lib/components/icons/SharpChevronDownIcon.svelte
@@ -0,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z"/></svg>
