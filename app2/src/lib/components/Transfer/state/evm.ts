335b1a60557fa6fdb11d8b9ef601717f9d69fc69
cor <cor@pruijs.dev>
2025-03-24 23:14:15 +0000

    refactor(app2): simplify evm transaction submission state machine

--- app2/src/lib/components/Transfer/state/evm.ts
+++ app2/src/lib/components/Transfer/state/evm.ts
@@ -1,41 +1,31 @@
-import {Data, Effect, Exit} from "effect";
-import {switchChain, type waitForTransferReceipt} from "$lib/services/transfer-ucs03-evm";
-import {ViemPublicClient, waitForTransactionReceipt, writeContract} from "@unionlabs/sdk/evm";
-import type {Chain, Hash, PublicClient, WalletClient} from "viem";
+import { Data, Effect, type Exit } from "effect"
+import { switchChain } from "$lib/services/transfer-ucs03-evm"
+import { ViemPublicClient, waitForTransactionReceipt, writeContract } from "@unionlabs/sdk/evm"
+import type { Chain, Hash, PublicClient, WalletClient } from "viem"
 
-export type EffectToExit<T> = T extends Effect.Effect<infer A, infer E, any> ? Exit.Exit<A, E> : never
-
-export type SwitchChainState = Data.TaggedEnum<{
-  InProgress: {}
-  Complete: { exit: EffectToExit<ReturnType<typeof switchChain>> } // TODO: yield requirements in SwitchChain
-}>
-export const SwitchChainState = Data.taggedEnum<SwitchChainState>()
-
-export type WriteContractState = Data.TaggedEnum<{
-  InProgress: {}
-  Complete: { exit: EffectToExit<ReturnType<typeof writeContract>> }
-}>
-export const WriteContractState = Data.taggedEnum<WriteContractState>()
-
-export type TransactionReceiptState = Data.TaggedEnum<{
-  InProgress: { readonly hash: Hash }
-  Complete: { exit: EffectToExit<ReturnType<typeof waitForTransferReceipt>> }
-}>
-export const TransactionReceiptState = Data.taggedEnum<TransactionReceiptState>()
+export type EffectToExit<T> = T extends Effect.Effect<infer A, infer E, any>
+  ? Exit.Exit<A, E>
+  : never
 
 export type TransactionSubmissionEvm = Data.TaggedEnum<{
   Filling: {}
-  SwitchChain: { state: SwitchChainState }
-  WriteContract: { state: WriteContractState }
-  TransactionReceipt: { state: TransactionReceiptState }
+  SwitchChainInProgress: {}
+  SwitchChainComplete: { exit: EffectToExit<ReturnType<typeof switchChain>> }
+  WriteContractInProgress: {}
+  WriteContractComplete: { exit: EffectToExit<ReturnType<typeof writeContract>> }
+  TransactionReceiptInProgress: { readonly hash: Hash }
+  TransactionReceiptComplete: { exit: EffectToExit<ReturnType<typeof waitForTransactionReceipt>> }
 }>
 
-export type StateWithExit =
-  | { _tag: "SwitchChain"; state: SwitchChainState }
-  | { _tag: "WriteContract"; state: WriteContractState }
-  | { _tag: "TransactionReceipt"; state: TransactionReceiptState }
-
 export const TransactionSubmissionEvm = Data.taggedEnum<TransactionSubmissionEvm>()
+const {
+  SwitchChainInProgress,
+  SwitchChainComplete,
+  WriteContractInProgress,
+  WriteContractComplete,
+  TransactionReceiptInProgress,
+  TransactionReceiptComplete
+} = TransactionSubmissionEvm
 
 export const evmNextState = async <P extends Parameters<typeof writeContract>[1]>(
   ts: TransactionSubmissionEvm,
@@ -45,55 +35,34 @@ export const evmNextState = async <P extends Parameters<typeof writeContract>[1]
   params: P
 ): Promise<TransactionSubmissionEvm> =>
   TransactionSubmissionEvm.$match(ts, {
-    Filling: () => TransactionSubmissionEvm.SwitchChain({ state: SwitchChainState.InProgress() }),
-    SwitchChain: ({ state }) =>
-      SwitchChainState.$match(state, {
-        InProgress: async () => {
-          const exit = await Effect.runPromiseExit(switchChain(chain))
-          return TransactionSubmissionEvm.SwitchChain({
-            state: SwitchChainState.Complete({ exit })
-          })
-        },
-        Complete: ({ exit }) =>
-          exit._tag === "Failure"
-            ? TransactionSubmissionEvm.SwitchChain({ state: SwitchChainState.InProgress() })
-            : TransactionSubmissionEvm.WriteContract({ state: WriteContractState.InProgress() })
+    Filling: () => SwitchChainInProgress(),
+    SwitchChainInProgress: async () =>
+      SwitchChainComplete({
+        exit: await Effect.runPromiseExit(switchChain(chain))
       }),
-    WriteContract: ({ state }) =>
-      WriteContractState.$match(state, {
-        InProgress: async () => {
-          const exit = await Effect.runPromiseExit(writeContract(walletClient, params))
-          return TransactionSubmissionEvm.WriteContract({
-            state: WriteContractState.Complete({ exit })
-          })
-        },
-        Complete: ({ exit }) =>
-          exit._tag === "Failure"
-            ? TransactionSubmissionEvm.WriteContract({ state: WriteContractState.InProgress() })
-            : TransactionSubmissionEvm.TransactionReceipt({
-              state: TransactionReceiptState.InProgress({ hash: exit.value })
-            })
+    SwitchChainComplete: ({ exit }) =>
+      exit._tag === "Failure" ? SwitchChainInProgress() : WriteContractInProgress(),
+    WriteContractInProgress: async () =>
+      WriteContractComplete({
+        exit: await Effect.runPromiseExit(writeContract(walletClient, params))
       }),
-    TransactionReceipt: ({ state }) =>
-      TransactionReceiptState.$match(state, {
-        InProgress: async ({ hash }) => {
-          const exit = await Effect.runPromiseExit(
-            waitForTransactionReceipt(hash).pipe(
-              Effect.provideService(ViemPublicClient, { client: publicClient })
-            )
+    WriteContractComplete: ({ exit }) =>
+      exit._tag === "Failure"
+        ? WriteContractInProgress()
+        : TransactionReceiptInProgress({ hash: exit.value }),
+    TransactionReceiptInProgress: async ({ hash }) =>
+      TransactionReceiptComplete({
+        exit: await Effect.runPromiseExit(
+          waitForTransactionReceipt(hash).pipe(
+            Effect.provideService(ViemPublicClient, { client: publicClient })
           )
-          return TransactionSubmissionEvm.TransactionReceipt({
-            state: TransactionReceiptState.Complete({ exit })
-          })
-        },
-        Complete: () => ts // There is no next state, return self
-      })
+        )
+      }),
+    TransactionReceiptComplete: () => ts
   })
 
 export const hasFailedExit = (state: TransactionSubmissionEvm) =>
-  state._tag !== "Filling" && state.state._tag === "Complete" && state.state.exit._tag === "Failure"
+  "exit" in state && state.exit._tag === "Failure"
 
 export const isComplete = (state: TransactionSubmissionEvm) =>
-  state._tag === "TransactionReceipt" &&
-  state.state._tag === "Complete" &&
-  state.state.exit._tag === "Success"
+  state._tag === "TransactionReceiptComplete" && state.exit._tag === "Success"
