4163aebe3de7c1b92accedaf9c9721bb2767b500
Lukas <Lukas.nyberg@gmail.com>
2025-03-27 18:12:53 +0100

    fix(app2): update channel query

--- app2/src/lib/queries/channels.svelte.ts
+++ app2/src/lib/queries/channels.svelte.ts
@@ -7,25 +7,27 @@ import { channels } from "$lib/stores/channels.svelte.ts"
 
 export const channelsQuery = () =>
   createQueryGraphql({
-    schema: Schema.Struct({ v1_ibc_union_channel_recommendations: Channels }),
+    schema: Schema.Struct({ v2_channels: Channels }),
     document: graphql(`
       query Ucs03Channels @cached(ttl: 60) {
-          v1_ibc_union_channel_recommendations(where: {_and: [{version: {_eq: "ucs03-zkgm-0"}}]}) {
-              source_port_id
-              source_chain_id
-              source_channel_id
-              source_connection_id
-              destination_port_id
-              destination_chain_id
+          v2_channels(args: {p_recommended: true}) {
               destination_channel_id
+              destination_client_id
               destination_connection_id
+              destination_port_id
+              destination_universal_chain_id
+              source_channel_id
+              source_client_id
+              source_connection_id
+              source_port_id
+              source_universal_chain_id
           }
       }
   `),
     variables: {},
     refetchInterval: "60 seconds",
     writeData: data => {
-      channels.data = data.pipe(Option.map(d => d.v1_ibc_union_channel_recommendations))
+      channels.data = data.pipe(Option.map(d => d.v2_channels))
     },
     writeError: error => {
       chains.error = error
--- app2/src/lib/schema/channel.ts
+++ app2/src/lib/schema/channel.ts
@@ -1,7 +1,8 @@
 import { Schema } from "effect"
 import { PortId } from "$lib/schema/port.ts"
-import { ChainId } from "$lib/schema/chain.ts"
+import {UniversalChainId} from "$lib/schema/chain.ts"
 import { ConnectionId } from "$lib/schema/connection.ts"
+import {ClientId} from "$lib/schema/client.ts";
 
 export const ChannelId = Schema.Int.pipe(Schema.brand("ChannelId"))
 export type ChannelId = typeof ChannelId.Type
@@ -9,14 +10,16 @@ export type ChannelId = typeof ChannelId.Type
 export const ChannelVersion = Schema.String.pipe(Schema.brand("ChannelVersion"))
 
 export class Channel extends Schema.Class<Channel>("Channel")({
-  source_port_id: PortId,
-  source_chain_id: ChainId,
-  source_channel_id: ChannelId,
-  source_connection_id: ConnectionId,
-  destination_port_id: PortId,
-  destination_chain_id: ChainId,
   destination_channel_id: ChannelId,
-  destination_connection_id: ConnectionId
+  destination_client_id: ClientId,
+  destination_connection_id: ConnectionId,
+  destination_port_id: PortId,
+  destination_universal_chain_id: UniversalChainId,
+  source_channel_id: ChannelId,
+  source_client_id: ClientId,
+  source_connection_id: ConnectionId,
+  source_port_id: PortId,
+  source_universal_chain_id: UniversalChainId
 }) {}
 
 export const Channels = Schema.Array(Channel)
