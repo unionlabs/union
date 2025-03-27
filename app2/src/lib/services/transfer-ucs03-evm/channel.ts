6d1be57619e1ba246b00d82637f2a47711cf0813
Lukas <Lukas.nyberg@gmail.com>
2025-03-27 20:08:37 +0100

    fix(app2): fix channels for v2

--- app2/src/lib/services/transfer-ucs03-evm/channel.ts
+++ app2/src/lib/services/transfer-ucs03-evm/channel.ts
@@ -3,15 +3,15 @@ import { Channel, type Channels } from "$lib/schema/channel.ts"
 import { ChannelValidationError } from "$lib/services/transfer-ucs03-evm/errors.ts"
 
 export const getChannelInfoEffect = (
-  source_chain_id: string,
-  destination_chain_id: string,
+  source_universal_chain_id: string,
+  destination_universal_chain_id: string,
   channels: typeof Channels.Type
 ): Effect.Effect<typeof Channel.Type, ChannelValidationError> =>
   Effect.gen(function* () {
     const channel = channels.find(
       chan =>
-        chan.source_chain_id === source_chain_id &&
-        chan.destination_chain_id === destination_chain_id
+        chan.destination_universal_chain_id === destination_universal_chain_id &&
+        chan.source_universal_chain_id === source_universal_chain_id
     )
 
     if (
@@ -25,21 +25,23 @@ export const getChannelInfoEffect = (
     ) {
       return yield* Effect.fail(
         new ChannelValidationError({
-          source_chain_id,
-          destination_chain_id,
+          source_universal_chain_id,
+          destination_universal_chain_id,
           cause: "Missing required channel information"
         })
       )
     }
 
     return new Channel({
-      source_chain_id,
+      source_universal_chain_id,
       source_connection_id: channel.source_connection_id,
       source_channel_id: channel.source_channel_id,
+      source_client_id: channel.source_client_id,
       source_port_id: channel.source_port_id,
-      destination_chain_id,
+      destination_universal_chain_id,
       destination_connection_id: channel.destination_connection_id,
       destination_channel_id: channel.destination_channel_id,
+      destination_client_id: channel.destination_client_id,
       destination_port_id: channel.destination_port_id
     })
   })
