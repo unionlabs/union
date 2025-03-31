50df8d3ff5647ba4d50e5383b0261d0317eabb00
benluelo <git@luelo.dev>
2025-03-31 22:40:53 +0100

    chore(rust): bump pinned nightly version

--- tools/rust/rust.nix
+++ tools/rust/rust.nix
@@ -9,8 +9,9 @@ _: {
       ...
     }:
     let
+      # https://releases.rs/
       # https://rust-lang.github.io/rustup-components-history/
-      nightlyVersion = "2024-10-11";
+      nightlyVersion = "2025-03-28";
       defaultChannel = "nightly-${nightlyVersion}";
 
       # # hopefully if we ever use wasi this issue will be resolved: https://github.com/NixOS/nixpkgs/pull/146274
