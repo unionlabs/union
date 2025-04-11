558811cb2f20c6adf4bb73b565b81dcf905d10b3
cor <cor@pruijs.dev>
2025-04-11 13:49:57 +0000

    feat(app2): add favicon

--- app2/src/routes/+page.svelte
+++ app2/src/routes/+page.svelte
@@ -4,7 +4,9 @@ import Sections from "$lib/components/ui/Sections.svelte"
 import SectionTitle from "$lib/components/ui/SectionTitle.svelte"
 </script>
 
+<!--
 <Sections>
   <SectionTitle>Chains</SectionTitle>
   <ChainList/>
 </Sections>
+!-->
