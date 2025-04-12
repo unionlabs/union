9e58497df33bbc5546b54f21ef879690391f5dee
cor <cor@pruijs.dev>
2025-04-12 13:45:41 +0000

    fix(app2): eta formatting

--- app2/src/lib/constants/settlement-times.ts
+++ app2/src/lib/constants/settlement-times.ts
@@ -7,11 +7,11 @@ type SettlementInfo = {
 
 export const settlementDelays = {
   "bob.60808": {
-    url: "",
-    interval: "x hours"
+    url: "https://etherscan.io/address/0xdDa53E23f8a32640b04D7256e651C1db98dB11C1",
+    interval: "12 hours"
   },
   "corn.21000000": {
-    url: "",
-    interval: "x hours"
+    url: "https://etherscan.io/address/0x828C71bc1D7A34F32FfA624240633b6B7272C3D6",
+    interval: "7 days"
   }
 } as Record<UniversalChainId, SettlementInfo>
