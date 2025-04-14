bdafcd2b24d83e8b809eeee99b010a9b9d156689
Lukas <Lukas.nyberg@gmail.com>
2025-04-14 11:57:22 +0200

    fix(app2): update url

--- app2/src/lib/config/app.ts
+++ app2/src/lib/config/app.ts
@@ -3,7 +3,7 @@ import base64Icon from "./base64.txt?raw"
 export const TESTNET_APP_INFO = {
   base64Icon,
   name: "Union",
-  baseUrl: "https://app.union.build",
+  baseUrl: "https://btc.union.build",
   docs: "https://docs.union.build",
   iconUrl: "https://app.union.build/images/logo.png"
 }
