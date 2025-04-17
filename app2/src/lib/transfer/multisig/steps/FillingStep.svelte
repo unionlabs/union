59edaa0fcabf6155e54ebdbdaafdb75a7d738e7d
Lukas <Lukas.nyberg@gmail.com>
2025-04-17 16:07:57 +0200

    refactor(app2): save refactor progress

--- app2/src/lib/transfer/multisig/steps/FillingStep.svelte
+++ app2/src/lib/transfer/multisig/steps/FillingStep.svelte
@@ -1,62 +1,64 @@
 <script lang="ts">
-import ChainAsset from "$lib/transfer/shared/components/ChainAsset/index.svelte"
-import Amount from "$lib/transfer/shared/components/Amount.svelte"
-import Button from "$lib/components/ui/Button.svelte"
-import { transferData } from "$lib/transfer/shared/data/transfer-data.svelte.ts"
-import {Match, Option, pipe} from "effect"
-import type { ContextFlowError } from "$lib/transfer/shared/errors"
-import InsetError from "$lib/components/model/InsetError.svelte"
-import Input from "$lib/components/ui/Input.svelte";
+  import ChainAsset from "$lib/transfer/shared/components/ChainAsset/index.svelte"
+  import Amount from "$lib/transfer/shared/components/Amount.svelte"
+  import Button from "$lib/components/ui/Button.svelte"
+  import {transferData} from "$lib/transfer/shared/data/transfer-data.svelte.ts"
+  import {Match, Option, Schema} from "effect"
+  import type {ContextFlowError} from "$lib/transfer/shared/errors"
+  import InsetError from "$lib/components/model/InsetError.svelte"
+  import Input from "$lib/components/ui/Input.svelte";
+  import {wallets} from "$lib/stores/wallets.svelte.ts";
+  import {Bech32FromAddressCanonicalBytesWithPrefix} from "@unionlabs/sdk/schema";
 
-type Props = {
-  onContinue: () => void
-  loading: boolean
-  onErrorClose?: () => void
-  statusMessage?: string
-  errors?: Option.Option<ContextFlowError>
-}
+  type Props = {
+    onContinue: () => void
+    loading: boolean
+    onErrorClose?: () => void
+    statusMessage?: string
+    errors?: Option.Option<ContextFlowError>
+  }
 
-const {
-  onContinue,
-  loading,
-  statusMessage,
-  errors = Option.none<ContextFlowError>()
-}: Props = $props()
+  const {
+    onContinue,
+    loading,
+    statusMessage,
+    errors = Option.none<ContextFlowError>()
+  }: Props = $props()
 
-let isModalOpen = $state(false)
+  let isModalOpen = $state(false)
 
-const uiStatus = $derived.by(() => {
-  return Option.match(errors, {
-    onSome: error => {
-      const match = Match.type<ContextFlowError>().pipe(
-        Match.tag("BalanceLookupError", () => ({
-          text: "Failed checking balance",
-          error
-        })),
-        Match.tag("AllowanceCheckError", () => ({
-          text: "Failed checking allowance",
-          error
-        })),
-        Match.tag("OrderCreationError", () => ({
-          text: "Could not create orders",
-          error
-        })),
-        Match.orElse(() => ({
-          text: statusMessage ?? "Continue",
-          error
-        }))
-      )
-      return match(error)
-    },
+  const uiStatus = $derived.by(() => {
+    return Option.match(errors, {
+      onSome: error => {
+        const match = Match.type<ContextFlowError>().pipe(
+          Match.tag("BalanceLookupError", () => ({
+            text: "Failed checking balance",
+            error
+          })),
+          Match.tag("AllowanceCheckError", () => ({
+            text: "Failed checking allowance",
+            error
+          })),
+          Match.tag("OrderCreationError", () => ({
+            text: "Could not create orders",
+            error
+          })),
+          Match.orElse(() => ({
+            text: statusMessage ?? "Continue",
+            error
+          }))
+        )
+        return match(error)
+      },
 
-    onNone: () => ({
-      text: statusMessage ?? "Continue",
-      error: null
+      onNone: () => ({
+        text: statusMessage ?? "Continue",
+        error: null
+      })
     })
   })
-})
 
-const isButtonEnabled = $derived.by(() => !loading)
+  const isButtonEnabled = $derived.by(() => !loading)
 </script>
 
 <div class="min-w-full p-4 flex flex-col grow">
@@ -73,6 +75,10 @@ const isButtonEnabled = $derived.by(() => !loading)
       inputmode="text"
       autocapitalize="none"
       value={transferData.raw.sender}
+      oninput={(event) => {
+        console.log(Schema.encodeUnknownSync(Bech32FromAddressCanonicalBytesWithPrefix(''))(event.target.value))
+        wallets.addInputAddress(Schema.encodeUnknownSync(Bech32FromAddressCanonicalBytesWithPrefix(''))(event.target.value))
+    }}
       class="h-14 text-center text-lg"
     />
     <Input
@@ -88,10 +94,13 @@ const isButtonEnabled = $derived.by(() => !loading)
       autocapitalize="none"
       value={transferData.raw.receiver}
       class="h-14 text-center text-lg"
+      oninput={(event) => {
+        transferData.raw.updateField('receiver', event)
+    }}
     />
-    <ChainAsset type="source" />
-    <ChainAsset type="destination" />
-    <Amount type="source" />
+    <ChainAsset type="source"/>
+    <ChainAsset type="destination"/>
+    <Amount type="source"/>
   </div>
 
   <div class="grow"></div>
