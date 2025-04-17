6d112320e04433f81c1ec3914d7a17b5bffb8cbc
Buckram <buckram123@gmail.com>
2025-04-17 14:30:41 +0300

    fix: update return values

--- cosmwasm/ibc-union/core/msg/src/query.rs
+++ cosmwasm/ibc-union/core/msg/src/query.rs
@@ -8,33 +8,30 @@ use unionlabs_primitives::H256;
     derive(cosmwasm_schema::QueryResponses, cw_orch::QueryFns)
 )]
 pub enum QueryMsg {
-    #[cfg_attr(feature = "cw-orch-interface", returns(Timestamp))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(ibc_union_spec::Timestamp))]
     GetTimestampAtHeight { client_id: ClientId, height: u64 },
     #[cfg_attr(feature = "cw-orch-interface", returns(u64))]
     GetLatestHeight { client_id: ClientId },
-    #[cfg_attr(feature = "cw-orch-interface", returns(cosmwasm_std::Binary))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(unionlabs_primitives::Bytes))]
     GetClientState { client_id: ClientId },
-    #[cfg_attr(feature = "cw-orch-interface", returns(cosmwasm_std::Binary))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(unionlabs_primitives::Bytes))]
     GetConsensusState { client_id: ClientId, height: u64 },
     #[cfg_attr(feature = "cw-orch-interface", returns(crate::lightclient::Status))]
     GetStatus { client_id: ClientId },
-    #[cfg_attr(feature = "cw-orch-interface", returns(u64))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(String))]
     GetClientType { client_id: ClientId },
-    #[cfg_attr(
-        feature = "cw-orch-interface",
-        returns(ibc_union_spec::types::Connection)
-    )]
+    #[cfg_attr(feature = "cw-orch-interface", returns(ibc_union_spec::Connection))]
     GetConnection { connection_id: ConnectionId },
-    #[cfg_attr(feature = "cw-orch-interface", returns(ibc_union_spec::types::Channel))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(ibc_union_spec::Channel))]
     GetChannel { channel_id: ChannelId },
     #[cfg_attr(feature = "cw-orch-interface", returns(std::collections::BTreeSet<u32>))]
     GetChannels { contract: String },
-    #[cfg_attr(feature = "cw-orch-interface", returns(Option<Vec<u8>>))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(Option<H256>))]
     GetBatchPackets { batch_hash: H256 },
-    #[cfg_attr(feature = "cw-orch-interface", returns(Option<Vec<u8>>))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(Option<H256>))]
     GetBatchReceipts { batch_hash: H256 },
     #[cfg_attr(feature = "cw-orch-interface", returns(cosmwasm_std::Addr))]
     GetClientImpl { client_id: ClientId },
-    #[cfg_attr(feature = "cw-orch-interface", returns(cosmwasm_std::Addr))]
+    #[cfg_attr(feature = "cw-orch-interface", returns(String))]
     GetRegisteredClientType { client_type: String },
 }
