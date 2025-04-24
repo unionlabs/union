3d75cfbf32b0db1f325308332571792b77603fd1
benluelo <git@luelo.dev>
2025-04-24 18:53:09 +0100

    feat(cw20-base): remove unnecessary validations on token metadata
    :]

--- cosmwasm/cw20-base/src/contract.rs
+++ cosmwasm/cw20-base/src/contract.rs
@@ -19,78 +19,13 @@ use crate::{
     },
     enumerable::{query_all_accounts, query_owner_allowances, query_spender_allowances},
     error::ContractError,
-    msg::{
-        validate_decimals, validate_name, validate_symbol, ExecuteMsg, InstantiateMsg, QueryMsg,
-    },
+    msg::{ExecuteMsg, InstantiateMsg, QueryMsg},
     state::{MinterData, TokenInfo, BALANCES, LOGO, MARKETING_INFO, TOKEN_INFO},
 };
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cw20-base";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-const LOGO_SIZE_CAP: usize = 5 * 1024;
-
-/// Checks if data starts with XML preamble
-fn verify_xml_preamble(data: &[u8]) -> Result<(), ContractError> {
-    // The easiest way to perform this check would be just match on regex, however regex
-    // compilation is heavy and probably not worth it.
-
-    let preamble = data
-        .split_inclusive(|c| *c == b'>')
-        .next()
-        .ok_or(ContractError::InvalidXmlPreamble {})?;
-
-    const PREFIX: &[u8] = b"<?xml ";
-    const POSTFIX: &[u8] = b"?>";
-
-    if !(preamble.starts_with(PREFIX) && preamble.ends_with(POSTFIX)) {
-        Err(ContractError::InvalidXmlPreamble {})
-    } else {
-        Ok(())
-    }
-
-    // Additionally attributes format could be validated as they are well defined, as well as
-    // comments presence inside of preable, but it is probably not worth it.
-}
-
-/// Validates XML logo
-fn verify_xml_logo(logo: &[u8]) -> Result<(), ContractError> {
-    verify_xml_preamble(logo)?;
-
-    if logo.len() > LOGO_SIZE_CAP {
-        Err(ContractError::LogoTooBig {})
-    } else {
-        Ok(())
-    }
-}
-
-/// Validates png logo
-fn verify_png_logo(logo: &[u8]) -> Result<(), ContractError> {
-    // PNG header format:
-    // 0x89 - magic byte, out of ASCII table to fail on 7-bit systems
-    // "PNG" ascii representation
-    // [0x0d, 0x0a] - dos style line ending
-    // 0x1a - dos control character, stop displaying rest of the file
-    // 0x0a - unix style line ending
-    const HEADER: [u8; 8] = [0x89, b'P', b'N', b'G', 0x0d, 0x0a, 0x1a, 0x0a];
-    if logo.len() > LOGO_SIZE_CAP {
-        Err(ContractError::LogoTooBig {})
-    } else if !logo.starts_with(&HEADER) {
-        Err(ContractError::InvalidPngHeader {})
-    } else {
-        Ok(())
-    }
-}
-
-/// Checks if passed logo is correct, and if not, returns an error
-fn verify_logo(logo: &Logo) -> Result<(), ContractError> {
-    match logo {
-        Logo::Embedded(EmbeddedLogo::Svg(logo)) => verify_xml_logo(logo),
-        Logo::Embedded(EmbeddedLogo::Png(logo)) => verify_png_logo(logo),
-        Logo::Url(_) => Ok(()), // Any reasonable url validation would be regex based, probably not worth it
-    }
-}
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -100,8 +35,7 @@ pub fn instantiate(
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    // check valid token info
-    msg.validate()?;
+
     // create initial accounts
     let total_supply = create_accounts(&mut deps, &msg.initial_balances)?;
 
@@ -131,7 +65,6 @@ pub fn instantiate(
 
     if let Some(marketing) = msg.marketing {
         let logo = if let Some(logo) = marketing.logo {
-            verify_logo(&logo)?;
             LOGO.save(deps.storage, &logo)?;
 
             match logo {
@@ -268,10 +201,6 @@ fn update_metadata(
     symbol: String,
     decimals: u8,
 ) -> Result<Response, ContractError> {
-    validate_name(&name)?;
-    validate_symbol(&symbol)?;
-    validate_decimals(decimals)?;
-
     TOKEN_INFO.update::<_, StdError>(deps.storage, |info| {
         Ok(TokenInfo {
             name,
@@ -527,8 +456,6 @@ pub fn execute_upload_logo(
     let mut marketing_info = MARKETING_INFO
         .may_load(deps.storage)?
         .ok_or(ContractError::Unauthorized {})?;
-
-    verify_logo(&logo)?;
 
     if marketing_info
         .marketing
--- cosmwasm/cw20-base/src/msg.rs
+++ cosmwasm/cw20-base/src/msg.rs
@@ -1,8 +1,6 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{StdError, Uint128};
+use cosmwasm_std::Uint128;
 use cw20::{Cw20Coin, Cw20ExecuteMsg, Logo, MinterResponse};
-
-use crate::ContractError;
 
 #[cw_serde]
 pub enum ExecuteMsg {
@@ -34,55 +32,9 @@ pub struct InstantiateMsg {
     pub marketing: Option<InstantiateMarketingInfo>,
 }
 
-pub fn validate_name(name: &str) -> Result<(), ContractError> {
-    let bytes = name.as_bytes();
-    if bytes.len() < 3 || bytes.len() > 50 {
-        return Err(
-            StdError::generic_err("Name is not in the expected format (3-50 UTF-8 bytes)").into(),
-        );
-    }
-    Ok(())
-}
-
-pub fn validate_symbol(symbol: &str) -> Result<(), ContractError> {
-    let bytes = symbol.as_bytes();
-    if bytes.len() < 3 || bytes.len() > 12 {
-        return Err(
-            StdError::generic_err("Ticker symbol is in expected format [a-zA-Z\\-]{3,12}").into(),
-        );
-    }
-    for byte in bytes.iter() {
-        if (*byte != 45) && (*byte < 65 || *byte > 90) && (*byte < 97 || *byte > 122) {
-            return Err(StdError::generic_err(
-                "Ticker symbol is not in expected format [a-zA-Z\\-]{3,12}",
-            )
-            .into());
-        }
-    }
-    Ok(())
-}
-
-pub fn validate_decimals(decimals: u8) -> Result<(), ContractError> {
-    if decimals > 18 {
-        return Err(StdError::generic_err("Decimals must not exceed 18").into());
-    }
-    Ok(())
-}
-
 impl InstantiateMsg {
     pub fn get_cap(&self) -> Option<Uint128> {
         self.mint.as_ref().and_then(|v| v.cap)
-    }
-
-    pub fn validate(&self) -> Result<(), ContractError> {
-        // Check name, symbol, decimals
-        validate_name(&self.name)?;
-
-        validate_symbol(&self.symbol)?;
-
-        validate_decimals(self.decimals)?;
-
-        Ok(())
     }
 }
 
@@ -159,36 +111,5 @@ mod tests {
         let transfer: ExecuteMsg = cosmwasm_std::from_json(&serialized).unwrap();
 
         assert_eq!(transfer, ExecuteMsg::Cw20ExecuteMsg(execute));
-    }
-
-    #[test]
-    fn validate_instantiatemsg_name() {
-        // Too short
-        assert!(validate_name(&str::repeat("a", 2)).is_err());
-
-        // In the correct length range
-        assert!(validate_name(&str::repeat("a", 3)).is_ok());
-
-        // Too long
-        assert!(validate_name(&str::repeat("a", 51)).is_err());
-    }
-
-    #[test]
-    fn validate_instantiatemsg_symbol() {
-        // Too short
-        assert!(validate_symbol(&str::repeat("a", 2)).is_err());
-
-        // In the correct length range
-        assert!(validate_symbol(&str::repeat("a", 3)).is_ok());
-
-        // Too long
-        assert!(validate_symbol(&str::repeat("a", 13)).is_err());
-
-        // Has illegal char
-        let illegal_chars = [[64u8], [91u8], [123u8]];
-        illegal_chars.iter().for_each(|c| {
-            let c = std::str::from_utf8(c).unwrap();
-            assert!(validate_symbol(&str::repeat(c, 3)).is_err());
-        });
     }
 }
