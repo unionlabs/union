ceb63f5937a822ce37db01064959125a2f21a279
benluelo <git@luelo.dev>
2025-04-27 17:04:09 +0100

    fix(cometbft-types): fix SignedMsgType serde

--- lib/cometbft-types/src/types/signed_msg_type.rs
+++ lib/cometbft-types/src/types/signed_msg_type.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use unionlabs::errors::UnknownEnumVariant;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(try_from = "i32", into = "i32")]
@@ -9,10 +10,28 @@ pub enum SignedMsgType {
     Proposal = 32,
 }
 
+impl TryFrom<i32> for SignedMsgType {
+    type Error = UnknownEnumVariant<i32>;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Unknown),
+            1 => Ok(Self::Prevote),
+            2 => Ok(Self::Precommit),
+            32 => Ok(Self::Proposal),
+            _ => Err(UnknownEnumVariant(value)),
+        }
+    }
+}
+
+impl From<SignedMsgType> for i32 {
+    fn from(value: SignedMsgType) -> Self {
+        value as i32
+    }
+}
+
 #[cfg(feature = "proto")]
 pub mod proto {
-    use unionlabs::errors::UnknownEnumVariant;
-
     use crate::types::signed_msg_type::SignedMsgType;
 
     impl From<SignedMsgType> for protos::cometbft::types::v1::SignedMsgType {
@@ -26,12 +45,6 @@ pub mod proto {
         }
     }
 
-    impl From<SignedMsgType> for i32 {
-        fn from(value: SignedMsgType) -> Self {
-            protos::cometbft::types::v1::SignedMsgType::from(value).into()
-        }
-    }
-
     impl From<protos::cometbft::types::v1::SignedMsgType> for SignedMsgType {
         fn from(value: protos::cometbft::types::v1::SignedMsgType) -> Self {
             match value {
@@ -40,23 +53,6 @@ pub mod proto {
                 protos::cometbft::types::v1::SignedMsgType::Precommit => Self::Precommit,
                 protos::cometbft::types::v1::SignedMsgType::Proposal => Self::Proposal,
             }
-        }
-    }
-
-    impl TryFrom<i32> for SignedMsgType {
-        type Error = UnknownEnumVariant<i32>;
-
-        fn try_from(value: i32) -> Result<Self, Self::Error> {
-            Ok(
-                match protos::cometbft::types::v1::SignedMsgType::try_from(value)
-                    .map_err(|_| UnknownEnumVariant(value))?
-                {
-                    protos::cometbft::types::v1::SignedMsgType::Unknown => Self::Unknown,
-                    protos::cometbft::types::v1::SignedMsgType::Prevote => Self::Prevote,
-                    protos::cometbft::types::v1::SignedMsgType::Precommit => Self::Precommit,
-                    protos::cometbft::types::v1::SignedMsgType::Proposal => Self::Proposal,
-                },
-            )
         }
     }
 }
