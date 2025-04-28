a0d77c085e5c34754b1959966491444194823d8d
aeryz <abdullaheryz@protonmail.com>
2025-04-28 16:26:53 +0300

    chore(osmosis-tokenfactory-token-minter): moar tests
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- cosmwasm/osmosis-tokenfactory-token-minter/src/contract.rs
+++ cosmwasm/osmosis-tokenfactory-token-minter/src/contract.rs
@@ -126,7 +126,9 @@ fn change_token_owner(
             owner: token_owner,
             sender: info.sender,
         });
-    } else if token_owner == new_owner {
+    }
+
+    if token_owner == new_owner {
         return Ok(Response::new());
     }
 
@@ -162,16 +164,25 @@ fn wrapped_burn_tokens(
     // supported by `TokenFactory` yet and you can only set `burn_from_address` to the token owner.
     // So we are ensuring here that the funds are attached to the call so that we can burn from the
     // token owner.
-    let contains_base_token = info
+    let fund_amount = info
         .funds
         .iter()
-        .any(|coin| coin.denom == denom && coin.amount == amount);
-    if !contains_base_token {
+        .find(|c| c.denom == denom)
+        .map(|c| c.amount)
+        .unwrap_or(Uint128::zero());
+
+    if fund_amount < amount {
         return Err(Error::MissingFunds {
             denom: denom.clone(),
             amount,
         });
+    } else if fund_amount > amount {
+        return Err(Error::MoreFundsThenNeeded {
+            needed: amount,
+            given: fund_amount,
+        });
     }
+
     delegate_token_operation(
         token_owner.clone(),
         env.contract.address,
@@ -317,9 +328,517 @@ fn calculate_salt(path: U256, channel_id: ChannelId, token: Vec<u8>) -> H256<Bas
 #[cfg(test)]
 mod tests {
     use alloy::hex;
-    use cosmwasm_std::{testing::mock_env, Addr};
+    use cosmwasm_std::{
+        testing::{message_info, mock_dependencies, mock_env, MockApi, MockQuerier, MockStorage},
+        Addr, Empty, OwnedDeps,
+    };
 
     use super::*;
+
+    const OPERATOR_ADDR: &str = "operator";
+    const ZKGM_ADDR_: &str = "zkgm";
+
+    fn setup(operator: &str, zkgm: &str) -> OwnedDeps<MockStorage, MockApi, MockQuerier, Empty> {
+        let mut deps = mock_dependencies();
+
+        instantiate(
+            deps.as_mut(),
+            mock_env(),
+            MessageInfo {
+                sender: Addr::unchecked(zkgm),
+                funds: vec![],
+            },
+            TokenMinterInitMsg::OsmosisTokenFactory {
+                zkgm_admin: Addr::unchecked(operator),
+            },
+        )
+        .unwrap();
+
+        deps
+    }
+
+    #[test]
+    fn wrapped_create_denom_ok() {
+        let zkgm = Addr::unchecked(ZKGM_ADDR_);
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        let denom = format!("factory/{}/helloworld", mock_env().contract.address);
+
+        let metadata = Metadata {
+            name: "Union Token".into(),
+            symbol: "UNO".into(),
+            decimals: 6,
+        };
+
+        let res = execute(
+            deps.as_mut(),
+            mock_env(),
+            message_info(
+                &zkgm,
+                &[Coin {
+                    denom: denom.clone(),
+                    amount: 100u128.into(),
+                }],
+            ),
+            ExecuteMsg::ZkgmExecuteMsg(ucs03_zkgm_token_minter_api::ExecuteMsg::Wrapped(
+                WrappedTokenMsg::CreateDenom {
+                    subdenom: denom.clone(),
+                    metadata: metadata.clone(),
+                    path: vec![].into(),
+                    channel_id: ChannelId!(1),
+                    token: vec![].into(),
+                },
+            )),
+        )
+        .unwrap();
+
+        assert_eq!(
+            &res.messages
+                .iter()
+                .map(|m| m.msg.clone())
+                .collect::<Vec<_>>(),
+            &[
+                CosmosMsg::Custom(TokenFactoryMsg::CreateDenom {
+                    subdenom: "helloworld".into(),
+                }),
+                // We are using stargate for now instead of `Any` to be safe in case we would want to
+                // deploy on < wasmvm 2 chain that uses Osmosis' Token Factory
+                #[allow(deprecated)]
+                CosmosMsg::Stargate {
+                    type_url: MsgSetDenomMetadata::type_url(),
+                    value: MsgSetDenomMetadata {
+                        sender: mock_env().contract.address.into(),
+                        metadata: Some(new_proto_metadata(denom.clone(), metadata).unwrap()),
+                    }
+                    .encode_to_vec()
+                    .into(),
+                },
+            ]
+        );
+
+        assert_eq!(
+            TOKEN_OWNERS.load(&deps.storage, denom).unwrap(),
+            mock_env().contract.address
+        );
+    }
+
+    #[test]
+    fn wrapped_create_denom_invalid_denom() {
+        let zkgm = Addr::unchecked(ZKGM_ADDR_);
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        let denom = format!("factory/omgomg/helloworld");
+
+        let metadata = Metadata {
+            name: "Union Token".into(),
+            symbol: "UNO".into(),
+            decimals: 6,
+        };
+
+        assert_eq!(
+            execute(
+                deps.as_mut(),
+                mock_env(),
+                message_info(
+                    &zkgm,
+                    &[Coin {
+                        denom: denom.clone(),
+                        amount: 100u128.into(),
+                    }],
+                ),
+                ExecuteMsg::ZkgmExecuteMsg(ucs03_zkgm_token_minter_api::ExecuteMsg::Wrapped(
+                    WrappedTokenMsg::CreateDenom {
+                        subdenom: denom.clone(),
+                        metadata: metadata.clone(),
+                        path: vec![].into(),
+                        channel_id: ChannelId!(1),
+                        token: vec![].into(),
+                    },
+                )),
+            ),
+            Err(Error::InvalidDenom(denom))
+        );
+    }
+
+    #[test]
+    fn wrapped_mint_fails_if_no_token() {
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        let denom = format!("factory/omgomg/helloworld");
+
+        let mint_tokens = |deps: DepsMut| {
+            execute(
+                deps,
+                mock_env(),
+                message_info(&Addr::unchecked(ZKGM_ADDR_), &[]),
+                ExecuteMsg::ZkgmExecuteMsg(ucs03_zkgm_token_minter_api::ExecuteMsg::Wrapped(
+                    WrappedTokenMsg::MintTokens {
+                        denom: denom.clone(),
+                        amount: 100u128.into(),
+                        mint_to_address: Addr::unchecked(OPERATOR_ADDR),
+                    },
+                )),
+            )
+        };
+
+        assert!(matches!(
+            mint_tokens(deps.as_mut()),
+            Err(Error::StdError(_))
+        ));
+    }
+
+    #[test]
+    fn wrapped_mint_tokens_ok() {
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        let denom = format!("factory/omgomg/helloworld");
+
+        TOKEN_OWNERS
+            .save(
+                &mut deps.storage,
+                denom.clone(),
+                &mock_env().contract.address,
+            )
+            .unwrap();
+
+        let mint_tokens = |deps: DepsMut| {
+            execute(
+                deps,
+                mock_env(),
+                message_info(&Addr::unchecked(ZKGM_ADDR_), &[]),
+                ExecuteMsg::ZkgmExecuteMsg(ucs03_zkgm_token_minter_api::ExecuteMsg::Wrapped(
+                    WrappedTokenMsg::MintTokens {
+                        denom: denom.clone(),
+                        amount: 100u128.into(),
+                        mint_to_address: Addr::unchecked(OPERATOR_ADDR),
+                    },
+                )),
+            )
+        };
+
+        let res = mint_tokens(deps.as_mut()).unwrap();
+
+        assert_eq!(
+            res.messages[0].msg,
+            CosmosMsg::Custom(
+                MintTokensMsg {
+                    denom: denom.clone(),
+                    amount: 100u128.into(),
+                    mint_to_address: Addr::unchecked(OPERATOR_ADDR),
+                }
+                .into()
+            )
+        );
+
+        TOKEN_OWNERS
+            .save(
+                &mut deps.storage,
+                denom.clone(),
+                &Addr::unchecked(ZKGM_ADDR_),
+            )
+            .unwrap();
+
+        let res = mint_tokens(deps.as_mut()).unwrap();
+
+        assert_eq!(
+            res.messages[0].msg,
+            CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+                contract_addr: ZKGM_ADDR_.into(),
+                msg: to_json_binary(&TokenFactoryMsg::MintTokens(MintTokensMsg {
+                    denom,
+                    amount: 100u128.into(),
+                    mint_to_address: Addr::unchecked(OPERATOR_ADDR),
+                }))
+                .unwrap(),
+                funds: vec![]
+            })
+        );
+    }
+
+    #[test]
+    fn wrapped_token_burn_ok() {
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        let denom = format!("factory/omgomg/helloworld");
+
+        TOKEN_OWNERS
+            .save(
+                &mut deps.storage,
+                denom.clone(),
+                &mock_env().contract.address,
+            )
+            .unwrap();
+
+        let burn_tokens = |deps: DepsMut| {
+            execute(
+                deps,
+                mock_env(),
+                message_info(
+                    &Addr::unchecked(ZKGM_ADDR_),
+                    &[Coin {
+                        denom: denom.clone(),
+                        amount: 100u128.into(),
+                    }],
+                ),
+                ExecuteMsg::ZkgmExecuteMsg(ucs03_zkgm_token_minter_api::ExecuteMsg::Wrapped(
+                    WrappedTokenMsg::BurnTokens {
+                        denom: denom.clone(),
+                        amount: 100u128.into(),
+                        burn_from_address: Addr::unchecked(OPERATOR_ADDR),
+                        sender: Addr::unchecked(ZKGM_ADDR_),
+                    },
+                )),
+            )
+        };
+
+        let res = burn_tokens(deps.as_mut()).unwrap();
+
+        assert_eq!(
+            res.messages[0].msg,
+            CosmosMsg::Custom(
+                BurnTokensMsg {
+                    denom: denom.clone(),
+                    amount: 100u128.into(),
+                    burn_from_address: mock_env().contract.address,
+                }
+                .into()
+            )
+        );
+
+        TOKEN_OWNERS
+            .save(
+                &mut deps.storage,
+                denom.clone(),
+                &Addr::unchecked(ZKGM_ADDR_),
+            )
+            .unwrap();
+
+        let res = burn_tokens(deps.as_mut()).unwrap();
+
+        assert_eq!(
+            res.messages[0].msg,
+            CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+                contract_addr: ZKGM_ADDR_.into(),
+                msg: to_json_binary(&TokenFactoryAdminOperation::BurnTokens(BurnTokensMsg {
+                    denom: denom.clone(),
+                    amount: 100u128.into(),
+                    burn_from_address: Addr::unchecked(ZKGM_ADDR_),
+                }))
+                .unwrap(),
+                funds: vec![Coin {
+                    denom,
+                    amount: 100u128.into()
+                }]
+            })
+        );
+    }
+
+    #[test]
+    fn wrapped_token_burn_insufficient_funds() {
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        let denom = format!("factory/omgomg/helloworld");
+
+        TOKEN_OWNERS
+            .save(
+                &mut deps.storage,
+                denom.clone(),
+                &mock_env().contract.address,
+            )
+            .unwrap();
+
+        let burn_tokens = |deps: DepsMut, fund_amount: u128| {
+            execute(
+                deps,
+                mock_env(),
+                message_info(
+                    &Addr::unchecked(ZKGM_ADDR_),
+                    if fund_amount == 0 {
+                        vec![]
+                    } else {
+                        vec![Coin {
+                            denom: denom.clone(),
+                            amount: fund_amount.into(),
+                        }]
+                    }
+                    .as_slice(),
+                ),
+                ExecuteMsg::ZkgmExecuteMsg(ucs03_zkgm_token_minter_api::ExecuteMsg::Wrapped(
+                    WrappedTokenMsg::BurnTokens {
+                        denom: denom.clone(),
+                        amount: 100u128.into(),
+                        burn_from_address: Addr::unchecked(OPERATOR_ADDR),
+                        sender: Addr::unchecked(ZKGM_ADDR_),
+                    },
+                )),
+            )
+        };
+
+        assert_eq!(
+            burn_tokens(deps.as_mut(), 50),
+            Err(Error::MissingFunds {
+                denom: denom.clone(),
+                amount: 100u128.into()
+            })
+        );
+
+        assert!(matches!(
+            burn_tokens(deps.as_mut(), 0),
+            Err(Error::MissingFunds { .. })
+        ));
+
+        assert!(matches!(
+            burn_tokens(deps.as_mut(), 101),
+            Err(Error::MoreFundsThenNeeded { .. })
+        ));
+    }
+
+    #[test]
+    fn change_token_owner() {
+        let self_addr = mock_env().contract.address;
+        let operator = Addr::unchecked(OPERATOR_ADDR);
+        let new_owner = Addr::unchecked(ZKGM_ADDR_);
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        TOKEN_OWNERS
+            .save(&mut deps.storage, "omg".into(), &self_addr)
+            .unwrap();
+
+        // 1. Operator is allowed to change the ownership if the owner of the denom is this contract.
+        // If the new owner is the same as the current, this call expects to do nothing and just early return.
+        let res = execute(
+            deps.as_mut(),
+            mock_env(),
+            message_info(&operator, &[]),
+            ExecuteMsg::ChangeTokenOwner {
+                denom: "omg".into(),
+                new_owner: self_addr.clone(),
+            },
+        )
+        .unwrap();
+
+        assert!(res.events.is_empty());
+        assert!(res.messages.is_empty());
+
+        // 2. The contract itself is allowed to change the ownership when it owns the denom, we expect the proper
+        // event and message to be set, and the storage update to be done.
+        let res = execute(
+            deps.as_mut(),
+            mock_env(),
+            message_info(&self_addr, &[]),
+            ExecuteMsg::ChangeTokenOwner {
+                denom: "omg".into(),
+                new_owner: new_owner.clone(),
+            },
+        )
+        .unwrap();
+
+        let owner_update_event = res
+            .events
+            .iter()
+            .find(|e| e.ty == "token_owner_update")
+            .cloned()
+            .unwrap();
+
+        assert_eq!(
+            owner_update_event,
+            Event::new("token_owner_update")
+                .add_attribute("denom", "omg")
+                .add_attribute("from", self_addr.as_str())
+                .add_attribute("to", ZKGM_ADDR_)
+        );
+
+        assert_eq!(res.messages.len(), 1);
+
+        // The owner is self, so we just return a custom msg, no need to a contract call
+        assert_eq!(
+            CosmosMsg::Custom(
+                ChangeAdminMsg {
+                    denom: "omg".into(),
+                    new_admin_address: new_owner.clone()
+                }
+                .into()
+            ),
+            res.messages[0].msg
+        );
+
+        assert_eq!(
+            TOKEN_OWNERS.load(&deps.storage, "omg".into()).unwrap(),
+            new_owner
+        );
+
+        // 3. Third party owners are allowed to change the ownership of their tokens too.
+        let res = execute(
+            deps.as_mut(),
+            mock_env(),
+            message_info(&new_owner, &[]),
+            ExecuteMsg::ChangeTokenOwner {
+                denom: "omg".into(),
+                new_owner: self_addr.clone(),
+            },
+        )
+        .unwrap();
+
+        assert_eq!(res.messages.len(), 1);
+
+        // This time the owner is not the self, so we do a contract call
+        assert_eq!(
+            CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+                contract_addr: new_owner.into_string(),
+                msg: to_json_binary(&TokenFactoryAdminOperation::ChangeAdmin(ChangeAdminMsg {
+                    denom: "omg".into(),
+                    new_admin_address: self_addr
+                }))
+                .unwrap(),
+                funds: vec![]
+            }),
+            res.messages[0].msg
+        );
+    }
+
+    #[test]
+    fn change_token_owner_unauthorized() {
+        let self_addr = mock_env().contract.address;
+        let third_party_owner = Addr::unchecked(ZKGM_ADDR_);
+        let mut deps = setup(OPERATOR_ADDR, ZKGM_ADDR_);
+
+        TOKEN_OWNERS
+            .save(&mut deps.storage, "omg".into(), &self_addr)
+            .unwrap();
+
+        let change_admin =
+            |deps: DepsMut, caller: &str| -> Result<Response<TokenFactoryMsg>, Error> {
+                execute(
+                    deps,
+                    mock_env(),
+                    message_info(&Addr::unchecked(caller), &[]),
+                    ExecuteMsg::ChangeTokenOwner {
+                        denom: "omg".into(),
+                        new_owner: self_addr.clone(),
+                    },
+                )
+            };
+
+        assert_eq!(
+            change_admin(deps.as_mut(), "unauthorized"),
+            Err(Error::UnauthorizedWhenSelfOwned)
+        );
+
+        TOKEN_OWNERS
+            .save(&mut deps.storage, "omg".into(), &third_party_owner)
+            .unwrap();
+
+        assert!(matches!(
+            change_admin(deps.as_mut(), "unauthorized"),
+            Err(Error::UnauthorizedThirdParty { .. })
+        ));
+
+        // The operator also cannot upgrade a third-party owned token
+        assert!(matches!(
+            change_admin(deps.as_mut(), OPERATOR_ADDR),
+            Err(Error::UnauthorizedThirdParty { .. })
+        ));
+    }
 
     #[test]
     fn deconstruct_factory_denom_ok() {
--- cosmwasm/osmosis-tokenfactory-token-minter/src/error.rs
+++ cosmwasm/osmosis-tokenfactory-token-minter/src/error.rs
@@ -12,6 +12,9 @@ pub enum Error {
     #[error("missing funds for denom {denom} with amount {amount}")]
     MissingFunds { denom: String, amount: Uint128 },
 
+    #[error("the protocol needs {needed} amount but given {given}")]
+    MoreFundsThenNeeded { needed: Uint128, given: Uint128 },
+
     #[error("invalid path: {0}")]
     InvalidPath(FromDecStrErr),
 
