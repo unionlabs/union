302b0c587edfdb6bdcca10235315ca1e5de5d265
cor <cor@pruijs.dev>
2025-04-29 23:58:11 +0000

    feat(app2): more faucets

--- app2/src/lib/constants/faucets.ts
+++ app2/src/lib/constants/faucets.ts
@@ -2,5 +2,7 @@ export const EXTERNAL_FAUCETS = {
   OKX: "https://web3.okx.com/faucet",
   Alchemy: "https://www.alchemy.com/faucets",
   Chainlink: "https://faucets.chain.link/",
-  "Google Cloud Web3": "https://cloud.google.com/application/web3/faucet"
+  "Google Cloud Web3": "https://cloud.google.com/application/web3/faucet",
+  "Bitcorn Testnet": "https://faucet.usecorn.com/",
+  "Xion Testnet": "https://faucet.xion.burnt.com/"
 } as Record<string, string>
