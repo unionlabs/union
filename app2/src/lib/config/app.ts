8f72b296e716251fb29ba56f486d10f5d3ba2e5b
Lukas <Lukas.nyberg@gmail.com>
2025-04-30 15:41:14 +0200

    fix(app2): wallet connect

--- app2/src/lib/config/app.ts
+++ app2/src/lib/config/app.ts
@@ -1,9 +1,10 @@
 import base64Icon from "./base64.txt?raw"
+import { uiStore } from "$lib/stores/ui.svelte.ts"
 
 export const TESTNET_APP_INFO = {
   base64Icon,
   name: "Union",
-  baseUrl: "https://btc.union.build",
+  baseUrl: `https://${uiStore.activeEdition}.union.build`,
   docs: "https://docs.union.build",
   iconUrl: "https://app.union.build/images/logo.png"
 }
