faa8f8f413eb2fe07e84d94f7934e9624c71487c
aeryz <abdullaheryz@protonmail.com>
2025-04-30 17:25:44 +0300

    chore(sui): initiate the light client
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

---
+++ lib/sui-light-client-types/src/header.rs
@@ -0,0 +1,4 @@
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "bincode", derive(bincode::Encode, bincode::Decode))]
+pub struct Header {}
