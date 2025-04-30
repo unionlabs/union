9ff87f41eb2bf4e171e77c0a5ffd2c33aaaa855e
aeryz <abdullaheryz@protonmail.com>
2025-04-30 17:26:06 +0300

    chore(sui): add more tx and events to voyager
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- voyager/plugins/event-source/sui/src/events.rs
+++ voyager/plugins/event-source/sui/src/events.rs
@@ -8,15 +8,60 @@ pub struct CreateClient {
     pub counterparty_chain_id: String,
 }
 
+struct ClientUpdated {
+    client_id: u32,
+    client_type: String,
+    height: u64,
+}
+
+struct ConnectionOpenInit {
+    connection_id: u32,
+    client_id: u32,
+    counterparty_client_id: u32,
+}
+
+struct ChannelOpenInit {
+    port_id: String,
+    channel_id: u32,
+    counterparty_port_id: Vec<u8>,
+    connection_id: u32,
+    version: String,
+}
+
+struct ChannelOpenTry {
+    port_id: String,
+    channel_id: u32,
+    counterparty_port_id: Vec<u8>,
+    counterparty_channel_id: u32,
+    connection_id: u32,
+    version: String,
+}
+
+struct ChannelOpenAck {
+    port_id: String,
+    channel_id: u32,
+    counterparty_port_id: Vec<u8>,
+    counterparty_channel_id: u32,
+    connection_id: u32,
+}
+
+struct ChannelOpenConfirm {
+    port_id: String,
+    channel_id: u32,
+    counterparty_port_id: Vec<u8>,
+    counterparty_channel_id: u32,
+    connection_id: u32,
+}
+
 #[model]
 #[derive(Enumorph)]
 pub enum IbcEvent {
     CreateClient(CreateClient),
-    // UpdateClient(ibc::UpdateClient),
-    // ConnectionOpenInit(ibc::ConnectionOpenInit),
-    // ConnectionOpenTry(ibc::ConnectionOpenTry),
-    // ConnectionOpenAck(ibc::ConnectionOpenAck),
-    // ConnectionOpenConfirm(ibc::ConnectionOpenConfirm),
+    UpdateClient(UpdateClient),
+    ConnectionOpenInit(ConnectionOpenInit),
+    ConnectionOpenTry(ConnectionOpenTry),
+    ConnectionOpenAck(ConnectionOpenAck),
+    ConnectionOpenConfirm(ConnectionOpenConfirm),
     // ChannelOpenInit(ibc::ChannelOpenInit),
     // ChannelOpenTry(ibc::ChannelOpenTry),
     // ChannelOpenAck(ibc::ChannelOpenAck),
