f2ed311f6699dbd31e08530cd27b75c2b0760532
Lukas <Lukas.nyberg@gmail.com>
2025-04-30 19:39:28 +0200

    feat(app2): two confs

--- app2/src/lib/config/app.ts
+++ app2/src/lib/config/app.ts
@@ -1,14 +1,22 @@
 import base64Icon from "./base64.txt?raw"
-import { uiStore } from "$lib/stores/ui.svelte.ts"
+import type { Edition } from "$lib/themes.ts"
 
-export function getTestnetAppInfo() {
-  const edition = uiStore.activeEdition
+const projectIds: Record<Edition, string> = {
+  app: "f544d5ee6eb61962408fd456c114e9ed",
+  btc: "49fe74ca5ded7142adefc69a7788d14a"
+}
 
-  return {
+export function getAppInfo(edition: Edition) {
+  const config = {
     base64Icon,
     name: "Union",
     baseUrl: `https://${edition}.union.build`,
     docs: "https://docs.union.build",
-    iconUrl: "https://app.union.build/images/logo.png"
+    iconUrl: "https://app.union.build/images/logo.png",
+    projectId: projectIds[edition]
   }
+
+  console.log(`[getAppInfo] Loaded config for edition "${edition}":`, config)
+
+  return config
 }
