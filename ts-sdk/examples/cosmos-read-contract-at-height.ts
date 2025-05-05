e8c387a20e5957d9c4905d21aeb1fe55fb140c7a
Kaan Caglan <caglankaan@gmail.com>
2025-05-05 17:11:15 +0300

    chore(ts-sdk): added xion totalsupply example
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- ts-sdk/examples/cosmos-read-contract-at-height.ts
+++ ts-sdk/examples/cosmos-read-contract-at-height.ts
@@ -20,64 +20,99 @@ BigInt["prototype"].toJSON = function () {
   return this.toString()
 }
 // Example CW20 token balance query
+// Effect.runPromiseExit(
+//   Effect.gen(function* () {
+//     // Create a CosmWasm client
+//     const rpc = "https://rpc.bbn-test-5.babylon.chain.kitchen"
+//     const client = yield* createCosmWasmClient(rpc)
+//     const contractAddr = "bbn1zsrv23akkgxdnwul72sftgv2xjt5khsnt3wwjhp0ffh683hzp5aq5a0h6n"
+//     const minter = "bbn1sakazthycqgzer50nqgr5ta4vy3gwz8wxla3s8rd8pql4ctmz5qssg39sf"
+//     const ucs03Addr = "bbn1336jj8ertl8h7rdvnz4dh5rqahd09cy0x43guhsxx6xyrztx292q77945h"
+//     // Query a CW20 token contract for a balance
+
+//     const balanceNow = yield* channelBalance(0n, "ubbn").pipe(
+//       Effect.provideService(CosmWasmClientDestination, { client }),
+//       Effect.provideService(CosmosChannelDestination, {
+//         ucs03address: ucs03Addr,
+//         channelId: 3
+//       }),
+//       Effect.tapError(e => Effect.logError("Error fetching channel balance:", e))
+//     )
+//     console.info("balance", balanceNow)
+
+//     const rest = "https://rest.bbn-test-5.babylon.chain.kitchen"
+//     const extClient = yield* createExtendedCosmWasmClient(rpc, rest)
+
+//     const balanceAtBlock123 = yield* channelBalanceAtHeight(0n, "ubbn", 912421).pipe(
+//       Effect.provideService(ExtendedCosmWasmClientContext, { client: extClient }),
+//       Effect.provideService(CosmosChannelDestination, {
+//         ucs03address: ucs03Addr,
+//         channelId: 3
+//       }),
+//       Effect.tapError(e => Effect.logError("height-query failed:", e))
+//     )
+
+//     console.info("Balance at 233799:", balanceAtBlock123)
+
+//     const withClient = Effect.provideService(CosmWasmClientContext, { client })
+
+//     const cw20balance = yield* readCw20Balance(contractAddr, minter).pipe(withClient)
+
+//     console.info("cw20balance", cw20balance)
+
+//     const withExtClient = Effect.provideService(ExtendedCosmWasmClientContext, {
+//       client: extClient
+//     })
+//     const cw20BalanceAtHeight = yield* readCw20BalanceAtHeight(contractAddr, minter, 912421).pipe(
+//       withExtClient
+//     )
+//     console.info("cw20BalanceAtHeight", cw20BalanceAtHeight)
+
+//     const cw20TotalSupply = yield* readCw20TotalSupply(contractAddr).pipe(withClient)
+
+//     console.info("cw20TotalSupply", cw20TotalSupply)
+
+//     const cw20TotalSupplyAtHeight = yield* readCw20TotalSupplyAtHeight(contractAddr, 912421).pipe(
+//       withExtClient
+//     )
+
+//     console.info("cw20TotalSupplyAtHeight", cw20TotalSupplyAtHeight)
+
+//     return balanceAtBlock123
+//   })
+// ).then(exit => console.log(JSON.stringify(exit, null, 2)))
+
+
 Effect.runPromiseExit(
   Effect.gen(function* () {
     // Create a CosmWasm client
-    const rpc = "https://rpc.bbn-test-5.babylon.chain.kitchen"
+    const rpc = "https://rpc.xion-testnet-2.xion.chain.cooking"
     const client = yield* createCosmWasmClient(rpc)
-    const contractAddr = "bbn1zsrv23akkgxdnwul72sftgv2xjt5khsnt3wwjhp0ffh683hzp5aq5a0h6n"
-    const minter = "bbn1sakazthycqgzer50nqgr5ta4vy3gwz8wxla3s8rd8pql4ctmz5qssg39sf"
-    const ucs03Addr = "bbn1336jj8ertl8h7rdvnz4dh5rqahd09cy0x43guhsxx6xyrztx292q77945h"
-    // Query a CW20 token contract for a balance
+    const tokenDenom = "xion100jj57u4rna4wcdnn8pxvnacxvm0fx6zaazj5xqq555syvvae2wsqsum0y"
+    const latest = yield* Effect.tryPromise({
+      try: () => {
+        return client.getHeight()
+      },
+      catch: (e) =>
+        new Error(`Failed to fetch blockNumber for ${rpc}: ${String(e)}`),
+    });
+    console.info("height: ", latest)
 
-    const balanceNow = yield* channelBalance(0n, "ubbn").pipe(
-      Effect.provideService(CosmWasmClientDestination, { client }),
-      Effect.provideService(CosmosChannelDestination, {
-        ucs03address: ucs03Addr,
-        channelId: 3
-      }),
+    const totalSupplyNow = yield* readCw20TotalSupply(tokenDenom).pipe(
+      Effect.provideService(CosmWasmClientContext, { client }),
       Effect.tapError(e => Effect.logError("Error fetching channel balance:", e))
     )
-    console.info("balance", balanceNow)
+    console.info("totalSupplyNow", totalSupplyNow)
 
-    const rest = "https://rest.bbn-test-5.babylon.chain.kitchen"
+    const rest = "https://rest.xion-testnet-2.xion.chain.cooking"
     const extClient = yield* createExtendedCosmWasmClient(rpc, rest)
 
-    const balanceAtBlock123 = yield* channelBalanceAtHeight(0n, "ubbn", 912421).pipe(
+    const totalSupplyAtHeight = yield* readCw20TotalSupplyAtHeight(tokenDenom, Number(latest)).pipe(
       Effect.provideService(ExtendedCosmWasmClientContext, { client: extClient }),
-      Effect.provideService(CosmosChannelDestination, {
-        ucs03address: ucs03Addr,
-        channelId: 3
-      }),
       Effect.tapError(e => Effect.logError("height-query failed:", e))
     )
 
-    console.info("Balance at 233799:", balanceAtBlock123)
+    console.info("totalSupplyAtHeight:", totalSupplyAtHeight)
 
-    const withClient = Effect.provideService(CosmWasmClientContext, { client })
-
-    const cw20balance = yield* readCw20Balance(contractAddr, minter).pipe(withClient)
-
-    console.info("cw20balance", cw20balance)
-
-    const withExtClient = Effect.provideService(ExtendedCosmWasmClientContext, {
-      client: extClient
-    })
-    const cw20BalanceAtHeight = yield* readCw20BalanceAtHeight(contractAddr, minter, 912421).pipe(
-      withExtClient
-    )
-    console.info("cw20BalanceAtHeight", cw20BalanceAtHeight)
-
-    const cw20TotalSupply = yield* readCw20TotalSupply(contractAddr).pipe(withClient)
-
-    console.info("cw20TotalSupply", cw20TotalSupply)
-
-    const cw20TotalSupplyAtHeight = yield* readCw20TotalSupplyAtHeight(contractAddr, 912421).pipe(
-      withExtClient
-    )
-
-    console.info("cw20TotalSupplyAtHeight", cw20TotalSupplyAtHeight)
-
-    return balanceAtBlock123
   })
 ).then(exit => console.log(JSON.stringify(exit, null, 2)))
