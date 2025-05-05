445d3d7245d7ecde00ec0738a5bf3865977a8522
Kaan Caglan <caglankaan@gmail.com>
2025-05-05 15:25:19 +0300

    chore(ts-sdk): nix-fmt applied
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- ts-sdk/examples/cosmos-read-contract-at-height.ts
+++ ts-sdk/examples/cosmos-read-contract-at-height.ts
@@ -4,11 +4,16 @@ import {
   createExtendedCosmWasmClient,
   ExtendedCosmWasmClientContext,
   CosmWasmClientContext
-} from "../src/cosmos/client.js";
+} from "../src/cosmos/client.js"
 import { channelBalance, channelBalanceAtHeight } from "../src/cosmos/channel-balance.js"
 import { CosmWasmClientDestination } from "../src/cosmos/client.js"
 import { CosmosChannelDestination } from "../src/cosmos/channel.js"
-import { readCw20BalanceAtHeight, readCw20Balance, readCw20TotalSupply, readCw20TotalSupplyAtHeight } from "../src/cosmos/cw20.js"
+import {
+  readCw20BalanceAtHeight,
+  readCw20Balance,
+  readCw20TotalSupply,
+  readCw20TotalSupplyAtHeight
+} from "../src/cosmos/cw20.js"
 
 // @ts-ignore
 BigInt["prototype"].toJSON = function () {
@@ -21,7 +26,7 @@ Effect.runPromiseExit(
     const rpc = "https://rpc.bbn-test-5.babylon.chain.kitchen"
     const client = yield* createCosmWasmClient(rpc)
     const contractAddr = "bbn1zsrv23akkgxdnwul72sftgv2xjt5khsnt3wwjhp0ffh683hzp5aq5a0h6n"
-    const minter = 'bbn1sakazthycqgzer50nqgr5ta4vy3gwz8wxla3s8rd8pql4ctmz5qssg39sf';
+    const minter = "bbn1sakazthycqgzer50nqgr5ta4vy3gwz8wxla3s8rd8pql4ctmz5qssg39sf"
     const ucs03Addr = "bbn1336jj8ertl8h7rdvnz4dh5rqahd09cy0x43guhsxx6xyrztx292q77945h"
     // Query a CW20 token contract for a balance
 
@@ -36,50 +41,43 @@ Effect.runPromiseExit(
     console.info("balance", balanceNow)
 
     const rest = "https://rest.bbn-test-5.babylon.chain.kitchen"
-    const extClient = yield* createExtendedCosmWasmClient(rpc, rest);
+    const extClient = yield* createExtendedCosmWasmClient(rpc, rest)
 
-    const balanceAtBlock123 = yield* channelBalanceAtHeight(
-        0n,
-        "ubbn",
-        912421
-      ).pipe(
-        Effect.provideService(ExtendedCosmWasmClientContext, { client: extClient }),
-        Effect.provideService(CosmosChannelDestination, {
-          ucs03address: ucs03Addr,
-          channelId: 3,
-        }),
-        Effect.tapError((e) => Effect.logError("height-query failed:", e))
-      );
+    const balanceAtBlock123 = yield* channelBalanceAtHeight(0n, "ubbn", 912421).pipe(
+      Effect.provideService(ExtendedCosmWasmClientContext, { client: extClient }),
+      Effect.provideService(CosmosChannelDestination, {
+        ucs03address: ucs03Addr,
+        channelId: 3
+      }),
+      Effect.tapError(e => Effect.logError("height-query failed:", e))
+    )
 
-      console.info("Balance at 233799:", balanceAtBlock123);
-      
+    console.info("Balance at 233799:", balanceAtBlock123)
 
-      const withClient = Effect.provideService(CosmWasmClientContext, { client })
+    const withClient = Effect.provideService(CosmWasmClientContext, { client })
 
-      
-      const cw20balance = yield* readCw20Balance(contractAddr, minter).pipe(withClient)
+    const cw20balance = yield* readCw20Balance(contractAddr, minter).pipe(withClient)
 
-      console.info("cw20balance", cw20balance)
+    console.info("cw20balance", cw20balance)
 
-      const withExtClient = Effect.provideService(ExtendedCosmWasmClientContext, { client: extClient });
-      const cw20BalanceAtHeight = yield* readCw20BalanceAtHeight(
-        contractAddr,
-        minter,
-        912421
-      ).pipe(withExtClient)
-      console.info("cw20BalanceAtHeight", cw20BalanceAtHeight)
+    const withExtClient = Effect.provideService(ExtendedCosmWasmClientContext, {
+      client: extClient
+    })
+    const cw20BalanceAtHeight = yield* readCw20BalanceAtHeight(contractAddr, minter, 912421).pipe(
+      withExtClient
+    )
+    console.info("cw20BalanceAtHeight", cw20BalanceAtHeight)
 
-      const cw20TotalSupply = yield* readCw20TotalSupply(contractAddr).pipe(withClient)
+    const cw20TotalSupply = yield* readCw20TotalSupply(contractAddr).pipe(withClient)
 
-      console.info("cw20TotalSupply", cw20TotalSupply)
+    console.info("cw20TotalSupply", cw20TotalSupply)
 
+    const cw20TotalSupplyAtHeight = yield* readCw20TotalSupplyAtHeight(contractAddr, 912421).pipe(
+      withExtClient
+    )
 
-      const cw20TotalSupplyAtHeight = yield* readCw20TotalSupplyAtHeight(contractAddr, 912421).pipe(withExtClient)
+    console.info("cw20TotalSupplyAtHeight", cw20TotalSupplyAtHeight)
 
-      console.info("cw20TotalSupplyAtHeight", cw20TotalSupplyAtHeight)
-
-
-      return balanceAtBlock123;
+    return balanceAtBlock123
   })
 ).then(exit => console.log(JSON.stringify(exit, null, 2)))
-
--- ts-sdk/src/cosmos/extended-cw-client.ts
+++ ts-sdk/src/cosmos/extended-cw-client.ts
@@ -1,15 +1,15 @@
-import { CosmWasmClient, type HttpEndpoint } from "@cosmjs/cosmwasm-stargate";
-import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+import { CosmWasmClient, type HttpEndpoint } from "@cosmjs/cosmwasm-stargate"
+import { Tendermint34Client } from "@cosmjs/tendermint-rpc"
 
 export class ExtendedCosmWasmClient extends CosmWasmClient {
-  private restUrl!: string;
+  private restUrl!: string
 
   static async connectWithHeightSupport(endpoint: HttpEndpoint | string, restEndpoint: string) {
-    const tmClient = await Tendermint34Client.connect(endpoint);
-    const client = new ExtendedCosmWasmClient(tmClient);
+    const tmClient = await Tendermint34Client.connect(endpoint)
+    const client = new ExtendedCosmWasmClient(tmClient)
 
-    client.restUrl = restEndpoint.replace(/\/+$/, "");
-    return client;
+    client.restUrl = restEndpoint.replace(/\/+$/, "")
+    return client
   }
 
   async queryContractSmartAtHeight(
@@ -17,20 +17,17 @@ export class ExtendedCosmWasmClient extends CosmWasmClient {
     queryMsg: Record<string, unknown>,
     height: number
   ) {
-    const base = this.restUrl;
-    const encoded = btoa(JSON.stringify(queryMsg));
-    const resp = await fetch(
-      `${base}/cosmwasm/wasm/v1/contract/${contract}/smart/${encoded}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "x-cosmos-block-height": height.toString(),
-        },
+    const base = this.restUrl
+    const encoded = btoa(JSON.stringify(queryMsg))
+    const resp = await fetch(`${base}/cosmwasm/wasm/v1/contract/${contract}/smart/${encoded}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-cosmos-block-height": height.toString()
       }
-    );
+    })
     if (!resp.ok) {
-      throw new Error(`HTTP ${resp.status}: ${await resp.text()}`);
+      throw new Error(`HTTP ${resp.status}: ${await resp.text()}`)
     }
-    return resp.json();
+    return resp.json()
   }
 }
