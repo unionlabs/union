ba90fb906752ed222f76c662593ddfce711ada34
Eric Hegnes <eric@hegnes.com>
2025-05-07 12:39:45 -0400

    fix(app2): validate packet_hash slug parameter
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- app2/src/routes/explorer/packets/[packet_hash]/+page.ts
+++ app2/src/routes/explorer/packets/[packet_hash]/+page.ts
@@ -1,7 +1,18 @@
+import { redirect } from "@sveltejs/kit"
+import { PacketHash } from "@unionlabs/sdk/schema"
+import * as E from "effect/Either"
+import * as S from "effect/Schema"
 import type { PageLoad } from "./$types.ts"
 
 export const load: PageLoad = ({ params }) => {
-  return {
-    packetHash: params.packet_hash
-  }
+  const packetHash = S.decodeEither(PacketHash)(params.packet_hash)
+
+  return E.match(packetHash, {
+    onLeft: () => {
+      throw redirect(302, "/explorer/packets")
+    },
+    onRight: (packetHash) => ({
+      packetHash,
+    }),
+  })
 }
