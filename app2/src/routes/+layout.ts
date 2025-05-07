bcf923c268897583847c68f4b217aacccb89ff1f
Eric Hegnes <eric@hegnes.com>
2025-05-07 13:12:59 -0400

    fix(app2): type LayoutLoad fn
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- app2/src/routes/+layout.ts
+++ app2/src/routes/+layout.ts
@@ -1,18 +1,28 @@
 import type { Edition } from "$lib/themes"
+import { Match, String as Str } from "effect"
+import { constant } from "effect/Function"
+import type { LayoutLoad } from "./$types"
 
 export const ssr = false
 export const prerender = true
 export const trailingSlash = "ignore"
 
-export const load = ({ url }) => {
+export const load: LayoutLoad = ({ url }) => {
   const hostname = url.hostname
-  let edition: Edition = "app"
 
-  if (hostname.startsWith("btc.") || hostname.startsWith("staging.btc.")) {
-    edition = "btc"
-  } else if (hostname.startsWith("app.") || hostname.startsWith("staging.app.")) {
-    edition = "app"
-  }
+  const edition = Match.value(hostname).pipe(
+    Match.whenOr(
+      Str.startsWith("btc."),
+      Str.startsWith("staging.btc."),
+      () => "btc" as const,
+    ),
+    Match.whenOr(
+      Str.startsWith("app."),
+      Str.startsWith("staging.app."),
+      () => "app" as const,
+    ),
+    Match.orElse(() => "app" as const),
+  )
 
   return {
     edition,
