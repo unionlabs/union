c788ab3af8dd14a77eb9928ff5fccbfae726aa26
benluelo <git@luelo.dev>
2025-05-12 03:37:42 +0100

    fix(voyager): json is pain

--- lib/beacon-api/src/routes/block.rs
+++ lib/beacon-api/src/routes/block.rs
@@ -1,10 +1,12 @@
 use beacon_api_types::{bellatrix, capella, deneb, electra, phase0};
 use serde::{Deserialize, Serialize};
-use unionlabs::primitives::H256;
+use serde_json::value::RawValue;
+use unionlabs::{primitives::H256, ErrorReporter};
 
 use crate::client::{VersionedResponse, VersionedResponseTypes};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(try_from = "BeaconBlockResponseRaw")]
 pub struct BeaconBlockResponse {
     #[serde(flatten)]
     pub response: VersionedResponse<BeaconBlockResponseTypes>,
@@ -29,4 +31,72 @@ impl VersionedResponseTypes for BeaconBlockResponseTypes {
     type Capella = capella::SignedBeaconBlock;
     type Deneb = deneb::SignedBeaconBlock;
     type Electra = electra::SignedBeaconBlock;
+}
+
+#[derive(Deserialize)]
+pub struct BeaconBlockResponseRaw<'a> {
+    // TODO: Make not optional
+    version: Option<String>,
+    #[serde(borrow)]
+    data: &'a RawValue,
+    execution_optimistic: Option<bool>,
+    finalized: Option<bool>,
+}
+
+impl<'a> TryFrom<BeaconBlockResponseRaw<'a>> for BeaconBlockResponse {
+    type Error = String;
+
+    fn try_from(value: BeaconBlockResponseRaw<'a>) -> std::result::Result<Self, Self::Error> {
+        Ok(match value.version.as_deref() {
+            Some("phase0") => Self {
+                execution_optimistic: value.execution_optimistic,
+                finalized: value.finalized,
+                response: VersionedResponse::Phase0(
+                    serde_json::from_str(value.data.get())
+                        .map_err(|e| ErrorReporter(e).to_string())?,
+                ),
+            },
+            Some("altair") => Self {
+                execution_optimistic: value.execution_optimistic,
+                finalized: value.finalized,
+                response: VersionedResponse::Altair(
+                    serde_json::from_str(value.data.get())
+                        .map_err(|e| ErrorReporter(e).to_string())?,
+                ),
+            },
+            Some("bellatrix") => Self {
+                execution_optimistic: value.execution_optimistic,
+                finalized: value.finalized,
+                response: VersionedResponse::Bellatrix(
+                    serde_json::from_str(value.data.get())
+                        .map_err(|e| ErrorReporter(e).to_string())?,
+                ),
+            },
+            Some("capella") => Self {
+                execution_optimistic: value.execution_optimistic,
+                finalized: value.finalized,
+                response: VersionedResponse::Capella(
+                    serde_json::from_str(value.data.get())
+                        .map_err(|e| ErrorReporter(e).to_string())?,
+                ),
+            },
+            Some("deneb") => Self {
+                execution_optimistic: value.execution_optimistic,
+                finalized: value.finalized,
+                response: VersionedResponse::Deneb(
+                    serde_json::from_str(value.data.get())
+                        .map_err(|e| ErrorReporter(e).to_string())?,
+                ),
+            },
+            Some("electra") | None => Self {
+                execution_optimistic: value.execution_optimistic,
+                finalized: value.finalized,
+                response: VersionedResponse::Electra(
+                    serde_json::from_str(value.data.get())
+                        .map_err(|e| ErrorReporter(e).to_string())?,
+                ),
+            },
+            v => return Err(format!("unknown version {v:?}")),
+        })
+    }
 }
