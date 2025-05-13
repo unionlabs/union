b03cf3864b28e5b6a6c5cf6d031bf5c75d24675b
benluelo <git@luelo.dev>
2025-05-13 17:59:40 +0100

    feat(pg-queue): prometheus -> opentelemetry

--- lib/pg-queue/src/metrics.rs
+++ lib/pg-queue/src/metrics.rs
@@ -1,30 +1,31 @@
-use std::sync::LazyLock;
+use opentelemetry::metrics::Histogram;
 
-use prometheus::{register_histogram, Histogram};
+#[derive(Debug, Clone)]
+pub struct Metrics {
+    pub item_processing_duration: Histogram<f64>,
+    pub optimize_processing_duration: Histogram<f64>,
+    pub optimize_item_count: Histogram<u64>,
+}
 
-pub static ITEM_PROCESSING_DURATION: LazyLock<Histogram> = LazyLock::new(|| {
-    register_histogram!(
-        "pg_queue_item_processing_duration_seconds",
-        "The time it takes to process an item in the queue.",
-    )
-    .unwrap()
-});
-
-// TODO: Add tag to this
-pub static OPTIMIZE_PROCESSING_DURATION: LazyLock<Histogram> = LazyLock::new(|| {
-    register_histogram!(
-        "pg_queue_optimize_processing_duration_seconds",
-        "The time it takes to run a pass over the optimize queue.",
-    )
-    .unwrap()
-});
-
-// TODO: Add tag to this
-pub static OPTIMIZE_ITEM_COUNT: LazyLock<Histogram> = LazyLock::new(|| {
-    register_histogram!(
-        "pg_queue_optimize_item_count",
-        "The amount of items processed in an optimize pass.",
-        vec![1.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0, 1000.0, 2000.0],
-    )
-    .unwrap()
-});
+impl Metrics {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self {
+            item_processing_duration: opentelemetry::global::meter("pg_queue")
+                .f64_histogram("pg_queue_item_processing_duration_nanoseconds")
+                .with_description("The time it takes to process an item in the queue.")
+                .build(),
+            optimize_processing_duration: opentelemetry::global::meter("pg_queue")
+                .f64_histogram("pg_queue_optimize_processing_duration_nanoseconds")
+                .with_description("The time it takes to run a pass over the optimize queue.")
+                .build(),
+            optimize_item_count: opentelemetry::global::meter("pg_queue")
+                .u64_histogram("pg_queue_optimize_item_count")
+                .with_description("The amount of items processed in an optimize pass.")
+                .with_boundaries(vec![
+                    1.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0, 1000.0, 2000.0,
+                ])
+                .build(),
+        }
+    }
+}
