6d8ab6b243511f1db174738f2087ef6995a4abd0
Lukas <Lukas.nyberg@gmail.com>
2025-05-15 21:16:51 +0200

    chore(app2): add missing package, fmt and move into dev dep

--- app2/src/routes/dashboard/+layout.ts
+++ app2/src/routes/dashboard/+layout.ts
@@ -3,6 +3,10 @@ import { redirect } from "@sveltejs/kit"
 import { Option } from "effect"
 import type { LayoutLoad } from "./$types"
 
+export const ssr = false
+export const prerender = true
+export const trailingSlash = "ignore"
+
 export const load: LayoutLoad = async () => {
   if (Option.isNone(dashboard.session)) {
     throw redirect(302, "/")
