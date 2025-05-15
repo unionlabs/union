f8c58fc0946728d5191f1b20b738228facb005ac
Eric Hegnes <eric@hegnes.com>
2025-05-15 10:05:08 -0400

    fix(ts-sdk): create FAO example
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- ts-sdk/examples/ucs03-create-fungible-asset-order.ts
+++ ts-sdk/examples/ucs03-create-fungible-asset-order.ts
@@ -1,9 +1,7 @@
-import { Effect } from "effect"
-import { createPublicClient, http, parseEther } from "viem"
+import { Effect, Logger, LogLevel, ManagedRuntime } from "effect"
+import { createPublicClient, http, parseEther, toHex } from "viem"
 import { holesky, sepolia } from "viem/chains"
-import { CosmosDestinationConfig } from "../src/cosmos/quote-token.js"
 import { ViemPublicClientDestination, ViemPublicClientSource } from "../src/evm/client.js"
-import { DestinationConfig } from "../src/evm/quote-token.js"
 import {
   createCosmosToCosmosFungibleAssetOrder,
   createCosmosToEvmFungibleAssetOrder,
@@ -11,6 +9,10 @@ import {
   createEvmToEvmFungibleAssetOrder,
 } from "../src/ucs03/fungible-asset-order.js"
 
+import { CosmosChannelDestination, CosmosChannelSource } from "@unionlabs/sdk/cosmos/channel"
+import { EvmChannelDestination } from "@unionlabs/sdk/evm/channel"
+import { ChannelId, TokenRawDenom, UniversalChainId } from "@unionlabs/sdk/schema"
+import { AddressCosmosZkgm, AddressEvmZkgm } from "@unionlabs/sdk/schema/address"
 import {
   CosmWasmClientDestination,
   CosmWasmClientSource,
@@ -22,28 +24,28 @@ BigInt["prototype"].toJSON = function() {
   return this.toString()
 }
 
+const { runPromiseExit } = ManagedRuntime.make(
+  Logger.minimumLogLevel(LogLevel.Error),
+)
+
 // Example 1: EVM to EVM transfer
-Effect.runPromiseExit(
+runPromiseExit(
   createEvmToEvmFungibleAssetOrder({
-    sender: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-    receiver: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-    baseToken: "0x94373a4919B3240D86eA41593D5eBa789FEF3848", // WETH on holesky
+    sender: AddressEvmZkgm.make("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
+    receiver: AddressEvmZkgm.make("0x70997970C51812dc3A010C7d01b50e0d17dc79C8"),
+    baseToken: TokenRawDenom.make("0x94373a4919B3240D86eA41593D5eBa789FEF3848"), // WETH on holesky
     baseAmount: parseEther("100"), // 100 tokens
     quoteAmount: parseEther("0.05"), // 0.05 quote tokens
+    sourceChainId: UniversalChainId.make("ethereum.17000"),
+    sourceChannelId: ChannelId.make(2),
   }).pipe(
     Effect.provideService(ViemPublicClientSource, {
-      client: createPublicClient({
-        chain: holesky,
-        transport: http(),
-      }),
+      client: createPublicClient({ chain: holesky, transport: http() }),
     }),
     Effect.provideService(ViemPublicClientDestination, {
-      client: createPublicClient({
-        chain: sepolia,
-        transport: http(),
-      }),
+      client: createPublicClient({ chain: sepolia, transport: http() }),
     }),
-    Effect.provideService(DestinationConfig, {
+    Effect.provideService(EvmChannelDestination, {
       ucs03address: "0x84F074C15513F15baeA0fbEd3ec42F0Bd1fb3efa",
       channelId: 8,
     }),
@@ -52,25 +54,24 @@ Effect.runPromiseExit(
 
 // Example 2: Cosmos to EVM transfer
 // console.log("\nCosmos to EVM transfer:")
-Effect.runPromiseExit(
+runPromiseExit(
   Effect.gen(function*() {
-    const client = yield* createCosmWasmClient("https://rpc.elgafar-1.stargaze-apis.com")
+    const client = yield* createCosmWasmClient("https://rpc.bbn-test-5.babylon.chain.kitchen")
 
     return yield* createCosmosToEvmFungibleAssetOrder({
-      sender: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-      receiver: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-      baseToken: "stars1qrde534d4jwk44dn7w7gu9e2rayutr7kqx8lfjhsk3rd7z9rzxhq2gh3lr", // WETH on stargaze
-      baseAmount: BigInt(1000000), // 1 token
+      sender: AddressCosmosZkgm.make(toHex("bbn122ny3mep2l7nhtafpwav2y9e5jrslhekrn8frh")),
+      receiver: AddressEvmZkgm.make("0x70997970c51812dc3a010c7d01b50e0d17dc79c8"),
+      baseToken: "ubbn" as unknown as any,
+      baseAmount: 1000000n, // 1 token
       quoteAmount: parseEther("0.05"), // 0.05 quote tokens
+      sourceChainId: UniversalChainId.make("babylon.bbn-test-5"),
+      sourceChannelId: ChannelId.make(2),
     }).pipe(
       Effect.provideService(CosmWasmClientSource, { client }),
       Effect.provideService(ViemPublicClientDestination, {
-        client: createPublicClient({
-          chain: sepolia,
-          transport: http(),
-        }),
+        client: createPublicClient({ chain: sepolia, transport: http() }),
       }),
-      Effect.provideService(DestinationConfig, {
+      Effect.provideService(EvmChannelDestination, {
         ucs03address: "0x84F074C15513F15baeA0fbEd3ec42F0Bd1fb3efa",
         channelId: 8,
       }),
@@ -79,26 +80,25 @@ Effect.runPromiseExit(
 ).then(exit => console.log("Cosmos to EVM", JSON.stringify(exit, null, 2)))
 
 // Example 3: EVM to Cosmos transfer
-Effect.runPromiseExit(
+runPromiseExit(
   Effect.gen(function*() {
-    const client = yield* createCosmWasmClient("https://rpc.elgafar-1.stargaze-apis.com")
+    const client = yield* createCosmWasmClient("https://rpc.bbn-test-5.babylon.chain.kitchen")
 
     return yield* createEvmToCosmosFungibleAssetOrder({
-      sender: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-      receiver: "stars1qcvavxpxw3t8d9j7mwaeq9wgytkf5vwputv5x4",
-      baseToken: "0x94373a4919B3240D86eA41593D5eBa789FEF3848", // WETH on holesky
+      sender: AddressEvmZkgm.make("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"),
+      receiver: AddressCosmosZkgm.make(toHex("bbn122ny3mep2l7nhtafpwav2y9e5jrslhekrn8frh")),
+      baseToken: TokenRawDenom.make("0xb476983cc7853797fc5adc4bcad39b277bc79656"),
       baseAmount: parseEther("100"), // 100 tokens
-      quoteAmount: BigInt(1000000), // 1 token
+      quoteAmount: 1000000n, // 1 token
+      sourceChainId: UniversalChainId.make("ethereum.17000"),
+      sourceChannelId: ChannelId.make(2),
     }).pipe(
       Effect.provideService(ViemPublicClientSource, {
-        client: createPublicClient({
-          chain: holesky,
-          transport: http(),
-        }),
+        client: createPublicClient({ chain: holesky, transport: http() }),
       }),
       Effect.provideService(CosmWasmClientDestination, { client }),
-      Effect.provideService(CosmosDestinationConfig, {
-        ucs03address: "stars1x2jzeup7uwfxjxxrtfna2ktcugltntgu6kvc0eeayk0d82l247cqsnqksg",
+      Effect.provideService(CosmosChannelDestination, {
+        ucs03address: "bbn1336jj8ertl8h7rdvnz4dh5rqahd09cy0x43guhsxx6xyrztx292q77945h",
         channelId: 3,
       }),
     )
@@ -106,28 +106,30 @@ Effect.runPromiseExit(
 ).then(exit => console.log("EVM to Cosmos", JSON.stringify(exit, null, 2)))
 
 // Example 4: Cosmos to Cosmos transfer
-Effect.runPromiseExit(
+runPromiseExit(
   Effect.gen(function*() {
     // Create CosmWasm clients for source and destination chains
-    const sourceClient = yield* createCosmWasmClient("https://rpc.elgafar-1.stargaze-apis.com")
-    const destClient = yield* createCosmWasmClient("https://rpc.testnet-9.union.build")
+    const sourceClient = yield* createCosmWasmClient("https://rpc.bbn-test-5.babylon.chain.kitchen")
+    const destClient = yield* createCosmWasmClient("https://rpc.osmo-test-5.osmosis.chain.kitchen")
 
     return yield* createCosmosToCosmosFungibleAssetOrder({
-      sender: "stars1qcvavxpxw3t8d9j7mwaeq9wgytkf5vwputv5x4",
-      receiver: "union1d95n4r6dnrfrps59szhl8mk7yqewsuzyw0zh5q", // Example cosmos address
-      baseToken: "stars1qrde534d4jwk44dn7w7gu9e2rayutr7kqx8lfjhsk3rd7z9rzxhq2gh3lr", // WETH on stargaze
-      baseAmount: BigInt(1000000), // 1 token
-      quoteAmount: BigInt(1000000), // Expected amount on destination chain
+      sender: AddressCosmosZkgm.make(toHex("bbn122ny3mep2l7nhtafpwav2y9e5jrslhekrn8frh")),
+      receiver: AddressCosmosZkgm.make(toHex("osmo122ny3mep2l7nhtafpwav2y9e5jrslhekuk9g2u")), // Example cosmos address
+      baseToken: "ubbn" as unknown as any,
+      baseAmount: 1000000n, // 1 token
+      quoteAmount: 1000000n, // Expected amount on destination chain
+      sourceChainId: UniversalChainId.make("babylon.bbn-test-5"),
+      sourceChannelId: ChannelId.make(2),
     }).pipe(
       Effect.provideService(CosmWasmClientSource, { client: sourceClient }),
       Effect.provideService(CosmWasmClientDestination, { client: destClient }),
       Effect.provideService(CosmosChannelDestination, {
-        ucs03address: "union15zcptld878lux44lvc0chzhz7dcdh62nh0xehwa8y7czuz3yljls7u4ry6",
+        ucs03address: "osmo1336jj8ertl8h7rdvnz4dh5rqahd09cy0x43guhsxx6xyrztx292qs2uecc",
         channelId: 1,
       }),
-      Effect.provideService(CosmosDestinationConfig, {
-        ucs03address: "union1x2jzeup7uwfxjxxrtfna2ktcugltntgu6kvc0eeayk0d82l247cqz669ee",
-        channelId: 4,
+      Effect.provideService(CosmosChannelSource, {
+        ucs03address: "bbn1336jj8ertl8h7rdvnz4dh5rqahd09cy0x43guhsxx6xyrztx292q77945h",
+        channelId: 5,
       }),
     )
   }),
