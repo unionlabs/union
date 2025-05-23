9bc60d3f97f916f747289e6ad983a67ef03a0434
Lukas <Lukas.nyberg@gmail.com>
2025-05-23 02:05:02 +0200

    chore(app2): fmt

--- app2/src/routes/auth/sign-in/+page.ts
+++ app2/src/routes/auth/sign-in/+page.ts
@@ -1,6 +1,6 @@
-import { redirect } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit"
 
 export const load = async () => {
   // Temporary redirect from sign-in page
-  throw redirect(302, "/");
-};
+  throw redirect(302, "/")
+}
