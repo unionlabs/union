3a6d874472131f5cc0e3c7fc836a3867152ebb60
Kaan Caglan <caglankaan@gmail.com>
2025-05-28 15:28:50 +0300

    (ts-sdk-sui): added coin balance read
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- ts-sdk/examples/sui-get-quote-token.ts
+++ ts-sdk/examples/sui-get-quote-token.ts
@@ -10,12 +10,12 @@ BigInt["prototype"].toJSON = function() {
   return this.toString()
 }
 
+function bytesToHex(bs: number[]): `0x${string}` {
+  return "0x" + bs.map(b => b.toString(16).padStart(2, "0")).join("");
+}
+
 Effect.runPromiseExit(
   Effect.gen(function*() {
-    // Create account from private key
-
-    const rpcUrl = "https://fullnode.testnet.sui.io:443"
-
     const config = {
       url: getFullnodeUrl("testnet")
     }
@@ -26,7 +26,7 @@ Effect.runPromiseExit(
 
     const token_address = "0x6d756e6f"
 
-    const result2 = yield* predictQuoteToken(token_address).pipe(
+    const result = yield* predictQuoteToken(token_address).pipe(
       Effect.provideService(SuiPublicClientDestination, { client: publicClient }),
       Effect.provideService(SuiChannelDestination, {
         ucs03address: "0x4429fafa71bf9730c9fda4a5ce6772aecb1ac6a1ef0522812949f71d241332b5",
@@ -34,12 +34,10 @@ Effect.runPromiseExit(
       }),
       Effect.tapErrorCause(cause => Effect.logError("Predict failed with cause", cause)),
     )
-
-
     const expected_result = "0x204c18721deddf1ea8ec97b187aaf067c09111f350d956cb624b7b4002f0c5e246"
-    assert(result2 === expected_result, `Expected wrapped token to be ${expected_result}, but got ${result2}`)
+    assert(bytesToHex(result) === expected_result, `Expected wrapped token to be ${expected_result}, but got ${result}`)
 
 
-    yield* Effect.log("Result:", result2)
+    yield* Effect.log("Result:", result)
   }),
 ).then(exit => console.log(JSON.stringify(exit, null, 2)))
---
+++ ts-sdk/src/sui/sui_coin.ts
@@ -0,0 +1,149 @@
+import { Effect } from "effect"
+import { SuiPublicClient } from "./client.js"
+import { readContract } from "./contract.js"
+
+export type Hex = `0x${string}`
+
+/**
+ * Interface for FA token metadata
+ */
+export interface FaTokenInfo {
+  decimals: number
+  icon_uri: string
+  name: string
+  project_uri: string
+  symbol: string
+}
+
+export const readCoinBalances = (contractAddress: string, address: string) =>
+  Effect.gen(function*() {
+    const client = (yield* SuiPublicClient).client
+    let params = {
+      owner: address,
+      coinType: contractAddress
+    };
+   
+    const coins = yield * Effect.tryPromise({
+      try: async () => {
+        const result = await client.getCoins(params);
+        return result.data
+      },
+      catch: error => new Error(`Failed to read FA balance: ${error}`)
+    })
+    return coins
+  })
+
+  export const readTotalCoinBalance = (contractAddress: string, address: string) =>
+    Effect.gen(function*() {
+      const client = (yield* SuiPublicClient).client
+      let params = {
+        owner: address,
+        coinType: contractAddress
+      };
+     
+  
+      const coins = yield * Effect.tryPromise({
+        try: async () => {
+          const result = await client.getCoins(params);
+          return result.data
+        },
+        catch: error => new Error(`Failed to read FA balance: ${error}`)
+      })
+      // Calculate total balance
+      const totalBalance = coins.reduce((acc, coin) => acc + BigInt(coin.balance), BigInt(0));
+  
+      return totalBalance
+    })
+
+// export const readFaName = (contractAddress: string) =>
+//   Effect.gen(function*() {
+//     const client = (yield* AptosPublicClient).client
+
+//     const module_name = "fungible_asset"
+//     const contract_address = "0x1"
+//     const function_name = "name"
+//     const type_arguments = ["0x1::fungible_asset::Metadata"]
+//     const function_arguments = [contractAddress]
+
+//     const result = yield* readContract(
+//       client,
+//       contract_address,
+//       module_name,
+//       function_name,
+//       type_arguments,
+//       function_arguments,
+//     )
+
+//     // Extract the address from the result tuple
+//     return result[0]
+//   })
+
+// export const readFaDecimals = (contractAddress: string) =>
+//   Effect.gen(function*() {
+//     const client = (yield* AptosPublicClient).client
+
+//     const contract_address = "0x1"
+//     const module_name = "fungible_asset"
+//     const function_name = "decimals"
+//     const type_arguments = ["0x1::fungible_asset::Metadata"]
+//     const function_arguments = [contractAddress]
+
+//     const result = yield* readContract(
+//       client,
+//       contract_address,
+//       module_name,
+//       function_name,
+//       type_arguments,
+//       function_arguments,
+//     )
+
+//     // Extract the address from the result tuple
+//     return result[0]
+//   })
+
+// export const readFaSymbol = (contractAddress: string) =>
+//   Effect.gen(function*() {
+//     const client = (yield* AptosPublicClient).client
+
+//     const contract_address = "0x1"
+//     const module_name = "fungible_asset"
+//     const function_name = "symbol"
+//     const type_arguments = ["0x1::fungible_asset::Metadata"]
+//     const function_arguments = [contractAddress]
+
+//     const result = yield* readContract(
+//       client,
+//       contract_address,
+//       module_name,
+//       function_name,
+//       type_arguments,
+//       function_arguments,
+//     )
+
+//     // Extract the address from the result tuple
+//     return result[0]
+//   })
+
+// export const readFaTokenInfo = (contractAddress: string) =>
+//   Effect.gen(function*() {
+//     const client = (yield* AptosPublicClient).client
+
+//     const contract_address = "0x1"
+//     const module_name = "fungible_asset"
+//     const function_name = "metadata"
+//     const type_arguments = ["0x1::fungible_asset::Metadata"]
+//     const function_arguments = [contractAddress]
+
+//     const result = yield* readContract(
+//       client,
+//       contract_address,
+//       module_name,
+//       function_name,
+//       type_arguments,
+//       function_arguments,
+//     )
+
+//     const token_info = result[0] as FaTokenInfo
+
+//     return token_info
+//   })
