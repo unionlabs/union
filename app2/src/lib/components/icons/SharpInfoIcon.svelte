b5a128ed8e0dd01a1cce65fb9660dfb9878174e3
Eric Hegnes <eric@hegnes.com>
2025-05-29 20:18:23 -0400

    chore(app2): pass checks
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- app2/src/lib/components/icons/SharpInfoIcon.svelte
+++ app2/src/lib/components/icons/SharpInfoIcon.svelte
@@ -1,13 +1,13 @@
-<svg 
-  xmlns="http://www.w3.org/2000/svg" 
-  width="1em" 
-  height="1em" 
-  viewBox="0 0 24 24" 
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="1em"
+  height="1em"
+  viewBox="0 0 24 24"
   {...$$props}
 >
   <!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE -->
-  <path 
-    fill="currentColor" 
+  <path
+    fill="currentColor"
     d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m1 15h-2v-6h2zm0-8h-2V7h2z"
   />
 </svg>
