c4574558e758f6ea3bac685b262cede36055dfeb
Lukas <Lukas.nyberg@gmail.com>
2025-06-04 00:05:24 +0200

    chore(app): fmt again lol

--- app2/src/lib/constants/disabled-chains.ts
+++ app2/src/lib/constants/disabled-chains.ts
@@ -1,4 +1,4 @@
-import { UniversalChainId } from "@unionlabs/sdk/schema";
+import { UniversalChainId } from "@unionlabs/sdk/schema"
 
 export const DISABLED_CHAINS = [
   UniversalChainId.make("xion.xion-testnet-2"),
