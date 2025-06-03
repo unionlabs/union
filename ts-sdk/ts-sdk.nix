419d7f1193a88882d8a70d838f8ca03a9b7be0d0
Kaan Caglan <caglankaan@gmail.com>
2025-06-03 13:29:15 +0300

    chore(ts-sdk-sui): fixed npm hashes
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- ts-sdk/ts-sdk.nix
+++ ts-sdk/ts-sdk.nix
@@ -28,7 +28,7 @@ _: {
           packageJsonPath = ./package.json;
           extraSrcs = [ ../ts-sdk ];
           pnpmWorkspaces = [ "@unionlabs/sdk" ];
-          hash = "sha256-Ze6FczSengfpzHlAzzkgx5mKr8AEIzrk81TnMlGgkrE=";
+          hash = "sha256-tGYYB67pBQxOMcbnaIBaeCFm3sDUyogY1BssJIVMFyA";
           doCheck = true;
           buildPhase = ''
             runHook preBuild
