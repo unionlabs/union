7e02eadbd4ac9a1f051d1099bf5f6f98a1fd73dd
Lukas <Lukas.nyberg@gmail.com>
2025-06-13 15:03:17 +0200

    feat(app): enable xion

--- app2/src/lib/constants/disabled-chains.ts
+++ app2/src/lib/constants/disabled-chains.ts
@@ -1,5 +1,6 @@
 import { UniversalChainId } from "@unionlabs/sdk/schema"
 
-export const DISABLED_CHAINS = [
-  UniversalChainId.make("xion.xion-testnet-2"),
-]
+// export const DISABLED_CHAINS = [
+//   UniversalChainId.make("xion.xion-testnet-2"),
+// ]
+
