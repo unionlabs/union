1ff7ed20813aee3a1c28d8678bdbe386e66c0516
jurriaan <jurriaan@pruijs.nl>
2025-06-20 18:16:06 +0200

    feat(hubble): ucs events - support unknown event

--- hubble/src/indexer/postgres/event_data.rs
+++ hubble/src/indexer/postgres/event_data.rs
@@ -3,7 +3,7 @@ use tracing::debug;
 
 use crate::indexer::{
     api::{BlockHeight, UniversalChainId},
-    event::BlockEvent,
+    event::SupportedBlockEvent,
 };
 
 pub async fn delete_event_data_at_height(
@@ -68,88 +68,99 @@ async fn has_event_data_at_height(
     .is_some())
 }
 
-pub async fn insert_event_data_with_events(
+pub async fn handle_block_events(
     tx: &mut sqlx::Transaction<'_, sqlx::Postgres>,
-    block_events: &[&BlockEvent],
+    block_events: &[&SupportedBlockEvent],
 ) -> sqlx::Result<bool> {
+    let mut change = false;
+
     for block_event in block_events {
-        match block_event {
-            BlockEvent::EthereumLog {
-                internal_chain_id,
-                block_hash,
-                data,
-                height,
-                time,
-            } => sqlx::query!(
-                "
-                INSERT INTO v2_evm.logs (internal_chain_id, block_hash, data, height, time)
-                VALUES ($1, $2, $3, $4, $5)
-                ",
-                internal_chain_id,
-                block_hash,
-                data,
-                i64::try_from(*height).expect("height fits"),
-                time
-            )
-            .execute(tx.as_mut()),
-            BlockEvent::TendermintBlock {
-                internal_chain_id,
-                hash,
-                data,
-                height,
-                time,
-            } => sqlx::query!(
-                "
-                INSERT INTO v2_cosmos.blocks (internal_chain_id, hash, data, height, time)
-                VALUES ($1, $2, $3, $4, $5)
-                ",
-                internal_chain_id,
-                hash,
-                data,
-                i64::try_from(*height).expect("height fits"),
-                time
-            )
-            .execute(tx.as_mut()),
-            BlockEvent::TendermintTransaction {
-                internal_chain_id,
-                block_hash,
-                height,
-                hash,
-                data,
-                index,
-            } => sqlx::query!("
-                INSERT INTO v2_cosmos.transactions (internal_chain_id, block_hash, height, hash, data, index) 
-                VALUES ($1, $2, $3, $4, $5, $6)
-                ",
-                internal_chain_id, block_hash, i64::try_from(*height).expect("height fits"), hash, data, index)
-            .execute(tx.as_mut()),
-            BlockEvent::TendermintEvent {
-                internal_chain_id,
-                block_hash,
-                height,
-                transaction_hash,
-                index,
-                transaction_index,
-                data,
-                time,
-                flow,
-            } =>     sqlx::query!("
-                INSERT INTO v2_cosmos.events (internal_chain_id, block_hash, height, transaction_hash, index, transaction_index, data, time, flow)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-                ",
-                internal_chain_id,
-                block_hash,
-                i64::try_from(*height).expect("height fits"),
-                transaction_hash as _,
-                index,
-                transaction_index as _,
-                data,
-                time,
-                flow)
-            .execute(tx.as_mut()),
-        }
-        .await?;
+        change = change || handle_block_event(tx, block_event).await?
     }
+
+    Ok(change)
+}
+
+pub async fn handle_block_event(
+    tx: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+    block_event: &SupportedBlockEvent,
+) -> sqlx::Result<bool> {
+    match block_event {
+        SupportedBlockEvent::EthereumLog {
+            internal_chain_id,
+            block_hash,
+            data,
+            height,
+            time,
+        } => sqlx::query!(
+            "
+            INSERT INTO v2_evm.logs (internal_chain_id, block_hash, data, height, time)
+            VALUES ($1, $2, $3, $4, $5)
+            ",
+            internal_chain_id,
+            block_hash,
+            data,
+            i64::try_from(*height).expect("height fits"),
+            time
+        )
+        .execute(tx.as_mut()),
+        SupportedBlockEvent::TendermintBlock {
+            internal_chain_id,
+            hash,
+            data,
+            height,
+            time,
+        } => sqlx::query!(
+            "
+            INSERT INTO v2_cosmos.blocks (internal_chain_id, hash, data, height, time)
+            VALUES ($1, $2, $3, $4, $5)
+            ",
+            internal_chain_id,
+            hash,
+            data,
+            i64::try_from(*height).expect("height fits"),
+            time
+        )
+        .execute(tx.as_mut()),
+        SupportedBlockEvent::TendermintTransaction {
+            internal_chain_id,
+            block_hash,
+            height,
+            hash,
+            data,
+            index,
+        } => sqlx::query!("
+            INSERT INTO v2_cosmos.transactions (internal_chain_id, block_hash, height, hash, data, index) 
+            VALUES ($1, $2, $3, $4, $5, $6)
+            ",
+            internal_chain_id, block_hash, i64::try_from(*height).expect("height fits"), hash, data, index)
+        .execute(tx.as_mut()),
+        SupportedBlockEvent::TendermintEvent {
+            internal_chain_id,
+            block_hash,
+            height,
+            transaction_hash,
+            index,
+            transaction_index,
+            data,
+            time,
+            flow,
+        } =>     sqlx::query!("
+            INSERT INTO v2_cosmos.events (internal_chain_id, block_hash, height, transaction_hash, index, transaction_index, data, time, flow)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+            ",
+            internal_chain_id,
+            block_hash,
+            i64::try_from(*height).expect("height fits"),
+            transaction_hash as _,
+            index,
+            transaction_index as _,
+            data,
+            time,
+            flow)
+        .execute(tx.as_mut()),
+    }
+    .await?;
 
     Ok(true)
 }
