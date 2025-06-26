0b4e0e19a695ad7820c180c0b93ebf0838e6f934
Eric Hegnes <eric@hegnes.com>
2025-06-26 19:20:33 -0400

    wip
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- ts-sdk/src/index.ts
+++ ts-sdk/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * `@unionlabs/sdk`
  *
- * See {@see http://localhost:4321/integrations/typescript} for additional integration guides and examples.
+ * See {@see https://docs.union.build/integrations/typescript/} for additional integration guides and examples.
  *
  * :::caution[Caution]
  * The SDK is undergoing stabilization efforts, starting with the release of `v2.0.0`, to provide a better developer experience. Breaking changes may occur until `v3.0.0` such that chain-agnostic APIs are feature complete.
