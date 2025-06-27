97dbfae19e8126af39d772070fed39416143d7d7
Eric Hegnes <eric@hegnes.com>
2025-06-27 01:55:16 -0400

    fix hashes
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- ts-sdk/ts-sdk.nix
+++ ts-sdk/ts-sdk.nix
@@ -28,7 +28,7 @@ _: {
           packageJsonPath = ./package.json;
           extraSrcs = [ ../ts-sdk ];
           pnpmWorkspaces = [ "@unionlabs/sdk" ];
-          hash = "sha256-tqPdH8J6xFH/1FfnGE0yeQ3SOWITbg0K52BSHWManSw=";
+          hash = "sha256-n4s3YUWjwodfEdi+yARcAAJt6nQw9rXRCWQy95/YCB4=";
           doCheck = true;
           buildPhase = ''
             runHook preBuild
