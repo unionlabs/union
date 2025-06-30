99ee74653507ef10b77cea85a0a940856b1771ac
Eric Hegnes <eric@hegnes.com>
2025-06-30 17:37:01 -0400

    wip
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- ts-sdk/examples/ucs03-send-babylon-to-bob.ts
+++ ts-sdk/examples/ucs03-send-babylon-to-bob.ts
@@ -1,176 +1,63 @@
 import { Decimal } from "@cosmjs/math"
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
-import { Cosmos, Evm } from "@unionlabs/sdk"
-import {
-  CosmWasmClientSource,
-  createCosmWasmClient,
-  createSigningCosmWasmClient,
-  SigningCosmWasmClientContext,
-} from "@unionlabs/sdk/cosmos"
-import { CosmosChannelSource } from "@unionlabs/sdk/cosmos"
-import {
-  createCosmosToEvmFungibleAssetOrder,
-  Instruction,
-  sendInstructionCosmos,
-} from "@unionlabs/sdk/ucs03"
-import { Effect } from "effect"
-import { http } from "viem"
-import { sepolia } from "viem/chains"
-
-// @ts-ignore
-BigInt["prototype"].toJSON = function() {
-  return this.toString()
-}
+import { Cosmos, Evm, FungibleAssetOrder, Ucs03, Ucs05 } from "@unionlabs/sdk"
+import { UniversalChainId } from "@unionlabs/sdk/schema/chain"
+import { ChannelId } from "@unionlabs/sdk/schema/channel"
+import { Effect, Exit, pipe } from "effect"
+import { http, toHex } from "viem"
+import { bobSepolia } from "viem/chains"
 
 const MNEMONIC = process.env.MNEMONIC || "memo memo memo"
 
-const SENDER = "0x52a648ef2157fd3bafa90bbac510b9a4870fdf36"
-const RECEIVER = "0xfaebe5bf141cc04a3f0598062b98d2df01ab3c4d"
-const SOURCE_UCS03_ADDRESS = "bbn15zcptld878lux44lvc0chzhz7dcdh62nh0xehwa8y7czuz3yljlspm2re6"
+const SENDER = Ucs05.AddressCosmosZkgm.make(toHex("bbn122ny3mep2l7nhtafpwav2y9e5jrslhekrn8frh"))
+const RECEIVER = Ucs05.AddressEvmZkgm.make("0xfaebe5bf141cc04a3f0598062b98d2df01ab3c4d")
+const SOURCE_UCS03_ADDRESS =
+  "bbn15zcptld878lux44lvc0chzhz7dcdh62nh0xehwa8y7czuz3yljlspm2re6" as const
 
-// toHex(intent.sender),
-// intent.receiver,
-// toHex(intent.baseToken),
+const main = Effect.gen(function*() {
+  const sourceClient = Cosmos.ClientSource.Live(
+    "https://rpc.bbn-1.babylon.chain.kitchen",
+  )
+  const destinationClient = Evm.PublicClientDestination.Live({
+    chain: bobSepolia,
+    transport: http(),
+  })
+  const wallet = yield* Effect.tryPromise(() =>
+    DirectSecp256k1HdWallet.fromMnemonic(MNEMONIC, { prefix: "bbn" })
+  )
+  const signingClient = Cosmos.SigningClientContext.Live(
+    "https://rest.bbn-1.babylon.chain.kitchen",
+    wallet,
+    { gasPrice: { amount: Decimal.fromUserInput("1", 6), denom: "ubbn" } },
+  )
 
-// Define token transfers
-const TRANSFERS = [
-  {
-    sender: SENDER,
-    receiver: RECEIVER,
-    // baseToken: "bbn1nznl7srgmtx9juevnrasr2srslqzze3083g30w8ug96f96rjpeuqg8f6uw",
-    baseToken: "ubbn",
-    baseAmount: 100n,
-    quoteAmount: 100n,
-  },
-] as const
-
-const createBatch = Effect.gen(function*() {
-  yield* Effect.log("creating transfer 1")
-  const transfer1 = yield* createCosmosToEvmFungibleAssetOrder(TRANSFERS[0])
-
-  console.log(JSON.stringify(transfer1, null, 2))
-
-  return Instruction.Batch.make({ operand: [transfer1] })
-}).pipe(Effect.withLogSpan("batch creation"))
-
-// Check and increase allowances if needed
-const checkAndIncreaseAllowances = Effect.gen(function*() {
-  const publicClient = (yield* ViemPublicClient).client
-
-  yield* Effect.log("Checking token allowances...")
-
-  // for (const transfer of TRANSFERS) {
-  //   yield* Effect.log(`checking ${transfer.baseToken} allowance...`)
-
-  //   // Check current allowance
-  //   const currentAllowance = yield* readErc20Allowance(
-  //     transfer.baseToken,
-  //     transfer.sender,
-  //     SOURCE_UCS03_ADDRESS
-  //   )
-
-  //   yield* Effect.log(`current ${transfer.baseToken} allowance: ${currentAllowance}`)
-
-  //   // If allowance is insufficient, increase it
-  //   if (currentAllowance < transfer.baseAmount) {
-  //     yield* Effect.log(`increasing ${transfer.baseToken} allowance...`)
-
-  //     // Approve exact amount needed
-  //     const txHash = yield* increaseErc20Allowance(
-  //       transfer.baseToken,
-  //       SOURCE_UCS03_ADDRESS,
-  //       transfer.baseAmount
-  //     )
-
-  //     yield* Effect.log(`approval transaction sent: ${txHash}`)
-
-  //     // Wait for transaction receipt
-  //     const receipt = yield* waitForTransactionReceipt(txHash)
-
-  //     yield* Effect.log(`approval confirmed in block: ${receipt.blockNumber}`)
-
-  //     // Verify new allowance
-  //     const newAllowance = yield* readErc20Allowance(
-  //       transfer.baseToken,
-  //       transfer.sender,
-  //       SOURCE_UCS03_ADDRESS
-  //     )
-
-  //     yield* Effect.log(`new ${transfer.baseToken} allowance: ${newAllowance}`)
-  //   } else {
-  //     yield* Effect.log(`${transfer.baseToken} allowance is sufficient`)
-  //   }
-  // }
-
-  yield* Effect.log("All allowances checked and increased if needed")
-}).pipe(Effect.withLogSpan("allowance check and increase"))
-
-Effect.runPromiseExit(
-  Effect.gen(function*() {
-    // Create clients and setup
-    yield* Effect.log("transferring from sepolia to stargaze")
-
-    yield* Effect.log("creating clients")
-    const cosmWasmClientSource = yield* createCosmWasmClient(
-      "https://rpc.rpc-node.union-testnet-10.union.build",
-    )
-
-    const publicDestinationClient = yield* createViemPublicClient({
-      chain: sepolia,
-      transport: http(),
-    })
-
-    // Create a wallet from mnemonic (in a real app, use a secure method to get this)
-    const wallet = yield* Effect.tryPromise(() =>
-      DirectSecp256k1HdWallet.fromMnemonic(MNEMONIC, { prefix: "union" })
-    )
-
-    // Get the first account address
-    const [firstAccount] = yield* Effect.tryPromise(() => wallet.getAccounts())
-
-    // Create a signing client
-    const signingClient = yield* createSigningCosmWasmClient(
-      "https://rpc.rpc-node.union-testnet-10.union.build",
-      wallet,
-      {
-        gasPrice: { amount: Decimal.fromUserInput("1", 6), denom: "muno" },
-      },
-    )
-
-    yield* Effect.log("clients created")
-
-    // Main effect: create the batch and send it
-    return yield* Effect.gen(function*() {
-      yield* Effect.log("creating batch")
-      const batch = yield* createBatch
-      yield* Effect.log("batch created", JSON.stringify(batch, null, 2))
-      yield* Effect.log("batch abi", Instruction.encodeAbi(batch))
-
-      // Check and increase allowances before sending the batch
-      // yield* Effect.log("checking and increasing allowances if needed")
-      // yield* checkAndIncreaseAllowances
-      // yield* Effect.log("allowances verified")
-
-      yield* Effect.log("sending batch")
-      return yield* sendInstructionCosmos(batch)
-    }).pipe(
-      Effect.provideService(SigningCosmWasmClientContext, {
-        client: signingClient,
-        address: firstAccount.address,
-      }),
-      Effect.provideService(CosmWasmClientSource, { client: cosmWasmClientSource }),
-      Effect.provideService(ViemPublicClientDestination, { client: publicDestinationClient }),
-      Effect.provideService(EvmChannelDestination, {
-        ucs03address: "0xe33534b7f8D38C6935a2F6Ad35E09228dA239962",
-        channelId: 1,
-      }),
-      Effect.provideService(CosmosChannelSource, {
-        ucs03address: SOURCE_UCS03_ADDRESS,
-        channelId: 1,
-      }),
-    )
-  }),
-).then(e => {
-  console.log(JSON.stringify(e, null, 2))
-  console.log(e)
+  return yield* pipe(
+    FungibleAssetOrder.cosmosToEvm({
+      sender: SENDER,
+      receiver: RECEIVER,
+      baseToken: "ubbn",
+      baseAmount: 100n,
+      quoteAmount: 100n,
+      sourceChainId: UniversalChainId.make("babylon.bbn-1"),
+      sourceChannelId: ChannelId.make(1),
+    }),
+    Effect.map((intent) => Ucs03.Batch.make({ operand: [intent] })),
+    Effect.andThen((instruction) => Cosmos.sendInstruction(instruction, SENDER)),
+    Effect.provide(signingClient),
+    Effect.provide(sourceClient),
+    Effect.provide(destinationClient),
+    Effect.provideService(Evm.ChannelDestination, {
+      ucs03address: "0xe33534b7f8D38C6935a2F6Ad35E09228dA239962",
+      channelId: 1,
+    }),
+    Effect.provideService(Cosmos.ChannelSource, {
+      ucs03address: SOURCE_UCS03_ADDRESS,
+      channelId: 1,
+    }),
+  )
 })
+
+Effect.runPromiseExit(main).then(Exit.match({
+  onFailure: (cause) => console.error(JSON.stringify(cause, null, 2)),
+  onSuccess: (result) => console.log(result),
+}))
