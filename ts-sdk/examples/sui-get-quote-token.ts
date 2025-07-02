c7a514a7e8ac276c55012b735580d74bf7639ded
Eric Hegnes <eric@hegnes.com>
2025-07-01 20:26:22 -0400

    more example fixes
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- ts-sdk/examples/sui-get-quote-token.ts
+++ ts-sdk/examples/sui-get-quote-token.ts
@@ -1,18 +1,18 @@
+import * as Sui from "@unionlabs/sdk/Sui"
 import { Effect, pipe } from "effect"
 import { assert } from "effect/Console"
-import * as Sui from "../src/Sui.js"
 
 const TOKEN_ADDRESS = "0x6d756e6f"
 const EXPECTED_RESULT = "0x4c18721deddf1ea8ec97b187aaf067c09111f350d956cb624b7b4002f0c5e246"
 
+const PublicClientDestination = Sui.PublicClientDestination.FromNode("testnet")
+const ChannelDestination = Sui.ChannelDestination.of({
+  ucs03address: "0xbd3eb037fda14d910c5574b8c950222cc5c4211675b9c2265c07a66cef3a7691",
+  channelId: 2,
+})
+
 Effect.runPromiseExit(
   Effect.gen(function*() {
-    const PublicClientDestination = Sui.PublicClientDestination.FromNode("testnet")
-    const ChannelDestination = Sui.ChannelDestination.of({
-      ucs03address: "0xbd3eb037fda14d910c5574b8c950222cc5c4211675b9c2265c07a66cef3a7691",
-      channelId: 2,
-    })
-
     const quoteToken = pipe(
       Sui.predictQuoteToken(TOKEN_ADDRESS),
       Effect.provide(PublicClientDestination),
