bfea3fb95b831fa3d8e47b85dc1b86b30addfaa9
Eric Hegnes <eric@hegnes.com>
2025-07-03 15:08:29 -0400

    pain
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- docs/postcss.config.cjs
+++ docs/postcss.config.cjs
@@ -1,11 +1,5 @@
-const path = require("node:path")
-const autoprefixer = require("autoprefixer")
-const postcssImport = require("postcss-import")
-
-/** @type {import('postcss-load-config').Config} */
 module.exports = {
-  plugins: [
-    postcssImport,
-    autoprefixer,
-  ],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 }
