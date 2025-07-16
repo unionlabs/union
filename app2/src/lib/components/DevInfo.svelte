c8145c922bdc756d330c1c2d1c8c5f161cc809be
Lukas <Lukas.nyberg@gmail.com>
2025-07-16 13:52:10 +0200

    feat(app): be able to hide devinfo

--- app2/src/lib/components/DevInfo.svelte
+++ app2/src/lib/components/DevInfo.svelte
@@ -2,6 +2,9 @@
 import { PUBLIC_GIT_REV, PUBLIC_LAST_MODIFIED_EPOCH } from "$env/static/public"
 import { ENV } from "$lib/constants"
 import { Number as N, Option as O, pipe, Schema as S, String as Str } from "effect"
+import Button from "./ui/Button.svelte"
+
+let isVisible = true
 
 const revision = O.liftPredicate(PUBLIC_GIT_REV, Str.isNonEmpty)
 const lastModified = pipe(
@@ -18,8 +21,8 @@ const lastModified = pipe(
   |&nbsp;
 {/snippet}
 
-{#if ENV() !== "PRODUCTION"}
-  <div class="absolute bottom-0 right-0 bg-zinc-900/[var(--bg-opacity)] [--bg-opacity:80%] text-accent px-[1em] py-2 font-mono rounded-tl-lg z-999">
+{#if ENV() !== "PRODUCTION" && isVisible}
+  <div class="absolute bottom-0 right-0 bg-zinc-900/[var(--bg-opacity)] [--bg-opacity:80%] text-accent px-[1em] py-2 font-mono rounded-tl-lg z-999 flex items-center gap-3">
     <span>
       {`${ENV().toLowerCase()}`}
       {#if O.isSome(revision)}
@@ -29,5 +32,11 @@ const lastModified = pipe(
         {@render sep()}{lastModified.value}
       {/if}
     </span>
+    <button
+      class="p-0 text-white transition-colors cursor-pointer"
+      onclick={() => isVisible = false}
+    > 
+      x
+  </button>
   </div>
 {/if}
