3608df715fd9268c4d12e42e0dc5bafd47eb5fc0
Lukas <Lukas.nyberg@gmail.com>
2025-07-23 17:28:10 +0200

    fix(app): spelling

--- app2/src/lib/dashboard/database.types.ts
+++ app2/src/lib/dashboard/database.types.ts
@@ -7,7 +7,7 @@ export type Json =
   | Json[]
 
 export type Database = {
-  // Allows to automatically instanciate createClient with right options
+  // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
     PostgrestVersion: "12.2.3 (519615d)"
