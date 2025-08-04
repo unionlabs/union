1efdcbf97c650c8336ccb5382191a440fd3e638b
Eric Hegnes <eric@hegnes.com>
2025-08-04 09:16:19 -0400

    wip
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- ts-sdk/src/ZkgmClientRequest.ts
+++ ts-sdk/src/ZkgmClientRequest.ts
@@ -1,6 +1,6 @@
 import type { Inspectable } from "effect/Inspectable"
 import type { Pipeable } from "effect/Pipeable"
-import * as internal from "./internal/zkgmRequest.js"
+import * as internal from "./internal/zkgmClientRequest.js"
 import { Chain } from "./schema/chain.js"
 import type * as ZkgmInstruction from "./ZkgmInstruction.js"
 
@@ -8,7 +8,7 @@ import type * as ZkgmInstruction from "./ZkgmInstruction.js"
  * @category type ids
  * @since 2.0.0
  */
-export const TypeId: unique symbol = Symbol.for("@unionlabs/sdk/ClientRequest")
+export const TypeId: unique symbol = Symbol.for("@unionlabs/sdk/ZkgmClientRequest")
 
 /**
  * @since 2.0.0
@@ -20,7 +20,7 @@ export type TypeId = typeof TypeId
  * @since 2.0.0
  * @category models
  */
-export interface ZkgmRequest extends Inspectable, Pipeable {
+export interface ZkgmClientRequest extends Inspectable, Pipeable {
   readonly [TypeId]: TypeId
   readonly source: Chain
   readonly destination: Chain
@@ -38,17 +38,6 @@ export interface Options {
 }
 
 /**
- * @since 2.0.0
- */
-export declare namespace Options {
-  /**
-   * @category models
-   * @since 2.0.0
-   */
-  export interface Send extends Omit<Options, "method" | "sender" | "receiver"> {}
-}
-
-/**
  * @category constructors
  * @since 2.0.0
  */
@@ -56,29 +45,22 @@ export declare const make: (options: {
   source: Chain
   destination: Chain
   instruction: ZkgmInstruction.ZkgmInstruction
-}) => ZkgmRequest
-
-/**
- * @category constructors
- * @since 2.0.0
- */
-export const send: (sender: string, receiver: string, options?: Options.Send) => ZkgmRequest =
-  internal.send
+}) => ZkgmClientRequest
 
 /**
  * @category combinators
  * @since 2.0.0
  */
-export const setSender: {
-  (sender: string): (self: ZkgmRequest) => ZkgmRequest
-  (self: ZkgmRequest, sender: string): ZkgmRequest
-} = internal.setSender
+export const setSource: {
+  (source: Chain): (self: ZkgmClientRequest) => ZkgmClientRequest
+  (self: ZkgmClientRequest, source: Chain): ZkgmClientRequest
+} = internal.setSource
 
 /**
  * @category combinators
  * @since 2.0.0
  */
-export const setReceiver: {
-  (receiver: string): (self: ZkgmRequest) => ZkgmRequest
-  (self: ZkgmRequest, receiver: string): ZkgmRequest
-} = internal.setSender
+export const setDestination: {
+  (destination: Chain): (self: ZkgmClientRequest) => ZkgmClientRequest
+  (self: ZkgmClientRequest, destination: Chain): ZkgmClientRequest
+} = internal.setDestination
--- ts-sdk/src/internal/clientError.ts
+++
@@ -1,6 +0,0 @@
-import type * as Error from "../ClientError.js"
-
-/** @internal */
-export const TypeId: Error.TypeId = Symbol.for(
-  "@unionlabs/sdk/ClientError",
-) as Error.TypeId
