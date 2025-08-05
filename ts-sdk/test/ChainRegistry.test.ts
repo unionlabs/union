049c08d03aa4146e3be43c3af8a79a10ed8277de
Eric Hegnes <eric@hegnes.com>
2025-08-05 10:59:05 -0400

    test both ChainRegistry approaches
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- ts-sdk/test/ChainRegistry.test.ts
+++ ts-sdk/test/ChainRegistry.test.ts
@@ -7,10 +7,23 @@ import { UniversalChainId } from "../src/schema/chain.js"
 import { IN_NIX_BUILD } from "./utils.js"
 
 describe.skipIf(IN_NIX_BUILD)("ChainRegistry", () => {
+  it.layer(ChainRegistry.ChainRegistry.Default)(
+    "ChainRegistry.Default",
+    (it) =>
+      it.effect("get by id (service)", () =>
+        Effect.gen(function*() {
+          const chainRegistry = ChainRegistry.ChainRegistry
+          const chain = yield* chainRegistry.byUniversalId(
+            UniversalChainId.make("ethereum.11155111"),
+          )
+          assert.instanceOf(chain, Chain)
+        })),
+  )
+
   it.layer(GraphQL.GraphQL.Default)(
     "GraphQL.Default",
     (it) =>
-      it.effect("get by id", () =>
+      it.effect("get by id (fn)", () =>
         Effect.gen(function*() {
           assert.instanceOf(
             yield* ChainRegistry.getChainById(
