621390a3f83d6fa0734c308dcae39b24ec5ffb3f
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2025-09-03 17:20:36 -0500

    fix: deployments tables

--- docs/src/components/svelte/tables/ContractDeploymentsTable.svelte
+++ docs/src/components/svelte/tables/ContractDeploymentsTable.svelte
@@ -3,9 +3,9 @@ import * as Table from "#/components/svelte/ui/table/index.ts"
 import deployments from "~root/deployments/deployments.json" with { type: "json" }
 </script>
 
-{#each deployments as network}
+{#each Object.entries(deployments) as [network, values]}
   <h3>
-    {network.universal_chain_id}
+    {network}
   </h3>
   <Table.Root class="w-full border border-neutral-500 rounded-sm">
     <Table.Row class="w-full">
@@ -27,10 +27,10 @@ import deployments from "~root/deployments/deployments.json" with { type: "json"
         ibc-union-core
       </Table.Cell>
       <Table.Cell>
-        {network.deployments.core.address}
+        {values.core.address}
       </Table.Cell>
     </Table.Row>
-    {#each Object.entries(network.deployments.lightclient) as [name, data], index}
+    {#each Object.entries(values.lightclient) as [name, data], index}
       <Table.Row>
         <Table.Cell class="w-full">
           {#if index === 0}
@@ -45,8 +45,8 @@ import deployments from "~root/deployments/deployments.json" with { type: "json"
         </Table.Cell>
       </Table.Row>
     {/each}
-    {#if Object.keys(network.deployments.app).length !== 0}
-      {#each Object.entries(network.deployments.app) as [name, data], index}
+    {#if Object.keys(values.app).length !== 0}
+      {#each Object.entries(values.app) as [name, data], index}
         <Table.Row>
           <Table.Cell class="w-full">
             {#if index === 0}
