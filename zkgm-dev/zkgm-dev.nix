7593499e949305c02bf3ac6d717dd6d317ebf79f
Eric Hegnes <eric@hegnes.com>
2025-09-09 00:51:44 -0400

    feat(zkgm-dev): migrate to pnpm
    Signed-off-by: Eric Hegnes <eric@hegnes.com>

--- zkgm-dev/zkgm-dev.nix
+++ zkgm-dev/zkgm-dev.nix
@@ -13,28 +13,34 @@ _: {
         python3
         nodePackages_latest.nodejs
       ];
-      packageJSON = lib.importJSON ./package.json;
+      buildPnpmPackage = import ../tools/typescript/buildPnpmPackage.nix {
+        inherit lib pkgs;
+      };
     in
     {
       packages = {
-        zkgm-dev = pkgsUnstable.buildNpmPackage {
-          npmDepsHash = "sha256-zHh5qqZMYuXqyMFs5BgAMdQGwW6oNlEwrZIJZYyqSaw=";
-          src = ./.;
-          sourceRoot = "zkgm-dev";
-          npmFlags = [ "--legacy-peer-deps" ];
-          pname = packageJSON.name;
-          inherit (packageJSON) version;
+        zkgm-dev = buildPnpmPackage {
+          hash = "sha256-THyYGHzP0FYc/NdCORvVJHYfCRjOSyjPSglVOa3r+FY=";
+          packageJsonPath = ./package.json;
+          extraSrcs = [
+            ../zkgm-dev
+          ];
           nativeBuildInputs = deps;
           buildInputs = deps;
+          buildPhase = ''
+            runHook preBuild
+            export NODE_OPTIONS="--no-warnings";
+            export VITE_BUCKET_ID="contributions";
+            export VITE_SUPABASE_URL="https://otfaamdxmgnkjqsosxye.supabase.co/";
+            export VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90ZmFhbWR4bWdua2pxc29zeHllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjA5NDMsImV4cCI6MjAzNjg5Njk0M30.q91NJPFFHKJXnbhbpUYwsB0NmimtD7pGPx6PkbB_A3w";
+            pnpm --filter=zkgm-dev build
+            runHook postBuild
+          '';
           installPhase = ''
             mkdir -p $out
-            cp -r ./build/* $out
+            cp -r ./zkgm-dev/build/* $out
           '';
           doDist = false;
-          NODE_OPTIONS = "--no-warnings";
-          VITE_BUCKET_ID = "contributions";
-          VITE_SUPABASE_URL = "https://otfaamdxmgnkjqsosxye.supabase.co/";
-          VITE_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90ZmFhbWR4bWdua2pxc29zeHllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjA5NDMsImV4cCI6MjAzNjg5Njk0M30.q91NJPFFHKJXnbhbpUYwsB0NmimtD7pGPx6PkbB_A3w";
         };
       };
 
@@ -53,7 +59,7 @@ _: {
               export VITE_SUPABASE_URL="https://otfaamdxmgnkjqsosxye.supabase.co/"
               export VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90ZmFhbWR4bWdua2pxc29zeHllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjA5NDMsImV4cCI6MjAzNjg5Njk0M30.q91NJPFFHKJXnbhbpUYwsB0NmimtD7pGPx6PkbB_A3w"
 
-              npm install
+              pnpm install
               npm run dev -- --host
             '';
           };
