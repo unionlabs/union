d9b6717f2ce56da9d93ff6264bd58e271166c19c
aeryz <abdullaheryz@protonmail.com>
2025-09-22 11:29:52 +0300

    chore: moar
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- tools/union-test/tests/lst/bond.rs
+++ tools/union-test/tests/lst/bond.rs
@@ -16,10 +16,12 @@ use ucs03_zkgm::{
 };
 use union_test::{
     cosmos::{self},
+    cosmos_helpers::calculate_proxy_address,
     evm::{
         self,
         zkgm::{Instruction as InstructionEvm, UCS03Zkgm},
     },
+    zkgm_helper,
 };
 use unionlabs::{
     encoding::{Encode, Json},
@@ -72,163 +74,37 @@ async fn test_escher_lst_success() {
     let dst_channel_id = 1;
     let src_channel_id = 1;
 
-    let vault_on_union = "union1skg5244hpkad603zz77kdekzw6ffgpfrde3ldk8rpdz06n62k4hqct0w4j";
+    // setting "muno" as the fungible counterparty
+    eth_set_fungible_counterparty(
+        &t.ctx.dst,
+        src_channel_id,
+        b"muno",
+        t.union_address.escrow_vault.as_bytes(),
+    )
+    .await
+    .unwrap();
 
-    let u_on_eth = hex_literal::hex!("0c8C6f58156D10d18193A8fFdD853e1b9F8D8836");
-
-    let metadata = SolverMetadata {
-        solverAddress: u_on_eth.to_vec().into(),
-        metadata: Default::default(),
-    }
-    .abi_encode_params();
-
-    let instruction_cosmos = Instruction {
-        version: INSTR_VERSION_2,
-        opcode: OP_TOKEN_ORDER,
-        operand: TokenOrderV2 {
-            sender: cosmos_address_bytes.clone().into(),
-            receiver: evm_address.to_vec().into(),
-            base_token: "muno".as_bytes().into(),
-            base_amount: "100000".parse().unwrap(),
-            kind: TOKEN_ORDER_KIND_SOLVE,
-            metadata: metadata.into(),
-            quote_token: u_on_eth.to_vec().into(),
-            quote_amount: "100000".parse().unwrap(),
-        }
-        .abi_encode_params()
-        .into(),
-    };
-
-    let (_, zkgm_deployer_provider) = t.ctx.dst.get_provider_privileged().await;
-    println!("registering u counterpart");
-    t.ctx
-        .dst
-        .u_register_fungible_counterpart(
-            H160::from(u_on_eth),
-            zkgm_deployer_provider.clone(),
-            alloy::primitives::U256::ZERO,
-            dst_channel_id,
-            b"muno".to_vec().into(),
-            evm::u::U::FungibleCounterparty {
-                beneficiary: vault_on_union.as_bytes().to_vec().into(),
-            },
-        )
-        .await
-        .unwrap();
-    println!("u counterpart is registered");
+    // funding the eth address that we are execute bond with, with muno
+    eth_fund_u(
+        &t,
+        src_channel_id,
+        cosmos_address.to_string(),
+        evm_address.into(),
+        100_000,
+        500_000,
+    )
+    .await
+    .unwrap();
 
     let mut salt_bytes = [0u8; 32];
     rand::rng().fill_bytes(&mut salt_bytes);
 
-    let cw_msg = ucs03_zkgm::msg::ExecuteMsg::Send {
-        channel_id: src_channel_id.try_into().unwrap(),
-        timeout_height: 0u64.into(),
-        timeout_timestamp: voyager_sdk::primitives::Timestamp::from_secs(u32::MAX.into()),
-        salt: salt_bytes.into(),
-        instruction: instruction_cosmos.abi_encode_params().into(),
-    };
-    let bin_msg: Vec<u8> = Encode::<Json>::encode(&cw_msg);
-
-    let funds = vec![Coin {
-        denom: "muno".into(),
-        amount: "100000".into(),
-    }];
-
-    let ack_packet_data = t
-        .ctx
-        .send_and_recv_and_ack_with_retry::<cosmos::Module, evm::Module>(
-            &t.ctx.src,
-            contract,
-            (bin_msg, funds),
-            &t.ctx.dst,
-            3,
-            Duration::from_secs(20),
-            Duration::from_secs(720),
-            cosmos_provider,
-        )
-        .await;
-
-    assert!(
-        ack_packet_data.is_ok(),
-        "Failed to send and ack packet: {:?}",
-        ack_packet_data.err()
+    let proxy_address = calculate_proxy_address(
+        &t.union_address.zkgm,
+        alloy::primitives::U256::ZERO,
+        dst_channel_id,
+        evm_address.as_slice(),
     );
-
-    let new_u_balance = t
-        .ctx
-        .dst
-        .zkgmerc20_balance_of(
-            H160::from(u_on_eth),
-            evm_address.into(),
-            evm_provider.clone(),
-        )
-        .await
-        .unwrap();
-
-    let new_vault_balance = t
-        .ctx
-        .src
-        .native_balance(Bech32::from_str(vault_on_union).unwrap(), "muno")
-        .await
-        .unwrap();
-
-    // both balances are updated
-    assert!(new_u_balance > U256::ZERO);
-    assert!(new_vault_balance > 0);
-
-    println!("new_u_balance: {}", new_u_balance);
-
-    let lst_hub = "union1fdg764zzxwvwyqkx3fuj0236l9ddh5xmutgvj2mv9cduffy82z9sp62ygc";
-    // let lst = "union1jansh23v7teaznyljq6ss4vx6eym8yrz0dsjchap4u7j3etx94vqhmcwn5";
-
-    let addr: Bech32<FixedBytes<32>> = Bech32::from_str(UNION_ZKGM_ADDRESS).unwrap();
-    let canon = cosmwasm_std::CanonicalAddr::from(addr.data().as_ref());
-
-    let zkgm_proxy_canon = instantiate2_address(
-        // Checksum of the base contract
-        &hex_literal::hex!("ec827349ed4c1fec5a9c3462ff7c979d4c40e7aa43b16ed34469d04ff835f2a1"),
-        &canon,
-        proxy_account_salt_for_tests(
-            alloy::primitives::U256::ZERO,
-            dst_channel_id,
-            evm_address.as_slice(),
-        )
-        .get()
-        .as_slice(),
-    )
-    .unwrap();
-
-    let hrp = Bech32::<FixedBytes<32>>::from_str(UNION_ZKGM_ADDRESS)
-        .unwrap()
-        .hrp()
-        .to_string();
-
-    // 3. Build a Bech32 address with same HRP
-    let zkgm_proxy_calculated = Bech32::<FixedBytes<32>>::new(
-        hrp,
-        FixedBytes::<32>::try_from(zkgm_proxy_canon.as_slice()).unwrap(),
-    );
-    println!("ZKGM Proxy: {}", zkgm_proxy_calculated);
-
-    // let zkgm_proxy = zkgm_proxy_calculated.to_string().as_str();
-
-    // let bond_message: Bytes<Base64> = json!({
-    //     "bond": {
-    //         "mint_to_address": "union1jk9psyhvgkrt2cumz8eytll2244m2nnz4yt2g2",
-    //         "min_mint_amount": "3"
-    //     }
-    // })
-    // .to_string()
-    // .as_bytes()
-    // .into();
-
-    // let zkgm_message = json!({
-    //     "contract": lst_hub,
-    //     "msg": bond_message.to_string(),
-    //     "funds": [{ "denom": "muno", "amount": "3" }],
-    //     "call_action": "call_proxy"
-    // })
-    // .to_string();
 
     let zkgm_msg_json = make_zkgm_call_payload(
         lst_hub,
@@ -241,80 +117,23 @@ async fn test_escher_lst_success() {
     let proxy_balance = t
         .ctx
         .src
-        .native_balance(zkgm_proxy_calculated.clone(), "muno")
+        .native_balance(proxy_address.clone(), "muno")
         .await
         .unwrap();
 
     println!("Proxy balance before: {}", proxy_balance);
 
-    let addr_str = zkgm_proxy_calculated.to_string();
-    let receiver = addr_str.into_bytes().into();
-
-    let instruction_from_evm_to_union = InstructionEvm {
-        version: INSTR_VERSION_0,
-        opcode: OP_BATCH,
-        operand: Batch {
-            instructions: vec![
-                Instruction {
-                    version: INSTR_VERSION_2,
-                    opcode: OP_TOKEN_ORDER,
-                    operand: TokenOrderV2 {
-                        sender: evm_address.to_vec().into(),
-                        receiver,
-                        base_token: u_on_eth.to_vec().into(),
-                        base_amount: "150".parse().unwrap(),
-                        kind: TOKEN_ORDER_KIND_SOLVE,
-                        metadata: SolverMetadata {
-                            solverAddress: vault_on_union.as_bytes().into(),
-                            metadata: Default::default(),
-                        }
-                        .abi_encode_params()
-                        .into(),
-                        quote_token: "muno".as_bytes().into(),
-                        quote_amount: "150".parse().unwrap(),
-                    }
-                    .abi_encode_params()
-                    .into(),
-                },
-                Instruction {
-                    version: INSTR_VERSION_0,
-                    opcode: OP_CALL,
-                    operand: Call {
-                        sender: evm_address.to_vec().into(),
-                        eureka: false,
-                        contract_address: zkgm_proxy_calculated
-                            .to_string()
-                            .as_bytes()
-                            .to_vec()
-                            .into(),
-                        contract_calldata: zkgm_msg_json.as_bytes().to_vec().into(),
-                    }
-                    .abi_encode_params()
-                    .into(),
-                },
-            ],
-        }
-        .abi_encode_params()
-        .into(),
-    };
-
-    let approve_tx_hash = t
+    let _ = t
         .ctx
         .dst
         .zkgmerc20_approve(
-            u_on_eth.into(),
-            ETH_ADDRESS_ZKGM.into(),
+            ETH_ADDRESS_U,
+            ETH_ADDRESS_ZKGM,
             U256::from(100000000000u64),
             evm_provider.clone(),
         )
-        .await;
-
-    assert!(
-        approve_tx_hash.is_ok(),
-        "Failed to send approve transaction: {:?}, from_account: {:?}",
-        approve_tx_hash.err(),
-        evm_address
-    );
+        .await
+        .unwrap();
 
     let ucs03_zkgm = UCS03Zkgm::new(ETH_ADDRESS_ZKGM.into(), evm_provider.clone());
 
@@ -324,7 +143,19 @@ async fn test_escher_lst_success() {
             0u64,
             4294967295000000000u64,
             salt_bytes.into(),
-            instruction_from_evm_to_union.clone(),
+            InstructionEvm::from(zkgm_helper::make_batch(vec![
+                evm_helper::make_token_order_v2(
+                    &t.union_address.escrow_vault,
+                    &evm_address,
+                    &proxy_address,
+                    alloy::primitives::U256::from(150u128),
+                ),
+                zkgm_helper::make_call(
+                    evm_address.to_vec().into(),
+                    proxy_address.as_bytes().to_vec().into(),
+                    zkgm_msg_json.as_bytes().to_vec().into(),
+                ),
+            ])),
         )
         .clear_decoder()
         .with_cloned_provider();
@@ -345,7 +176,7 @@ async fn test_escher_lst_success() {
 
     let acked_packet = acked_packet.unwrap();
     assert!(
-        acked_packet.tag == 1,
+        acked_packet.tag == TAG_ACK_SUCCESS,
         "Packet is not acked successfully, but it should be. Tag: {:?}",
         acked_packet.tag
     );
@@ -1533,15 +1364,3 @@ async fn test_escher_lst_success() {
 
 //     println!("Proxy balance before: {}", proxy_balance);
 // }
-
-fn proxy_account_salt_for_tests(
-    path: alloy::primitives::U256,
-    channel_id: u32,
-    sender: &[u8],
-) -> unionlabs::primitives::H256 {
-    use alloy_sol_types::SolValue;
-    use unionlabs::ethereum::keccak256;
-
-    let encoded = (path, channel_id, sender.to_vec()).abi_encode_params();
-    keccak256(encoded)
-}
