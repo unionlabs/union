217fa3d49e7ba506583cf696f56f8d2cd3aeefde
aeryz <abdullaheryz@protonmail.com>
2025-10-04 10:13:17 +0300

    chore: bunch of stuff
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- solana/ibc/src/state/latest_client_id.rs
+++ solana/ibc/src/state/latest_client_id.rs
@@ -1,9 +1,53 @@
 use ibc_union_spec::ClientId;
-use pinocchio::program_error::ProgramError;
+use pinocchio::{account_info::AccountInfo, program_error::ProgramError};
 
-use super::Serializable;
+use super::{Serializable, TypedAccount2};
 
 pub struct LatestClientId(pub ClientId);
+
+pub struct LatestClientId2<'a> {
+    pub client_id: ClientId,
+    pub account_info: &'a AccountInfo,
+}
+
+impl<'a> TypedAccount2<'a> for LatestClientId2<'a> {
+    type Data = ClientId;
+
+    fn new(data: Self::Data, account: &'a AccountInfo) -> Self {
+        Self {
+            client_id: data,
+            account_info: account,
+        }
+    }
+
+    fn data(&self) -> &Self::Data {
+        &self.client_id
+    }
+
+    fn account_info(&self) -> &'a AccountInfo {
+        self.account_info
+    }
+}
+
+impl Serializable for ClientId {
+    fn serialized_size(&self) -> usize {
+        4
+    }
+
+    fn serialize_into(&self, data: &mut [u8]) {
+        data.copy_from_slice(self.raw().to_le_bytes().as_slice())
+    }
+
+    fn deserialize(data: &[u8]) -> Result<Self, ProgramError> {
+        let client_id = u32::from_le_bytes(
+            data[0..4]
+                .try_into()
+                .map_err(|_| ProgramError::InvalidArgument)?,
+        );
+
+        Ok(ClientId::from_raw(client_id).ok_or(ProgramError::InvalidArgument)?)
+    }
+}
 
 impl LatestClientId {
     pub fn increment(&mut self) {
@@ -28,6 +72,10 @@ impl TryFrom<&[u8]> for LatestClientId {
 }
 
 impl Serializable for LatestClientId {
+    fn serialized_size(&self) -> usize {
+        4
+    }
+
     fn serialize_into(&self, data: &mut [u8]) {
         data.copy_from_slice(self.0.raw().to_le_bytes().as_slice())
     }
