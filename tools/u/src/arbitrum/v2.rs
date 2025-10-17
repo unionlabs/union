7930c918e33f09ce8085787638d9f96e13b5780f
benluelo <git@luelo.dev>
2025-10-17 19:55:22 +0100

    feat: arb v2

---
+++ tools/u/src/arbitrum/v2.rs
@@ -0,0 +1,75 @@
+use alloy::{network::AnyNetwork, providers::ProviderBuilder};
+use anyhow::Result;
+use arbitrum_client::v2::{
+    assertion_created_event_at_l1_block_height, finalized_l2_block_of_l1_height,
+};
+use clap::Subcommand;
+use unionlabs::primitives::H160;
+
+use crate::print_json;
+
+#[derive(Debug, Subcommand)]
+pub enum Cmd {
+    AssertionCreatedEventAtL1BlockHeight {
+        #[arg(long)]
+        l1_rpc_url: String,
+        #[arg(long)]
+        l1_contract_address: H160,
+        #[arg(long)]
+        l1_height: u64,
+    },
+    FinalizedL2BlockOfL1Height {
+        #[arg(long)]
+        l1_rpc_url: String,
+        #[arg(long)]
+        l2_rpc_url: String,
+        #[arg(long)]
+        l1_contract_address: H160,
+        #[arg(long)]
+        l1_height: u64,
+    },
+}
+
+impl Cmd {
+    pub async fn run(self) -> Result<()> {
+        match self {
+            Cmd::AssertionCreatedEventAtL1BlockHeight {
+                l1_rpc_url,
+                l1_contract_address,
+                l1_height,
+            } => {
+                let assertion_created_event = assertion_created_event_at_l1_block_height(
+                    ProviderBuilder::new().connect(&l1_rpc_url).await?,
+                    l1_contract_address,
+                    l1_height,
+                )
+                .await
+                .map_err(anyhow::Error::from_boxed)?;
+
+                print_json(&assertion_created_event);
+            }
+            Cmd::FinalizedL2BlockOfL1Height {
+                l1_rpc_url,
+                l2_rpc_url,
+                l1_contract_address,
+                l1_height,
+            } => {
+                let l2_block = finalized_l2_block_of_l1_height(
+                    ProviderBuilder::new().connect(&l1_rpc_url).await?,
+                    ProviderBuilder::new()
+                        .network::<AnyNetwork>()
+                        .connect(&l2_rpc_url)
+                        .await?,
+                    l1_contract_address,
+                    l1_height,
+                )
+                .await
+                .map_err(anyhow::Error::from_boxed)?;
+
+                print_json(&l2_block);
+            }
+        }
+
+        Ok(())
+    }
+}
