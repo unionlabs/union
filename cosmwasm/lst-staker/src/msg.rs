9ae0c7eca363be4f3456db5022250864257ced5e
benluelo <git@luelo.dev>
2025-10-21 21:14:05 +0100

    feat(lst): redelegations

--- cosmwasm/lst-staker/src/msg.rs
+++ cosmwasm/lst-staker/src/msg.rs
@@ -8,7 +8,14 @@ use serde::{Deserialize, Serialize};
 pub enum ExecuteMsg {
     /// Set the LST hub address.
     SetLstHubAddress(Addr),
-    /// Set the validators to stake against. This completely overwrites the existing configuration.
+    /// Configure the delegation set. This completely overwrites the existing configuration.
+    ///
+    /// If this is not the first time configuring the delegation set, the LST will rebase and the
+    /// current delegated amounts will be rebalanced amongst the new set with `MsgRedelegate`s. Due
+    /// to the [redelegation queue], it is (in practice) only possible to re-configure the validator
+    /// set once per unbonding period.
+    ///
+    /// [redelegation queue]: https://docs.cosmos.network/main/build/modules/staking#redelegation
     SetValidators(BTreeMap<Addr, Uint128>),
     #[serde(untagged)]
     CwAccount(cw_account::msg::ExecuteMsg),
