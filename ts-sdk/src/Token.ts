e2e914d9ec028921e6c413d69b5b3b4c5c16286d
Kaan Caglan <caglankaan@gmail.com>
2025-11-04 18:01:30 +0300

    chore: fixing docs
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- ts-sdk/src/Token.ts
+++ ts-sdk/src/Token.ts
@@ -237,9 +237,10 @@ export const TokenFromString = S.transformOrFail(
 //     },
 //   )
 
-// /**
-//  * @since 2.0.0
-//  */
+
+/**
+ * @since 2.0.0
+ */
 export const AnyFromEncoded = (rpcType: Chain.RpcType) => {
   const From = rpcType === "sui"
     ? S.Union(Hex.Hex, S.String)
