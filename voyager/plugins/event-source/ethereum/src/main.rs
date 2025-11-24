3a12b06abef23822e3926cc9abd03a27b537d2dd
benluelo <git@luelo.dev>
2025-11-25 00:10:59 +0100

    docs(voyager): add docs for ethereum event source plugin

--- voyager/plugins/event-source/ethereum/src/main.rs
+++ voyager/plugins/event-source/ethereum/src/main.rs
@@ -326,7 +326,7 @@ impl Module {
                     None::<()>,
                 ));
             } else if block_number == until {
-                // if this is a ranged fetch, we need to fetch the upper bound of the range individually sinnce FetchBlocks is exclusive on the upper bound
+                // if this is a ranged fetch, we need to fetch the upper bound of the range individually since FetchBlocks is exclusive on the upper bound
                 return Ok(call(PluginMessage::new(
                     self.plugin_name(),
                     ModuleCall::from(FetchGetLogs { block_number }),
