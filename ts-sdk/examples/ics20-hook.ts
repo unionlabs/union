16a162d53a08502562e7c7def4023fa2aed349fa
benluelo <git@luelo.dev>
2026-01-12 22:55:10 +0300

    wip

--- ts-sdk/examples/ics20-hook.ts
+++ ts-sdk/examples/ics20-hook.ts
@@ -11,19 +11,25 @@ import { ChainRegistry } from "@unionlabs/sdk/ChainRegistry"
 import { UniversalChainId } from "@unionlabs/sdk/schema/chain"
 import { ChannelId } from "@unionlabs/sdk/schema/channel"
 import * as Token from "@unionlabs/sdk/Token"
+import { Console } from "effect"
 import * as A from "effect/Array"
 import * as Cause from "effect/Cause"
 import * as Effect from "effect/Effect"
+import { encodeBase64 } from "effect/Encoding"
 import { pipe } from "effect/Function"
 import * as Match from "effect/Match"
 import * as ParseResult from "effect/ParseResult"
 import * as Schema from "effect/Schema"
+import { bytesToHex, fromHex, keccak256 } from "viem"
 
 const OSMOSIS_CHAIN_ID = UniversalChainId.make("osmosis.osmosis-1")
 const ETHEREUM_CHAIN_ID = UniversalChainId.make("ethereum.1")
 
-const ATOMONE_TO_OSMOSIS_CHANNEL = "channel-2"
+const ATOMONE_TO_OSMOSIS_CHANNEL = "channel-94814"
+const OSMOSIS_TO_ATOMONE_CHANNEL = "channel-2"
 const ATOMONE_TO_OSMOSIS_PORT = "transfer"
+
+const ZKGM_ADDRESS = "osmo1336jj8ertl8h7rdvnz4dh5rqahd09cy0x43guhsxx6xyrztx292qs2uecc"
 
 const SOURCE_CHANNEL_ID = ChannelId.make(2)
 const SEND_AMOUNT = 1n
@@ -48,33 +54,87 @@ export const ATONE_ERC20 = Token.Erc20.make({
   address: "0xA1a1d0B9182339e86e80db519218eA03Ec09a1A1",
 })
 
+const PROXY_ACCOUNT_FACTORY = Ucs05.CosmosDisplay.make({
+  address: "osmo13jcvgpy2cjl6tg7zz5pcr9pv6lgqz70h7n64krjve7mp7tsexvys82mlqs",
+})
+
 const sha256 = (data: BufferSource) =>
   Effect.tryPromise(() => globalThis.crypto.subtle.digest("SHA-256", data))
 
 const calculateIbcCallbackAddress = Effect.fn("calculateIbcCallbackAddress")(
   function*(sender: string, channelId: string) {
-    return Ucs05.CosmosDisplay.make({
+    const preimage = new Uint8Array([
+      ...new Uint8Array(
+        yield* sha256(new globalThis.TextEncoder().encode("ibc-wasm-hook-intermediary")),
+      ),
+      ...new globalThis.TextEncoder().encode(`${channelId}/${sender}`),
+    ])
+
+    const addr = Ucs05.CosmosDisplay.make({
       address: yield* Schema.decode(
         Ucs05.Bech32FromCanonicalBytesWithPrefix(
           "osmo",
         ),
       )(
-        `0x${yield* Schema.encode(
-          Schema.Uint8ArrayFromHex,
-        )(
-          new Uint8Array(
-            yield* sha256(
-              new Uint8Array([
-                ...new Uint8Array(
-                  yield* sha256(new globalThis.TextEncoder().encode("ibc-wasm-hook-intermediary")),
-                ),
-                ...new globalThis.TextEncoder().encode(`${channelId}/${sender}`),
-              ]),
-            ),
-          ),
+        `0x${yield* Schema.encode(Schema.Uint8ArrayFromHex)(
+          new Uint8Array(yield* sha256(preimage)),
         )}`,
       ),
     })
+
+    yield* Console.log({ addr, preimage })
+
+    return addr
+  },
+)
+
+// proxy_account_factory::predict_call_proxy_account
+export const predictProxy = Effect.fn("predictProxy")(
+  function*(sender: Ucs05.CosmosDisplay) {
+    const BYTECODE_BASE_CHECKSUM =
+      "0xec827349ed4c1fec5a9c3462ff7c979d4c40e7aa43b16ed34469d04ff835f2a1" as const
+    const CANONICAL_PROXY_ACCOUNT_FACTORY = Ucs05.anyDisplayToCanonical(
+      PROXY_ACCOUNT_FACTORY,
+    )
+    const MODULE_HASH =
+      "0x120970d812836f19888625587a4606a5ad23cef31c8684e601771552548fc6b9" as const
+
+    const canonical_sender = fromHex(Ucs05.anyDisplayToCanonical(sender), "bytes")
+    const salt = yield* sha256(canonical_sender.buffer)
+
+    const u64toBeBytes = (n: bigint) => {
+      const buffer = new ArrayBuffer(8)
+      const view = new DataView(buffer)
+      view.setBigUint64(0, n)
+      return new Uint8Array(view.buffer)
+    }
+
+    const address = yield* pipe(
+      Uint8Array.from(
+        [
+          ...fromHex(MODULE_HASH, "bytes"),
+          ...new TextEncoder().encode("wasm"),
+          0,
+          ...u64toBeBytes(32n),
+          ...fromHex(BYTECODE_BASE_CHECKSUM, "bytes"),
+          ...u64toBeBytes(32n),
+          ...fromHex(CANONICAL_PROXY_ACCOUNT_FACTORY, "bytes"),
+          ...u64toBeBytes(32n),
+          ...new Uint8Array(salt),
+          ...u64toBeBytes(0n),
+        ],
+      ),
+      sha256,
+      Effect.map((r) => new Uint8Array(r)),
+      Effect.map(bytesToHex),
+      Effect.flatMap(
+        Schema.decode(Ucs05.Bech32FromCanonicalBytesWithPrefix("osmo")),
+      ),
+    )
+
+    yield* Console.log({ address })
+
+    return Ucs05.CosmosDisplay.make({ address })
   },
 )
 
@@ -135,7 +195,9 @@ const createUcs03 = Effect.gen(function*() {
   return yield* TokenOrder.make({
     source: ethereumChain,
     destination: osmosisChain,
-    sender: yield* calculateIbcCallbackAddress(SENDER_ATOMONE.address, ATOMONE_TO_OSMOSIS_CHANNEL),
+    sender: yield* predictProxy(
+      yield* calculateIbcCallbackAddress(SENDER_ATOMONE.address, OSMOSIS_TO_ATOMONE_CHANNEL),
+    ),
     receiver: RECEIVER,
     baseToken: ATONE_IBC_DENOM_ON_OSMOSIS,
     baseAmount: SEND_AMOUNT,
@@ -157,5 +219,30 @@ createUcs03.then(
       sourceChannelId: SOURCE_CHANNEL_ID,
     }),
 )
+  .then(msg =>
+    JSON.stringify({
+      wasm: {
+        contract: PROXY_ACCOUNT_FACTORY.address,
+        msg: {
+          call_proxy: [
+            {
+              wasm: {
+                execute: {
+                  contract_addr: ZKGM_ADDRESS,
+                  msg: encodeBase64(JSON.stringify(msg)),
+                  funds: [
+                    {
+                      amount: SEND_AMOUNT.toString(),
+                      denom: ATONE_IBC_DENOM_ON_OSMOSIS.address,
+                    },
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
+    })
+  )
   .then(console.log)
   .then(console.error)
