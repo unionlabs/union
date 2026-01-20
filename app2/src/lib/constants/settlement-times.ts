61af55d64b22e979bb63a417f04aa06d994a172f
cor <cor@pruijs.dev>
2026-01-20 22:14:22 +0100

    fix(app): update more settlement times

--- app2/src/lib/constants/settlement-times.ts
+++ app2/src/lib/constants/settlement-times.ts
@@ -7,11 +7,15 @@ type SettlementInfo = {
 
 export const settlementDelays = {
   "base.8453": {
-    url: "https://etherscan.io/address/0xdDa53E23f8a32640b04D7256e651C1db98dB11C1",
+    url: "https://etherscan.io/address/0x43edB88C4B80fDD2AdFF2412A7BebF9dF42cB40e",
+    interval: "2 hours",
+  },
+  "base.84532": {
+    url: "https://etherscan.io/address/0xd6E6dBf4F7EA0ac412fD8b65ED297e64BB7a06E1",
     interval: "2 hours",
   },
   "bob.60808": {
-    url: "https://etherscan.io/address/0xdDa53E23f8a32640b04D7256e651C1db98dB11C1",
+    url: "https://etherscan.io/address/0x96123dbFC3253185B594c6a7472EE5A21E9B1079",
     interval: "12 hours",
   },
   "corn.21000000": {
