eef16d7e16514198d4fc57c08dbaf3ccda70c5b5
benluelo <git@luelo.dev>
2026-04-28 12:28:11 +0300

    feat(bob): upgrade to latest version

--- lib/bob-light-client-types/src/header.rs
+++ lib/bob-light-client-types/src/header.rs
@@ -1,8 +1,9 @@
 use ethereum_light_client_types::{AccountProof, StorageProof};
 use rlp::Encodable;
 use unionlabs::{
+    errors::InvalidLength,
     ethereum::keccak256,
-    primitives::{Bytes, H64, H72, H160, H256, H2048, U256},
+    primitives::{Bytes, H64, H160, H256, H2048, U256},
 };
 
 #[derive(Debug, Clone, PartialEq)]
@@ -65,7 +66,7 @@ pub struct L2Header {
     pub gas_used: u64,
     #[cfg_attr(feature = "serde", serde(with = "::serde_utils::u64_hex"))]
     pub timestamp: u64,
-    pub extra_data: H72,
+    pub extra_data: BytesMax32,
     pub mix_hash: H256,
     pub nonce: H64,
     #[cfg_attr(
@@ -89,6 +90,64 @@ impl L2Header {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(try_from = "Bytes", into = "Bytes")
+)]
+pub struct BytesMax32(Bytes);
+
+impl rlp::Encodable for BytesMax32 {
+    fn rlp_append(&self, s: &mut rlp::RlpStream) {
+        s.encoder().encode_value(&self.0);
+    }
+}
+
+#[cfg(feature = "bincode")]
+impl bincode::Encode for BytesMax32 {
+    fn encode<E: bincode::enc::Encoder>(
+        &self,
+        encoder: &mut E,
+    ) -> Result<(), bincode::error::EncodeError> {
+        bincode::Encode::encode(&self.0, encoder)
+    }
+}
+
+#[cfg(feature = "bincode")]
+impl<Context> bincode::Decode<Context> for BytesMax32 {
+    fn decode<D: bincode::de::Decoder<Context = Context>>(
+        decoder: &mut D,
+    ) -> Result<Self, bincode::error::DecodeError> {
+        <Bytes as bincode::Decode<Context>>::decode(decoder)?
+            .try_into()
+            .map_err(|_| bincode::error::DecodeError::Other("invalid BytesMax32 length"))
+    }
+}
+#[cfg(feature = "bincode")]
+bincode::impl_borrow_decode!(BytesMax32);
+
+impl TryFrom<Bytes> for BytesMax32 {
+    type Error = InvalidLength;
+
+    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
+        if value.len() > 32 {
+            Err(InvalidLength {
+                expected: unionlabs::errors::ExpectedLength::LessThan(33),
+                found: value.len(),
+            })
+        } else {
+            Ok(Self(value))
+        }
+    }
+}
+
+impl From<BytesMax32> for Bytes {
+    fn from(value: BytesMax32) -> Self {
+        value.0
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use hex_literal::hex;
@@ -98,39 +157,39 @@ mod tests {
     #[test]
     fn hash() {
         let header = L2Header {
-            parent_hash: hex!("327686d326438b9f95b8300c1ceed12050a3d685fcfbe895f23f8a812e57ee15").into(),
+            parent_hash: hex!("53636825d8a9e633b4fcd73fbd3980317519695427e3514689b4d783c15b265e").into(),
             sha3_uncles: hex!("1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347").into(),
             miner: hex!("4200000000000000000000000000000000000011").into(),
-            state_root: hex!("096c1251fad148c6e8d39934dfb1b1e677232ab7b7a7ab294195a99826ec2e2b").into(),
+            state_root: hex!("8f079c04ffe7b0782ea9e3ec105fec4417a6876b2d1c73233fadcce2d4d3c833").into(),
             transactions_root: hex!(
-                "333c9ba379b9c46686ca661ebb6a033f9d5a0cdc11f00b175d2e1706c34f9614"
+                "fcf5f195056c9e94d2e017b1e850b2c996481dcef04316753262b3b4b56e8e6b"
             ).into(),
             receipts_root: hex!(
-                "4d8e11add88fec0151369ca910de47451e314d30d7c7fd81d1906a34ca10de0b"
+                "f2343e43f271a1ad1382f680bd2d263103ca3ecb56b32ce816d6d4a46a3ccf80"
             ).into(),
-            logs_bloom: Box::new(hex!("57bbb78267e7bf5d0cfe97ffb7ee6bd5fefed75fb6efbf3dafd77bc8febbbf7cedeed71e5cdd7df43763f8fdcf3eb77bf47f8b749eedfb8cffffaf0ff7a7fbe0b777dd9bb27ffc2d6d7ef2eebd5768fee7efacafcbffeff7ec7ef7dcbbfa3674ffbecfb61f7fd7ef7b3feaabef1f9e2b595f7f6f4efd979cde6bb1df157f97eff7cf2e2bffe57fcecf6bdf3af80efcd3b773ffefff7bd799fdeeb87fffd6d5fb1fef195ff7f8f3a9d7fff7425ef5a5fffdf179ba65be1dddb3efbb5b8d9efbb63a548ffed9fbf577cbc92fdd276b7fdf750fc7afffda3bf2bfbe07877b9fffb6c6debedbedb5f77b7ffdbfefbadf77a5eda2bfe62ffe5effee58dbc3575f2bdf").into()),
+            logs_bloom: Box::new(hex!("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").into()),
             difficulty: 0_u64.into(),
-            number: 32376940_u64.into(),
-            gas_limit: 0x8f0d180,
-            gas_used: 0x254cdb6,
-            timestamp: 0x68666dbb,
-            extra_data: hex!("000000003200000003").into(),
-            mix_hash: hex!("40bfcd42c3cb3b7966a467ce8cdc2638cbd6b03558448a82a47b42ad5504ef72").into(),
+            number: 0x1ec241d_u64.into(),
+            gas_limit: 0x1c9c380,
+            gas_used: 0xb4ea,
+            timestamp: 0x69f07b5d,
+            extra_data: <Bytes>::from(hex!("01000000fa000000060000000000000000")).try_into().unwrap(),
+            mix_hash: hex!("5153f2e659ea5f7841427ec4fe15415eb68bf14fea44f7fb53cabb25c9597fb5").into(),
             nonce: hex!("0000000000000000").into(),
-            base_fee_per_gas: 0x326e8f_u64.into(),
+            base_fee_per_gas: 0xfc_u64.into(),
             withdrawals_root: hex!(
-                "6ee1a180fdbccc2e70984ac91116d64c58feb817a417a81500b9b0cbd69a9373"
+                "cca4e8849ead727dc9f5e8223eeb80e3e5ad03cccddf1f82b5ee9664905ec215"
             ).into(),
             blob_gas_used: 0,
             excess_blob_gas: 0,
             parent_beacon_block_root: hex!(
-                "4fccae25b4204cb426da9b8b2961be949be574f964e000c6b64627e7c98be4c3"
+                "f88b3eb7a821ca63323b5964588d24e973c8399bc7125de7fd963dc7e1726ebb"
             ).into(),
             requests_hash: hex!("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855").into(),
         };
 
         let hash: H256 =
-            hex!("f5b06eb8b0bacf8b030dbd596964b6bf346f7602f906f57db958e12c726367f1").into();
+            hex!("4b4db973e20f92507f07cab622c2e80821e72a7a185c42ea5658be0db5a310c3").into();
 
         assert_eq!(hash, header.hash());
     }
